ca6fde6379283a6ea3f5308849f79d64a3a97c14
unchama <nintendowii@jcom.home.ne.jp>
2016-07-14 12:48:47 +0900

    first version

---
+++ src/com/github/unchama/multiseichieffect/MultiSeichiEffect.java
@@ -0,0 +1,82 @@
+package com.github.unchama.multiseichieffect;
+
+import java.util.ArrayList;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MultiSeichiEffect extends JavaPlugin implements Listener {
+	
+	//このクラス自身を表すインスタンス
+	//public static SeichiPlugin instance;
+
+	//タスクキル判定用ArrayList
+	public static ArrayList<Player> tasks = new ArrayList<Player>();
+
+	@Override
+	public void onEnable(){
+
+		//Configが"なかったら"コピーする
+		saveDefaultConfig();
+
+		//config.ymlを読み込む．読み出し方法はconf.getString("key")
+		FileConfiguration conf=getConfig();
+
+		//リスナーの登録
+		getServer().getPluginManager().registerEvents(this, this);
+
+		getLogger().info("SeichiPlugin is Enabled!");
+
+	}
+
+	@Override
+	public void onDisable() {
+
+		getLogger().info("SeichiPlugin is Disabled!");
+	}
+
+	//プレイヤーがjoinした時に実行
+	@EventHandler
+	public void onplayerJoinEvent(PlayerJoinEvent event){
+		
+		//誰がjoinしたのか取得しPlayer型の変数playerに格納
+		Player player = event.getPlayer();
+		
+		//一定時間おきに処理を実行するインスタンスを立ち上げ、そのインスタンスに変数playerを代入
+		new TestRunnable(player).runTaskTimer(this, 0, 100);
+		
+		//タスクキル判定用に変数playerをArrayListに登録
+		tasks.add(player);
+		
+		getLogger().info("new runTaskTimer create Success!");
+		getLogger().info("Number of Running task : " + tasks.size());
+	}
+
+//	デバッグ用(runTaskTimerが残らずきちんと終了してることを確認する時に使った)
+//	@EventHandler
+//	public void onclick(BlockBreakEvent event){
+//		Player player = event.getPlayer();
+//		tasks.remove(player);
+//		getLogger().info("new runTaskTimer delete Success!");
+//		getLogger().info("Number of Running task : " + tasks.size());
+//	}
+
+	//プレイヤーがleftした時に実行
+	@EventHandler
+	public void messageQuitEvent(PlayerQuitEvent event){
+		
+		//誰がleftしたのか取得しPlayer型の変数playerに格納
+		Player player = event.getPlayer();
+		
+		//タスクキル判定用に変数playerをArrayListから削除
+		tasks.remove(player);
+		
+		getLogger().info("new runTaskTimer delete Success!");
+		getLogger().info("Number of Running task : " + tasks.size());
+	}
+}
---
+++ src/com/github/unchama/multiseichieffect/TestRunnable.java
@@ -0,0 +1,54 @@
+package com.github.unchama.multiseichieffect;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class TestRunnable extends BukkitRunnable{
+	
+	//このクラス自身を表すインスタンス
+	//public static TestRunnable instance;
+	
+	//private SeichiPlugin plugin;
+	//public TestRunnable(SeichiPlugin plugin){
+	//	this.plugin = plugin;
+	//}
+	
+	//値の宣言
+	private Player player;
+	private int p_num;
+	
+	//newインスタンスが立ち上がる際に変数を初期化したり代入したりする処理
+	TestRunnable(Player _player) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		
+		//インスタンスにプレイヤー情報設定
+		player = _player;
+		
+		//ログイン人数取得用関数初期化
+		p_num = 0;
+	}
+	
+	@Override
+	public void run() {
+		// スケジュールで実行する処理の内容をここに書きます。
+		
+		//タスクキル判定用ArrayListに自分の名前が無かったらこのスゲジュールを削除する
+		if(!SeichiPlugin.tasks.contains(player)){
+			cancel();
+		}
+		
+		//ログイン人数を取得しp_numを決定
+		p_num = ( Bukkit.getOnlinePlayers().size() - 1 )  * 50;
+		
+		//プレイヤーにポーションエフェクトを付加
+		player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 110, p_num), true);
+	}
+	
+	//@EventHandler
+	//public void onplayerQuitEvent(PlayerQuitEvent event){
+	//	this.cancel();
+	//}
+}
