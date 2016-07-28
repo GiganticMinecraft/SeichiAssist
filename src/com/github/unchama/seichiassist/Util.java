74bb08c11d032de7e13304e76cdc34ae8fbc271c
BuildTools <unconfigured@null.spigotmc.org>
2016-07-28 14:19:32 +0900

    fix1

--- src/com/github/unchama/seichiassist/Util.java
+++ src/com/github/unchama/seichiassist/Util.java
@@ -1,15 +1,25 @@
 package com.github.unchama.seichiassist;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.FireworkEffect.Builder;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 public class Util {
+	static private FireworkEffect.Type[] types = { FireworkEffect.Type.BALL,
+		FireworkEffect.Type.BALL_LARGE, FireworkEffect.Type.BURST,
+		FireworkEffect.Type.CREEPER, FireworkEffect.Type.STAR, };
 	public static ItemStack getskull(){
 		ItemStack skull;
 		SkullMeta skullmeta;
@@ -87,5 +97,49 @@ public class Util {
 	}
 	public static String getName(String name) {
 		return name.toLowerCase();
+	}
+	public static void launchFireWorks(Location loc) {
+		// 花火を作る
+		Firework firework = loc.getWorld().spawn(loc, Firework.class);
+
+		// 花火の設定情報オブジェクトを取り出す
+		FireworkMeta meta = firework.getFireworkMeta();
+		Builder effect = FireworkEffect.builder();
+		Random rand = new Random();
+		// 形状をランダムに決める
+		effect.with(types[rand.nextInt(types.length)]);
+
+		// 基本の色を単色～5色以内でランダムに決める
+		effect.withColor(getRandomCrolors(1 + rand.nextInt(5)));
+
+		// 余韻の色を単色～3色以内でランダムに決める
+		effect.withFade(getRandomCrolors(1 + rand.nextInt(3)));
+
+		// 爆発後に点滅するかをランダムに決める
+		effect.flicker(rand.nextBoolean());
+
+		// 爆発後に尾を引くかをランダムに決める
+		effect.trail(rand.nextBoolean());
+
+		// 打ち上げ高さを1以上4以内でランダムに決める
+		meta.setPower(1 + rand.nextInt(4));
+
+		// 花火の設定情報を花火に設定
+		meta.addEffect(effect.build());
+		firework.setFireworkMeta(meta);
+
+	}
+	public static Color[] getRandomCrolors(int length) {
+		// 配列を作る
+		Color[] colors = new Color[length];
+		Random rand = new Random();
+		// 配列の要素を順に処理していく
+		for (int n = 0; n != length; n++) {
+			// 24ビットカラーの範囲でランダムな色を決める
+			colors[n] = Color.fromBGR(rand.nextInt(1 << 24));
+		}
+
+		// 配列を返す
+		return colors;
 	}
 }
