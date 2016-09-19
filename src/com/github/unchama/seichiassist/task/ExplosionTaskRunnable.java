5bf8174898caea1349a9b63d4e16bce89238926e
tar0sscom <tar0sscom@gmail.com>
2016-09-19 20:27:26 +0900

    あ

--- src/com/github/unchama/seichiassist/task/ExplosionTaskRunnable.java
+++ src/com/github/unchama/seichiassist/task/ExplosionTaskRunnable.java
@@ -1,5 +1,19 @@
 package com.github.unchama.seichiassist.task;
 
-public class ExplosionTaskRunnable {
+import org.bukkit.entity.Projectile;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class ExplosionTaskRunnable extends BukkitRunnable{
+	Projectile proj;
+
+	public ExplosionTaskRunnable(Projectile proj) {
+		this.proj = proj;
+	}
+
+	@Override
+	public void run() {
+		proj.remove();
+	}
 
 }
+
