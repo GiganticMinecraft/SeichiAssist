a82b3566545409bbadfec60a593ece3996fe510d
tar0sscom <tar0sscom@gmail.com>
2016-09-25 21:17:17 +0900

    シルバーフィッシュ石を破壊できるようにした。

--- src/com/github/unchama/seichiassist/SeichiAssist.java
+++ src/com/github/unchama/seichiassist/SeichiAssist.java
@@ -151,6 +151,7 @@ public class SeichiAssist extends JavaPlugin{
 			,Material.SANDSTONE,Material.QUARTZ_ORE,Material.END_BRICKS,Material.ENDER_STONE
 			,Material.ICE,Material.PACKED_ICE,Material.OBSIDIAN,Material.MAGMA,Material.SOUL_SAND,Material.LEAVES,Material.LEAVES_2
 			,Material.CLAY,Material.STAINED_CLAY,Material.COBBLESTONE,Material.MOSSY_COBBLESTONE,Material.HARD_CLAY
+			,Material.MONSTER_EGGS
 			));
 	public static final List<Material> luckmateriallist = new ArrayList<Material>(Arrays.asList(
 			Material.COAL_ORE,Material.DIAMOND_ORE,Material.LAPIS_ORE,Material.EMERALD_ORE,
--- src/com/github/unchama/seichiassist/util/Util.java
+++ src/com/github/unchama/seichiassist/util/Util.java
@@ -26,6 +26,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -638,6 +639,10 @@ public class Util {
 					dropmaterial = Material.CLAY_BALL;
 					dropitem = new ItemStack(dropmaterial,4);
 					break;
+				case MONSTER_EGGS:
+					Location loc = breakblock.getLocation();
+					breakblock.getWorld().spawnEntity(loc, EntityType.SILVERFISH);
+					dropitem = null;
 				default:
 					//breakblcokのままのアイテムスタックを保存
 					dropitem = new ItemStack(breakmaterial,1,b);
@@ -703,9 +708,9 @@ public class Util {
 		rotation += 360.0;
 		}
 
-		if(pitch <= -30){
+		if(pitch <= -10){
 			return "U";
-		}else if(pitch >= 25){
+		}else if(pitch >= 15){
 			return "D";
 		}else if (0 <= rotation && rotation < 45.0) {
 			return "N";
