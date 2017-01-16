321366c659502e574de3e1b812263f72c02abb5f
taaa150 <thend160@yahoo.co.jp>
2017-01-16 18:27:38 +0900

    ミス修正

--- src/com/github/unchama/seichiassist/listener/PlayerInventoryListener.java
+++ src/com/github/unchama/seichiassist/listener/PlayerInventoryListener.java
@@ -2948,7 +2948,7 @@ public class PlayerInventoryListener implements Listener {
     			ItemMeta itemmeta = itemstackcurrent.getItemMeta();
     			player.playSound(player.getLocation(), Sound.BLOCK_STONE_BUTTON_CLICK_ON, 1, 1);
     			player.sendMessage("この実績は自動解禁式です。毎分の処理をお待ちください。");
-    			player.openInventory(MenuInventoryData.getTitleTimeData(player));
+    			player.openInventory(MenuInventoryData.getTitleSupportData(player));
     		}
     		else if (itemstackcurrent.getType().equals(Material.DIAMOND_BLOCK)){
     			ItemMeta itemmeta = itemstackcurrent.getItemMeta();
@@ -2985,7 +2985,7 @@ public class PlayerInventoryListener implements Listener {
     				playerdata.displayTitleNo = 6008 ;
     				player.sendMessage("二つ名「"+ SeichiAssist.config.getTitle(6008) +"」が設定されました。");
     			}
-    			player.openInventory(MenuInventoryData.getTitleTimeData(player));
+    			player.openInventory(MenuInventoryData.getTitleSupportData(player));
     		}
     		//実績メニューに戻る
     		else if(itemstackcurrent.getType().equals(Material.SKULL_ITEM) && ((SkullMeta)itemstackcurrent.getItemMeta()).getOwner().equals("MHF_ArrowLeft")){
