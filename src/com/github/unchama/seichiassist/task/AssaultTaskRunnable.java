8d6045524010068ca59cdb185c9ff58750c8b5ed
ga2ku <ga2kunico@gmail.com>
2017-09-12 23:29:13 +0900

    凝固時の整地判定の修正

--- src/com/github/unchama/seichiassist/task/AssaultTaskRunnable.java
+++ src/com/github/unchama/seichiassist/task/AssaultTaskRunnable.java
@@ -324,13 +324,13 @@ public class AssaultTaskRunnable extends BukkitRunnable{
 		if(waterflag){
 			for(int waternum = 0 ; waternum <waterlist.size();waternum++){
 				waterlist.get(waternum).setType(Material.PACKED_ICE);
-				player.incrementStatistic(Statistic.MINE_BLOCK, waterlist.get(waternum).getType());
+				player.incrementStatistic(Statistic.MINE_BLOCK, Material.STONE);
 				BreakUtil.logPlace(player,waterlist.get(waternum));
 			}
 		}else if(lavaflag){
 			for(int lavanum = 0 ; lavanum <lavalist.size();lavanum++){
 				lavalist.get(lavanum).setType(Material.MAGMA);
-				player.incrementStatistic(Statistic.MINE_BLOCK, lavalist.get(lavanum).getType());
+				player.incrementStatistic(Statistic.MINE_BLOCK, Material.STONE);
 				BreakUtil.logPlace(player,lavalist.get(lavanum));
 			}
 		}else if(breakflag){
