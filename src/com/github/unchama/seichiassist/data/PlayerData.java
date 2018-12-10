deaaa30c3844c1bd5305603f24409a74010a7e89
yuki256R <yuki2000jr@gmail.com>
2018-12-10 22:11:19 +0900

    スターレベル
    sqlに保存してないのでプレイヤーデータ読み込み時に毎回レベルアップしちゃう。
    taaaさんが改造するとのことなのでとりあえず実装取り消し

--- src/com/github/unchama/seichiassist/data/PlayerData.java
+++ src/com/github/unchama/seichiassist/data/PlayerData.java
@@ -433,15 +433,7 @@ public class PlayerData {
 	//表示される名前に整地レベルor二つ名を追加
 	public void setDisplayName(Player p) {
 		String displayname = Util.getName(p);
-		//スターレベル用の計算
-		long starlevelb = starlevel;
 		starlevel = ( totalbreaknum / 87115000 ) - 1 ;
-		//スターレベルアップ
-		if(starlevel != starlevelb && starlevel >= 1){
-			p.sendMessage(ChatColor.GOLD+"ﾑﾑｯwwwwwwwﾚﾍﾞﾙｱｯﾌﾟwwwwwww【Lv(☆"+(starlevelb)+")→Lv(☆"+(starlevel)+")】");
-			Location loc = p.getLocation();
-			Util.launchFireWorks(loc);
-		}
 
 		//表示を追加する処理
 		if(displayTitle1No == 0 && displayTitle2No == 0 && displayTitle3No == 0){
