0c01ea2989f7ff2a4d904fc6dc02fa44dff7db4c
KisaragiEffective <kisaragi.effective@gmail.com>
2019-04-13 23:02:30 +0900

    [UPDATE] 外部プラグインへのアクセスを別クラスへ移動

--- src/main/java/com/github/unchama/seichiassist/util/Util.java
+++ src/main/java/com/github/unchama/seichiassist/util/Util.java
@@ -470,46 +470,6 @@ public class Util {
 		// 配列を返す
 		return colors;
 	}
-	//コアプロテクトAPIを返す
-	public static CoreProtectAPI getCoreProtect() {
-		Plugin plugin = SeichiAssist.plugin.getServer().getPluginManager().getPlugin("CoreProtect");
-
-		// Check that CoreProtect is loaded
-		if (!(plugin instanceof CoreProtect)) {
-		    return null;
-		}
-
-		// Check that the API is enabled
-		CoreProtectAPI CoreProtect = ((CoreProtect)plugin).getAPI();
-		if (!CoreProtect.isEnabled()){
-		    return null;
-		}
-
-		// Check that a compatible version of the API is loaded
-		if (CoreProtect.APIVersion() < 4){
-		    return null;
-		}
-
-		return CoreProtect;
-	}
-	//ワールドガードAPIを返す
-	public static WorldGuardPlugin getWorldGuard() {
-		Plugin plugin = SeichiAssist.plugin.getServer().getPluginManager().getPlugin("WorldGuard");
-
-	    // WorldGuard may not be loaded
-	    if (!(plugin instanceof WorldGuardPlugin)) {
-	        throw new NullPointerException("WorldGuardPluginが見つかりませんでした。");
-	    }
-
-	    return (WorldGuardPlugin) plugin;
-	}
-	//ワールドエディットAPIを返す
-	public static WorldEditPlugin getWorldEdit() {
-        Plugin pl = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
-        if(pl instanceof WorldEditPlugin)
-            return (WorldEditPlugin)pl;
-        else return null;
-    }
 
 	//ガチャアイテムを含んでいるか調べる
 	public static boolean containsGachaTicket(Player player) {
