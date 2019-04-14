32941e0191eb94deeb73de152a9a454ce7646301
eighty88 <49605393+eighty88@users.noreply.github.com>
2019-04-14 15:56:06 +0900

    ダウンローダーの前提ファイル

--- core.bat
+++ core.bat
@@ -1,11 +1,15 @@
 @echo off
-call :wget %1
+set Dir=%2
+set ifcreate=%3
+if "%ifcreate%"=="Create" mkdir %Dir%
+call :wget %1 %2
 goto :END
 
 :wget
 set URL=%1
+set Dir=%2
 set FILENAME=%~n1%~x1
-bitsadmin /RawReturn /TRANSFER getfile %URL% %CD%\%FILENAME%
+bitsadmin /RawReturn /TRANSFER getfile %URL% %CD%\%Dir%\%FILENAME%
 goto :EOF
 
 :END
