c45c56f3a301a30bbe62f23ac2871fa08657b2d2
KisaragiEffective <kisaragi.effective@gmail.com>
2019-04-14 14:22:57 +0900

    [UPDATE] 変数をインライン化 (再)

--- src/main/java/com/github/unchama/seichiassist/task/VotingFairyTaskRunnable.java
+++ src/main/java/com/github/unchama/seichiassist/task/VotingFairyTaskRunnable.java
@@ -51,14 +51,10 @@ public class VotingFairyTaskRunnable {
 
 	public static String dispToggleVFTime(int toggle){
 
-		String s;
-
-		s = toggle == 1 ? "30分"
+		return toggle == 1 ? "30分"
 		  : toggle == 2 ? "1時間"
 		  : toggle == 3 ? "1時間30分"
 		  : toggle == 4 ? "2時間"
 				  : "エラー";
-
-		return s;
 	}
 }
