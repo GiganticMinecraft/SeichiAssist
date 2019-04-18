6a9ba687a4f05ed999e55e7bb5708e12405dbda3
kory33 <6561358+kory33@users.noreply.github.com>
2019-04-19 00:23:02 +0800

    [REFACTOR] Unitの宣言順を修正

--- src/main/java/com/github/unchama/util/Unit.java
+++ src/main/java/com/github/unchama/util/Unit.java
@@ -4,7 +4,7 @@ package com.github.unchama.util;
  * 単一の情報量を持たない値が存在するようなシングルトンクラス
  */
 public final class Unit {
-    private Unit() {}
-
     public static final Unit instance = new Unit();
+
+    private Unit() {}
 }
