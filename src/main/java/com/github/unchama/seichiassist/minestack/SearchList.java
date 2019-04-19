0b1a6c7cffc72aa3a54544667e3e6b12b7a7291e
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2019-04-19 22:53:30 +0900

    Update SearchList.java

--- src/main/java/com/github/unchama/seichiassist/minestack/SearchList.java
+++ src/main/java/com/github/unchama/seichiassist/minestack/SearchList.java
@@ -1,13 +1,1 @@
-package com.github.unchama.seichiassist.minestack;
 
-import java.util.*;
-
-/**
- * Created by karayuu on 2018/06/20
- */
-public class SearchList {
-	public static Map<MineStackObj, MineStackSearchData> searchDataMap = new HashMap() {
-		{
-		}
-	};
-}
