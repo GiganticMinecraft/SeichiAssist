30724cd925257c9de922e8e98995077ae9a2ca79
KisaragiEffective <kisaragi.effective@gmail.com>
2019-04-19 23:47:05 +0900

    [FIX] 名前を間違えていた

--- src/main/java/com/github/unchama/seichiassist/commands/RegisterMineStackCommand.java
+++ src/main/java/com/github/unchama/seichiassist/commands/RegisterMineStackCommand.java
@@ -11,12 +11,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
-public class RegistryMineStackCommand implements TabCompleter, CommandExecutor {
+public class RegisterMineStackCommand implements TabCompleter, CommandExecutor, TabExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player) {
