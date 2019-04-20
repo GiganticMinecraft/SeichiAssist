cefc9c7c7da7ea266f3797633e7131a2112431e5
KisaragiEffective <kisaragi.effective@gmail.com>
2019-04-20 18:22:59 +0900

    [UPDATE] ガード節を使って深さを軽減

--- src/main/java/com/github/unchama/seichiassist/commands/RegisterMineStackCommand.java
+++ src/main/java/com/github/unchama/seichiassist/commands/RegisterMineStackCommand.java
@@ -20,56 +20,55 @@ import java.util.List;
 public class RegisterMineStackCommand implements TabCompleter, CommandExecutor, TabExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (sender instanceof Player) {
-			final Player player = (Player) sender;
-			final ItemStack item = player.getInventory().getItemInMainHand();
-			if (item == null) {
-				sender.sendMessage("手にアイテムを持ってください。");
-				return true;
-			}
-
-			// [type] [objname] [level]
-			if (args.length == 3) {
-				final String internalId = args[1];
-				/* TODO: レベルとは??? */
-				final int theLevel = Integer.parseInt(args[2]);
-				switch (args[0].toLowerCase()) {
-					case "build":
-						MineStackRegistry.addBuildingMaterial(new MineStackBuildObj(internalId, null, theLevel, item.getType(), item.getDurability()));
-						break;
-					case "drop":
-						MineStackRegistry.addDropMaterial(new MineStackDropObj(internalId, null, theLevel, item.getType(), item.getDurability()));
-						break;
-					case "farm":
-						MineStackRegistry.addFarmMaterial(new MineStackFarmObj(internalId, null, theLevel, item.getType(), item.getDurability()));
-						break;
-					case "gacha":
-						MineStackRegistry.addGachaMaterial(new MineStackGachaObj(internalId, null, theLevel, item.getType(), item.getDurability()));
-						break;
-					case "mine":
-						MineStackRegistry.addMiningMaterial(new MineStackMineObj(internalId, null, theLevel, item.getType(), item.getDurability()));
-						break;
-					case "redstone":
-						MineStackRegistry.addRedstoneMaterial(new MineStackRsObj(internalId, null, theLevel, item.getType(), item.getDurability()));
-						break;
-					default:
-						sender.sendMessage(" 以下の引数が有効です:\n" +
-								"build -> 建材\n" +
-								"drop -> ドロップ\n" +
-								"farm -> 農業\n" +
-								"gacha -> ガチャ\n" +
-								"mine -> 採掘\n" +
-								"redstone -> レッドストーン\n" +
-								""
-						);
-						break;
-				}
-			} else {
-				sender.sendMessage("1番目にタイプ、2番目に内部ID、3番目にレベルを入力してください。");
-			}
-		} else {
+		if (!(sender instanceof Player)) {
 			sender.sendMessage("このコマンドはプレイヤーから実行してください。");
 			return true;
+		}
+		final Player player = (Player) sender;
+		final ItemStack item = player.getInventory().getItemInMainHand();
+		if (item == null) {
+			sender.sendMessage("手にアイテムを持ってください。");
+			return true;
+		}
+
+		// [type] [objname] [level]
+		if (args.length != 3) {
+			sender.sendMessage("1番目にタイプ、2番目に内部ID、3番目にレベルを入力してください。");
+		}
+
+		final String internalId = args[1];
+		/* TODO: レベルとは??? */
+		final int theLevel = Integer.parseInt(args[2]);
+		switch (args[0].toLowerCase()) {
+			case "build":
+				MineStackRegistry.addBuildingMaterial(new MineStackBuildObj(internalId, null, theLevel, item.getType(), item.getDurability()));
+				break;
+			case "drop":
+				MineStackRegistry.addDropMaterial(new MineStackDropObj(internalId, null, theLevel, item.getType(), item.getDurability()));
+				break;
+			case "farm":
+				MineStackRegistry.addFarmMaterial(new MineStackFarmObj(internalId, null, theLevel, item.getType(), item.getDurability()));
+				break;
+			case "gacha":
+				MineStackRegistry.addGachaMaterial(new MineStackGachaObj(internalId, null, theLevel, item.getType(), item.getDurability()));
+				break;
+			case "mine":
+				MineStackRegistry.addMiningMaterial(new MineStackMineObj(internalId, null, theLevel, item.getType(), item.getDurability()));
+				break;
+			case "redstone":
+				MineStackRegistry.addRedstoneMaterial(new MineStackRsObj(internalId, null, theLevel, item.getType(), item.getDurability()));
+				break;
+			default:
+				sender.sendMessage(" 以下の引数が有効です:\n" +
+						"build -> 建材\n" +
+						"drop -> ドロップ\n" +
+						"farm -> 農業\n" +
+						"gacha -> ガチャ\n" +
+						"mine -> 採掘\n" +
+						"redstone -> レッドストーン\n" +
+						""
+				);
+				break;
 		}
 		return false;
 	}
