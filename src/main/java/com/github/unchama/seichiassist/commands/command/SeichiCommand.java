d03dcda12df78c773011dbc4db58d44e6b5afdbc
unicroak <unicroak@gmail.com>
2019-04-24 22:41:13 +0900

    [FIX] プレイヤー以外からコマンドが実行された際のエラーハンドリングを個別で行うように

--- src/main/java/com/github/unchama/seichiassist/commands/command/SeichiCommand.java
+++ src/main/java/com/github/unchama/seichiassist/commands/command/SeichiCommand.java
@@ -3,9 +3,9 @@ package com.github.unchama.seichiassist.commands.command;
 import com.github.unchama.seichiassist.commands.command.context.ArgumentSatisfier;
 import com.github.unchama.seichiassist.commands.command.context.CommandContext;
 import com.github.unchama.seichiassist.commands.command.result.CommandResult;
-import com.github.unchama.seichiassist.commands.command.result.ExecutedOnConsole;
 import com.github.unchama.seichiassist.commands.command.result.IllegalArgument;
 import com.github.unchama.seichiassist.commands.command.result.NotEnoughArgument;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -21,8 +21,10 @@ public final class SeichiCommand {
 
     private final List<ArgumentSatisfier> satisfierList;
     private final Map<String, Consumer<CommandContext>> resultActionMap;
-    private boolean mustBeExecutedByPlayer;
     private Function<CommandContext, CommandResult> action;
+
+    private boolean mustBeExecutedByPlayer;
+    private Consumer<CommandContext> failByExecutingOnConsole;
 
     private SeichiCommand() {
         this.satisfierList = new ArrayList<>(64);
@@ -33,20 +35,21 @@ public final class SeichiCommand {
         SeichiCommand seichiCommand = new SeichiCommand();
         CommandContext commandContext = new CommandContext(sender, Arrays.asList(rawArguments), seichiCommand.satisfierList);
 
-        CommandResult result;
         if (seichiCommand.mustBeExecutedByPlayer && !(sender instanceof Player)) {
-            result = new ExecutedOnConsole();
-        } else {
-            try {
-                creator.accept(seichiCommand);
-                seichiCommand.action.apply(commandContext);
+            seichiCommand.failByExecutingOnConsole.accept(commandContext);
+            return true;
+        }
 
-                result = new CommandResult.Success();
-            } catch (IllegalArgumentException | ClassCastException ex) {
-                result = new IllegalArgument();
-            } catch (IndexOutOfBoundsException ex) {
-                result = new NotEnoughArgument();
-            }
+        CommandResult result;
+        try {
+            creator.accept(seichiCommand);
+            seichiCommand.action.apply(commandContext);
+
+            result = new CommandResult.Success();
+        } catch (IllegalArgumentException | ClassCastException ex) {
+            result = new IllegalArgument();
+        } catch (IndexOutOfBoundsException ex) {
+            result = new NotEnoughArgument();
         }
 
         seichiCommand
@@ -56,6 +59,21 @@ public final class SeichiCommand {
                 .accept(commandContext);
 
         return result.isSuccess();
+    }
+
+    public SeichiCommand mustBeExecutedByPlayer(boolean mustBeExecutedByPlayer, Consumer<CommandContext> ifFail) {
+        this.mustBeExecutedByPlayer = mustBeExecutedByPlayer;
+        this.failByExecutingOnConsole = ifFail;
+
+        return this;
+    }
+
+    public SeichiCommand mustBeExecutedByPlayer(boolean mustBeExecutedByPlayer) {
+        this.mustBeExecutedByPlayer(
+                mustBeExecutedByPlayer,
+                context -> context.getSender().sendMessage(ChatColor.RED + "This command must be executed by player!")
+        );
+        return this;
     }
 
     @SuppressWarnings("unchecked")
@@ -72,11 +90,6 @@ public final class SeichiCommand {
     public <R extends CommandResult> SeichiCommand ifOn(Class<R> clazz, Consumer<CommandContext> action) {
         String clazzName = clazz.getName();
         resultActionMap.put(clazzName, action);
-        return this;
-    }
-
-    public SeichiCommand mustBeExecutedByPlayer(boolean mustBeExecutedByPlayer) {
-        this.mustBeExecutedByPlayer = mustBeExecutedByPlayer;
         return this;
     }
 
