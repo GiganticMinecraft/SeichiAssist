24e38f4d6d206932517975040f1e3e2b7f00a170
kory33 <6561358+kory33@users.noreply.github.com>
2019-04-30 05:56:40 +0800

    [FIX] DBマイグレーションの検索パスを変更

--- src/main/java/com/github/unchama/seichiassist/database/DatabaseGateway.java
+++ src/main/java/com/github/unchama/seichiassist/database/DatabaseGateway.java
@@ -161,6 +161,7 @@ public class DatabaseGateway {
 				() -> Flyway.configure()
 						.dataSource(databaseUrl, loginId, password)
 						.baselineOnMigrate(true)
+						.locations("db/migration", "com/github/unchama/seichiassist/database/migration")
 						.baselineVersion("1.0.0")
 						.schemas("flyway_managed_schema")
 						.load().migrate()
