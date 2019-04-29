cd14ce42dff5d932321325cd83b661d25f85e733
kory33 <6561358+kory33@users.noreply.github.com>
2019-04-29 21:38:51 +0800

    [TEMP CHANGE] データベースの定数を追加する

--- src/main/java/com/github/unchama/seichiassist/database/DatabaseConstants.java
+++ src/main/java/com/github/unchama/seichiassist/database/DatabaseConstants.java
@@ -7,4 +7,7 @@ public class DatabaseConstants {
     public static final String DONATEDATA_TABLENAME = "donatedata";
     public static final String MINESTACK_GACHADATA_TABLENAME = "msgachadata";
 
+    public static final String MINESTACK_TABLENAME = "mine_stack";
+    public static final String GRID_TEMPLATE_TABLENAME = "grid_template";
+
 }
