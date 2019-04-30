634e2dbd867142c5192c7424ef80d2f971aaf234
KisaragiEffective <kisaragi.effective@gmail.com>
2019-04-30 17:55:40 +0900

    [UPDATE] 勝った！！！！！！ (デフォルトコーディングをUTF-8へ、Apache Commons Lang3とcommons-codecを追加、レポジトリを追加)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ repositories {
     maven { url = URI("https://repo.spongepowered.org/maven") }
     maven { url = URI("https://repo.maven.apache.org/maven2") }
     maven { url = URI("https://hub.spigotmc.org/nexus/content/repositories/snapshots")}
+    maven { url = URI("https://oss.sonatype.org/content/repositories/snapshots")}
+    mavenCentral()
 }
 
 dependencies {
@@ -36,6 +38,10 @@ dependencies {
     implementation("org.jetbrains:annotations:17.0.0")
 
     implementation("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
+
+    implementation("org.apache.commons:commons-lang3:3.9")
+
+    implementation("commons-codec:commons-codec:1.12")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
     testImplementation("junit:junit:4.4")
@@ -55,4 +61,8 @@ tasks.processResources {
         filter<ReplaceTokens>("tokens" to tokenReplacementMap)
     }
     from(projectDir) { include("LICENSE") }
+}
+
+tasks.withType(JavaCompile::class.java).all {
+    this.options.encoding = "UTF-8"
 }
