f8a84040f75d400e6fe0e83c29a274ca9ff1fd76
KisaragiEffective <kisaragi.effective@gmail.com>
2019-05-05 16:07:12 +0900

    [UPDATE] ループ内での文字列連結をStringbuilderで置換

--- src/main/java/com/github/unchama/seichiassist/database/init/ddl/PlayerDataTableQueryGenerator.java
+++ src/main/java/com/github/unchama/seichiassist/database/init/ddl/PlayerDataTableQueryGenerator.java
@@ -4,6 +4,9 @@ import com.github.unchama.seichiassist.ActiveSkillEffect;
 import com.github.unchama.seichiassist.ActiveSkillPremiumEffect;
 import com.github.unchama.seichiassist.Config;
 import com.github.unchama.seichiassist.SeichiAssist;
+import com.github.unchama.seichiassist.minestack.MineStackObj;
+
+import java.util.List;
 
 /**
  * playerdataテーブルの初期化クエリを計算するクラス
@@ -29,8 +32,9 @@ public class PlayerDataTableQueryGenerator implements TableInitializationQueryGe
     @Override
     public String generateColumnCreationQuery() {
         //必要なcolumnを随時追加
-        String command =
-                "alter table " + tableReferenceName +
+        // 多分、どうあがいても1000字以上はある
+        StringBuilder command = new StringBuilder(1000);
+                command.append("alter table " + tableReferenceName +
                         " add column if not exists effectflag tinyint default 0" +
                         ",add column if not exists minestackflag boolean default true" +
                         ",add column if not exists messageflag boolean default false" +
@@ -72,7 +76,7 @@ public class PlayerDataTableQueryGenerator implements TableInitializationQueryGe
                         ",add column if not exists starlevel int default 0" +
                         ",add column if not exists starlevel_Break int default 0" +
                         ",add column if not exists starlevel_Time int default 0" +
-                        ",add column if not exists starlevel_Event int default 0" ;
+                        ",add column if not exists starlevel_Event int default 0");
 
         //MineStack関連をすべてfor文に変更
         if(SeichiAssist.minestack_sql_enable){
@@ -81,83 +85,61 @@ public class PlayerDataTableQueryGenerator implements TableInitializationQueryGe
             }
         }
 
-        command +=
-                ",add column if not exists playtick int default 0" +
-                        ",add column if not exists killlogflag boolean default false" +
-                        ",add column if not exists worldguardlogflag boolean default true" +//
+        command.append(",add column if not exists playtick int default 0" + ",add column if not exists killlogflag boolean default false" + ",add column if not exists worldguardlogflag boolean default true" +//
 
-                        ",add column if not exists multipleidbreakflag boolean default false" +
-
-                        ",add column if not exists pvpflag boolean default false" +
-                        ",add column if not exists loginflag boolean default false" +
-                        ",add column if not exists p_vote int default 0" +
-                        ",add column if not exists p_givenvote int default 0" +
-                        ",add column if not exists effectpoint int default 0" +
-                        ",add column if not exists premiumeffectpoint int default 0" +
-                        ",add column if not exists mana double default 0.0" +
-                        ",add column if not exists expvisible boolean default true" +
-                        ",add column if not exists totalexp int default 0" +
-                        ",add column if not exists expmarge tinyint unsigned default 0" +
-                        ",add column if not exists shareinv blob" +
-                        ",add column if not exists everysound boolean default true" +
-                        ",add column if not exists everymessage boolean default true" +
-
-                        ",add column if not exists homepoint_" + config.getServerNum() + " varchar(" + config.getSubHomeMax() * SeichiAssist.SUB_HOME_DATASIZE + ") default ''"+
-                        ",add column if not exists subhome_name_" + config.getServerNum() + " blob default null" +
+                ",add column if not exists multipleidbreakflag boolean default false" + ",add column if not exists pvpflag boolean default false" + ",add column if not exists loginflag boolean default false" + ",add column if not exists p_vote int default 0" + ",add column if not exists p_givenvote int default 0" + ",add column if not exists effectpoint int default 0" + ",add column if not exists premiumeffectpoint int default 0" + ",add column if not exists mana double default 0.0" + ",add column if not exists expvisible boolean default true" + ",add column if not exists totalexp int default 0" + ",add column if not exists expmarge tinyint unsigned default 0" + ",add column if not exists shareinv blob" + ",add column if not exists everysound boolean default true" + ",add column if not exists everymessage boolean default true" + ",add column if not exists homepoint_").append(config.getServerNum()).append(" varchar(").append(config.getSubHomeMax() * SeichiAssist.SUB_HOME_DATASIZE).append(") default ''").append(",add column if not exists subhome_name_").append(config.getServerNum()).append(" blob default null").append(
 
 
-                        //BuildAssistのデータ
-                        ",add column if not exists build_lv int default 1" +//
-                        ",add column if not exists build_count double default 0" +//
-                        ",add column if not exists build_count_flg TINYINT UNSIGNED default 0" +//
+                //BuildAssistのデータ
+                ",add column if not exists build_lv int default 1").append(//
+                ",add column if not exists build_count double default 0").append(//
+                ",add column if not exists build_count_flg TINYINT UNSIGNED default 0").append(//
 
-                        ",add column if not exists anniversary boolean default false" +//
+                ",add column if not exists anniversary boolean default false").append(//
 
-                        //投票妖精関連
-                        ",add column if not exists canVotingFairyUse boolean default false" +//
-                        ",add column if not exists newVotingFairyTime varchar(" + SeichiAssist.VOTE_FAIRYTIME_DATASIZE + ") default ''" +//
-                        ",add column if not exists VotingFairyRecoveryValue int default 0" +//
-                        ",add column if not exists hasVotingFairyMana int default 0"+//
-                        ",add column if not exists toggleGiveApple int default 1"+//
-                        ",add column if not exists toggleVotingFairy int default 1"+//
-                        ",add column if not exists p_apple bigint default 0"+
+                //投票妖精関連
+                ",add column if not exists canVotingFairyUse boolean default false").append(//
+                ",add column if not exists newVotingFairyTime varchar(").append(SeichiAssist.VOTE_FAIRYTIME_DATASIZE).append(") default ''").append(//
+                ",add column if not exists VotingFairyRecoveryValue int default 0").append(//
+                ",add column if not exists hasVotingFairyMana int default 0").append(//
+                ",add column if not exists toggleGiveApple int default 1").append(//
+                ",add column if not exists toggleVotingFairy int default 1").append(//
+                ",add column if not exists p_apple bigint default 0").append(
 
-                        //貢献pt関連
-                        ",add column if not exists contribute_point int default 0"+//
-                        ",add column if not exists added_mana int default 0" +
-
-                        ",add column if not exists lastvote varchar(40) default null" +
-                        ",add column if not exists chainvote int default 0" +
-
-                        ",add column if not exists GBstage int default 0" +
-                        ",add column if not exists GBexp int default 0" +
-                        ",add column if not exists GBlevel int default 0" +
-                        ",add column if not exists isGBStageUp boolean default false";
+                //貢献pt関連
+                ",add column if not exists contribute_point int default 0").append(//
+                ",add column if not exists added_mana int default 0").append(
+                ",add column if not exists lastvote varchar(40) default null").append(
+                ",add column if not exists chainvote int default 0").append(
+                ",add column if not exists GBstage int default 0").append(
+                ",add column if not exists GBexp int default 0").append(
+                ",add column if not exists GBlevel int default 0").append(
+                ",add column if not exists isGBStageUp boolean default false");
 
         for (int i = 0; i <= config.getTemplateKeepAmount() - 1; i++) {
-            command += ",add column if not exists ahead_" + i + " int default 0";
-            command += ",add column if not exists behind_" + i + " int default 0";
-            command += ",add column if not exists right_" + i + " int default 0";
-            command += ",add column if not exists left_" + i + " int default 0";
+            command.append(",add column if not exists ahead_").append(i).append(" int default 0");
+            command.append(",add column if not exists behind_").append(i).append(" int default 0");
+            command.append(",add column if not exists right_").append(i).append(" int default 0");
+            command.append(",add column if not exists left_").append(i).append(" int default 0");
         }
 
         ActiveSkillEffect[] activeSkillEffects = ActiveSkillEffect.values();
         for (final ActiveSkillEffect activeSkillEffect : activeSkillEffects) {
-            command += ",add column if not exists " + activeSkillEffect.getsqlName() + " boolean default false";
+            command.append(",add column if not exists ").append(activeSkillEffect.getsqlName()).append(" boolean default false");
         }
 
         ActiveSkillPremiumEffect[] premiumEffects = ActiveSkillPremiumEffect.values();
         for (final ActiveSkillPremiumEffect activeSkillPremiumEffect : premiumEffects) {
-            command +=  ",add column if not exists " + activeSkillPremiumEffect.getsqlName() + " boolean default false";
+            command.append(",add column if not exists ").append(activeSkillPremiumEffect.getsqlName()).append(" boolean default false");
         }
 
         //正月Event用
-        command += ",add column if not exists hasNewYearSobaGive boolean default false";
-        command += ",add column if not exists newYearBagAmount int default 0";
+        command.append(",add column if not exists hasNewYearSobaGive boolean default false");
+        command.append(",add column if not exists newYearBagAmount int default 0");
 
         //バレンタインイベント用
-        command += ",add column if not exists hasChocoGave boolean default false";
+        command.append(",add column if not exists hasChocoGave boolean default false");
 
-        return command;
+        return command.toString();
     }
 }
