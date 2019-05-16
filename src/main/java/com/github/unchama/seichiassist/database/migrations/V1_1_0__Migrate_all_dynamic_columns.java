85a84de6ed8b92b5b40aeb5fb4d961127f7d26f2
kory33 <6561358+kory33@users.noreply.github.com>
2019-05-17 01:18:41 +0800

    Revert "[ADD] チェックサムを返すマイグレーションのクラスを追加(未実装)"
    This reverts commit a150cadd

--- src/main/java/com/github/unchama/seichiassist/database/migrations/V1_1_0__Migrate_all_dynamic_columns.java
+++ src/main/java/com/github/unchama/seichiassist/database/migrations/V1_1_0__Migrate_all_dynamic_columns.java
@@ -4,6 +4,7 @@ import com.github.unchama.util.collection.ImmutableListFactory;
 import com.github.unchama.util.collection.SetFactory;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,7 +19,7 @@ import java.util.stream.Stream;
 import static com.github.unchama.seichiassist.util.TypeConverter.isParsableToInteger;
 
 @SuppressWarnings("unused")
-public class V1_1_0__Migrate_all_dynamic_columns extends ChecksumProvidingMigration {
+public class V1_1_0__Migrate_all_dynamic_columns extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
