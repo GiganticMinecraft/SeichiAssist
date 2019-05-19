2b45048027c074b88b1a61284b38b2277f22de14
kory33 <6561358+kory33@users.noreply.github.com>
2019-05-20 01:21:30 +0800

    [CLEAN] EffectCommandの整理

--- src/main/java/com/github/unchama/seichiassist/commands/EffectCommand.java
+++ src/main/java/com/github/unchama/seichiassist/commands/EffectCommand.java
@@ -3,68 +3,74 @@ package com.github.unchama.seichiassist.commands;
 import com.github.unchama.seichiassist.SeichiAssist;
 import com.github.unchama.seichiassist.data.GachaData;
 import com.github.unchama.seichiassist.data.PlayerData;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.UUID;
-
 public class EffectCommand implements CommandExecutor {
+
+	// TODO これはここにあるべきではない
+	private static void toggleEffect(PlayerData playerData) {
+		int newEffectFlag = (playerData.effectflag + 1) % 6;
+		Player player = Bukkit.getPlayer(playerData.uuid);
+
+		if (newEffectFlag == 0) {
+			player.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(無制限)");
+		} else if (newEffectFlag == 1) {
+			player.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(127制限)");
+		} else if (newEffectFlag == 2) {
+			player.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(200制限)");
+		} else if (newEffectFlag == 3) {
+			player.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(400制限)");
+		} else if (newEffectFlag == 4) {
+			player.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(600制限)");
+		} else {
+			player.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:OFF");
+		}
+
+		player.sendMessage(ChatColor.GREEN + "再度 /ef コマンドを実行することでトグルします。");
+
+		//切り替えたフラグを反映
+		playerData.effectflag = newEffectFlag;
+	}
+
+	// TODO これはここにあるべきではない
+	private static void toggleMessageFlag(PlayerData playerData) {
+		boolean newMessageFlag = !playerData.messageflag;
+		Player player = Bukkit.getPlayer(playerData.uuid);
+
+		if (newMessageFlag){
+			player.sendMessage(ChatColor.GREEN + "内訳表示:ON(OFFに戻したい時は再度コマンドを実行します。)");
+		}else{
+			player.sendMessage(ChatColor.GREEN + "内訳表示:OFF");
+		}
+
+		playerData.messageflag = newMessageFlag;
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		//プレイヤーを取得
-		Player player = (Player)sender;
-		//プレイヤーネーム
-		//String name = Util.getName(player);
-		//UUIDを取得
-		UUID uuid = player.getUniqueId();
-		//playerdataを取得
-		PlayerData playerdata = SeichiAssist.playermap.get(uuid);
 		//プレイヤーからの送信でない時処理終了
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.GREEN + "このコマンドはゲーム内から実行してください。");
 			return true;
-		}else if(args.length == 0){
-			//エフェクトフラグを切り替え
-			int effectflag = (playerdata.effectflag + 1) % 6;
-			if (effectflag == 0) {
-				sender.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(無制限)");
-			} else if (effectflag == 1) {
-				sender.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(127制限)");
-			} else if (effectflag == 2) {
-				sender.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(200制限)");
-			} else if (effectflag == 3) {
-				sender.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(400制限)");
-			} else if (effectflag == 4) {
-				sender.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:ON(600制限)");
-			} else {
-				sender.sendMessage(ChatColor.GREEN + "採掘速度上昇効果:OFF");
-			}
-			sender.sendMessage(ChatColor.GREEN + "再度コマンドを実行することでトグルします。");
-			//切り替えたフラグを反映
-			playerdata.effectflag = effectflag;
+		}
+
+		PlayerData senderPlayerData = SeichiAssist.playermap.get(((Player) sender).getUniqueId());
+
+		if(args.length == 0) {
+			toggleEffect(senderPlayerData);
 			return true;
-		}else if(args.length == 1){
-			//コマンド長が１の時
-			if(args[0].equalsIgnoreCase("smart")){
-				//コマンドがef smartの時の処理
+		}
 
-				//メッセージフラグを反転
-				boolean messageflag = !playerdata.messageflag;
-				if (messageflag){
-					sender.sendMessage(ChatColor.GREEN + "内訳表示:ON(OFFに戻したい時は再度コマンドを実行します。)");
-				}else{
-					sender.sendMessage(ChatColor.GREEN + "内訳表示:OFF");
-				}
-				//反転したフラグで更新
-				playerdata.messageflag = messageflag;
+		if(args.length == 1) {
+			if (args[0].equalsIgnoreCase("smart")) {
+				toggleMessageFlag(senderPlayerData);
 				return true;
-
-
-			}else if (args[0].equalsIgnoreCase("demo")){
+			} else if (args[0].equalsIgnoreCase("demo")) {
 				//ガチャ券を1000回試行してみる処理
 				int i = 0;
 				double p;
