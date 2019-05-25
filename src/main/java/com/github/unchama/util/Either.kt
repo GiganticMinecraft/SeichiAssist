684173d05719c5adb88482187ee4fc620308b734
kory33 <6561358+kory33@users.noreply.github.com>
2019-05-26 07:27:04 +0800

    [CLEAN] Either.mergeの実装を単純化

--- src/main/java/com/github/unchama/util/Either.kt
+++ src/main/java/com/github/unchama/util/Either.kt
@@ -1,8 +1,5 @@
 package com.github.unchama.util
 
-import arrow.core.Either
+import arrow.core.*
 
-fun <T> Either<T, T>.merge(): T = when (this) {
-    is Either.Left -> this.a
-    is Either.Right -> this.b
-}
+fun <T> Either<T, T>.merge(): T = fold({ it }, { it })
