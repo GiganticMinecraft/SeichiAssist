6d671dd722efa2a90833cedecc22a38637063698
kory33 <6561358+kory33@users.noreply.github.com>
2019-05-28 01:55:15 +0800

    [DELETE] PartiallyParsedArgs#parseArgHeadを削除
    ビルダー側でパーサを組み立てるため不要になった

--- src/main/java/com/github/unchama/seichiassist/commands/abstract/ArgTransFailureCause.kt
+++
@@ -1,6 +0,0 @@
-package com.github.unchama.seichiassist.commands.abstract
-
-/**
- * コマンド引数の変換が失敗した理由の列挙子
- */
-enum class ArgTransFailureCause { NOT_ENOUGH_ARG, TRANSFORM_FAILED }
--- src/main/java/com/github/unchama/seichiassist/commands/abstract/Context.kt
+++ src/main/java/com/github/unchama/seichiassist/commands/abstract/Context.kt
@@ -1,10 +1,5 @@
 package com.github.unchama.seichiassist.commands.abstract
 
-import arrow.core.*
-import arrow.core.extensions.either.monad.binding
-import arrow.syntax.collections.firstOption
-import com.github.unchama.seichiassist.commands.abstract.ArgTransFailureCause.NOT_ENOUGH_ARG
-import com.github.unchama.seichiassist.commands.abstract.ArgTransFailureCause.TRANSFORM_FAILED
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 
@@ -26,24 +21,7 @@ data class RawCommandContext(val sender: CommandSender,
  * @param parsed コマンド引数のうち, [Any?]を型上限とするオブジェクトに変換されたもの.
  * @param yetToBeParsed コマンド引数のうち, [parsed]へと変換されていない文字列.
  */
-data class PartiallyParsedArgs(val parsed: List<Any?>, val yetToBeParsed: List<String>) {
-    /**
-     * [yetToBeParsed]の先頭にある文字列を[parser]で変換した値を[parsed]に取る新しい[ParsedArgCommandContext]を計算する.
-     * 引数が不足していたり, 変換に失敗していた場合[ArgTransFailureCause]を[Either.left]経由で返す.
-     *
-     * @param parser 変換に失敗したとき[None]を, そうでなければ成功値を含んだ[Option]を返す関数
-     */
-    fun <R> parseArgHead(parser: (String) -> Option<R>): Either<ArgTransFailureCause, PartiallyParsedArgs> =
-            binding {
-                val (nonParsedArgHead) = yetToBeParsed.firstOption().toEither { NOT_ENOUGH_ARG }
-                val (transformedArgHead) = parser(nonParsedArgHead).toEither { TRANSFORM_FAILED }
-
-                this@PartiallyParsedArgs.copy(
-                        parsed = parsed.plusElement(transformedArgHead),
-                        yetToBeParsed = yetToBeParsed.drop(1)
-                )
-            }
-}
+data class PartiallyParsedArgs(val parsed: List<Any?>, val yetToBeParsed: List<String>)
 
 /**
  * コマンドの実行時のコマンド引数や実行者などの情報を変換, 加工したデータ.
