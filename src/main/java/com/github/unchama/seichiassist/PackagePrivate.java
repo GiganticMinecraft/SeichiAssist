d00bc7ae0ad4878868ce44e2d7b83c84b78226c9
KisaragiEffective <kisaragi.effective@gmail.com>
2019-06-01 22:58:31 +0900

    [UPDATE] コメントをアノテーション化

--- src/main/java/com/github/unchama/seichiassist/PackagePrivate.java
+++ src/main/java/com/github/unchama/seichiassist/PackagePrivate.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface PackagePrivate {
 }
