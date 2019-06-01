3d2914f1a25b10583832c50b7641bb7bb6f364eb
KisaragiEffective <kisaragi.effective@gmail.com>
2019-06-01 22:56:44 +0900

    [ADD] @PackagePrivate

---
+++ src/main/java/com/github/unchama/seichiassist/PackagePrivate.java
@@ -0,0 +1,11 @@
+package com.github.unchama.seichiassist;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+public @interface PackagePrivate {
+}
