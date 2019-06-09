89aa76195112b2fd9a5db3fd3b954f34906e3818
KisaragiEffective <kisaragi.effective@gmail.com>
2019-06-09 20:58:14 +0900

    [ADD] 保持する値を追加

--- src/main/java/com/github/unchama/seichiassist/util/ItemMetaHolder.java
+++ src/main/java/com/github/unchama/seichiassist/util/ItemMetaHolder.java
@@ -15,6 +15,7 @@ public class ItemMetaHolder {
     private static final ItemFactory FACTORY = Bukkit.getItemFactory();
     public static final ValueHolder<SkullMeta> SKULL = new ValueHolder<>((SkullMeta) FACTORY.getItemMeta(Material.SKULL_ITEM), SkullMeta::clone);
     public static final ValueHolder<PotionMeta> TIPPED_AWRROW = new ValueHolder<>((PotionMeta) FACTORY.getItemMeta(Material.TIPPED_ARROW), PotionMeta::clone);
+    public static final ValueHolder<PotionMeta> SPLASH_POTION = new ValueHolder<>((PotionMeta) FACTORY.getItemMeta(Material.SPLASH_POTION), PotionMeta::clone);
     public static final ValueHolder<ItemMeta> DIAMOND_BLOCK = new ValueHolder<>(FACTORY.getItemMeta(Material.DIAMOND_BLOCK), ItemMeta::clone);
     public static final ValueHolder<ItemMeta> GOLDEN_APPLE = new ValueHolder<>(FACTORY.getItemMeta(Material.GOLDEN_APPLE), ItemMeta::clone);
 }
