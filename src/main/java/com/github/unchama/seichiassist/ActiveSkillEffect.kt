278dd0c2a61ea625cc2575786fb5a63fcf544abf
KisaragiEffective <kisaragi.effective@gmail.com>
2019-06-14 16:24:36 +0900

    [UPDATE] Convert to kotlin

---
+++ src/main/java/com/github/unchama/seichiassist/ActiveSkillEffect.kt
@@ -0,0 +1,103 @@
+package com.github.unchama.seichiassist
+
+import com.github.unchama.seichiassist.effect.arrow.ArrowBlizzardTask
+import com.github.unchama.seichiassist.effect.arrow.ArrowExplosionTask
+import com.github.unchama.seichiassist.effect.arrow.ArrowMeteoTask
+import com.github.unchama.seichiassist.effect.breaking.BlizzardTask
+import com.github.unchama.seichiassist.effect.breaking.ExplosionTask
+import com.github.unchama.seichiassist.effect.breaking.MeteoTask
+import com.github.unchama.seichiassist.data.Coordinate
+import com.github.unchama.seichiassist.data.PlayerData
+import com.github.unchama.seichiassist.effect.arrow.AbstractEffectTask
+import com.okkero.skedule.SynchronizationContext
+import com.okkero.skedule.schedule
+import org.bukkit.*
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+import java.util.ArrayList
+import java.util.Arrays
+
+enum class ActiveSkillEffect constructor(val num: Int, private val sql_name: String, val desc: String, val explain: String, val usePoint: Int, val material: Material) {
+
+  EXPLOSION(1, "ef_explosion", ChatColor.RED.toString() + "エクスプロージョン", "単純な爆発", 50, Material.TNT),
+  BLIZZARD(2, "ef_blizzard", ChatColor.AQUA.toString() + "ブリザード", "凍らせる", 70, Material.PACKED_ICE),
+  METEO(3, "ef_meteo", ChatColor.DARK_RED.toString() + "メテオ", "隕石を落とす", 100, Material.FIREBALL);
+
+  internal var plugin = SeichiAssist.instance
+  fun getsqlName(): String {
+    return this.sql_name
+  }
+
+  fun getName(): String {
+    return desc
+  }
+
+  //エフェクトの実行処理分岐 範囲破壊と複数範囲破壊
+  fun runBreakEffect(player: Player, playerdata: PlayerData, tool: ItemStack, breaklist: List<Block>, start: Coordinate, end: Coordinate, standard: Location) {
+    when (this) {
+      EXPLOSION -> ExplosionTask(player, playerdata, tool, breaklist, start, end, standard).runTaskLater(plugin, 0)
+      BLIZZARD -> if (playerdata.activeskilldata.skillnum < 3) {
+        BlizzardTask(player, playerdata, tool, breaklist, start, end, standard).runTaskLater(plugin, 1)
+      } else {
+        if (SeichiAssist.DEBUG) {
+          BlizzardTask(player, playerdata, tool, breaklist, start, end, standard).runTaskTimer(plugin, 0, 100)
+        } else {
+          BlizzardTask(player, playerdata, tool, breaklist, start, end, standard).runTaskTimer(plugin, 0, 10)
+        }
+
+      }
+      METEO -> if (playerdata.activeskilldata.skillnum < 3) {
+        MeteoTask(player, playerdata, tool, breaklist, start, end, standard).runTaskLater(plugin, 1)
+      } else {
+        MeteoTask(player, playerdata, tool, breaklist, start, end, standard).runTaskLater(plugin, 10)
+      }
+    }
+  }
+
+  //エフェクトの実行処理分岐
+  fun runProjectileEffect(player: Player) {
+    for (i in 0..99) {
+      Bukkit.getScheduler().schedule(SeichiAssist.instance, SynchronizationContext.ASYNC) {
+        waitFor(i.toLong()) // TODO: これであってるのか？
+        when (this@ActiveSkillEffect) {
+          EXPLOSION -> ArrowExplosionTask(player)
+          BLIZZARD -> ArrowBlizzardTask(player)
+          METEO -> ArrowMeteoTask(player)
+        }
+      }
+    }
+  }
+
+  fun runAssaultEffect(player: Player, playerdata: PlayerData,
+                       tool: ItemStack, arrayList: ArrayList<Block>, start: Coordinate,
+                       end: Coordinate, centerofblock: Location) {
+    when (this) {
+      EXPLOSION -> player.world.spawnParticle(Particle.EXPLOSION_NORMAL, player.eyeLocation, 1, 3.0, 3.0, 3.0, 1.0)
+      BLIZZARD -> player.world.spawnParticle(Particle.SNOW_SHOVEL, player.eyeLocation, 1, 3.0, 3.0, 3.0, 1.0)
+      METEO -> player.world.spawnParticle(Particle.DRIP_LAVA, player.eyeLocation, 1, 3.0, 3.0, 3.0, 1.0)
+    }
+  }
+
+  companion object {
+
+
+    fun getNamebyNum(effectnum: Int): String {
+      val skilleffect = values()
+      return Arrays.stream(skilleffect)
+          .filter { activeSkillEffect -> activeSkillEffect.num == effectnum }
+          .findFirst()
+          .map { it.getName() }
+          .orElse("未設定")
+    }
+
+    fun fromSqlName(sqlName: String): ActiveSkillEffect? {
+      return Arrays
+          .stream(values())
+          .filter { effect -> sqlName == effect.sql_name }
+          .findFirst()
+          .orElse(null)
+    }
+  }
+}
---
+++ src/main/java/com/github/unchama/seichiassist/ActiveSkillPremiumEffect.kt
@@ -0,0 +1,63 @@
+package com.github.unchama.seichiassist
+
+import com.github.unchama.seichiassist.effect.arrow.ArrowMagicTask
+import com.github.unchama.seichiassist.effect.breaking.MagicTask
+import com.github.unchama.seichiassist.data.Coordinate
+import com.github.unchama.seichiassist.data.PlayerData
+import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+import java.util.Arrays
+
+enum class ActiveSkillPremiumEffect(val num: Int, private val sql_name: String, val desc: String, val explain: String, val usePoint: Int, val material: Material) {
+  MAGIC(1, "ef_magic", ChatColor.RED.toString() + "" + ChatColor.UNDERLINE + "" + ChatColor.BOLD + "マジック", "鶏が出る手品", 10, Material.RED_ROSE);
+
+  internal var plugin = SeichiAssist.instance
+
+  fun getsqlName(): String {
+    return this.sql_name
+  }
+
+  //プレイヤーが所持しているかどうか
+  fun isObtained(flagmap: Map<Int, Boolean>): Boolean? {
+    return flagmap[num]
+  }
+
+  //獲得させる処理
+  fun setObtained(flagmap: MutableMap<Int, Boolean>) {
+    flagmap[num] = true
+  }
+
+  //エフェクトの実行処理分岐 範囲破壊と複数範囲破壊
+  fun runBreakEffect(player: Player, playerdata: PlayerData, tool: ItemStack, breaklist: List<Block>, start: Coordinate, end: Coordinate, standard: Location) {
+    when (this) {
+      MAGIC -> if (SeichiAssist.DEBUG) {
+        MagicTask(player, playerdata, tool, breaklist, start, end, standard).runTaskTimer(plugin, 0, 100)
+      } else {
+        MagicTask(player, playerdata, tool, breaklist, start, end, standard).runTaskTimer(plugin, 0, 10)
+      }
+    }
+  }
+
+  //エフェクトの実行処理分岐
+  fun runArrowEffect(player: Player) {
+    when (this) {
+      MAGIC -> ArrowMagicTask(player)
+    }
+  }
+
+  companion object {
+
+    fun fromSqlName(sqlName: String): ActiveSkillPremiumEffect? {
+      return Arrays
+          .stream(values())
+          .filter { effect -> sqlName == effect.sql_name }
+          .findFirst()
+          .orElse(null)
+    }
+  }
+}
