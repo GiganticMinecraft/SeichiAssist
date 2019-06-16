9319141444ca26bfdbdd69614e525c86885efd24
kory33 <6561358+kory33@users.noreply.github.com>
2019-06-16 17:16:44 +0800

    [UPDATE] Buttonに別のコンストラクタを定義する

--- src/main/java/com/github/unchama/menuinventory/slot/button/Button.kt
+++ src/main/java/com/github/unchama/menuinventory/slot/button/Button.kt
@@ -8,6 +8,9 @@ import org.bukkit.inventory.ItemStack
 
 /**
  * メニューインベントリ上で「ボタン」として動作する[Slot]のクラス.
+ *
+ * [actionList]により与えられる作用をリストの順で一つづつ実行していきます.
+ *
  * [runEffect]は常に与えられた[InventoryClickEvent]をキャンセルします.
  *
  * @param itemStack  [Inventory] へセットする [ItemStack]
@@ -15,6 +18,11 @@ import org.bukkit.inventory.ItemStack
  */
 class Button(override val itemStack: ItemStack,
              private val actionList: List<ButtonAction>) : Slot {
+
+  /**
+   * 単一の[action]を作用として発生させる [Slot] を構築します.
+   */
+  constructor(itemStack: ItemStack, action: ButtonAction): this(itemStack, listOf(action))
 
   override fun runEffect(event: InventoryClickEvent) {
     event.isCancelled = true
