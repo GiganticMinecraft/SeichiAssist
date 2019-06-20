1ab269d6261c805320600ce4d2881b082f19ad29
kory33 <6561358+kory33@users.noreply.github.com>
2019-06-21 00:03:56 +0800

    [UPDATE] 木の棒メニューを開くためのボタンを追加する

--- src/main/java/com/github/unchama/seichiassist/menus/MineStackUI.kt
+++ src/main/java/com/github/unchama/seichiassist/menus/MineStackUI.kt
@@ -1,11 +1,19 @@
 package com.github.unchama.seichiassist.menus
 
+import com.github.unchama.menuinventory.IndexedSlotLayout
 import com.github.unchama.targetedeffect.EmptyEffect
 import com.github.unchama.targetedeffect.TargetedEffect
 import com.github.unchama.targetedeffect.computedEffect
 import org.bukkit.entity.Player
 
 object MineStackUI {
+  private suspend fun Player.computeMineStackLayout(): IndexedSlotLayout {
+
+    return IndexedSlotLayout(
+        45 to CommonButtons.openStickMenu
+    )
+  }
+
   val open: TargetedEffect<Player> = computedEffect { player ->
     EmptyEffect
   }
