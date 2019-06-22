6fe9ccb830c4bdb45036bc81c476ac544ceb30e9
kory33 <6561358+kory33@users.noreply.github.com>
2019-06-23 01:18:14 +0800

    [UPDATE] asyncの代わりにlaunchを使用する

--- src/main/java/com/github/unchama/menuinventory/IndexedSlotLayout.kt
+++ src/main/java/com/github/unchama/menuinventory/IndexedSlotLayout.kt
@@ -3,8 +3,8 @@ package com.github.unchama.menuinventory
 import com.github.unchama.menuinventory.slot.Slot
 import com.github.unchama.targetedeffect.EmptyEffect
 import com.github.unchama.targetedeffect.TargetedEffect
-import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
@@ -30,7 +30,7 @@ data class IndexedSlotLayout(private val map: Map<Int, Slot>) {
   internal suspend fun asynchronouslySetItemsOn(inventory: Inventory) {
     coroutineScope {
       for (slotIndex in 0 until inventory.size) {
-        async {
+        launch {
           val itemStack = map[slotIndex]?.itemStack ?: ItemStack(Material.AIR)
           inventory.setItem(slotIndex, itemStack)
         }
