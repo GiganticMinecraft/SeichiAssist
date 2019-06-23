53fb7b070edb37b9e1bc8dec5b0495fd4940e9af
KisaragiEffective <kisaragi.effective@gmail.com>
2019-06-23 17:52:28 +0900

    [UPDATE] inline cache (per kory33's review)

--- src/main/java/com/github/unchama/seichiassist/util/ItemMetaHolder.java
+++ src/main/java/com/github/unchama/seichiassist/util/ItemMetaHolder.java
@@ -7,22 +7,18 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.function.UnaryOperator;
-
+@SuppressWarnings("UtilityClassCanBeEnum")
 public final class ItemMetaHolder {
     private ItemMetaHolder() {
 
     }
 
-    // 明示的にインスタンスを一つにするための変数
-    private static final UnaryOperator<PotionMeta> A = PotionMeta::clone;
-    private static final UnaryOperator<ItemMeta> B = ItemMeta::clone;
     private static final ItemFactory FACTORY = Bukkit.getItemFactory();
 
     public static final ValueHolder<SkullMeta> SKULL = new ValueHolder<>((SkullMeta) FACTORY.getItemMeta(Material.SKULL_ITEM), SkullMeta::clone);
-    public static final ValueHolder<PotionMeta> TIPPED_ARROW = new ValueHolder<>((PotionMeta) FACTORY.getItemMeta(Material.TIPPED_ARROW), A);
-    public static final ValueHolder<PotionMeta> SPLASH_POTION = new ValueHolder<>((PotionMeta) FACTORY.getItemMeta(Material.SPLASH_POTION), A);
-    public static final ValueHolder<ItemMeta> DIAMOND_BLOCK = new ValueHolder<>(FACTORY.getItemMeta(Material.DIAMOND_BLOCK), B);
-    public static final ValueHolder<ItemMeta> BEDROCK = new ValueHolder<>(FACTORY.getItemMeta(Material.BEDROCK), B);
-    public static final ValueHolder<ItemMeta> GOLDEN_APPLE = new ValueHolder<>(FACTORY.getItemMeta(Material.GOLDEN_APPLE), B);
+    public static final ValueHolder<PotionMeta> TIPPED_ARROW = new ValueHolder<>((PotionMeta) FACTORY.getItemMeta(Material.TIPPED_ARROW), PotionMeta::clone);
+    public static final ValueHolder<PotionMeta> SPLASH_POTION = new ValueHolder<>((PotionMeta) FACTORY.getItemMeta(Material.SPLASH_POTION), PotionMeta::clone);
+    public static final ValueHolder<ItemMeta> DIAMOND_BLOCK = new ValueHolder<>(FACTORY.getItemMeta(Material.DIAMOND_BLOCK), ItemMeta::clone);
+    public static final ValueHolder<ItemMeta> BEDROCK = new ValueHolder<>(FACTORY.getItemMeta(Material.BEDROCK), ItemMeta::clone);
+    public static final ValueHolder<ItemMeta> GOLDEN_APPLE = new ValueHolder<>(FACTORY.getItemMeta(Material.GOLDEN_APPLE), ItemMeta::clone);
 }
