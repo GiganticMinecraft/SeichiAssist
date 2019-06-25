a6df5fa92435873c04a16f6edbc45a0acaa6e497
kory33 <6561358+kory33@users.noreply.github.com>
2019-06-25 14:20:14 +0800

    [UPDATE] 自動回収トグルボタンの位置を変更する

---
+++ src/main/java/com/github/unchama/seichiassist/UUIDs.kt
@@ -0,0 +1,14 @@
+package com.github.unchama.seichiassist
+
+import java.util.*
+
+/**
+ * コード内から直接参照されるようなプレーヤーのUUIDを持つオブジェクト
+ */
+object UUIDs {
+  // MHF_ArrowUp
+  val MHFArrowUp: UUID = UUID.fromString("fef039ef-e6cd-4987-9c84-26a3e6134277")
+
+  // MHF_ArrowDown
+  val MHFArrowDown: UUID = UUID.fromString("68f59b9b-5b0b-4b05-a9f2-e1d1405aa348")
+}
