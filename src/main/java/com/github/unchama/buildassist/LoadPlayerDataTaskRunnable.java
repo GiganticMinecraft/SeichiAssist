15236c12c2fb4fb6fafc3ff829594347ad022eba
KisaragiEffective <kisaragi.effective@gmail.com>
2019-08-25 16:22:35 +0900

    [UPDATE] avoid call Plugin#getServer()

--- src/main/java/com/github/unchama/buildassist/LoadPlayerDataTaskRunnable.java
+++ src/main/java/com/github/unchama/buildassist/LoadPlayerDataTaskRunnable.java
@@ -29,15 +29,15 @@ public class LoadPlayerDataTaskRunnable extends BukkitRunnable{
 		// 接続時にスケジュールで実行する処理
 
 		//対象プレイヤーがオフラインなら処理終了
-		if(BuildAssist.plugin.getServer().getPlayer(uuid) == null){
+		if(Bukkit.getServer().getPlayer(uuid) == null){
 			cancel();
-			plugin.getServer().getConsoleSender().sendMessage(ChatColor.RED + p.getName() + "はオフラインの為取得処理を中断");
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + p.getName() + "はオフラインの為取得処理を中断");
 			return;
 		}
 		//読み込み失敗が規定回数超えたら終わる
  		if(retryCount >= 7){
 			cancel();
- 			plugin.getServer().getConsoleSender().sendMessage(ChatColor.RED + p.getName() + "の建築系データが読み込めませんでした");
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + p.getName() + "の建築系データが読み込めませんでした");
  			p.sendMessage(ChatColor.RED + "建築系データが読み込めませんでした。再接続をお願いします。改善されない場合はお手数ですがお問い合わせください");
  			p.kickPlayer(ChatColor.RED + "建築系データが読み込めませんでした。再接続をお願いします。改善されない場合はお手数ですがお問い合わせください");
  			return;
@@ -45,8 +45,8 @@ public class LoadPlayerDataTaskRunnable extends BukkitRunnable{
  		//DBから読み込み終わるまで待つ
 		final com.github.unchama.seichiassist.data.PlayerData playerdata_s = SeichiAssist.Companion.getPlayermap().get(uuid);
 		if(playerdata_s == null){
- 			plugin.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + p.getName() + "の建築系データ取得待機…(" + (i+1) + "回目)");
- 			i++;
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + p.getName() + "の建築系データ取得待機…(" + (retryCount +1) + "回目)");
+ 			retryCount++;
  			return;
 		}
 		cancel();
@@ -62,8 +62,6 @@ public class LoadPlayerDataTaskRunnable extends BukkitRunnable{
 		}
 		//建築系データ読み込み
 		playerdata.buildload(p);
-
-		plugin.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "建築系データ読み込み完了");
-		return;
+		Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "建築系データ読み込み完了");
 	}
 }
