ee0337b25aa4aef4c7339b45dffd6150c2dd4ce6
kory33 <6561358+kory33@users.noreply.github.com>
2019-09-07 02:53:59 +0800

    [FIX] MineStackメインメニュー遷移のボタンに音をつける

--- src/main/java/com/github/unchama/seichiassist/menus/minestack/CategorizedMineStackMenu.kt
+++ src/main/java/com/github/unchama/seichiassist/menus/minestack/CategorizedMineStackMenu.kt
@@ -56,7 +56,11 @@ object CategorizedMineStackMenu {
                 .title("$YELLOW$UNDERLINE${BOLD}MineStackメインメニューへ")
                 .lore(listOf("$RESET$DARK_RED${UNDERLINE}クリックで移動"))
                 .build(),
-            FilteredButtonEffect(ClickEventFilter.ALWAYS_INVOKE, MineStackMainMenu.open)
+            FilteredButtonEffect(
+                ClickEventFilter.ALWAYS_INVOKE,
+                CommonSoundEffects.menuTransitionFenceSound,
+                MineStackMainMenu.open
+            )
         )
 
         singleSlotLayout { (9 * 5) to mineStackMainMenuButton }
