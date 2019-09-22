2c4b69849a5d5242570763f4ad351c9c9fc13a73
kory33 <6561358+kory33@users.noreply.github.com>
2019-09-23 03:41:30 +0800

    [UPDATE] fix errors in ImmutableListFactory

--- src/main/java/com/github/unchama/util/collection/ImmutableListFactory.scala
+++ src/main/java/com/github/unchama/util/collection/ImmutableListFactory.scala
@@ -1,19 +1,19 @@
 package com.github.unchama.util.collection
 
-import scala.collection.JavaConversions._
+import scala.jdk.CollectionConverters._
 
 object ImmutableListFactory {
 
   // Checked, no scala usage found
-  def of[E](): java.util.List[E] = Nil
+  def of[E](): java.util.List[E] = Nil.asJava
 
   // Checked, no scala usage found
-  def of[E](o: E): java.util.List[E] = List(o)
+  def of[E](o: E): java.util.List[E] = List(o).asJava
 
-  def of[E](o: Array[E]): java.util.List[E] = o.toList
+  def of[E](o: Array[E]): java.util.List[E] = o.toList.asJava
 
   @SafeVarargs
   // Checked, no scala usage found
-  def of[E](o: E*): java.util.List[E] = List(o: _*)
+  def of[E](o: E*): java.util.List[E] = List(o: _*).asJava
 
 }
