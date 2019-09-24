a53581be498c427f8f4eb218aac455f3f19a453d
blackbracken <me@bracken.black>
2019-09-24 21:00:45 +0900

    [UPDATE] scala-ize ClosedRangeWithComparator

--- src/main/java/com/github/unchama/seichiassist/util/ClosedRangeWithComparator.scala
+++ src/main/java/com/github/unchama/seichiassist/util/ClosedRangeWithComparator.scala
@@ -1,11 +1,14 @@
 package com.github.unchama.seichiassist.util
 
 import java.util.Comparator
+
 class ClosedRangeWithComparator[E](val start: E, val endInclusive: E, val comparator: Comparator[E]) {
-  operator def contains(another: E): Boolean = {
+
+  def contains(another: E): Boolean = {
     // start <= another <= endInclusive
-    return comparator.compare(start, another) <= 0 && comparator.compare(another, endInclusive) [= 0
+    comparator.compare(start, another) <= 0 && comparator.compare(another, endInclusive) <= 0
   }
 
-  def isEmpty() = comparator.compare(start, endInclusive) ] 0
+  def isEmpty(): Boolean = comparator.compare(start, endInclusive) > 0
+
 }
