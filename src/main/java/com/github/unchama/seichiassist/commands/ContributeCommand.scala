08867441f059a21e1bbdec459080e8e662065ecf
karayuumac <y-kara0706@docomo.ne.jp>
2019-09-25 02:34:05 +0900

    [UPDATE] Scalaize ContributeCommand

--- src/main/java/com/github/unchama/seichiassist/commands/ContributeCommand.scala
+++ src/main/java/com/github/unchama/seichiassist/commands/ContributeCommand.scala
@@ -1,69 +1,79 @@
 package com.github.unchama.seichiassist.commands
 
-import com.github.unchama.contextualexecutor.ContextualExecutor
-import com.github.unchama.contextualexecutor.builder.ContextualExecutorBuilder
-import com.github.unchama.contextualexecutor.builder.Parsers.{identity, nonNegativeInteger}
-import com.github.unchama.contextualexecutor.executors.BranchedExecutor
+import cats.effect.IO
+import com.github.unchama.contextualexecutor.builder.{ContextualExecutorBuilder, Parsers}
+import com.github.unchama.contextualexecutor.executors.EchoExecutor
 import com.github.unchama.seichiassist.SeichiAssist
-import com.github.unchama.targetedeffect.TargetedEffect
+import com.github.unchama.seichiassist.commands.ContributeCommand.ContributeOperation._
+import com.github.unchama.targetedeffect.MessageEffects._
+import com.github.unchama.targetedeffect.TargetedEffect.TargetedEffect
 import com.github.unchama.util.kotlin2scala.SuspendingMethod
+import enumeratum._
 import org.bukkit.ChatColor._
-import org.bukkit.command.{CommandExecutor, CommandSender}
+import org.bukkit.command.{CommandSender, TabExecutor}
 
 object ContributeCommand {
-  private @SuspendingMethod def addContributionPoint(targetPlayerName: String, point: Int): TargetedEffect[CommandSender] =
-      SeichiAssist.databaseGateway.playerDataManipulator
-          .addContributionPoint(targetPlayerName, point)
-          .map {
-            val operationResponse =
-                if (point >= 0) {
-                  s"${GREEN}${targetPlayerName}に貢献度ポイントを${point}追加しました"
-                } else {
-                  s"${GREEN}${targetPlayerName}の貢献度ポイントを${point}減少させました"
-                }
+  sealed trait ContributeOperation extends EnumEntry
+  object ContributeOperation extends Enum[ContributeOperation] {
+    val values = findValues
 
-            operationResponse.asMessageEffect()
-          }.merge()
+    case object ADD extends ContributeOperation
+    case object REMOVE extends ContributeOperation
+  }
 
-  private val helpMessage: TargetedEffect[CommandSender] = List(
-    s"${YELLOW}${BOLD}[コマンドリファレンス]",
-    s"${RED}/contribute add [プレイヤー名] [増加分ポイント]",
+  @SuspendingMethod private def addContributionPoint(targetPlayerName: String, point: Int): TargetedEffect[CommandSender] = {
+    SeichiAssist.databaseGateway.playerDataManipulator
+      .addContributionPoint(targetPlayerName, point)
+      .map(_ => {
+        val operationResponse =
+          if (point >= 0) {
+            s"${GREEN}${targetPlayerName}に貢献度ポイントを${point}追加しました"
+          } else {
+            s"${GREEN}${targetPlayerName}の貢献度ポイントを${point}減少させました"
+          }
+        operationResponse.asMessageEffect()
+      })
+      .merge
+  }
+
+  private val printHelpMessageExecutor = new EchoExecutor(
+    List(
+      s"${YELLOW}${BOLD}[コマンドリファレンス]",
+      s"${RED}/contribute add [プレイヤー名] [増加分ポイント]",
       "指定されたプレイヤーの貢献度ptを指定分増加させます",
-    s"${RED}/contribute remove [プレイヤー名] [減少分ポイント]",
+      s"${RED}/contribute remove [プレイヤー名] [減少分ポイント]",
       "指定されたプレイヤーの貢献度ptを指定分減少させます(入力ミス回避用)"
-  ).asMessageEffect()
+    ).asMessageEffect()
+  )
 
-  private val printHelpExecutor: ContextualExecutor = ContextualExecutorBuilder.beginConfiguration()
-      .execution { helpMessage }
-      .build()
+  private val operationParser = Parsers.fromOptionParser(
+    ContributeOperation.withNameLowercaseOnlyOption,
+    "操作はadd/removeで与えてください。".asMessageEffect()
+  )
 
-  private val parserConfiguredBuilder = ContextualExecutorBuilder.beginConfiguration()
-      .argumentsParsers(List(
-          identity,
-        nonNegativeInteger(s"${RED}増加分ポイントは0以上の整数を指定してください。".asMessageEffect())
-      ), onMissingArguments = printHelpExecutor)
+  private val pointParser = Parsers.nonNegativeInteger(
+    s"${RED}増加分ポイントは0以上の整数を指定してください。".asMessageEffect()
+  )
 
-  private val addPointExecutor: ContextualExecutor = parserConfiguredBuilder
-      .execution { context =>
-        val targetPlayerName = context.args.parsed[0].asInstanceOf[String]
-        val point = context.args.parsed[1].asInstanceOf[Int]
+  val executor: TabExecutor = ContextualExecutorBuilder.beginConfiguration()
+    .argumentsParsers(
+      List(operationParser, pointParser),
+      onMissingArguments = printHelpMessageExecutor
+    )
+    .execution { context =>
+      val operation = context.args.parsed[0].asInstanceOf[ContributeOperation]
+      val targetPlayerName = context.args.parsed[1].asInstanceOf[String]
+      val point = context.args.parsed[2].asInstanceOf[Int]
 
-        addContributionPoint(targetPlayerName, point)
+      operation match {
+        case ADD => IO {
+          addContributionPoint(targetPlayerName, point)
+        }
+        case REMOVE => IO {
+          addContributionPoint(targetPlayerName, -point)
+        }
       }
-      .build()
-
-  private val removePointExecutor: ContextualExecutor = parserConfiguredBuilder
-      .execution { context =>
-        val targetPlayerName = context.args.parsed[0].asInstanceOf[String]
-        val point = context.args.parsed[1].asInstanceOf[Int]
-
-        addContributionPoint(targetPlayerName, -point)
-      }
-      .build()
-
-  val executor: CommandExecutor =
-      BranchedExecutor(
-          mapOf("add" to addPointExecutor, "remove" to removePointExecutor),
-          whenArgInsufficient = printHelpExecutor, whenBranchNotFound = printHelpExecutor
-      ).asNonBlockingTabExecutor()
+    }
+    .build()
+    .asNonBlockingTabExecutor()
 }
