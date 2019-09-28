d5ff7b3213782c0a4899d4a50968adb46bcdcba3
karayuumac <y-kara0706@docomo.ne.jp>
2019-09-28 23:03:25 +0900

    [UPDATE] Scala-ize MineHeadCommand

--- src/main/java/com/github/unchama/seichiassist/commands/MineHeadCommand.scala
+++ src/main/java/com/github/unchama/seichiassist/commands/MineHeadCommand.scala
@@ -1,16 +1,18 @@
 package com.github.unchama.seichiassist.commands
 
+import cats.effect.IO
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seichiassist.util.{StaticGachaPrizeFactory, Util}
+import com.github.unchama.targetedeffect.MessageEffects._
 import org.bukkit.ChatColor._
+import org.bukkit.command.TabExecutor
 
 object MineHeadCommand {
-  val executor = playerCommandBuilder
-      .execution { context =>
-        Util.addItemToPlayerSafely(context.sender, StaticGachaPrizeFactory.mineHeadItem())
-
-        s"${GREEN}専用アイテムを付与しました。".asMessageEffect()
-      }
-      .build()
-      .asNonBlockingTabExecutor()
+  val executor: TabExecutor = playerCommandBuilder
+    .execution { context =>
+      Util.addItemToPlayerSafely(context.sender, StaticGachaPrizeFactory.getMineHeadItem)
+      IO(s"${GREEN}専用アイテムを付与しました。".asMessageEffect())
+    }
+    .build()
+    .asNonBlockingTabExecutor()
 }
