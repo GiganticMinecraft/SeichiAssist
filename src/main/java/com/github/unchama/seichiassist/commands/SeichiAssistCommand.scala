ac86b8f1fba0b98cc052bc535640428bdbee17ca
karayuumac <y-kara0706@docomo.ne.jp>
2019-09-29 15:35:54 +0900

    [UPDATE] Scala-ize SeichiAssistCommand

--- src/main/java/com/github/unchama/seichiassist/commands/SeichiAssistCommand.scala
+++ src/main/java/com/github/unchama/seichiassist/commands/SeichiAssistCommand.scala
@@ -1,34 +1,39 @@
 package com.github.unchama.seichiassist.commands
 
+import cats.effect.IO
 import com.github.unchama.contextualexecutor.builder.ContextualExecutorBuilder
-import com.github.unchama.contextualexecutor.executors.BranchedExecutor
+import com.github.unchama.contextualexecutor.executors.{BranchedExecutor, EchoExecutor}
 import com.github.unchama.seichiassist.SeichiAssist
+import com.github.unchama.targetedeffect.MessageEffects._
 import org.bukkit.ChatColor._
-import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.command.{ConsoleCommandSender, TabExecutor}
 
 object SeichiAssistCommand {
-  private val descriptionExecutor = EchoExecutor(List(
+  private val descriptionExecutor = new EchoExecutor(List(
     s"${YELLOW}${BOLD}[コマンドリファレンス]",
     s"${RED}/seichiassist reload-config",
-      "config.ymlの設定値を再読み込みします",
+    "config.ymlの設定値を再読み込みします",
     s"${RED}/seichiassist toggle-debug",
-      "デバッグモードのON,OFFを切り替えます",
-      "config.ymlのdebugmodeの値が1の場合のみ、コンソールから使用可能",
+    "デバッグモードのON,OFFを切り替えます",
+    "config.ymlのdebugmodeの値が1の場合のみ、コンソールから使用可能",
     s"${RED}/seichiassist set-anniversary-flag",
-      "1周年記念フラグを立てる（コンソール限定コマンド）"
+    "1周年記念フラグを立てる（コンソール限定コマンド）"
   ).asMessageEffect())
 
   private val reloadConfigExecutor = ContextualExecutorBuilder.beginConfiguration()
-      .execution {
+    .execution { _ =>
+      IO {
         SeichiAssist.seichiAssistConfig.reloadConfig()
         "config.ymlの設定値を再読み込みしました".asMessageEffect()
       }
-      .build()
+    }
+    .build()
 
   private val toggleDebugExecutor = ContextualExecutorBuilder.beginConfiguration()
-      .execution {
+    .execution { _ =>
+      IO {
         //debugフラグ反転処理
-        if (SeichiAssist.seichiAssistConfig.debugMode == 1) {
+        if (SeichiAssist.seichiAssistConfig.getDebugMode == 1) {
           //メッセージフラグを反転
           SeichiAssist.DEBUG = !SeichiAssist.DEBUG
           SeichiAssist.instance.restartRepeatedJobs()
@@ -47,23 +52,26 @@ object SeichiAssistCommand {
           ).asMessageEffect()
         }
       }
-      .build()
+    }
+    .build()
 
   private val setAnniversaryFlagExecutor = ContextualExecutorBuilder.beginConfiguration()
-      .refineSenderWithError[ConsoleCommandSender]("コンソール専用コマンドです")
-      .execution {
+    .refineSenderWithError[ConsoleCommandSender]("コンソール専用コマンドです")
+    .execution { _ =>
+      IO {
         SeichiAssist.databaseGateway.playerDataManipulator.setAnniversary(true, null)
 
         "Anniversaryアイテムの配布を開始しました。".asMessageEffect()
       }
-      .build()
+    }
+    .build()
 
-  val executor = BranchedExecutor(
-      mapOf(
-          "reload-config" to reloadConfigExecutor,
-          "toggle-debug" to toggleDebugExecutor,
-          "set-anniversary-flag" to setAnniversaryFlagExecutor
-      ),
-      whenArgInsufficient = descriptionExecutor, whenBranchNotFound = descriptionExecutor
+  val executor: TabExecutor = BranchedExecutor(
+    Map(
+      "reload-config" -> reloadConfigExecutor,
+      "toggle-debug" -> toggleDebugExecutor,
+      "set-anniversary-flag" -> setAnniversaryFlagExecutor
+    ),
+    whenArgInsufficient = Some(descriptionExecutor), whenBranchNotFound = Some(descriptionExecutor)
   ).asNonBlockingTabExecutor()
 }
