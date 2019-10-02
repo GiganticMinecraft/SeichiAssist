fab4c30ae500673a01b53fd47f7da39741b6e955
kory33 <6561358+kory33@users.noreply.github.com>
2019-10-02 17:33:34 +0800

    [FIX] Scala側エラーをすべて修正する

--- src/main/scala/com/github/unchama/util/collection/ImmutableListFactory.scala
+++ src/main/scala/com/github/unchama/util/collection/ImmutableListFactory.scala
@@ -8,11 +8,7 @@ object ImmutableListFactory {
   def of[E](): java.util.List[E] = Nil.asJava
 
   // Checked, no scala usage found
-  def of[E](o: E): java.util.List[E] = List(o).asJava
+  def of[E](o: E, os: E*): java.util.List[E] = (List(o) ++ os).asJava
 
   def of[E](o: Array[E]): java.util.List[E] = o.toList.asJava
-
-  @SafeVarargs
-  def of[E](o: E*): java.util.List[E] = List(o: _*).asJava
-
 }
