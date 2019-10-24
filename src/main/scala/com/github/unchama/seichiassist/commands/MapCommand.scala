39c3dbea384ebd538d4178eda88c1aa8b4fd9900
kory33 <6561358+kory33@users.noreply.github.com>
2019-10-25 05:23:43 +0800

    [UPDATE] /mapの出力を他のURL出すタイプのコマンドと合わせる

--- src/main/scala/com/github/unchama/seichiassist/commands/MapCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/commands/MapCommand.scala
@@ -4,6 +4,7 @@ import cats.effect.IO
 import com.github.unchama.seichiassist.SeichiAssist
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates
 import com.github.unchama.targetedeffect.MessageEffects._
+import org.bukkit.ChatColor._
 import org.bukkit.command.TabExecutor
 
 object MapCommand {
@@ -12,7 +13,7 @@ object MapCommand {
       IO {
         val location = context.sender.getLocation
         val url =
-          s"http://map-s${SeichiAssist.seichiAssistConfig.getServerNum}.minecraftserver.jp" +
+          s"$RED${UNDERLINE}http://map-s${SeichiAssist.seichiAssistConfig.getServerNum}.minecraftserver.jp" +
             s"/?worldname=${location.getWorld.getName}&mapname=flat&zoom=2&" +
             s"x=${location.getBlockX}&y=${location.getBlockY}&z=${location.getBlockZ}"
         url.asMessageEffect()
