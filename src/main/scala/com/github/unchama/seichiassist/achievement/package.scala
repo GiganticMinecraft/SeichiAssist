d145193edb3c7d6a8ad68d7a81969d95f869d41f
kory33 <6561358+kory33@users.noreply.github.com>
2019-11-09 07:35:39 +0800

    [UPDATE] 実績の称号へのマッピングを導入する

--- src/main/scala/com/github/unchama/seichiassist/achievement/package.scala
+++ src/main/scala/com/github/unchama/seichiassist/achievement/package.scala
@@ -4,6 +4,7 @@ import cats.effect.IO
 import org.bukkit.entity.Player
 
 package object achievement {
+  type AchievementId = Int
   type PlayerPredicate = Player => IO[Boolean]
   type ParameterizedText[A] = A => String
 }
