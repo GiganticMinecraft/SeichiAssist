62d039c94e3f94664af1837c1fe1f85308bed068
kory33 <6561358+kory33@users.noreply.github.com>
2019-11-14 08:01:11 +0800

    [FIX] ColorScheme.navigationの先頭にChatColor.RESETをつける

--- src/main/scala/com/github/unchama/seichiassist/menus/ColorScheme.scala
+++ src/main/scala/com/github/unchama/seichiassist/menus/ColorScheme.scala
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor._
 object ColorScheme {
   type Scheme = String => String
 
-  val navigation: Scheme = s"$YELLOW$UNDERLINE$BOLD" + _
+  val navigation: Scheme = s"$RESET$YELLOW$UNDERLINE$BOLD" + _
 
   val clickResultDescription: Scheme = s"$RESET$DARK_RED$UNDERLINE" + _
 }
