95d04303cc0266425ede6a3d0ac412ca5cb97f0f
kory33 <6561358+kory33@users.noreply.github.com>
2019-11-25 01:33:55 +0800

    Revert "[TEMP UPDATE] 連続ログイン・投票の判定幅を4日にする"
    This reverts commit 3430aa95

--- src/main/scala/com/github/unchama/seichiassist/database/manipulators/PlayerDataManipulator.scala
+++ src/main/scala/com/github/unchama/seichiassist/database/manipulators/PlayerDataManipulator.scala
@@ -208,7 +208,7 @@ class PlayerDataManipulator(private val gateway: DatabaseGateway) {
 
           val dateDiff = (TodayLong - LastLong) / (1000 * 60 * 60 * 24)
           val count =
-            if (dateDiff <= 4L)
+            if (dateDiff <= 2L)
               lrs.getInt("chainvote") + 1
             else
               1
--- src/main/scala/com/github/unchama/seichiassist/task/PlayerDataLoading.scala
+++ src/main/scala/com/github/unchama/seichiassist/task/PlayerDataLoading.scala
@@ -285,7 +285,7 @@ object PlayerDataLoading {
           if (dateDiff >= 1L) {
             val newTotalLoginDay = playerData.loginStatus.totalLoginDay + 1
             val newConsecutiveLoginDays =
-              if (dateDiff <= 4L)
+              if (dateDiff <= 2L)
                 playerData.loginStatus.consecutiveLoginDays + 1
               else
                 1
