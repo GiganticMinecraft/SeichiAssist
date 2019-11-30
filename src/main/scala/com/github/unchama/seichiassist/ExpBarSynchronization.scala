75450fc8156afe0675e5452e25b5162517c809dc
KisaragiEffective <kisaragi.effective@gmail.com>
2019-11-30 20:31:22 +0900

    [UPDATE] Add star on exp bar

--- src/main/scala/com/github/unchama/seichiassist/ExpBarSynchronization.scala
+++ src/main/scala/com/github/unchama/seichiassist/ExpBarSynchronization.scala
@@ -37,7 +37,7 @@ class ExpBarSynchronization {
 
     if (playerLevel >= LevelThresholds.levelExpThresholds.size) {
       // BarをMAXにして総整地量を表示
-      val text = s"$GOLD${BOLD}Lv $playerLevel(総整地量: ${String.format("%,d", playerData.totalbreaknum)})"
+      val text = s"$GOLD${BOLD}Lv $playerLevel☆${playerData.starLevels.fromBreakAmount}(総整地量: ${String.format("%,d", playerData.totalbreaknum)})"
       val progress = 1.0
 
       ExpBarProperties(text, progress)
