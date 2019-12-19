1b33879fece8eb2c6200d6335aaac54469ad862d
kory33 <6561358+kory33@users.noreply.github.com>
2019-12-20 06:18:42 +0800

    [UPDATE] XYZTupleのsyntaxに/を追加する

--- src/main/scala/com/github/unchama/seichiassist/data/syntax/XYZTupleSyntax.scala
+++ src/main/scala/com/github/unchama/seichiassist/data/syntax/XYZTupleSyntax.scala
@@ -5,9 +5,12 @@ import org.bukkit.{Location, World}
 
 trait XYZTupleSyntax {
   implicit class XYZTupleIntOps(k: Int) {
-    def *(tuple: XYZTuple) = XYZTuple(tuple.x * k, tuple.y * k, tuple.z * k)
+    def *(tuple: XYZTuple): XYZTuple = tuple.mapEachComponent(_ * k)
   }
+
   implicit class XYZTupleOps(a: XYZTuple) {
+    def mapEachComponent(f: Int => Int): XYZTuple = XYZTuple(f(a.x), f(a.y), f(a.z))
+
     def +(another: XYZTuple) = XYZTuple(a.x + another.x, a.y + another.y, a.z + another.z)
 
     def negative: XYZTuple = (-1) * a
@@ -15,6 +18,8 @@ trait XYZTupleSyntax {
     def -(another: XYZTuple): XYZTuple = a + another.negative
 
     def toLocation(world: World): Location = new Location(world, a.x.toDouble, a.y.toDouble, a.z.toDouble)
+
+    def /(k: Double): XYZTuple = mapEachComponent(c => (c / k).toInt)
   }
 }
 
