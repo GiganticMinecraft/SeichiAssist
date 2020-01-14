d8c0099608997fda8d392b188b6dcd7d991a2dd6
kory33 <6561358+kory33@users.noreply.github.com>
2020-01-15 01:07:11 +0800

    [FIX] DBマイグレーション1.2.1の構文エラーと参照エラーを修正する

--- src/main/resources/db/migration/V1.2.1__Change_columns_width.sql
+++ src/main/resources/db/migration/V1.2.1__Change_columns_width.sql
@@ -3,20 +3,20 @@ use seichiassist;
 -- UUID = char[36]
 -- Player.name = vchar[16]
 
-alter table donatedata alter column playername varchar(16);
-alter table donatedata alter column uuid char(36);
+alter table donatedata modify playername varchar(16);
+alter table donatedata modify playeruuid char(36);
 
-alter table playerdata alter column name varchar(16);
-alter table playerdata alter column uuid char(36);
+alter table playerdata modify name varchar(16);
+alter table playerdata modify uuid char(36);
 
-alter table mine_stack alter column player_uuid char(36);
+alter table mine_stack modify player_uuid char(36);
 
-alter table sub_home alter column player_uuid char(36);
+alter table sub_home modify player_uuid char(36);
 
-alter table grid_template alter column designer_uuid char(36);
+alter table grid_template modify designer_uuid char(36);
 
-alter table unlocked_active_skill_effect alter column player_uuid char(36);
+alter table unlocked_active_skill_effect modify player_uuid char(36);
 
-alter table unlocked_active_skill_premium_effect alter column player_uuid char(36);
+alter table unlocked_active_skill_premium_effect modify player_uuid char(36);
 
-alter table playerdata alter column gachapoint bigint default 0;
+alter table playerdata modify gachapoint bigint default 0;
