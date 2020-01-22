4726ce3824f5c1535c1a281acae9f6ebf9de3fb5
kory33 <6561358+kory33@users.noreply.github.com>
2020-01-22 23:11:06 +0800

    [update] ResourceScopeのインターフェースを改善する

--- src/main/scala/com/github/unchama/generic/effect/ResourceScope.scala
+++ src/main/scala/com/github/unchama/generic/effect/ResourceScope.scala
@@ -10,19 +10,23 @@ trait ResourceScope[ResourceHandler, F[_]] {
   import cats.implicits._
 
   /**
-   * 与えられた`resource`の確保を行い、
-   * 得られたリソースとその開放を行う計算の組を返す(c.f. [[Resource.allocated]])。
-   *
-   * ここで確保したリソースは必ずしも開放されるとは限らない。
-   * 具体的には、この関数より返される開放の計算を走らせるか、
-   * [[releaseAll]]を走らせるかを行わない限りリソースは開放されずに残り続ける。
+   * 与えられた`resource`に対して、確保時にこのスコープの管理下に置き、
+   * 開放前に管理下から外してから開放するような新たなリソースを作成する。
    */
-  def allocateAndTrack(resource: Resource[F, ResourceHandler]): F[CancelToken[F]]
+  def tracked[R <: ResourceHandler](resource: Resource[F, R]): Resource[F, R]
 
   /**
    * 与えられたハンドラがこのスコープの管理下にあるならば開放する計算を返し、そうでなければ空の値を返す計算
    */
   def getCancelToken(handler: ResourceHandler): F[Option[CancelToken[F]]]
+
+  /**
+   * 与えられたハンドラが管理下にあれば開放するような計算
+   */
+  def release(handler: ResourceHandler): CancelToken[F] = for {
+    optionToken <- getCancelToken(handler)
+    _ <- optionToken.getOrElse(syncF.unit)
+  } yield ()
 
   /**
    * 管理下にあるすべてのリソースを開放する計算
@@ -52,8 +56,8 @@ object ResourceScope {
      * リソースへのハンドラから`F[Unit]`の値への`Map`。
      *
      * この`Map`の終域にある`F[Unit]`は、原像のハンドラが指し示すリソースに対し
-     * ・リソースの意味で開放する
      * ・このオブジェクトの管理下から外す
+     * ・リソースの意味で開放する
      * の順で処理をする計算である。
      * ここで、このオブジェクトの管理下から外すというのは、単にこの`Map`からハンドラを取り除く処理である。
      */
@@ -62,15 +66,24 @@ object ResourceScope {
     import cats.implicits._
     import syncF._
 
-    override def allocateAndTrack(resource: Resource[F, ResourceHandler]): F[CancelToken[F]] =
-      for {
-        allocated <- resource.allocated
-        (handler, releaseToken) = allocated
+    override def tracked[R <: ResourceHandler](resource: Resource[F, R]): Resource[F, R] = {
+      val trackedResource =
+        for {
+          allocated <- resource.allocated
+          (handler, releaseToken) = allocated
 
-        releaseAndDismiss = delay { trackedResources.remove(handler) } *> releaseToken
+          dismiss = defer { trackedResources.remove(handler).getOrElse(Sync[F].unit) }
 
-        _ <- delay { trackedResources += (handler -> releaseAndDismiss) }
-      } yield releaseAndDismiss
+          dismissAndRelease = dismiss *> releaseToken
+
+          register = delay { trackedResources += (handler -> dismissAndRelease) }
+
+          // 二重確保を避けるため確保されていたリソースがあれば開放してから確保する
+          _ <- dismiss *> register
+        } yield (handler, dismissAndRelease)
+
+      Resource(trackedResource)
+    }
 
     override def getCancelToken(handler: ResourceHandler): F[Option[CancelToken[F]]] =
       delay { trackedResources.get(handler) }
