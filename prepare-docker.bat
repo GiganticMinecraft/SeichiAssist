030581a6810340418b4e46b521639e095439f2c0
kory33 <6561358+kory33@users.noreply.github.com>
2020-02-16 22:36:42 +0000

    [fix] eula.txtのファイルコピーを修正する
    Signed-off-by: kory33 <6561358+kory33@users.noreply.github.com>

--- prepare-docker.bat
+++ prepare-docker.bat
@@ -1,4 +1,7 @@
 call sbt assembly
-call robocopy /xc /xn /xo docker\spigot\eula.txt docker\spigot\serverfiles\eula.txt
+
+REM https://stackoverflow.com/a/23812179
+call echo n | copy /-y docker\spigot\eula.txt docker\spigot\serverfiles\eula.txt
+
 call docker-compose build -m 2g
 call docker-compose up
