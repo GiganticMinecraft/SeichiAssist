6308d0f088e7710c909cde7e6de6163b5ac60188
kory33 <6561358+kory33@users.noreply.github.com>
2020-02-16 05:38:22 +0000

    Revert "[temp fix] ロールバックのため日付判定の幅を4日にする"
    This reverts commit 3c553d5d
    
    Signed-off-by: kory33 <6561358+kory33@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/task/PlayerDataLoading.scala
+++ src/main/scala/com/github/unchama/seichiassist/task/PlayerDataLoading.scala
@@ -286,7 +286,7 @@ object PlayerDataLoading {
           if (dateDiff >= 1L) {
             val newTotalLoginDay = playerData.loginStatus.totalLoginDay + 1
             val newConsecutiveLoginDays =
-              if (dateDiff <= 4L)
+              if (dateDiff <= 2L)
                 playerData.loginStatus.consecutiveLoginDays + 1
               else
                 1
