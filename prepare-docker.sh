c06c14cc33d47c990cfb92e7330c477c7eee5b09
unchama <nintendowii@jcom.home.ne.jp>
2020-02-16 16:27:06 +0900

    [update]pluginの移動をdocker-compose側で九州してしまったので修正(存在を忘れていた…)

--- prepare-docker.sh
+++ prepare-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sbt assembly
-mkdir -p docker/spigot/plugins
-cp localDependencies/*.jar docker/spigot/plugins/
-cp target/build/*.jar docker/spigot/plugins/
+docker-compose down
+docker-compose up -d
