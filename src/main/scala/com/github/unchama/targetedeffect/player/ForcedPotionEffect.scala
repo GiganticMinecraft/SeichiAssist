94618a90c43a476ddbd5fb6d820647dfe8c7872b
kory33 <6561358+kory33@users.noreply.github.com>
2020-02-20 02:27:21 +0000

    [update] ChatInterceptorとForcedPotionEffect周りの型をpackageに移す
    Signed-off-by: kory33 <6561358+kory33@users.noreply.github.com>

--- src/main/scala/com/github/unchama/targetedeffect/player/ForcedPotionEffect.scala
+++ src/main/scala/com/github/unchama/targetedeffect/player/ForcedPotionEffect.scala
@@ -2,14 +2,12 @@ package com.github.unchama.targetedeffect.player
 
 import com.github.unchama
 import com.github.unchama.generic
-import com.github.unchama.generic.tag.tag.@@
 import com.github.unchama.targetedeffect.TargetedEffect
 import org.bukkit.entity.Player
 import org.bukkit.potion.PotionEffect
 
 object ForcedPotionEffect {
   trait Tag
-  type ForcedPotionEffect = TargetedEffect[Player] @@ Tag
 
   def apply(effect: PotionEffect): ForcedPotionEffect = {
     val potionEffect = unchama.targetedeffect.delay { player: Player => player.addPotionEffect(effect) }
