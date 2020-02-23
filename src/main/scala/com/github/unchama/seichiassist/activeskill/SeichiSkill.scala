9180fa28398578e7298fba9ab3d98c521616d108
kory33 <6561358+kory33@users.noreply.github.com>
2020-02-23 13:05:23 +0000

    [update] coolDownSeconds -> maxCoolDownTicks
    Signed-off-by: kory33 <6561358+kory33@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/activeskill/SeichiSkill.scala
+++ src/main/scala/com/github/unchama/seichiassist/activeskill/SeichiSkill.scala
@@ -5,16 +5,16 @@ import enumeratum._
 
 sealed abstract class SeichiSkill(val name: String,
                                   val range: SkillRange,
-                                  val coolDownSeconds: Option[Double],
+                                  val maxCoolDownTicks: Option[Int],
                                   val manaCost: Int,
                                   val requiredActiveSkillPoint: Int) extends Snakecase
 
 sealed abstract class ActiveSkill(name: String,
                                   range: SkillRange,
-                                  coolDownSeconds: Option[Double],
+                                  maxCoolDownTicks: Option[Int],
                                   manaCost: Int,
                                   requiredActiveSkillPoint: Int) extends
-  SeichiSkill(name, range, coolDownSeconds, manaCost, requiredActiveSkillPoint)
+  SeichiSkill(name, range, maxCoolDownTicks, manaCost, requiredActiveSkillPoint)
 
 sealed abstract class AssaultSkill(name: String,
                                    range: AssaultSkillRange,
@@ -35,26 +35,26 @@ object SeichiSkill extends Enum[SeichiSkill] {
   case object DualBreak extends ActiveSkill("デュアル・ブレイク", singleArea(1, 2, 1), None, 1, 10)
   case object TrialBreak extends ActiveSkill("トリアル・ブレイク", singleArea(3, 2, 1), None, 3, 20)
   case object Explosion extends ActiveSkill("エクスプロージョン", singleArea(3, 3, 3), None, 12, 30)
-  case object MirageFlare extends ActiveSkill("ミラージュ・フレア", singleArea(5, 5, 3), Some(0.7), 30, 40)
-  case object Dockarn extends ActiveSkill("ドッ・カーン", singleArea(7, 7, 5), Some(1.5), 70, 50)
-  case object GiganticBomb extends ActiveSkill("ギガンティック・ボム", singleArea(9, 9, 7), Some(2.5), 100, 60)
-  case object BrilliantDetonation extends ActiveSkill("ブリリアント・デトネーション", singleArea(11, 11, 9), Some(3.5), 200, 70)
-  case object LemuriaImpact extends ActiveSkill("レムリア・インパクト", singleArea(13, 13, 11), Some(5.0), 350, 80)
-  case object EternalVice extends ActiveSkill("エターナル・ヴァイス", singleArea(15, 15, 13), Some(7.0), 500, 90)
+  case object MirageFlare extends ActiveSkill("ミラージュ・フレア", singleArea(5, 5, 3), Some(14), 30, 40)
+  case object Dockarn extends ActiveSkill("ドッ・カーン", singleArea(7, 7, 5), Some(30), 70, 50)
+  case object GiganticBomb extends ActiveSkill("ギガンティック・ボム", singleArea(9, 9, 7), Some(50), 100, 60)
+  case object BrilliantDetonation extends ActiveSkill("ブリリアント・デトネーション", singleArea(11, 11, 9), Some(70), 200, 70)
+  case object LemuriaImpact extends ActiveSkill("レムリア・インパクト", singleArea(13, 13, 11), Some(100), 350, 80)
+  case object EternalVice extends ActiveSkill("エターナル・ヴァイス", singleArea(15, 15, 13), Some(140), 500, 90)
 
-  case object TomBoy extends ActiveSkill("トム・ボウイ", MultiArea(3, 3, 3)(3), Some(0.6), 28, 40)
-  case object Thunderstorm extends ActiveSkill("サンダーストーム", MultiArea(3, 3, 3)(7), Some(1.4), 65, 50)
-  case object StarlightBreaker extends ActiveSkill("スターライト・ブレイカー", MultiArea(5, 5, 5)(3), Some(2.4), 90, 60)
-  case object EarthDivide extends ActiveSkill("アース・ディバイド", MultiArea(5, 5, 5)(5), Some(3.4), 185, 70)
-  case object HeavenGaeBolg extends ActiveSkill("ヘヴン・ゲイボルグ", MultiArea(7, 7, 7)(3), Some(4.8), 330, 80)
-  case object Decision extends ActiveSkill("ディシジョン", MultiArea(7, 7, 7)(7), Some(6.8), 480, 90)
+  case object TomBoy extends ActiveSkill("トム・ボウイ", MultiArea(3, 3, 3)(3), Some(12), 28, 40)
+  case object Thunderstorm extends ActiveSkill("サンダーストーム", MultiArea(3, 3, 3)(7), Some(28), 65, 50)
+  case object StarlightBreaker extends ActiveSkill("スターライト・ブレイカー", MultiArea(5, 5, 5)(3), Some(48), 90, 60)
+  case object EarthDivide extends ActiveSkill("アース・ディバイド", MultiArea(5, 5, 5)(5), Some(68), 185, 70)
+  case object HeavenGaeBolg extends ActiveSkill("ヘヴン・ゲイボルグ", MultiArea(7, 7, 7)(3), Some(96), 330, 80)
+  case object Decision extends ActiveSkill("ディシジョン", MultiArea(7, 7, 7)(7), Some(136), 480, 90)
 
-  case object EbifriDrive extends ActiveSkill("エビフライ・ドライブ", RemoteArea(3, 3, 3), Some(0.2), 18, 40)
-  case object HolyShot extends ActiveSkill("ホーリー・ショット", RemoteArea(5, 5, 3), Some(1.3), 35, 50)
-  case object TsarBomba extends ActiveSkill("ツァーリ・ボンバ", RemoteArea(7, 7, 5), Some(1.6), 80, 60)
-  case object ArcBlast extends ActiveSkill("アーク・ブラスト", RemoteArea(9, 9, 7), Some(2.7), 110, 70)
-  case object PhantasmRay extends ActiveSkill("ファンタズム・レイ", RemoteArea(11, 11, 9), Some(3.8), 220, 80)
-  case object Supernova extends ActiveSkill("スーパー・ノヴァ", RemoteArea(13, 13, 11), Some(5.5), 380, 90)
+  case object EbifriDrive extends ActiveSkill("エビフライ・ドライブ", RemoteArea(3, 3, 3), Some(4), 18, 40)
+  case object HolyShot extends ActiveSkill("ホーリー・ショット", RemoteArea(5, 5, 3), Some(26), 35, 50)
+  case object TsarBomba extends ActiveSkill("ツァーリ・ボンバ", RemoteArea(7, 7, 5), Some(32), 80, 60)
+  case object ArcBlast extends ActiveSkill("アーク・ブラスト", RemoteArea(9, 9, 7), Some(54), 110, 70)
+  case object PhantasmRay extends ActiveSkill("ファンタズム・レイ", RemoteArea(11, 11, 9), Some(76), 220, 80)
+  case object Supernova extends ActiveSkill("スーパー・ノヴァ", RemoteArea(13, 13, 11), Some(110), 380, 90)
 
   case object WhiteBreath extends AssaultSkill("ホワイト・ブレス", condenseWater(7, 7, 7), 30, 70)
   case object AbsoluteZero extends AssaultSkill("アブソリュート・ゼロ", condenseWater(11, 11, 11), 80, 80)
