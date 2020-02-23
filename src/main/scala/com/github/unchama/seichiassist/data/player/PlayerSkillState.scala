8dd37273f13b497d4f44f052c22a3ba6048a20d3
kory33 <6561358+kory33@users.noreply.github.com>
2020-02-23 13:59:56 +0000

    [update] PlayerSkillStateの形を変更する
    Signed-off-by: kory33 <6561358+kory33@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/data/player/PlayerSkillState.scala
+++ src/main/scala/com/github/unchama/seichiassist/data/player/PlayerSkillState.scala
@@ -2,39 +2,21 @@ package com.github.unchama.seichiassist.data.player
 
 import com.github.unchama.seichiassist.activeskill.effect.ActiveSkillEffect
 import com.github.unchama.seichiassist.activeskill.{ActiveSkill, AssaultSkill, BreakArea, BreakSide, SeichiSkill}
-import com.github.unchama.seichiassist.data.player.PlayerSkillState.{PlayerActiveSkillState, PlayerAssaultSkillState, PlayerSkillEffectState}
 
-class PlayerSkillState {
-  var obtainedSkills: Set[SeichiSkill] = Set()
+case class PlayerSkillEffectState(obtainedEffects: Set[ActiveSkillEffect],
+                                  selection: Option[ActiveSkillEffect])
 
+case class PlayerSkillState(obtainedSkills: Set[SeichiSkill],
+                            activeSkillBreakSide: Option[BreakSide], // TODO should be in configuration
+                            isActiveSkillAvailable: Boolean,
+                            activeSkill: Option[ActiveSkill],
+                            assaultSkill: Option[AssaultSkill]) {
   def consumedActiveSkillPoint(): Int =
     obtainedSkills.map(_.requiredActiveSkillPoint).sum
 
-  val effectState = new PlayerSkillEffectState()
-  val activeSkillState = new PlayerActiveSkillState()
-  val assaultSkillState = new PlayerAssaultSkillState()
-}
+  val activeSkillArea: Option[BreakArea] =
+    activeSkill.map(new BreakArea(_, activeSkillBreakSide))
 
-object PlayerSkillState {
-  class PlayerSkillEffectState {
-    var obtainedEffects: Set[ActiveSkillEffect] = Set()
-
-    var effectSelection: Option[ActiveSkillEffect] = None
-  }
-
-  class PlayerActiveSkillState {
-    var activeSkillSelection: Option[ActiveSkill] = None
-
-    var breakSideSelection: Option[BreakSide] = None
-
-    var isSkillAvailable: Boolean = true
-
-    def activeSkillArea(): Option[BreakArea] = activeSkillSelection.map(new BreakArea(_, breakSideSelection))
-  }
-
-  class PlayerAssaultSkillState {
-    var assaultSkillSelection: Option[AssaultSkill] = None
-
-    def assaultSkillArea(): Option[BreakArea] = assaultSkillSelection.map(new BreakArea(_, None))
-  }
+  val assaultSkillArea: Option[BreakArea] =
+    assaultSkill.map(new BreakArea(_, None))
 }
