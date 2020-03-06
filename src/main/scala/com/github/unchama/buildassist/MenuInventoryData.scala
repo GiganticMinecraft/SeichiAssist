49eaf9ba06f23eb5281325143a168dde5dc74c81
soundofhorizon <49669345+soundofhorizon@users.noreply.github.com>
2020-03-06 16:48:42 +0900

    [CHANGE]補完した文章にして、不要な変数を削除する

--- src/main/scala/com/github/unchama/buildassist/MenuInventoryData.scala
+++ src/main/scala/com/github/unchama/buildassist/MenuInventoryData.scala
@@ -34,11 +34,11 @@ object MenuInventoryData {
     //プレイヤーデータ
     val playerdata = BuildAssist.playermap(uuid) // If NPE, player is already offline
 
-    val inventory = Bukkit.getServer.createInventory(null, 4 * 9, DARK_PURPLE.toString + "" + BOLD + "「範囲設置スキル」設定画面")
+    val inventory = Bukkit.getServer.createInventory(null, 4 * 9, s"$DARK_PURPLE$BOLD「範囲設置スキル」設定画面")
     var itemstack = new ItemStack(Material.BARRIER, 1)
     var itemmeta = Bukkit.getItemFactory.getItemMeta(Material.BARRIER)
     var skullmeta = ItemMetaFactory.SKULL.getValue
-    var lore = List(RESET.toString + "" + DARK_RED + "" + UNDERLINE + "クリックで移動")
+    var lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
 
     // FIXME: BAD NAME
     val ZSSkill = if (playerdata.ZoneSetSkillFlag) {
@@ -66,7 +66,7 @@ object MenuInventoryData {
 
     //初期画面へ移動
     itemstack.setDurability(3.toShort)
-    itemmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "元のページへ")
+    itemmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}元のページへ")
     itemmeta.setLore(lore.asJava)
     itemstack.setItemMeta(itemmeta)
     inventory.setItem(0, itemstack)
@@ -74,8 +74,11 @@ object MenuInventoryData {
     //土設置のON/OFF
     itemstack = new ItemStack(Material.DIRT, 1)
     itemmeta = Bukkit.getItemFactory.getItemMeta(Material.STONE)
-    itemmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "設置時に下の空洞を埋める機能")
-    lore = List(RESET.toString + "" + AQUA + "" + UNDERLINE + "機能の使用設定：" + ZSDirt, RESET.toString + "" + AQUA + "" + UNDERLINE + "機能の範囲：地下5マスまで")
+    itemmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}設置時に下の空洞を埋める機能")
+    lore = List(
+      s"$RESET$AQUA${UNDERLINE}機能の使用設定：$ZSDirt",
+      s"$RESET$AQUA${UNDERLINE}機能の範囲：地下5マスまで"
+    )
     itemmeta.setLore(lore.asJava)
     itemstack.setItemMeta(itemmeta)
     inventory.setItem(4, itemstack)
@@ -83,8 +86,11 @@ object MenuInventoryData {
     //設定状況の表示
     itemstack = new ItemStack(Material.STONE, 1)
     itemmeta = Bukkit.getItemFactory.getItemMeta(Material.STONE)
-    itemmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "現在の設定は以下の通りです")
-    lore = List(RESET.toString + "" + AQUA + "" + UNDERLINE + "スキルの使用設定：" + ZSSkill, RESET.toString + "" + AQUA + "" + UNDERLINE + "スキルの範囲設定：" + ZSSkillA + "×" + ZSSkillA, RESET.toString + "" + AQUA + "" + UNDERLINE + "MineStack優先設定:" + ZSSkill_Minestack)
+    itemmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}現在の設定は以下の通りです")
+    lore = List(
+      s"$RESET$AQUA${UNDERLINE}スキルの使用設定：$ZSSkill",
+      s"$RESET$AQUA${UNDERLINE}スキルの範囲設定：$ZSSkillA×$ZSSkillA",
+      s"$RESET$AQUA${UNDERLINE}MineStack優先設定:$ZSSkill_Minestack")
     itemmeta.setLore(lore.asJava)
     itemstack.setItemMeta(itemmeta)
     inventory.setItem(13, itemstack)
@@ -92,8 +98,11 @@ object MenuInventoryData {
     //範囲をMAXへ
     itemstack = new ItemStack(Material.SKULL_ITEM, 11)
     itemstack.setDurability(3.toShort)
-    skullmeta.setDisplayName(RED.toString + "" + UNDERLINE + "" + BOLD + "範囲設定を最大値に変更")
-    lore = List(RESET.toString + "" + AQUA + "" + "現在の範囲設定：" + ZSSkillA + "×" + ZSSkillA, RESET.toString + "" + AQUA + "" + UNDERLINE + "変更後の範囲設定：11×11")
+    skullmeta.setDisplayName(s"$RED$UNDERLINE${BOLD}範囲設定を最大値に変更")
+    lore = List(
+      s"$RESET${AQUA}現在の範囲設定：$ZSSkillA×$ZSSkillA",
+      s"$RESET$AQUA${UNDERLINE}変更後の範囲設定：11×11"
+    )
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowUp")
     itemstack.setItemMeta(skullmeta)
@@ -103,8 +112,12 @@ object MenuInventoryData {
     itemstack = new ItemStack(Material.SKULL_ITEM, 7)
     skullmeta = ItemMetaFactory.SKULL.getValue
     itemstack.setDurability(3.toShort)
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "範囲設定を一段階大きくする")
-    lore = List(RESET.toString + "" + AQUA + "" + "現在の範囲設定：" + ZSSkillA + "×" + ZSSkillA, RESET.toString + "" + AQUA + "" + UNDERLINE + "変更後の範囲設定：" + (ZSSkillA + 2) + "×" + (ZSSkillA + 2), RESET.toString + "" + RED + "" + "※範囲設定の最大値は11×11※")
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}範囲設定を一段階大きくする")
+    lore = List(
+      s"$RESET${AQUA}現在の範囲設定：$ZSSkillA×$ZSSkillA",
+      s"$RESET$AQUA${UNDERLINE}変更後の範囲設定：${ZSSkillA + 2}×${ZSSkillA + 2}",
+      s"$RESET$RED※範囲設定の最大値は11×11※"
+    )
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowUp")
     itemstack.setItemMeta(skullmeta)
@@ -114,8 +127,11 @@ object MenuInventoryData {
     itemstack = new ItemStack(Material.SKULL_ITEM, 5)
     skullmeta = ItemMetaFactory.SKULL.getValue
     itemstack.setDurability(3.toShort)
-    skullmeta.setDisplayName(RED.toString + "" + UNDERLINE + "" + BOLD + "範囲設定を初期値に変更")
-    lore = List(RESET.toString + "" + AQUA + "" + "現在の範囲設定：" + ZSSkillA + "×" + ZSSkillA, RESET.toString + "" + AQUA + "" + UNDERLINE + "変更後の範囲設定：5×5")
+    skullmeta.setDisplayName(s"$RED$UNDERLINE${BOLD}範囲設定を初期値に変更")
+    lore = List(
+      s"$RESET${AQUA}現在の範囲設定：$ZSSkillA×$ZSSkillA",
+      s"$RESET$AQUA${UNDERLINE}変更後の範囲設定：5×5"
+    )
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_TNT")
     itemstack.setItemMeta(skullmeta)
@@ -125,8 +141,12 @@ object MenuInventoryData {
     itemstack = new ItemStack(Material.SKULL_ITEM, 3)
     skullmeta = ItemMetaFactory.SKULL.getValue
     itemstack.setDurability(3.toShort)
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "範囲設定を一段階小さくする")
-    lore = List(RESET.toString + "" + AQUA + "" + "現在の範囲設定：" + ZSSkillA + "×" + ZSSkillA, RESET.toString + "" + AQUA + "" + UNDERLINE + "変更後の範囲設定：" + (ZSSkillA - 2) + "×" + (ZSSkillA - 2), RESET.toString + "" + RED + "" + "※範囲設定の最小値は3×3※")
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}範囲設定を一段階小さくする")
+    lore = List(
+      s"$RESET${AQUA}現在の範囲設定：$ZSSkillA×$ZSSkillA",
+      s"$RESET$AQUA${UNDERLINE}変更後の範囲設定：${ZSSkillA - 2}×${ZSSkillA - 2}",
+      s"$RESET$RED※範囲設定の最小値は3×3※"
+    )
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowDown")
     itemstack.setItemMeta(skullmeta)
@@ -137,8 +157,11 @@ object MenuInventoryData {
     itemstack = new ItemStack(Material.SKULL_ITEM, 1)
     skullmeta = ItemMetaFactory.SKULL.getValue
     itemstack.setDurability(3.toShort)
-    skullmeta.setDisplayName(RED.toString + "" + UNDERLINE + "" + BOLD + "範囲設定を最小値に変更")
-    lore = List(RESET.toString + "" + AQUA + "" + "現在の範囲設定：" + ZSSkillA + "×" + ZSSkillA, RESET.toString + "" + AQUA + "" + UNDERLINE + "変更後の範囲設定：3×3")
+    skullmeta.setDisplayName(s"$RED$UNDERLINE${BOLD}範囲設定を最小値に変更")
+    lore = List(
+      s"$RESET${AQUA}現在の範囲設定：$ZSSkillA×$ZSSkillA",
+      s"$RESET$AQUA${UNDERLINE}変更後の範囲設定：3×3"
+    )
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowDown")
     itemstack.setItemMeta(skullmeta)
@@ -148,8 +171,12 @@ object MenuInventoryData {
     //MineStackの方を優先して消費する設定
     itemstack = new ItemStack(Material.CHEST, 1)
     itemmeta = Bukkit.getItemFactory.getItemMeta(Material.CHEST)
-    itemmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "MineStack優先設定：" + ZSSkill_Minestack)
-    lore = List(RESET.toString + "" + GRAY + "スキルでブロックを並べるとき", RESET.toString + "" + GRAY + "MineStackの在庫を優先して消費します。", RESET.toString + "" + GRAY + "建築LV" + BuildAssist.config.getZoneskillMinestacklevel + "以上で利用可能", RESET.toString + "" + GRAY + "クリックで切り替え"
+    itemmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}MineStack優先設定：$ZSSkill_Minestack")
+    lore = List(
+      s"$RESET${GRAY}スキルでブロックを並べるとき",
+      s"$RESET${GRAY}MineStackの在庫を優先して消費します。",
+      s"$RESET${GRAY}建築LV${BuildAssist.config.getZoneskillMinestacklevel}以上で利用可能",
+      s"$RESET${GRAY}クリックで切り替え"
     )
     itemmeta.setLore(lore.asJava)
     itemstack.setItemMeta(itemmeta)
@@ -167,7 +194,7 @@ object MenuInventoryData {
     //プレイヤーデータ
     val playerdata = BuildAssist.playermap(uuid)
 
-    val inventory = Bukkit.getServer.createInventory(null, 4 * 9, DARK_PURPLE.toString + "" + BOLD + "「ブロックを並べるスキル（仮）」設定")
+    val inventory = Bukkit.getServer.createInventory(null, 4 * 9, s"$DARK_PURPLE$BOLD「ブロックを並べるスキル（仮）」設定")
     var itemstack = new ItemStack(Material.SKULL_ITEM, 1)
     var itemmeta: ItemMeta = Bukkit.getItemFactory.getItemMeta(Material.WOOD)
     val skullmeta: SkullMeta = ItemMetaFactory.SKULL.getValue
@@ -197,7 +224,7 @@ object MenuInventoryData {
     //ブロックを並べるスキルハーフブロック設定
     itemstack = new ItemStack(Material.STEP, 1)
     itemmeta = Bukkit.getItemFactory.getItemMeta(Material.STEP)
-    itemmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "ハーフブロック設定 ：" + BuildAssist.line_up_step_str(playerdata.line_up_step_flg))
+    itemmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}ハーフブロック設定 ：${BuildAssist.line_up_step_str(playerdata.line_up_step_flg)}")
     lore = List(
       s"$RESET${GRAY}ハーフブロックを並べる時の位置を決めます。",
       s"$RESET${GRAY}クリックで切り替え"
@@ -209,7 +236,7 @@ object MenuInventoryData {
     //ブロックを並べるスキル一部ブロックを破壊して並べる設定
     itemstack = new ItemStack(Material.TNT, 1)
     itemmeta = Bukkit.getItemFactory.getItemMeta(Material.TNT)
-    itemmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "破壊設定 ：" + BuildAssist.line_up_off_on_str(playerdata.line_up_des_flg))
+    itemmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}破壊設定 ：${BuildAssist.line_up_off_on_str(playerdata.line_up_des_flg)}")
     lore = List(
       s"$RESET${GRAY}ブロックを並べるとき特定のブロックを破壊して並べます。",
       s"$RESET${GRAY}破壊対象ブロック：草,花,水,雪,松明,きのこ",
@@ -239,22 +266,16 @@ object MenuInventoryData {
 
   //MineStackブロック一括クラフトメニュー
   def getBlockCraftData(p: Player): Inventory = {
-    //プレイヤーを取得
-    val player = p.getPlayer
-    //UUID取得
-    val uuid = player.getUniqueId
-    val playerdata_s = SeichiAssist.playermap(uuid) // if thrown NPE, player has already left
 
-    val inventory = Bukkit.getServer.createInventory(null, 6 * 9, DARK_PURPLE.toString + "" + BOLD + "MineStackブロック一括クラフト1")
-    var itemmeta: ItemMeta = null
+    val inventory = Bukkit.getServer.createInventory(null, 6 * 9, s"$DARK_PURPLE${BOLD}MineStackブロック一括クラフト1")
     var lore: List[String] = null
     var itemstack: ItemStack = null
-    var skullmeta = ItemMetaFactory.SKULL.getValue
+    val skullmeta = ItemMetaFactory.SKULL.getValue
 
-    // ホーム目を開く
+    // ホームを開く
     itemstack = new ItemStack(Material.SKULL_ITEM, 1, 3.toShort)
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "ホームへ")
-    lore = List(RESET.toString + "" + DARK_RED + "" + UNDERLINE + "クリックで移動")
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}ホームへ")
+    lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowLeft")
     itemstack.setItemMeta(skullmeta)
@@ -262,9 +283,9 @@ object MenuInventoryData {
 
     // 2ページ目を開く
     itemstack = new ItemStack(Material.SKULL_ITEM, 1, 3.toShort)
-    skullmeta = ItemMetaFactory.SKULL.getValue
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "2ページ目へ")
-    lore = List(s"""$RESET$DARK_RED${UNDERLINE}クリックで移動""")
+
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}2ページ目へ")
+    lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowDown")
     itemstack.setItemMeta(skullmeta)
@@ -300,23 +321,16 @@ object MenuInventoryData {
 
   //MineStackブロック一括クラフトメニュー2
   def getBlockCraftData2(p: Player): Inventory = {
-    //プレイヤーを取得
-    val player = p.getPlayer
-    //UUID取得
-    val uuid = player.getUniqueId
-    //プレイヤーデータ
-    val playerdata_s = SeichiAssist.playermap(uuid)
 
-    val inventory = Bukkit.getServer.createInventory(null, 6 * 9, DARK_PURPLE.toString + "" + BOLD + "MineStackブロック一括クラフト2")
+    val inventory = Bukkit.getServer.createInventory(null, 6 * 9, s"$DARK_PURPLE${BOLD}MineStackブロック一括クラフト2")
     var itemstack: ItemStack = null
-    var itemmeta: ItemMeta = null
-    var skullmeta: SkullMeta = ItemMetaFactory.SKULL.getValue
+    val skullmeta: SkullMeta = ItemMetaFactory.SKULL.getValue
     var lore: List[String] = null
 
     // 1ページ目を開く
     itemstack = new ItemStack(Material.SKULL_ITEM, 1, 3)
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "1ページ目へ")
-    lore = List(RESET.toString + "" + DARK_RED + "" + UNDERLINE + "クリックで移動")
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}1ページ目へ")
+    lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowUp")
     itemstack.setItemMeta(skullmeta)
@@ -324,9 +338,9 @@ object MenuInventoryData {
 
     // 3ページ目を開く
     itemstack = new ItemStack(Material.SKULL_ITEM, 1, 3)
-    skullmeta = ItemMetaFactory.SKULL.getValue
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "3ページ目へ")
-    lore = List(RESET.toString + "" + DARK_RED + "" + UNDERLINE + "クリックで移動")
+
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}3ページ目へ")
+    lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowDown")
     itemstack.setItemMeta(skullmeta)
@@ -367,31 +381,26 @@ object MenuInventoryData {
 
   //MineStackブロック一括クラフトメニュー3
   def getBlockCraftData3(p: Player): Inventory = {
-    //プレイヤーを取得
-    val player = p.getPlayer
-    //UUID取得
-    val uuid = player.getUniqueId
-    val playerdata_s = SeichiAssist.playermap(uuid)
 
-    val inventory = Bukkit.getServer.createInventory(null, 6 * 9, DARK_PURPLE.toString + "" + BOLD + "MineStackブロック一括クラフト3")
+    val inventory = Bukkit.getServer.createInventory(null, 6 * 9, s"$DARK_PURPLE${BOLD}MineStackブロック一括クラフト3")
     var itemstack: ItemStack = null
-    var itemmeta: ItemMeta = null
+    val skullmeta = ItemMetaFactory.SKULL.getValue
     var lore: List[String] = null
 
+
     // 2ページ目を開く
-  	val skullmeta = ItemMetaFactory.SKULL.getValue
     itemstack = new ItemStack(Material.SKULL_ITEM, 1, 3.toShort)
-    skullmeta.setDisplayName(YELLOW.toString + "" + UNDERLINE + "" + BOLD + "2ページ目へ")
-    lore = List(RESET.toString + "" + DARK_RED + "" + UNDERLINE + "クリックで移動")
+    skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}2ページ目へ")
+    lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
     skullmeta.setLore(lore.asJava)
     skullmeta.setOwner("MHF_ArrowUp")
     itemstack.setItemMeta(skullmeta)
     AsyncInventorySetter.setItemAsync(inventory, 45, itemstack)
 
     // 4ページ目を開く
-		// 3 = プレイヤーの頭
+    // 3 = プレイヤーの頭
     itemstack = new ItemStack(Material.SKULL_ITEM, 1, 3)
-    
+
     skullmeta.setDisplayName(s"$YELLOW$UNDERLINE${BOLD}4ページ目へ")
     lore = List(s"$RESET$DARK_RED${UNDERLINE}クリックで移動")
     skullmeta.setLore(lore.asJava)
@@ -426,13 +435,13 @@ object MenuInventoryData {
       for (x <- minPower10Index to maxPower10Index) {
         val p10 = power10(x)
         val itemstack = products.head._1.itemStack
-        val itemmeta = itemstack.getItemMeta()
+        val itemmeta = itemstack.getItemMeta
         itemmeta.setDisplayName(s"$YELLOW$UNDERLINE$BOLD${ingredients.map(mineStackObj => mineStackObj._1.uiName.get).mkString("と")}を${products.map(mineStackObj => mineStackObj._1.uiName.get).mkString("と")}に変換します")
         val ingredientRequest = ingredients.map(mineStackObjName => s"${mineStackObjName._1.uiName.get}${p10 * mineStackObjName._2}個").mkString("+")
         val lore = List(
           s"$RESET$GRAY$ingredientRequest→${products.head._1.uiName.get}${p10 * products.head._2}個"
         ) ::: {ingredients.map(mineStackObjName => s"$RESET$GRAY${mineStackObjName._1.uiName.get}の数:${comma(playerdata_s.minestack.getStackedAmountOf(Util.findMineStackObjectByName(mineStackObjName._1.mineStackObjName)))}")} :::
-          {products.map(mineStackObjName => s"${RESET.toString}$GRAY${mineStackObjName._1.uiName.get}の数:${comma(playerdata_s.minestack.getStackedAmountOf(Util.findMineStackObjectByName(mineStackObjName._1.mineStackObjName)))}")} :::
+          {products.map(mineStackObjName => s"$RESET$GRAY${mineStackObjName._1.uiName.get}の数:${comma(playerdata_s.minestack.getStackedAmountOf(Util.findMineStackObjectByName(mineStackObjName._1.mineStackObjName)))}")} :::
           List(
             s"$RESET${GRAY}建築LV${BuildAssist.config.getMinestackBlockCraftlevel(minestackBlockCraftlevel)}以上で利用可能",
             s"$RESET$DARK_RED${UNDERLINE}クリックで変換"
