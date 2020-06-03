60fb15b4e531ab0cdfcf9c77af2380eeafb91eff
Kory <6561358+kory33@users.noreply.github.com>
2020-06-03 15:45:38 +0900

    [update] doWhileTaskをrecMTaskに変更する

--- src/main/scala/com/github/unchama/concurrent/RepeatingRoutine.scala
+++ src/main/scala/com/github/unchama/concurrent/RepeatingRoutine.scala
@@ -20,10 +20,23 @@ object RepeatingRoutine {
     Monad[IO].foreverM(sleepWith(getInterval) >> recoveringAction)
   }
 
-  def doWhileTask(getInterval: IO[FiniteDuration], action: IO[Boolean])
-                 (implicit context: RepeatingTaskContext): IO[Unit] = {
-    val recoveringAction = SyncExtra.recoverWithStackTrace("繰り返し実行タスクの実行に失敗しました", false, action)
+  /**
+   * 初期状態 `init` から、`getInterval` によってスリープしたのち `action` にて
+   * 状態を副作用付きで更新するという操作を、 `action` がNoneを返すまで繰り返す
+   * @tparam State ループにて保持される状態の型
+   * @return
+   */
+  def recMTask[State](init: State)(action: State => IO[Option[State]])
+                     (getInterval: IO[FiniteDuration])(implicit context: RepeatingTaskContext): IO[Unit] = {
+    val recoveringAction: State => IO[Option[State]] = a =>
+      SyncExtra.recoverWithStackTrace("繰り返し実行タスクの実行に失敗しました", None, action(a))
 
-    Monad[IO].iterateWhile(sleepWith(getInterval) >> recoveringAction)(identity).as(())
+    Monad[IO].tailRecM(init) { state =>
+      sleepWith(getInterval) >> recoveringAction(state) map {
+        case Some(value) => Left(value)
+        case None => Right(())
+      }
+    }
   }
+
 }
