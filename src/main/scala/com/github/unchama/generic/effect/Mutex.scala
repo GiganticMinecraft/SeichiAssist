786495adde4e53c962c88009044467efef9f3694
Kory <6561358+kory33@users.noreply.github.com>
2020-06-07 08:53:49 +0900

    [fix] Mutexで、計算が失敗した場合のロック返却処理を追記する

--- src/main/scala/com/github/unchama/generic/effect/Mutex.scala
+++ src/main/scala/com/github/unchama/generic/effect/Mutex.scala
@@ -1,29 +1,35 @@
 package com.github.unchama.generic.effect
 
-import cats.Monad
-import cats.data.OptionT
 import cats.effect.concurrent.MVar
-import cats.effect.{Concurrent, Sync}
+import cats.effect.{Bracket, Concurrent, ExitCase, Sync}
 
-final class Mutex[F[_]: Monad, A] private(mVar: MVar[F, A]) {
+final class Mutex[F[_], A] private (mVar: MVar[F, A])
+                                   (implicit fBracket: Bracket[F, Throwable]) {
   import cats.implicits._
 
+  /**
+   *  - 変数のロックを取得
+   *  - `use`により新たな値を計算
+   *  - 変数を更新
+   *  したうえで、`use` の計算結果を反映する `B` を返却するような計算。
+   *
+   *  `use` が失敗した場合、外側の計算も失敗するが、変数は元の状態へ戻ることが保証される。
+   *  また、複数の並行するプロセスがこの計算を行う場合、
+   *  ロック取得から変数更新の間にあるプロセスはどの時点でもただ一つしか存在しないことが保証され、
+   *  したがってロックを取得できるまで外側の計算が (意味論的な) ブロッキングを行うことになる。
+   */
   def lockAndModify[B](use: A => F[(A, B)]): F[B] =
-    for {
-      a <- mVar.take
-      newAB <- use(a)
-      (newA, newB) = newAB
-      _ <- mVar.put(newA)
-    } yield newB
-
-  def tryLockAndModify[B](use: A => F[(A, B)]): F[Option[B]] = {
-    for {
-      a <- OptionT(mVar.tryTake)
-      newAB <- OptionT.liftF(use(a))
-      (newA, newB) = newAB
-      _ <- OptionT.liftF(mVar.put(newA))
-    } yield newB
-  }.value
+    Bracket[F, Throwable].bracketCase(mVar.take) { a =>
+      use(a) >>= { case (newA, b) => mVar.put(newA).as(b) }
+    } {
+      case (_, ExitCase.Completed) =>
+        // このケースではmVarに値はputされている
+        ().pure
+      case (a, _) =>
+        // 元の値をputし直すことでロックを返却する
+        // 外側の `F` は失敗する
+        mVar.put(a)
+    }
 }
 
 object Mutex {
