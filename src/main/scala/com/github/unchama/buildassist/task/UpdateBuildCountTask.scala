c7116d220e2e6a2a36a9db1cf3b04e9b48f454d9
KisaragiEffective <kisaragi.effective@gmail.com>
2020-06-13 11:40:22 +0900

    [UPDATE] optimize import

--- src/main/scala/com/github/unchama/buildassist/task/UpdateBuildCountTask.scala
+++ src/main/scala/com/github/unchama/buildassist/task/UpdateBuildCountTask.scala
@@ -1,12 +1,9 @@
 package com.github.unchama.buildassist.task
 
 import java.math.BigDecimal
-import java.util.UUID
 
 import com.github.unchama.buildassist.BuildAssist
-import com.github.unchama.buildassist.data.PlayerData
 import org.bukkit.Bukkit
-import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
 
 class UpdateBuildCountTask extends BukkitRunnable {
@@ -15,9 +12,9 @@ class UpdateBuildCountTask extends BukkitRunnable {
       val player = Bukkit.getServer.getPlayer(playerdata.uuid)
 
       // 1分間の建築量を加算する
-      // デルタが上限以上なら上限に丸める
       val actualBuildCount = playerdata.buildCountBuffer.doubleValue
       val limit = BuildAssist.config.getBuildNum1minLimit
+      // デルタが上限以上なら上限に丸める
       val delta = if (actualBuildCount > limit)
         new BigDecimal(limit)
       else
