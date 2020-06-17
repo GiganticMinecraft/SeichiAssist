2653bf4d6ea8be4068e65126b0bed06292d43bc1
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2020-06-17 22:55:32 +0900

    Update PresentCommand.java

--- src/main/scala/com/github/unchama/seichiassist/commands/legacy/PresentCommand.java
+++ src/main/scala/com/github/unchama/seichiassist/commands/legacy/PresentCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
