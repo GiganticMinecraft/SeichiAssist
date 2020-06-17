86c019137fd8fad1d4536e5b46438f589652f3c4
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2020-06-18 00:20:01 +0900

    Update PresentCommand.java

--- src/main/scala/com/github/unchama/seichiassist/commands/legacy/PresentCommand.java
+++ src/main/scala/com/github/unchama/seichiassist/commands/legacy/PresentCommand.java
@@ -11,6 +11,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 
 /**
