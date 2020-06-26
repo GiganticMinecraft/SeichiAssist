3b39f02f14b171da88703fcd616ea5a1af37e880
Kory <6561358+kory33@users.noreply.github.com>
2020-06-26 12:40:37 +0900

    [update] add time measurement to getAllGeneratedChunks

--- src/main/scala/com/github/unchama/util/external/ExternalServices.scala
+++ src/main/scala/com/github/unchama/util/external/ExternalServices.scala
@@ -1,6 +1,7 @@
 package com.github.unchama.util.external
 
 import cats.effect.{ContextShift, IO}
+import com.github.unchama.util.MillisecondTimer
 import com.github.unchama.util.bukkit.WorldUtil
 import inventory_search.{Chunk, ChunkCoord, SearchResult}
 import org.bukkit.World
@@ -13,30 +14,29 @@ object ExternalServices {
                            (implicit contextShift: ContextShift[IO]): IO[Map[World, Seq[(Int, Int)]]] = {
     val mvc = ExternalPlugins.getMultiverseCore
 
+    def getChunkCoordinates(world: World): IO[(World, Seq[(Int, Int)])] =
+      MillisecondTimer.timeIO(s"Search for chunks in ${world.getName}")(IO {
+        val command = s"$chunkSearchCommand ${WorldUtil.getAbsoluteWorldFolder(world)}"
+        val result =
+          SearchResult.parseFrom(Runtime.getRuntime.exec(command).getInputStream)
+            .result
+            .flatMap {
+              // https://bukkit.org/threads/combining-world-and-world_nether.60319/
+              // Bukkitの挙動でワールドとフォルダが一対一に対応するため、dimIdは無視して良い
+              case Chunk(Some(ChunkCoord(x, z, _)), _, _) =>
+                Some((x, z))
+              case _ =>
+                None
+            }
+        world -> result
+      })
+
     import scala.jdk.CollectionConverters._
     import cats.implicits._
 
     for {
       worlds <- IO { mvc.getMVWorldManager.getMVWorlds.asScala.toList.map(_.getCBWorld) }
-      result <-
-        worlds.map { world => IO {
-          val command = s"$chunkSearchCommand ${WorldUtil.getAbsoluteWorldFolder(world)}"
-          val result =
-            SearchResult.parseFrom(Runtime.getRuntime.exec(command).getInputStream)
-              .result
-              .flatMap {
-                // https://bukkit.org/threads/combining-world-and-world_nether.60319/
-                // Bukkitの挙動でワールドとフォルダが一対一に対応するため、dimIdは無視して良い
-                case Chunk(Some(ChunkCoord(x, z, _)), _, _) =>
-                  Some((x, z))
-                case _ =>
-                  None
-              }
-
-          world -> result
-        }}
-        .parSequence
-        .map(_.toMap)
+      result <- worlds.map(getChunkCoordinates).parSequence.map(_.toMap)
     } yield result
   }
 }
