39efd4c3b16fc011d63a8d9bde22934185eb7628
Kory <6561358+kory33@users.noreply.github.com>
2020-06-26 11:59:43 +0900

    [update] ワールドごとにチャンク検索を並列化する

--- src/main/scala/com/github/unchama/util/external/ExternalServices.scala
+++ src/main/scala/com/github/unchama/util/external/ExternalServices.scala
@@ -1,35 +1,42 @@
 package com.github.unchama.util.external
 
-import cats.effect.IO
+import cats.effect.{ContextShift, IO}
 import com.github.unchama.util.bukkit.WorldUtil
 import inventory_search.{Chunk, ChunkCoord, SearchResult}
 import org.bukkit.World
 
 object ExternalServices {
   // TODO inject this from config
-  val defaultCommand: String = "dotnet /inventory-search/InventorySearch.dll"
+  val defaultCommand: String = "dotnet /chunk-search/ChunkSearch.dll"
 
-  def getAllChunkCoordsWithPotentialItems(inventorySearchCommand: String): IO[Map[World, Seq[(Int, Int)]]] = IO {
+  def getAllGeneratedChunks(chunkSearchCommand: String)
+                           (implicit contextShift: ContextShift[IO]): IO[Map[World, Seq[(Int, Int)]]] = {
     val mvc = ExternalPlugins.getMultiverseCore
 
     import scala.jdk.CollectionConverters._
+    import cats.implicits._
 
-    mvc.getMVWorldManager.getMVWorlds
-      .asScala.toList.map(_.getCBWorld)
-      .map { world =>
-        val command = s"$inventorySearchCommand ${WorldUtil.getAbsoluteWorldFolder(world)}"
-        val result =
-          SearchResult.parseFrom(Runtime.getRuntime.exec(command).getInputStream)
-            .result
-            .flatMap {
-              // https://bukkit.org/threads/combining-world-and-world_nether.60319/
-              // Bukkitの挙動でワールドとフォルダが一対一に対応するため、dimIdは無視して良い
-              case Chunk(Some(ChunkCoord(x, z, _)), _, _) =>
-                Some((x, z))
-              case _ =>
-                None
-            }
-        world -> result
-      }.toMap
+    for {
+      worlds <- IO { mvc.getMVWorldManager.getMVWorlds.asScala.toList.map(_.getCBWorld) }
+      result <-
+        worlds.map { world => IO {
+          val command = s"$chunkSearchCommand ${WorldUtil.getAbsoluteWorldFolder(world)}"
+          val result =
+            SearchResult.parseFrom(Runtime.getRuntime.exec(command).getInputStream)
+              .result
+              .flatMap {
+                // https://bukkit.org/threads/combining-world-and-world_nether.60319/
+                // Bukkitの挙動でワールドとフォルダが一対一に対応するため、dimIdは無視して良い
+                case Chunk(Some(ChunkCoord(x, z, _)), _, _) =>
+                  Some((x, z))
+                case _ =>
+                  None
+              }
+
+          world -> result
+        }}
+        .parSequence
+        .map(_.toMap)
+    } yield result
   }
 }
