de986c2c9b19e69733874caa4c6fed2379262136
yanorei32 <yanorei@hotmail.co.jp>
2020-07-12 19:56:12 +0900

    [clean] toTimeStringの修正。

--- src/main/scala/com/github/unchama/seichiassist/util/TypeConverter.java
+++ src/main/scala/com/github/unchama/seichiassist/util/TypeConverter.java
@@ -3,6 +3,7 @@ package com.github.unchama.seichiassist.util;
 import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 
 public final class TypeConverter {
     // 不必要なインスタンス化を防ぐため封印
@@ -20,26 +21,12 @@ public final class TypeConverter {
         return _tick / 20;
     }
 
-    public static String toTimeString(int second) {
-        final int minute;
-        final int hour;
-        String time = "";
-        // Math.floor は double を返す
-        hour = (int) Math.floor(second / 3600);
-        second -= hour * 3600;
-        minute = Math.round(second / 60);
-        if (hour != 0) {
-            time = hour + "時間";
-        }
-        if (minute != 0) {
-            time += minute + "分";
-        }
-		/*
-		if(second != 0){
-			time = time + second + "秒";
-		}
-		*/
-        return time;
+    public static String toTimeString(int seconds) {
+        final int totalMinutes = Math.round(seconds / 60);
+        final int hours = totalMinutes / 60;
+        final int minutes = totalMinutes % 60;
+
+        return (hours == 0 ? "" : hours + "時間") + minutes + "分";
     }
 
     /**
