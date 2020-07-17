e7202bc749f574a8c6aaf5060c0aef60e0aff3b1
Kory <6561358+kory33@users.noreply.github.com>
2020-07-17 09:59:51 +0900

    [clean] PersistedItemsMigrationVersionRepositoryの型エイリアス名を修正する

--- src/main/scala/com/github/unchama/seichiassist/infrastructure/migration/executors/PersistedItemsMigrationVersionRepository.scala
+++ src/main/scala/com/github/unchama/seichiassist/infrastructure/migration/executors/PersistedItemsMigrationVersionRepository.scala
@@ -2,19 +2,20 @@ package com.github.unchama.seichiassist.infrastructure.migration.executors
 
 import cats.effect.{IO, Resource}
 import com.github.unchama.itemmigration.domain.{ItemMigrationVersionNumber, ItemMigrationVersionRepository}
-import com.github.unchama.seichiassist.infrastructure.migration.executors.PersistedItemsMigrationVersionRepository.PI
+import com.github.unchama.seichiassist.infrastructure.migration.executors.PersistedItemsMigrationVersionRepository.PersistedItems
 import com.github.unchama.seichiassist.infrastructure.migration.targets.SeichiAssistPersistedItems
 
-class PersistedItemsMigrationVersionRepository extends ItemMigrationVersionRepository[IO, PI] {
-  override type PersistenceLock[TInstance <: Singleton with PI] = Nothing
+class PersistedItemsMigrationVersionRepository extends ItemMigrationVersionRepository[IO, PersistedItems] {
+  override type PersistenceLock[TInstance <: PersistedItems] = Nothing
 
-  override def lockVersionPersistence(target: PI): Resource[IO, PersistenceLock[PI]] = ???
+  override def lockVersionPersistence(target: PersistedItems): Resource[IO, PersistenceLock[PersistedItems]] = ???
 
-  override def getVersionsAppliedTo(target: PI): PersistenceLock[target.type] => IO[Set[ItemMigrationVersionNumber]] = ???
+  override def getVersionsAppliedTo(target: PersistedItems): PersistenceLock[target.type] => IO[Set[ItemMigrationVersionNumber]] = ???
 
-  override def persistVersionsAppliedTo(target: PI, versions: Iterable[ItemMigrationVersionNumber]): PersistenceLock[PI] => IO[Unit] = ???
+  override def persistVersionsAppliedTo(target: PersistedItems,
+                                        versions: Iterable[ItemMigrationVersionNumber]): PersistenceLock[PersistedItems] => IO[Unit] = ???
 }
 
 object PersistedItemsMigrationVersionRepository {
-  private type PI = SeichiAssistPersistedItems.type
+  private type PersistedItems = SeichiAssistPersistedItems.type
 }
