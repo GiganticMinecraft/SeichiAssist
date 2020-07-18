aa598e6dd065bb2f66ba3376add69354a119e280
stouma915 <sasakitouma@yahoo.ne.jp>
2020-07-18 16:20:57 +0900

    Revert "全てのMebiusにUUIDのNBTがある前提に変更"
    This reverts commit 4fe36a6d4a7630b0d3b2bde82bbe259ff235f7dd.

--- src/main/scala/com/github/unchama/seichiassist/listener/MebiusListener.java
+++ src/main/scala/com/github/unchama/seichiassist/listener/MebiusListener.java
@@ -341,7 +341,7 @@ public class MebiusListener implements Listener {
         String mebiusOwnerUUID = nbtItem.getString("ownerUUID");
 
         // 所有者が異なる場合
-        if (!mebiusOwnerUUID.equals(player.getUniqueId().toString())) {
+        if (!player.getName().equals(getOwner(mebius)) && !mebiusOwnerUUID.equals(player.getUniqueId().toString())) {
             return;
         }
 
