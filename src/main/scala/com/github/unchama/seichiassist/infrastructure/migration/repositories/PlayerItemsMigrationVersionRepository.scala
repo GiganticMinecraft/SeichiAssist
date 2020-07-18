14e53f8278f6fd16f4cbbe101b0b6ae8d7026b23
Kory <6561358+kory33@users.noreply.github.com>
2020-07-18 16:23:37 +0900

    [update] 同一DBSessionを使うようにする

--- src/main/scala/com/github/unchama/seichiassist/infrastructure/migration/repositories/PlayerItemsMigrationVersionRepository.scala
+++ src/main/scala/com/github/unchama/seichiassist/infrastructure/migration/repositories/PlayerItemsMigrationVersionRepository.scala
@@ -6,35 +6,37 @@ import com.github.unchama.itemmigration.targets.PlayerInventoriesData
 import scalikejdbc._
 
 class PlayerItemsMigrationVersionRepository(serverId: String) extends ItemMigrationVersionRepository[IO, PlayerInventoriesData] {
-  /**
-   * プレーヤーは単一サーバーに1人しか存在しないためロックは不要
-   */
-  override type PersistenceLock[TInstance <: PlayerInventoriesData] = Any
+  override type PersistenceLock[TInstance <: PlayerInventoriesData] = DBSession
 
   override def lockVersionPersistence(target: PlayerInventoriesData): Resource[IO, PersistenceLock[target.type]] = {
-    Resource.pure[IO, Any](())
+    /**
+     * プレーヤーは単一サーバーに1人しか存在しないためロックは不要
+     */
+    Resource.make(
+      IO {
+        AutoSession
+      }
+    )(
+      _ => IO.unit
+    )
   }
 
-  override def getVersionsAppliedTo(target: PlayerInventoriesData): PersistenceLock[target.type] => IO[Set[ItemMigrationVersionNumber]] = _ => IO {
-    implicit val session: DBSession = AutoSession
-
-    val versions = DB readOnly { implicit session =>
+  override def getVersionsAppliedTo(target: PlayerInventoriesData): PersistenceLock[target.type] => IO[Set[ItemMigrationVersionNumber]] =
+    implicit session => IO {
       sql"""
         select version_string from seichiassist.player_in_server_item_migration
           where server_id = $serverId and player_uuid = ${target.player.getUniqueId}
-      """.map { rs => rs.string("version_string") }.list.apply()
+      """
+        .map { rs => rs.string("version_string") }
+        .list.apply()
+        .flatMap(ItemMigrationVersionNumber.fromString).toSet
     }
 
-    versions.flatMap(ItemMigrationVersionNumber.fromString).toSet
-  }
-
   override def persistVersionsAppliedTo(target: PlayerInventoriesData,
-                                        versions: Iterable[ItemMigrationVersionNumber]): PersistenceLock[target.type] => IO[Unit] = _ => IO {
-    implicit val session: DBSession = AutoSession
+                                        versions: Iterable[ItemMigrationVersionNumber]): PersistenceLock[target.type] => IO[Unit] =
+    implicit session => IO {
+      val batchParams = versions.map(version => Seq(ItemMigrationVersionNumber.convertToString(version)))
 
-    val batchParams = versions.map(version => Seq(ItemMigrationVersionNumber.convertToString(version)))
-
-    DB localTx { implicit session =>
       sql"""
         insert into seichiassist.player_in_server_item_migration(player_uuid, server_id, version_string, completed_at)
         values (${target.player.getUniqueId}, $serverId, ?, cast(now() as datetime))
@@ -42,6 +44,5 @@ class PlayerItemsMigrationVersionRepository(serverId: String) extends ItemMigrat
         .batch(batchParams.toSeq: _*)
         .apply[List]()
     }
-  }
 }
 
--- src/main/scala/com/github/unchama/seichiassist/infrastructure/migration/repositories/WorldLevelItemsMigrationVersionRepository.scala
+++ src/main/scala/com/github/unchama/seichiassist/infrastructure/migration/repositories/WorldLevelItemsMigrationVersionRepository.scala
@@ -3,36 +3,39 @@ package com.github.unchama.seichiassist.infrastructure.migration.repositories
 import cats.effect.{IO, Resource}
 import com.github.unchama.itemmigration.domain.{ItemMigrationVersionNumber, ItemMigrationVersionRepository}
 import com.github.unchama.itemmigration.targets.WorldLevelData
-import scalikejdbc.{AutoSession, DB, DBSession, _}
+import scalikejdbc.{AutoSession, DBSession, _}
 
 class WorldLevelItemsMigrationVersionRepository(serverId: String) extends ItemMigrationVersionRepository[IO, WorldLevelData] {
-  /**
-   * サーバーIDが一意なら更新が一サーバーIDに対して一個しか走らないためロックは不要
-   */
-  override type PersistenceLock[TInstance <: WorldLevelData] = Unit
+  override type PersistenceLock[TInstance <: WorldLevelData] = DBSession
 
   override def lockVersionPersistence(target: WorldLevelData): Resource[IO, PersistenceLock[target.type]] = {
-    Resource.pure[IO, Unit](())
+    /**
+     * サーバーIDが一意なら更新が一サーバーIDに対して一個しか走らないためロックは不要
+     */
+    Resource.make(
+      IO {
+        AutoSession
+      }
+    )(
+      _ => IO.unit
+    )
   }
 
-  override def getVersionsAppliedTo(target: WorldLevelData): PersistenceLock[target.type] => IO[Set[ItemMigrationVersionNumber]] = _ => IO {
-    implicit val session: DBSession = AutoSession
-
-    val versions = DB readOnly { implicit session =>
+  override def getVersionsAppliedTo(target: WorldLevelData): PersistenceLock[target.type] => IO[Set[ItemMigrationVersionNumber]] =
+    implicit session => IO {
       sql"""
         select version_string from seichiassist.item_migration_in_server_world_levels where server_id = $serverId
-      """.map { rs => rs.string("version_string") }.list.apply()
+      """
+        .map { rs => rs.string("version_string") }
+        .list.apply()
+        .flatMap(ItemMigrationVersionNumber.fromString).toSet
     }
 
-    versions.flatMap(ItemMigrationVersionNumber.fromString).toSet
-  }
+  override def persistVersionsAppliedTo(target: WorldLevelData,
+                                        versions: Iterable[ItemMigrationVersionNumber]): PersistenceLock[target.type] => IO[Unit] =
+    implicit session => IO {
+      val batchParams = versions.map(version => Seq(ItemMigrationVersionNumber.convertToString(version)))
 
-  override def persistVersionsAppliedTo(target: WorldLevelData, versions: Iterable[ItemMigrationVersionNumber]): PersistenceLock[target.type] => IO[Unit] = _ => IO {
-    implicit val session: DBSession = AutoSession
-
-    val batchParams = versions.map(version => Seq(ItemMigrationVersionNumber.convertToString(version)))
-
-    DB localTx { implicit session =>
       sql"""
         insert into seichiassist.item_migration_in_server_world_levels(server_id, version_string, completed_at)
         values ($serverId, ?, cast(now() as datetime))
@@ -40,5 +43,4 @@ class WorldLevelItemsMigrationVersionRepository(serverId: String) extends ItemMi
         .batch(batchParams.toSeq: _*)
         .apply[List]()
     }
-  }
 }
