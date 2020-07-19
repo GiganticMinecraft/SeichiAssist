3cb531fc5410edf7f49d35189929dd62b987387d
KisaragiEffective <kisaragi.effective@gmail.com>
2020-07-19 23:51:40 +0900

    [UPDATE] make 'em public

--- src/main/scala/com/github/unchama/buildassist/BuildBlock.java
+++ src/main/scala/com/github/unchama/buildassist/BuildBlock.java
@@ -6,19 +6,15 @@ import org.bukkit.entity.Player;
 
 import java.math.BigDecimal;
 
-import com.github.unchama.seichiassist.PackagePrivate;
-
-@PackagePrivate
-class BuildBlock {
+public class BuildBlock {
     private BuildBlock() {}
 
     // TODO:
     //  sum (last-valueとも言う)。
     //  既にこれによって壊れたデータがありそう。
-    @PackagePrivate
-    static BigDecimal calcBuildBlock(final Player player) {
+    public static BigDecimal calcBuildBlock(final Player player) {
         BigDecimal sum = BigDecimal.ZERO;
-        for (final Material m : BuildAssist.materiallist()) {
+        for (final Material m : BuildAssist.fillBlocks()) {
             sum = new BigDecimal(player.getStatistic(Statistic.USE_ITEM, m));
         }
         return sum;
