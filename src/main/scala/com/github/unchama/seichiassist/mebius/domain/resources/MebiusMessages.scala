95e4064e80ffb0e06462f49dbc5f054094704846
Kory <6561358+kory33@users.noreply.github.com>
2020-07-21 01:13:45 +0900

    [move] MebiusMessagesの階層を変更する

--- src/main/scala/com/github/unchama/seichiassist/mebius/domain/resources/MebiusMessages.scala
+++ src/main/scala/com/github/unchama/seichiassist/mebius/domain/resources/MebiusMessages.scala
@@ -1,4 +1,6 @@
-package com.github.unchama.seichiassist.mebius.domain
+package com.github.unchama.seichiassist.mebius.domain.resources
+
+import com.github.unchama.seichiassist.mebius.domain.MebiusTalk
 
 object MebiusMessages {
 
