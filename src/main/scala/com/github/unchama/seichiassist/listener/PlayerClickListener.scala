8cbc40a7a140c238b95a3c5d78cbb7f4d977dbc4
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2020-07-21 22:20:19 +0900

    Update PlayerClickListener.scala
    make changes as kory's review

--- src/main/scala/com/github/unchama/seichiassist/listener/PlayerClickListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/listener/PlayerClickListener.scala
@@ -353,16 +353,11 @@ class PlayerClickListener extends Listener {
 
     if (player.getInventory.getItemInMainHand.getType != Material.STICK) return
 
-    //actionがPHYSICALだった場合は感圧版を踏んだかどうかの判定
-    if (action == Action.PHYSICAL) {
-      val pressurePlates: List[Material] = List(Material.STONE_PLATE, Material.WOOD_PLATE, Material.GOLD_PLATE, Material.IRON_PLATE)
-      event.setCancelled(!pressurePlates.contains(event.getClickedBlock.getType))
-    }
-    else event.setCancelled(true)
-
+    // PHYSICALだった場合はあえて判定しない  
     // 右クリックの処理ではない
     if (!(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)) return
     if (event.getHand == EquipmentSlot.OFF_HAND) return
+    event.setCancelled(true)
 
     import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.layoutPreparationContext
 
