06d401fc7d7bcd1d4863f47cfd361599237f83df
Kory <6561358+kory33@users.noreply.github.com>
2020-07-22 01:37:22 +0900

    [update] MebiusPropertyにdocを追加する

--- src/main/scala/com/github/unchama/seichiassist/mebius/domain/MebiusProperty.scala
+++ src/main/scala/com/github/unchama/seichiassist/mebius/domain/MebiusProperty.scala
@@ -1,7 +1,13 @@
 package com.github.unchama.seichiassist.mebius.domain
 
+/**
+ * @param ownerName     オーナーのプレーヤーID
+ * @param enchantments  付与されるエンチャントメント
+ * @param level         Mebiusのレベル
+ * @param ownerNickname オーナーをMebiusがどう呼ぶか
+ */
 case class MebiusProperty(ownerName: String, enchantments: Map[MebiusEnchantment, Int],
-                          level: MebiusLevel, name: Option[String]) {
+                          level: MebiusLevel, ownerNickname: Option[String]) {
   require {
     enchantments.forall { case (MebiusEnchantment(_, unlockLevel, maxLevel, _), enchantmentLevel) =>
       unlockLevel.value >= level.value && 1 <= enchantmentLevel && enchantmentLevel <= maxLevel
