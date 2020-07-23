5e04a3e41b9fa1818e91c882a512965a3c852523
Kory <6561358+kory33@users.noreply.github.com>
2020-07-23 18:33:12 +0900

    [update] docを追加する

--- src/main/scala/com/github/unchama/seichiassist/mebius/service/MebiusDroppingService.scala
+++ src/main/scala/com/github/unchama/seichiassist/mebius/service/MebiusDroppingService.scala
@@ -8,6 +8,9 @@ import scala.util.Random
 object MebiusDroppingService {
 
   // Mebiusドロップ率
+  // パラメータpの幾何分布の平均は1/pであるから、
+  // 1ブロック壊すごとに 1 / averageBlocksToBeBrokenPerMebiusDrop の確率でドロップが起これば
+  // 平均 averageBlocksToBeBrokenPerMebiusDrop 回の試行でドロップすることになる。
   private val averageBlocksToBeBrokenPerMebiusDrop = 50000
 
   def tryForDrop(ownerName: String): IO[Option[MebiusProperty]] = IO {
