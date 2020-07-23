734b345bcfc8e5a6680c3309917e21bfc13f290d
Kory <6561358+kory33@users.noreply.github.com>
2020-07-24 03:02:34 +0900

    [clean] isEquip/getNicknameをinlineする

--- src/main/scala/com/github/unchama/seichiassist/mebius/controller/listeners/MebiusInteractionResponder.scala
+++ src/main/scala/com/github/unchama/seichiassist/mebius/controller/listeners/MebiusInteractionResponder.scala
@@ -14,6 +14,7 @@ import org.bukkit.event.{EventHandler, EventPriority, Listener}
 import scala.util.Random
 
 class MebiusInteractionResponder extends Listener {
+  // TODO check owner
 
   // メッセージリストからランダムに取り出し、タグを置換する
   private def getMessage(messages: Set[String], str1: String, str2: String) = {
@@ -25,29 +26,18 @@ class MebiusInteractionResponder extends Listener {
     msg
   }
 
-  private def getNickname(player: Player): Option[String] =
-    ItemStackMebiusCodec
-      .decodeMebiusProperty(player.getInventory.getHelmet)
-      .map(_.ownerNickname)
-
-  /** Mebiusを装備しているか */
-  private def isEquip(player: Player): Boolean = ItemStackMebiusCodec.isMebius(player.getInventory.getHelmet)
-
-  // ダメージを受けた時
-  @EventHandler def onDamage(event: EntityDamageByEntityEvent): Unit = {
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  def onDamage(event: EntityDamageByEntityEvent): Unit = {
     // プレイヤーがダメージを受けた場合
     event.getEntity match {
       case player: Player =>
-        // プレイヤーがMebiusを装備していない場合は除外
-        if (!isEquip(player)) return
-        val mebius = player.getInventory.getHelmet
-
         val helmet = player.getInventory.getHelmet
+        val mebiusProperty = ItemStackMebiusCodec.decodeMebiusProperty(helmet).getOrElse(return)
 
         // 耐久閾値を超えていたら破損警告
-        if (mebius.getDurability >= mebius.getType.getMaxDurability - 10) {
+        if (helmet.getDurability >= helmet.getType.getMaxDurability - 10) {
           SeichiAssist.playermap(player.getUniqueId).mebius
-            .speak(getMessage(MebiusMessages.onDamageBreaking, getNickname(player).get, ""))
+            .speak(getMessage(MebiusMessages.onDamageBreaking, mebiusProperty.ownerNickname, ""))
         }
 
         // モンスターからダメージを受けた場合
@@ -55,32 +45,31 @@ class MebiusInteractionResponder extends Listener {
           case monster: Monster =>
             // 対モンスターメッセージ
             SeichiAssist.playermap(player.getUniqueId).mebius
-              .speak(getMessage(MebiusMessages.onDamageWarnEnemy, getNickname(player).get, monster.getName))
+              .speak(getMessage(MebiusMessages.onDamageWarnEnemy, mebiusProperty.ownerNickname, monster.getName))
           case _ =>
         }
       case _ =>
     }
   }
 
-  // 壊れたとき
-  @EventHandler def onBreak(event: PlayerItemBreakEvent): Unit = {
-    val messages = MebiusMessages.onMebiusBreak
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  def onBreak(event: PlayerItemBreakEvent): Unit = {
     val brokenItem = event.getBrokenItem
+    val player = event.getPlayer
 
     ItemStackMebiusCodec
       .decodeMebiusProperty(brokenItem)
       .foreach { property =>
-        val player = event.getPlayer
         SeichiAssist.playermap(event.getPlayer.getUniqueId).mebius
-          .speak(getMessage(messages, property.ownerNickname, ""))
+          .speak(getMessage(MebiusMessages.onMebiusBreak, property.ownerNickname, ""))
         player.sendMessage(s"${MebiusListener.getName(brokenItem)}${RESET}が旅立ちました。")
         // エンドラが叫ぶ
         player.playSound(player.getLocation, Sound.ENTITY_ENDERDRAGON_DEATH, 1.0f, 0.1f)
       }
   }
 
-  // モンスターを倒した時
-  @EventHandler def onKill(event: EntityDeathEvent): Unit = {
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  def onKill(event: EntityDeathEvent): Unit = {
     val messages = MebiusMessages.onMonsterKill
 
     // プレイヤーがモンスターを倒した場合以外は除外
@@ -90,28 +79,30 @@ class MebiusInteractionResponder extends Listener {
     val killerPlayer = killedMonster.getKiller
     if (killerPlayer == null) return
 
-    if (!isEquip(killerPlayer)) return
-
     //もしモンスター名が取れなければ除外
     val killedMonsterName = killedMonster.getName
     if (killedMonsterName == "") return
 
+    val mebiusProperty =
+      ItemStackMebiusCodec
+        .decodeMebiusProperty(killerPlayer.getInventory.getHelmet)
+        .getOrElse(return)
+
     SeichiAssist.playermap(killerPlayer.getUniqueId).mebius
-      .speak(getMessage(messages, getNickname(killerPlayer).get, killedMonsterName))
+      .speak(getMessage(messages, mebiusProperty.ownerNickname, killedMonsterName))
   }
 
-  /**
-   * ブロックを破壊した時
-   * 保護と重力値に問題無く、ブロックタイプがmateriallistに登録されていたらメッセージを送る。
-   */
-  @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = false)
   def sendMebiusMessageOn(event: BlockBreakEvent): Unit = {
     if (!MaterialSets.materials.contains(event.getBlock.getType)) return
 
     val player = event.getPlayer
-    if (isEquip(player)) {
-      val message = getMessage(MebiusMessages.onBlockBreak, getNickname(player).get, "")
-      SeichiAssist.playermap(player.getUniqueId).mebius.speak(message)
-    }
+
+    val mebiusProperty = ItemStackMebiusCodec
+      .decodeMebiusProperty(player.getInventory.getHelmet)
+      .getOrElse(return)
+
+    SeichiAssist.playermap(player.getUniqueId).mebius
+      .speak(getMessage(MebiusMessages.onBlockBreak, mebiusProperty.ownerNickname, ""))
   }
 }
