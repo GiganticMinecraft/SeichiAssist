f1b07d0f009b5d3b7b2156479401dad35a01e120
Kory <6561358+kory33@users.noreply.github.com>
2020-07-24 03:04:00 +0900

    [clean] MebiusLevelUpTrialListenerを単純化する

--- src/main/scala/com/github/unchama/seichiassist/mebius/controller/listeners/MebiusLevelUpTrialListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/mebius/controller/listeners/MebiusLevelUpTrialListener.scala
@@ -5,7 +5,6 @@ import com.github.unchama.seichiassist.mebius.controller.codec.ItemStackMebiusCo
 import com.github.unchama.seichiassist.mebius.domain.PropertyModificationMessages
 import com.github.unchama.seichiassist.mebius.domain.resources.MebiusMessages
 import com.github.unchama.seichiassist.mebius.service.MebiusLevellingService
-import org.bukkit.Material
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.{EventHandler, EventPriority, Listener}
 
@@ -13,14 +12,8 @@ class MebiusLevelUpTrialListener(implicit messages: PropertyModificationMessages
   @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
   def tryMebiusLevelUpOn(event: BlockBreakEvent): Unit = {
     val player = event.getPlayer
-    val playerInventory = player.getInventory
 
-    val oldHelmet = {
-      val helmet = playerInventory.getHelmet
-      if (helmet == null || helmet.getType == Material.AIR) return else helmet
-    }
-
-    val oldMebiusProperty = ItemStackMebiusCodec.decodeMebiusProperty(oldHelmet).getOrElse(return)
+    val oldMebiusProperty = ItemStackMebiusCodec.decodeMebiusProperty(player.getInventory.getHelmet).getOrElse(return)
     val newMebiusProperty = MebiusLevellingService.attemptLevelUp(oldMebiusProperty).unsafeRunSync()
 
     if (newMebiusProperty != oldMebiusProperty) {
@@ -31,7 +24,7 @@ class MebiusLevelUpTrialListener(implicit messages: PropertyModificationMessages
       SeichiAssist.playermap(player.getUniqueId).mebius
         .speakForce(MebiusMessages.talkOnLevelUp(newMebiusProperty.level).mebiusMessage)
 
-      playerInventory.setHelmet {
+      player.getInventory.setHelmet {
         ItemStackMebiusCodec.materialize(newMebiusProperty, damageValue = 0)
       }
     }
