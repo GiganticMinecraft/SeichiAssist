598cd977db58c429b84b7bc4ceff038feb0b9ee1
Kory <6561358+kory33@users.noreply.github.com>
2020-07-25 02:39:22 +0900

    [update] JoinToQuitPlayerDataRepositoryを追加する

--- src/main/scala/com/github/unchama/playerdatarepository/PreLoginToQuitPlayerDataRepository.scala
+++ src/main/scala/com/github/unchama/playerdatarepository/PreLoginToQuitPlayerDataRepository.scala
@@ -35,21 +35,20 @@ abstract class PreLoginToQuitPlayerDataRepository[R] extends PlayerDataRepositor
    * この計算は必ず同期的に実行される。
    * 何故なら、プレーヤーのjoin処理が終了した時点で
    * このリポジトリはそのプレーヤーに関する[[R]]を格納している必要があるからである。
+   *
+   * [[Player]] を使って初期化する必要があるケースでは [[PreLoginToQuitPlayerDataRepository]] の使用を検討せよ。
    */
-  val loadData: (String, UUID) => SyncIO[Either[Option[String], R]]
+  protected val loadData: (String, UUID) => SyncIO[Either[Option[String], R]]
 
   /**
    * プレーヤーが退出したときに、格納されたデータをもとに終了処理を行う。
    */
-  val unloadData: (Player, R) => IO[Unit]
+  protected val unloadData: (Player, R) => IO[Unit]
 
-  /**
-   * ログイン中の [[Player]] に対して関連付けられた [[R]] を取得する。
-   */
   final def apply(player: Player): R = state(player.getUniqueId)
 
   @EventHandler(priority = EventPriority.LOWEST)
-  final def onPlayerJoin(event: AsyncPlayerPreLoginEvent): Unit = {
+  final def onPlayerPreLogin(event: AsyncPlayerPreLoginEvent): Unit = {
     loadData(event.getName, event.getUniqueId).unsafeRunSync() match {
       case Left(errorMessageOption) =>
         errorMessageOption.foreach(event.setKickMessage)
@@ -63,8 +62,6 @@ abstract class PreLoginToQuitPlayerDataRepository[R] extends PlayerDataRepositor
   final def onPlayerLeave(event: PlayerQuitEvent): Unit = {
     val player = event.getPlayer
     val uuid = player.getUniqueId
-
-    // safe because player has been online
     val storedValue = state.remove(uuid).get
 
     unloadData(player, storedValue).unsafeRunAsync {
@@ -72,4 +69,5 @@ abstract class PreLoginToQuitPlayerDataRepository[R] extends PlayerDataRepositor
       case Right(_) => ()
     }
   }
+
 }
