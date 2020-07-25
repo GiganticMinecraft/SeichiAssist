ba76258e078e38810a88cf42dd03d6023318c690
Kory <6561358+kory33@users.noreply.github.com>
2020-07-26 02:56:05 +0900

    [temp] JdbcBackedUuidRepositoryを追加する

---
+++ src/main/scala/com/github/unchama/seichiassist/infrastructure/minecraft/JdbcBackedUuidRepository.scala
@@ -0,0 +1,53 @@
+package com.github.unchama.seichiassist.infrastructure.minecraft
+
+import java.util.UUID
+
+import cats.effect.IO
+import com.github.unchama.seichiassist.domain.minecraft.UuidRepository
+import org.slf4j.Logger
+
+import scala.collection.mutable
+
+/**
+ * SeichiAssistのDBによる解決を先ず試み、
+ * 解決できなければMojangに問い合わせるような [[UuidRepository]]。
+ *
+ * Mojangへの問い合わせを最小化したいため、結果は必ずキャッシュする。
+ */
+class JdbcBackedUuidRepository(implicit val logger: Logger) extends UuidRepository[IO] {
+
+  val loadDBCache: IO[Unit] = IO {
+    ???
+
+    isDBCacheCreated = true
+    logger.info(s"DBから${0}件の名前-UUIDペアを読み込みました。")
+  }
+  private var isDBCacheCreated = false
+
+  import cats.implicits._
+
+  private var cache: mutable.Map[String, UUID] = mutable.Map()
+
+  override def getUuid(playerName: String): IO[Option[UUID]] = {
+    for {
+      _ <- IO {
+        if (!isDBCacheCreated) loadDBCache else IO.unit
+      }.flatten
+      cachedValue <- IO {
+        cache.get(playerName)
+      }
+      result <- cachedValue match {
+        case Some(resolvedCache) => IO.pure(Option(resolvedCache))
+        case None => resolveUuidUsingMojangApi(playerName)
+      }
+    } yield result
+  }
+
+  def resolveUuidUsingMojangApi(playerName: String): IO[Option[UUID]] = {
+    logger.warn(s"Mojangへ${playerName}のUUIDを問い合わせています...")
+
+    ???
+  }
+
+}
+
