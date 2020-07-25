70fbb69f43cfd3dfcdbc30432999a0fb685ce5df
Ryosuke Kondo <6561358+kory33@users.noreply.github.com>
2020-07-25 22:39:40 +0900

    remove redundant (ChatColor).toString
    Co-authored-by: KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/listener/PlayerJoinListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/listener/PlayerJoinListener.scala
@@ -102,8 +102,8 @@ class PlayerJoinListener extends Listener {
     // 初見さんへの処理
     if (!player.hasPlayedBefore) {
       //初見さんであることを全体告知
-      Util.sendEveryMessage(s"${LIGHT_PURPLE.toString}$BOLD${player.getName}さんはこのサーバーに初めてログインしました！")
-      Util.sendEveryMessage(s"${WHITE.toString}webサイトはもう読みましたか？→$YELLOW${UNDERLINE}https://www.seichi.network/gigantic")
+      Util.sendEveryMessage(s"${LIGHT_PURPLE}$BOLD${player.getName}さんはこのサーバーに初めてログインしました！")
+      Util.sendEveryMessage(s"${WHITE}webサイトはもう読みましたか？→$YELLOW${UNDERLINE}https://www.seichi.network/gigantic")
       Util.sendEverySound(Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f)
       //初見プレイヤーに木の棒、エリトラ、ピッケルを配布
       player.getInventory.addItem(new ItemStack(Material.STICK))
