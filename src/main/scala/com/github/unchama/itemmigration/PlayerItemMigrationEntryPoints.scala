e42f77fb66d3ec492d244b96cfd49c4d9d5e0c64
Kory <6561358+kory33@users.noreply.github.com>
2020-08-05 21:19:59 +0900

    [clean] unused import

--- src/main/scala/com/github/unchama/itemmigration/PlayerItemMigrationEntryPoints.scala
+++ src/main/scala/com/github/unchama/itemmigration/PlayerItemMigrationEntryPoints.scala
@@ -2,7 +2,7 @@ package com.github.unchama.itemmigration
 
 import cats.effect.{Concurrent, IO}
 import com.github.unchama.itemmigration.controllers.player.{PlayerItemMigrationController, PlayerItemMigrationStateRepository}
-import com.github.unchama.itemmigration.domain.{ItemMigrationVersionRepository, ItemMigrations}
+import com.github.unchama.itemmigration.domain.ItemMigrations
 import com.github.unchama.itemmigration.service.ItemMigrationService
 import com.github.unchama.itemmigration.targets.PlayerInventoriesData
 
