57fd3e94a74f26421df2f1f8576d39c34e6852e6
Kory <6561358+kory33@users.noreply.github.com>
2020-08-19 14:40:10 +0900

    [fix] forward referenceを修正する

--- src/main/scala/com/github/unchama/buildassist/domain/explevel/BuildExpAmount.scala
+++ src/main/scala/com/github/unchama/buildassist/domain/explevel/BuildExpAmount.scala
@@ -4,13 +4,15 @@ import com.github.unchama.seichiassist.util.typeclass.HasMinimum
 
 case class BuildExpAmount private(amount: BigDecimal) extends AnyVal
 
-private[explevel] abstract class SeichiExpAmountInstances {
-  implicit val ordering: Ordering[BuildExpAmount] = Ordering.by(_.amount)
+private[explevel] abstract class BuildExpAmountInstances {
+  implicit lazy val ordering: Ordering[BuildExpAmount] = Ordering.by(_.amount)
 
-  implicit val hasMinimum: HasMinimum[BuildExpAmount] = HasMinimum.as(BuildExpAmount.ofNonNegative(0))
+  implicit lazy val hasMinimum: HasMinimum[BuildExpAmount] = {
+    HasMinimum.as(BuildExpAmount.ofNonNegative(0))
+  }
 }
 
-object BuildExpAmount extends SeichiExpAmountInstances {
+object BuildExpAmount extends BuildExpAmountInstances {
   def ofNonNegative(amount: BigDecimal): BuildExpAmount = {
     require(amount >= 0)
     BuildExpAmount(amount)
