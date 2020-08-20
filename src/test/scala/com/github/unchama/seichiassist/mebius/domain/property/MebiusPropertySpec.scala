c53ac59fca3112150ee35b63b378966e6b5eac60
Kory <6561358+kory33@users.noreply.github.com>
2020-08-21 02:48:59 +0900

    [test update] MebiusPropertySpecにテストを追加する

--- src/test/scala/com/github/unchama/seichiassist/mebius/domain/property/MebiusPropertySpec.scala
+++ src/test/scala/com/github/unchama/seichiassist/mebius/domain/property/MebiusPropertySpec.scala
@@ -2,17 +2,34 @@ package com.github.unchama.seichiassist.mebius.domain.property
 
 import java.util.UUID
 
+import cats.Monad
+import cats.effect.IO
 import org.scalatest.wordspec.AnyWordSpec
 
 class MebiusPropertySpec extends AnyWordSpec {
-  "initial mebius property" should {
-    "be valid" in {
-      val testPlayerName = "testPlayer"
-      val testPlayerUuid = UUID.randomUUID().toString
+  "Initial mebius property" should {
+    val testPlayerName = "testPlayer"
+    val testPlayerUuid = UUID.randomUUID().toString
 
+    "be valid" in {
       // exception thrown if invalid
       MebiusProperty.initialProperty(testPlayerName, testPlayerUuid) equals
         MebiusProperty.initialProperty(testPlayerName, testPlayerUuid)
+    }
+
+    "be able to be upgraded all the way to the maximum level" in {
+      val upgradedToMaximum = {
+        Monad[IO]
+          .iterateWhileM {
+            MebiusProperty.initialProperty(testPlayerName, testPlayerUuid)
+          } {
+            _.incrementLevel.randomlyUpgradeEnchantment(MebiusEnchantment.values.toSet)
+          } {
+            !_.level.isMaximum
+          }.unsafeRunSync()
+      }
+
+      assert(upgradedToMaximum.level.isMaximum)
     }
   }
 }
