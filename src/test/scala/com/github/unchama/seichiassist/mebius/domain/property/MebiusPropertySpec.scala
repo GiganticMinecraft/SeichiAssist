9b4ecba70aefa3337208b28915efc9cab60fca21
Kory <6561358+kory33@users.noreply.github.com>
2020-08-22 03:41:34 +0900

    [update] MebiusPropertySpecにテストをさらに追加する

--- src/test/scala/com/github/unchama/seichiassist/mebius/domain/property/MebiusPropertySpec.scala
+++ src/test/scala/com/github/unchama/seichiassist/mebius/domain/property/MebiusPropertySpec.scala
@@ -7,10 +7,10 @@ import cats.effect.IO
 import org.scalatest.wordspec.AnyWordSpec
 
 class MebiusPropertySpec extends AnyWordSpec {
-  "Initial mebius property" should {
-    val testPlayerName = "testPlayer"
-    val testPlayerUuid = UUID.randomUUID().toString
+  val testPlayerName: String = "testPlayer"
+  val testPlayerUuid: String = UUID.randomUUID().toString
 
+  "Initial mebius property" should {
     "be valid" in {
       // exception thrown if invalid
       MebiusProperty.initialProperty(testPlayerName, testPlayerUuid) equals
@@ -27,6 +27,48 @@ class MebiusPropertySpec extends AnyWordSpec {
       }
 
       assert(upgradedToMaximum.level.isMaximum)
+    }
+  }
+
+  "Property with the largest level" should {
+    "always contain Unbreakable enchantment" in {
+      val upgradedToMaximum = {
+        val initialProperty = MebiusProperty.initialProperty(testPlayerName, testPlayerUuid)
+
+        Monad[IO]
+          .iterateWhileM(initialProperty)(_.upgradeByOneLevel)(!_.level.isMaximum)
+          .unsafeRunSync()
+      }
+
+      assert(upgradedToMaximum.enchantmentLevels.of(MebiusEnchantment.Unbreakable) == 1)
+    }
+  }
+
+  "Levelling up" should {
+    "upgrade exactly one enchantment at a time" in {
+      val testIterationCount = 1000
+
+      (1 to testIterationCount).foreach { _ =>
+        val initialProperty = MebiusProperty.initialProperty(testPlayerName, testPlayerUuid)
+
+        import cats.implicits._
+
+        Monad[IO]
+          .iterateWhileM(initialProperty) { property =>
+            property.upgradeByOneLevel.flatTap { upgraded =>
+              IO {
+                val oldLevels = property.enchantmentLevels
+                val newLevels = upgraded.enchantmentLevels
+
+                val diff = newLevels.differenceFrom(oldLevels)
+
+                assert(diff.size == 1)
+                assert(newLevels.of(diff.head) == oldLevels.of(diff.head) + 1)
+              }
+            }
+          }(!_.level.isMaximum)
+          .unsafeRunSync()
+      }
     }
   }
 }
