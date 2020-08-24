1c6ee8e5286daa3e9d68193256dc04f6b7e9e202
Kory <6561358+kory33@users.noreply.github.com>
2020-08-24 22:34:13 +0900

    [update] EntityUtil.spawnにて実行時ダウンキャストを行うようにする

--- src/main/scala/com/github/unchama/util/bukkit/EntityUtil.scala
+++ src/main/scala/com/github/unchama/util/bukkit/EntityUtil.scala
@@ -8,12 +8,28 @@ import scala.reflect.ClassTag
 
 object EntityUtil {
 
-  def spawn[F[_] : Sync, EntityType <: Entity : ClassTag](location: Location): F[EntityType] = {
-    val entityClass = implicitly[ClassTag[EntityType]].runtimeClass.asSubclass(classOf[Entity])
+  def spawn[F[_], EntityType <: Entity : ClassTag](location: Location)
+                                                  (implicit F: Sync[F]): F[EntityType] = {
+    val classTag = implicitly[ClassTag[EntityType]]
+    val entityClass = classTag.runtimeClass.asSubclass(classOf[Entity])
 
-    Sync[F].delay {
-      location.getWorld.spawn(location, entityClass)
-    }
+    import cats.implicits._
+
+    for {
+      entity <- F.delay {
+        location.getWorld.spawn(location, entityClass)
+      }
+      refinedEntity <- entity match {
+        case entity: EntityType =>
+          F.pure(entity)
+        case _ =>
+          F.delay {
+            entity.remove()
+          } >> F.raiseError(
+            new ClassCastException(s"Spawned entity is not an instance of $classTag")
+          )
+      }
+    } yield refinedEntity
   }
 
 }
