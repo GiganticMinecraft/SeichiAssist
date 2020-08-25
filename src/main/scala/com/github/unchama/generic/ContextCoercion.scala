832e8d9c985c67a4355bec11f925040c86476c94
Kory <6561358+kory33@users.noreply.github.com>
2020-08-25 21:55:52 +0900

    [update] ContextCoercionを追加する

---
+++ src/main/scala/com/github/unchama/generic/ContextCoercion.scala
@@ -0,0 +1,24 @@
+package com.github.unchama.generic
+
+import cats.~>
+
+/**
+ * 文脈FからGへの(自明な)変換を与える型クラス。
+ *
+ * [[cats.arrow.FunctionK]] と同じだが、より自明な、
+ * 例えば [[cats.effect.SyncIO]] から [[cats.effect.IO]] のような変換を与えるオブジェクトとして機能する。
+ */
+trait ContextCoercion[F[_], G[_]] {
+  def apply[A](fa: F[A]): G[A]
+}
+
+object ContextCoercion {
+
+  def apply[F[_], G[_], A](fa: F[A])(implicit coercion: ContextCoercion[F, G]): G[A] = coercion(fa)
+
+  def fromFunctionK[F[_], G[_]](functionK: F ~> G): ContextCoercion[F, G] = {
+    new ContextCoercion[F, G] {
+      def apply[A](fa: F[A]): G[A] = functionK(fa)
+    }
+  }
+}
