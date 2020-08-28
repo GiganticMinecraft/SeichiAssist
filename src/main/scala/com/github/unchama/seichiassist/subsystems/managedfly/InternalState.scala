ef3f1b82aa49eeae4bd70659960444c4dfc35933
Kory <6561358+kory33@users.noreply.github.com>
2020-08-28 17:13:37 +0900

    [update] InternalStateにReadOnlyRefを使うようにする

--- src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/InternalState.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/InternalState.scala
@@ -1,7 +1,7 @@
 package com.github.unchama.seichiassist.subsystems.managedfly
 
-import cats.effect.concurrent.Ref
+import com.github.unchama.concurrent.ReadOnlyRef
 import com.github.unchama.playerdatarepository.PlayerDataRepository
 import com.github.unchama.seichiassist.subsystems.managedfly.domain.RemainingFlyDuration
 
-case class InternalState[F[_]](playerFlyDurations: PlayerDataRepository[Ref[F, Option[RemainingFlyDuration]]])
+case class InternalState[F[_]](playerFlyDurations: PlayerDataRepository[ReadOnlyRef[F, Option[RemainingFlyDuration]]])
