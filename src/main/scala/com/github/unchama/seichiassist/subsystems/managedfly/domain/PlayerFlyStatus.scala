b6d0befffa0480591a6e1a8080b1a651d168e5d9
Kory <6561358+kory33@users.noreply.github.com>
2020-08-29 00:48:57 +0900

    [update] PlayerFlyStatusにtickOneMinuteを露出する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/domain/PlayerFlyStatus.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/domain/PlayerFlyStatus.scala
@@ -1,7 +1,18 @@
 package com.github.unchama.seichiassist.subsystems.managedfly.domain
 
-sealed trait PlayerFlyStatus
+sealed trait PlayerFlyStatus {
+  val tickOneMinute: PlayerFlyStatus
+}
 
-case class Flying(remainingDuration: RemainingFlyDuration) extends PlayerFlyStatus
+case class Flying(remainingDuration: RemainingFlyDuration) extends PlayerFlyStatus {
+  override lazy val tickOneMinute: PlayerFlyStatus = {
+    remainingDuration.tickOneMinute match {
+      case Some(value) => Flying(value)
+      case None => NotFlying
+    }
+  }
+}
 
-case object NotFlying extends PlayerFlyStatus
+case object NotFlying extends PlayerFlyStatus {
+  override val tickOneMinute: PlayerFlyStatus = this
+}
