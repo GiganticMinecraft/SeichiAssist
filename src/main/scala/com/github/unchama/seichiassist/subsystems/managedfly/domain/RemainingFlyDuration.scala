ab6b4034a347c4c83d95ecbcd2e501f69184fe76
Kory <6561358+kory33@users.noreply.github.com>
2020-08-28 17:28:05 +0900

    [update] RemainingFlyDurationを最適化する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/domain/RemainingFlyDuration.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/domain/RemainingFlyDuration.scala
@@ -4,20 +4,25 @@ import com.github.unchama.seichiassist.subsystems.managedfly.domain.RemainingFly
 
 sealed trait RemainingFlyDuration {
 
-  def tickOneMinute: Option[RemainingFlyDuration]
+  /**
+   * 「1分」減らせるなら減らしたものを、無理ならNoneを返す
+   *
+   * @return
+   */
+  val tickOneMinute: Option[RemainingFlyDuration]
 
-  def hasExpired: Boolean = tickOneMinute.isEmpty
+  lazy val hasExpired: Boolean = tickOneMinute.isEmpty
 
 }
 
 object RemainingFlyDuration {
 
   case object Infinity extends RemainingFlyDuration {
-    override def tickOneMinute: Option[RemainingFlyDuration] = Some(this)
+    override val tickOneMinute: Option[RemainingFlyDuration] = Some(this)
   }
 
   case class Minutes private(value: Int) extends RemainingFlyDuration {
-    override def tickOneMinute: Option[RemainingFlyDuration] = {
+    override lazy val tickOneMinute: Option[RemainingFlyDuration] = {
       value match {
         case 0 => None
         case _ => Some(fromNonNegative(value - 1))
