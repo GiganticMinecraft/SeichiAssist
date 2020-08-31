4dec05b31981db95e9f141fbc2e7a526a98c59fc
Kory <6561358+kory33@users.noreply.github.com>
2020-09-01 05:21:42 +0900

    [update] FlyStatusの同期化機構を実装する

--- src/main/scala/com/github/unchama/concurrent/MinecraftServerThreadShift.scala
+++ src/main/scala/com/github/unchama/concurrent/MinecraftServerThreadShift.scala
@@ -6,3 +6,9 @@ import cats.effect.ContextShift
  * マインクラフトサーバーが走るスレッドへの `ContextShift`
  */
 trait MinecraftServerThreadShift[F[_]] extends ContextShift[F]
+
+object MinecraftServerThreadShift {
+
+  def apply[F[_]](implicit ev: MinecraftServerThreadShift[F]): MinecraftServerThreadShift[F] = ev
+
+}
