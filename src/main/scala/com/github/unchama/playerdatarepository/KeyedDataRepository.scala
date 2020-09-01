f0843463bdc06bb6c8bdc52c0708635b3ab5c559
Kory <6561358+kory33@users.noreply.github.com>
2020-09-01 12:23:23 +0900

    [update] KeyedDataRepositoryまで抽象化する

---
+++ src/main/scala/com/github/unchama/playerdatarepository/KeyedDataRepository.scala
@@ -0,0 +1,12 @@
+package com.github.unchama.playerdatarepository
+
+/**
+ * `K` => `V` の関数オブジェクトのうち、リポジトリとして考えることができるオブジェクトのtrait。
+ *
+ * 主に依存性注入での使用を想定している。
+ */
+trait KeyedDataRepository[K, V] extends (K => V) {
+
+  override def apply(v1: K): V
+
+}
--- src/main/scala/com/github/unchama/playerdatarepository/PlayerDataRepository.scala
+++ src/main/scala/com/github/unchama/playerdatarepository/PlayerDataRepository.scala
@@ -1,10 +1,9 @@
 package com.github.unchama.playerdatarepository
 
-import cats.Functor
 import org.bukkit.entity.Player
 
 /**
- * ログイン中の[[Player]]に対して必ず `R` を返せるようなリポジトリのSAM trait.
+ * ログイン中の[[Player]]に対して必ず `R` を返せるようなリポジトリ。
  *
  * どの期間についてプレーヤーに対して値を返せるかどうかは実装に依存するが、
  * 遅くても `PlayerJoinEvent` の `EventPriority.HIGH` から、
@@ -14,22 +13,8 @@ import org.bukkit.entity.Player
  *
  * @tparam R レポジトリが [[Player]] に関連付ける値の型
  */
-trait PlayerDataRepository[R] {
+trait PlayerDataRepository[R] extends KeyedDataRepository[Player, R] {
 
-  def apply(player: Player): R
-
-}
-
-private[playerdatarepository] abstract class PlayerDataRepositoryInstances {
-
-  implicit val playerDataRepositoryFunctor: Functor[PlayerDataRepository] = {
-    new Functor[PlayerDataRepository] {
-      override def map[A, B](fa: PlayerDataRepository[A])(f: A => B): PlayerDataRepository[B] = {
-        (player: Player) => f(fa.apply(player))
-      }
-    }
-  }
+  override def apply(player: Player): R
 
 }
-
-object PlayerDataRepository extends PlayerDataRepositoryInstances
