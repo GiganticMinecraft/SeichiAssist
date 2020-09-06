e0861b1f399a1dc857067842c95a868276303958
Kory <6561358+kory33@users.noreply.github.com>
2020-09-07 06:49:27 +0900

    [update] ManagedFlyシステムの設定オブジェクトを定義する

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/application/SystemConfiguration.scala
@@ -0,0 +1,7 @@
+package com.github.unchama.seichiassist.subsystems.managedfly.application
+
+case class SystemConfiguration(expConsumptionAmount: Int) {
+  require {
+    expConsumptionAmount > 0
+  }
+}
