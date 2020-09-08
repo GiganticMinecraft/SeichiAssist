a262a5b4b7c82924b4046c4599b059a7eb74a6f2
Kory <6561358+kory33@users.noreply.github.com>
2020-09-08 19:02:23 +0900

    [update] RemainingFlyDurationSpecをもう少し厳しいテストにする

--- src/test/scala/com/github/unchama/seichiassist/subsystems/managedfly/domain/RemainingFlyDurationSpec.scala
+++ src/test/scala/com/github/unchama/seichiassist/subsystems/managedfly/domain/RemainingFlyDurationSpec.scala
@@ -1,34 +1,28 @@
 package com.github.unchama.seichiassist.subsystems.managedfly.domain
 
-import org.scalacheck.Gen
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-import scala.annotation.tailrec
-
 class RemainingFlyDurationSpec extends AnyWordSpec with ScalaCheckPropertyChecks with Matchers {
-  @tailrec
-  private def repeat[A](f: A => A, n: Int)(a: A): A = {
-    n match {
-      case _ if n < 1 => a
-      case _ => repeat(f, n - 1)(f(a))
+  "RemainingFlyDuration#tickOneMinute" should {
+    "decrement remaining minute if greater than 1" in {
+      forAll { minute: Int =>
+        whenever(minute > 1) {
+          RemainingFlyDuration.PositiveMinutes.fromPositive(minute).tickOneMinute shouldBe Some {
+            RemainingFlyDuration.PositiveMinutes.fromPositive(minute - 1)
+          }
+        }
+      }
     }
-  }
 
-  "RemainingFlyDuration" should {
-    "tick to None in exactly the given minute amount" in {
-      val testMinuteLimit = 1000000
+    "tick to None if only a minute remains" in {
+      RemainingFlyDuration.PositiveMinutes.fromPositive(1).tickOneMinute
+    }
 
-      forAll(Gen.choose(1, testMinuteLimit)) { minute: Int =>
-        val tickOneMinute: Option[RemainingFlyDuration] => Option[RemainingFlyDuration] = _.flatMap(_.tickOneMinute)
-        val duration: Option[RemainingFlyDuration] = Some(RemainingFlyDuration.PositiveMinutes.fromPositive(minute))
-
-        val secondLast = repeat(tickOneMinute, minute - 1)(duration)
-        val last = tickOneMinute(secondLast)
-
-        secondLast shouldBe Some(RemainingFlyDuration.PositiveMinutes.fromPositive(1))
-        last shouldBe None
+    "fix Infinity" in {
+      RemainingFlyDuration.Infinity.tickOneMinute shouldBe Some {
+        RemainingFlyDuration.Infinity
       }
     }
   }
