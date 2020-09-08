dc8f4865dce2aaa06552a0387428bfd1709ed0d7
Kory <6561358+kory33@users.noreply.github.com>
2020-09-09 01:29:13 +0900

    [update] 状態操作を提供する型クラスを定義する

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/managedfly/application/PlayerFlyStatusManipulation.scala
@@ -0,0 +1,38 @@
+package com.github.unchama.seichiassist.subsystems.managedfly.application
+
+import com.github.unchama.seichiassist.subsystems.managedfly.domain.PlayerFlyStatus
+import simulacrum.typeclass
+
+/**
+ * プレーヤーの飛行状態に`F`の文脈で干渉する手段を与える型クラスインスタンスのtrait。
+ *
+ * `F` は `Kleisli[G, Player, *]` の形をしていることを想定している。
+ */
+@typeclass trait PlayerFlyStatusManipulation[F[_]] extends AnyRef {
+  /**
+   * 飛行に必要な経験値をプレーヤーが持っていることを保証するアクション。
+   * このアクションは [[PlayerExpNotEnough]] を `raiseError` してよい。
+   */
+  val ensurePlayerExp: F[Unit]
+
+  /**
+   * 飛行に必要な経験値をプレーヤーに消費させるアクション。
+   * このアクションは [[PlayerExpNotEnough]] を `raiseError` してよい。
+   */
+  val consumePlayerExp: F[Unit]
+
+  /**
+   * プレーヤーがアイドル状態であるかを判定するアクション。
+   */
+  val isPlayerIdle: F[Boolean]
+
+  /**
+   * プレーヤーの飛行状態を[[PlayerFlyStatus]]に基づいてセットするアクション。
+   */
+  val synchronizeFlyStatus: PlayerFlyStatus => F[Unit]
+
+  /**
+   * [[InternalInterruption]] に対応して、プレーヤーへセッションが終了することを通知するアクション。
+   */
+  val sendNotificationsOnInterruption: InternalInterruption => F[Unit]
+}
