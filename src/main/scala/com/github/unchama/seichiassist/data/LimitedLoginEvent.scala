c897b53c796d3483e6a01b8b3e5e15ce8f9aa1d4
Sasaki Touma <sasakitouma@yahoo.ne.jp>
2020-09-10 17:28:48 +0900

    LimitedLoginEvent.java -> LimitedLoginEvent.scala
    LimitedLoginEvent.javaをScalaに移行

---
+++ src/main/scala/com/github/unchama/seichiassist/data/LimitedLoginEvent.scala
@@ -0,0 +1,94 @@
+package com.github.unchama.seichiassist.data
+
+import com.github.unchama.seichiassist.Config
+import com.github.unchama.seichiassist.SeichiAssist
+import com.github.unchama.seichiassist.data.player.PlayerData
+import com.github.unchama.seichiassist.util.Util
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import scala.collection.mutable
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.UUID
+
+
+object LimitedLoginEvent {
+  private val config = SeichiAssist.seichiAssistConfig
+}
+
+class LimitedLoginEvent {
+  final private val playermap = SeichiAssist.playermap
+  private var lastcheckdate: String = null
+
+  def setLastCheckDate(s: String): Unit = lastcheckdate = s
+
+  //ここで処理対象のユーザーと、そのtitleNoを拾って処理を行う。
+  def TryGetItem(p: Player): Unit = {
+    val uuid = p.getUniqueId
+    val playerdata: PlayerData = playermap.getOrElse(uuid, null)
+    val skull = Util.getskull(p.getName)
+    val cal = Calendar.getInstance
+    val sdf = new SimpleDateFormat("yyyy/MM/dd")
+    if (lastcheckdate == null) return
+    if (lastcheckdate.isEmpty) return
+    try {
+      val TodayDate = sdf.parse(sdf.format(cal.getTime))
+      val LastDate = sdf.parse(lastcheckdate)
+      val LLEStart = sdf.parse(LimitedLoginEvent.config.getLimitedLoginEventStart)
+      val LLEEnd = sdf.parse(LimitedLoginEvent.config.getLimitedLoginEventEnd)
+      val TodayLong = TodayDate.getTime
+      val LastLong = LastDate.getTime
+      val LLEStartLong = LLEStart.getTime
+      val LLEEndLong = LLEEnd.getTime
+      var loginDays = playerdata.LimitedLoginCount
+      var configDays = 0
+      var internalItemId = 0
+      var amount = 0
+      //開催期間内かどうか
+      val today2start = (TodayLong - LLEStartLong) / (1000 * 60 * 60 * 24)
+      val today2end = (TodayLong - LLEEndLong) / (1000 * 60 * 60 * 24)
+      val last2start = (LastLong - LLEStartLong) / (1000 * 60 * 60 * 24)
+      val last2end = (LastLong - LLEEndLong) / (1000 * 60 * 60 * 24)
+      if ((today2start >= 0) && (today2end <= 0)) { //最終ログインが開催期間内だったか
+        if (!((last2start >= 0) && (last2end <= 0))) { //開催期間内初のログイン時、開催終了後初のログイン時にここを処理
+          //期間限定の累計ログイン数のデータをリセットする。
+          loginDays = 0
+        }
+        loginDays += 1
+        configDays = 0
+        do {
+          internalItemId = LimitedLoginEvent.config.getLimitedLoginEventItem(configDays)
+          amount = LimitedLoginEvent.config.getLimitedLoginEventAmount(configDays)
+          internalItemId match {
+            case 1 => //配布対象「ガチャ券」
+
+              var message: String = null
+              if (configDays == 0) message = "限定ログボ！"
+              else message = "限定ログボ" + loginDays + "日目記念！"
+              p.sendMessage("【" + message + "】" + amount + "個のガチャ券をプレゼント！")
+              for (i <- 1 to amount) {
+                if (p.getInventory.contains(skull) || !Util.isPlayerInventoryFull(p)) Util.addItem(p, skull)
+                else Util.dropItem(p, skull)
+              }
+
+            case 2 => //配布対象「未定」
+
+            //配布処理記入場所
+            //今後の追加のためのサンプルです。
+
+          }
+          configDays += loginDays
+        } while ( {
+          configDays == loginDays
+        })
+      }
+      playerdata.LimitedLoginCount_$eq(loginDays)
+    } catch {
+      case e: ParseException =>
+        e.printStackTrace
+    }
+  }
+}
