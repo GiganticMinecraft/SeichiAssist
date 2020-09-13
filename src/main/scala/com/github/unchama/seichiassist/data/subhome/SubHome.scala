f990ce3292da8c00855b8b0593e916d79cef4848
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-09-13 14:33:13 +0900

    Update src/main/scala/com/github/unchama/seichiassist/data/subhome/SubHome.scala
    Co-authored-by: KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/data/subhome/SubHome.scala
+++ src/main/scala/com/github/unchama/seichiassist/data/subhome/SubHome.scala
@@ -8,7 +8,6 @@ import org.jetbrains.annotations.Nullable
 /**
  * サブホームオブジェクトのクラス
  */
-// TODO Scalaize and make this case class
 class SubHome(val location: Location, val name: String) {
   def getLocation: Location = location.clone // BukkitのLocationはミュータブルなのでコピーして返す必要がある
 }
