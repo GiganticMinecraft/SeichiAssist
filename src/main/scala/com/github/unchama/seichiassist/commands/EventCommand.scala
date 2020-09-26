9a79f0ee53223b4bf3a973cdd5e7ca320c89053f
Lucky3028 <lucky3028.0@gmail.com>
2020-09-26 22:17:37 +0900

    [Add] ハロウィンイベントの限定ポーションをコマンドで入手できるように

--- src/main/scala/com/github/unchama/seichiassist/commands/EventCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/commands/EventCommand.scala
@@ -2,6 +2,7 @@ package com.github.unchama.seichiassist.commands
 
 import cats.effect.IO
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
+import com.github.unchama.seichiassist.data.HalloweenItemData
 import com.github.unchama.seichiassist.listener.new_year_event.{NewYearBagListener, NewYearItemListener}
 import com.github.unchama.seichiassist.util.Util
 import com.github.unchama.targetedeffect.TargetedEffect._
@@ -11,17 +12,23 @@ import org.bukkit.entity.Player
 object EventCommand {
   import com.github.unchama.targetedeffect._
 
-  val grantEffect: TargetedEffect[Player] =
+  val newYearGrantEffect: TargetedEffect[Player] =
     Util.grantItemStacksEffect(
       NewYearBagListener.getNewYearBag,
       NewYearItemListener.getNewYearApple
     )
 
+  val halloweenGrantEffect: TargetedEffect[Player] =
+    Util.grantItemStacksEffect(
+      HalloweenItemData.getHalloweenPotion
+    )
+
   val executor: TabExecutor = playerCommandBuilder
     .execution { context =>
       val effect = context.args.yetToBeParsed match {
-        case "get" :: _ => emptyEffect
-        case _ => grantEffect
+        case "newyear" :: _ => newYearGrantEffect
+        case "halloween" :: _ => halloweenGrantEffect
+        case _ => emptyEffect
       }
 
       IO.pure(effect)
