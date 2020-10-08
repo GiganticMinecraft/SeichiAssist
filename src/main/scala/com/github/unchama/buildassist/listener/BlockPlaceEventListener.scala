18a36ece08089132e839b1db99a0535a785a99b7
karayuumac <karaatena0706@gmail.com>
2020-10-08 21:35:44 +0900

    [update] インライン化

--- src/main/scala/com/github/unchama/buildassist/listener/BlockPlaceEventListener.scala
+++ src/main/scala/com/github/unchama/buildassist/listener/BlockPlaceEventListener.scala
@@ -1,21 +1,16 @@
 package com.github.unchama.buildassist.listener
 
 import java.math.BigDecimal
-import java.util.UUID
 
-import com.github.unchama.buildassist.{BuildAssist, PlayerData, Util}
+import com.github.unchama.buildassist.{BuildAssist, Util}
 import com.github.unchama.seichiassist.ManagedWorld._
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.BlockPlaceEvent
-
-import scala.collection.mutable
 
 /**
  * Created by karayuu on 2020/10/07
  */
 class BlockPlaceEventListener extends TypedEventListener[BlockPlaceEvent] {
-  private val playermap: mutable.HashMap[UUID, PlayerData] = BuildAssist.playermap
-
   @EventHandler
   override def onEvent(event: BlockPlaceEvent): Unit = {
     val player = event.getPlayer
@@ -25,7 +20,7 @@ class BlockPlaceEventListener extends TypedEventListener[BlockPlaceEvent] {
     if (event.isCancelled) return
 
     val uuid = player.getUniqueId
-    val playerdata = playermap.getOrElse(uuid, () => null)
+    val playerdata = BuildAssist.playermap.getOrElse(uuid, () => null)
 
     //プレイヤーデータが無い場合は処理終了
     if (playerdata == null) return
