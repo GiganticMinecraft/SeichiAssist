e8611bed2703cdaf62328d30ac1ebdf6458d4a05
karayuumac <y-kara0706@docomo.ne.jp>
2020-10-12 20:31:33 +0900

    [update] IOを使わずにそのまま記載するよう変更

--- src/main/scala/com/github/unchama/seichiassist/subsystems/rescueplayer/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/rescueplayer/System.scala
@@ -1,11 +1,10 @@
 package com.github.unchama.seichiassist.subsystems.rescueplayer
 
-import com.github.unchama.generic.effect.unsafe.EffectEnvironment
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 import com.github.unchama.seichiassist.subsystems.rescueplayer.bukkit.listeners.RescuePlayerListener
 
 object System {
-  def wired(implicit effectEnvironment: EffectEnvironment): Subsystem = {
+  def wired: Subsystem = {
     val listeners = Seq(
       new RescuePlayerListener()
     )
--- src/main/scala/com/github/unchama/seichiassist/subsystems/rescueplayer/bukkit/listeners/RescuePlayerListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/rescueplayer/bukkit/listeners/RescuePlayerListener.scala
@@ -1,40 +1,28 @@
 package com.github.unchama.seichiassist.subsystems.rescueplayer.bukkit.listeners
 
-import cats.effect.IO
-import com.github.unchama.generic.effect.unsafe.EffectEnvironment
 import org.bukkit.ChatColor._
 import org.bukkit.event.player.{PlayerJoinEvent, PlayerMoveEvent}
 import org.bukkit.event.{EventHandler, Listener}
 
-class RescuePlayerListener(implicit effectEnvironment: EffectEnvironment) extends Listener {
+class RescuePlayerListener extends Listener {
   @EventHandler
   def onPlayerFallenToVoid(event: PlayerMoveEvent): Unit = {
     if (event.getTo.getBlockY < 0.0) {
-      effectEnvironment.runEffectAsync(
-        "voidに落下したプレイヤーをワールドのスポーン地点にテレポートさせる",
-        IO {
-          val player = event.getPlayer
-          val worldSpawnLocation = player.getWorld.getSpawnLocation
+      val player = event.getPlayer
+      val worldSpawnLocation = player.getWorld.getSpawnLocation
 
-          player.teleport(worldSpawnLocation)
-          player.sendMessage(s"${RED}voidに落下していたため、ワールドのスポーン地点にテレポートしました。")
-        }
-      )
+      player.teleport(worldSpawnLocation)
+      player.sendMessage(s"${RED}voidに落下していたため、ワールドのスポーン地点にテレポートしました。")
     }
   }
 
   @EventHandler
   def onPlayerJoin(event: PlayerJoinEvent): Unit = {
     if (event.getPlayer.isDead) {
-      effectEnvironment.runEffectAsync(
-        "ログイン時デス状態のプレイヤーをワールドのスポーン地点にテレポートさせる",
-        IO {
-          val player = event.getPlayer
-          val worldSpawnLocation = player.getWorld.getSpawnLocation
+      val player = event.getPlayer
+      val worldSpawnLocation = player.getWorld.getSpawnLocation
 
-          player.teleport(worldSpawnLocation)
-        }
-      )
+      player.teleport(worldSpawnLocation)
     }
   }
 }
