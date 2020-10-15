7f71a4a2b5865aa39cbc37b9e4b648db911e1896
Lucky3028 <lucky3028.0@gmail.com>
2020-10-15 23:29:06 +0900

    [Add] HalloweenHoeを使用した時の処理

--- src/main/scala/com/github/unchama/seichiassist/listener/HalloweenItemListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/listener/HalloweenItemListener.scala
@@ -1,9 +1,14 @@
 package com.github.unchama.seichiassist.listener
 
-import org.bukkit.event.player.PlayerItemConsumeEvent
+import com.github.unchama.seichiassist.data.HalloweenItemData.{isHalloweenHoe, isHalloweenPotion}
+import com.github.unchama.util.external.WorldGuardWrapper.isRegionMember
+import org.bukkit.entity.Player
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.{PlayerInteractEvent, PlayerItemConsumeEvent}
 import org.bukkit.event.{EventHandler, Listener}
-import com.github.unchama.seichiassist.data.HalloweenItemData.isHalloweenPotion
+import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.potion.{PotionEffect, PotionEffectType}
+import org.bukkit.{Location, Material}
 
 class HalloweenItemListener extends Listener {
 
@@ -14,5 +19,39 @@ class HalloweenItemListener extends Listener {
       // 10分
       event.getPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 20 * 60 * 10, 0), true)
     }
+  }
+
+  @EventHandler
+  def onPlayerRightClickWithHalloweenHoe(event: PlayerInteractEvent): Unit = {
+    if (!isHalloweenHoe(event.getItem)) return
+
+    // 特殊エンチャント：
+    if (event.getHand == EquipmentSlot.OFF_HAND) return
+    if (event.getAction != Action.RIGHT_CLICK_BLOCK) return
+
+    val clickedBlock = event.getClickedBlock
+    if (clickedBlock == null) return
+    val clickedBlockLoc = clickedBlock.getLocation
+    val standardLoc = (clickedBlockLoc.getX.toInt, clickedBlockLoc.getZ.toInt)
+
+    val player = event.getPlayer
+    // まず、Playerが自分でクリックしたブロックについて判定する
+    if (!canBeReplacedWithSoil(player, clickedBlockLoc)) return
+    clickedBlock.setType(Material.SOIL)
+
+    // 次にクリックされたブロックから半径4ブロック以内のブロックについて判定する
+    for (x <- standardLoc._1 - 4 to standardLoc._1 + 4) {
+      for (z <- standardLoc._2 - 4 to standardLoc._2 + 4) {
+        val loc = new Location(clickedBlockLoc.getWorld, x.toDouble, clickedBlockLoc.getY, z.toDouble)
+        val block = loc.getBlock
+
+        if (block != null && canBeReplacedWithSoil(player, loc)) block.setType(Material.SOIL)
+      }
+    }
+  }
+
+  private def canBeReplacedWithSoil(player: Player, loc: Location) = {
+    val block = loc.getBlock
+    isRegionMember(player, loc) && (block.getType == Material.DIRT || block.getType == Material.GRASS)
   }
 }
