812a7e4db0774901682cdcf4fe9dbbc08c51b329
Lucky3028 <lucky3028.0@gmail.com>
2020-10-31 16:05:34 +0900

    [Fix] 例外の握りつぶしをしない

--- src/main/scala/com/github/unchama/seasonalevents/Util.scala
+++ src/main/scala/com/github/unchama/seasonalevents/Util.scala
@@ -18,27 +18,15 @@ object Util {
   def randomlyDropItemAt(entity: Entity, item: ItemStack): Unit = {
     val rate = SeasonalEvents.config.itemDropRate
     val rand = new Random().nextDouble() * 100
-    if (rand < rate) {
-      // 報酬をドロップ
-      entity.getWorld.dropItemNaturally(entity.getLocation, item)
-    }
+    if (rand < rate) entity.getWorld.dropItemNaturally(entity.getLocation, item)
   }
 
   /**
-   * 指定された年月日を`LocalDate`に変換する
+   * 指定された年月日を`LocalDate`に変換する。
    * @param year 年（西暦）
    * @param month 月
    * @param daysOfMonth 日
-   * @return `LocalDate`。存在しない日付の場合は、エラーを出力して`LocalDate.of(2020,1,1)`（2020年1月1日）を返す。
+   * @return `LocalDate`
    */
-  def localDateFromYearMonthDays(year: Int, month: Int, daysOfMonth: Int): LocalDate = {
-    try {
-      LocalDate.of(year, month, daysOfMonth)
-    } catch {
-      case e: java.time.DateTimeException =>
-        Bukkit.getServer.getLogger.severe("SeasonalEventsは日付の処理を正常に完了できませんでした。以下にエラーを表示します。")
-        e.printStackTrace()
-        LocalDate.of(2020, 1, 1)
-    }
-  }
+  def localDateFromYearMonthDays(year: Int, month: Int, daysOfMonth: Int): LocalDate = LocalDate.of(year, month, daysOfMonth)
 }
