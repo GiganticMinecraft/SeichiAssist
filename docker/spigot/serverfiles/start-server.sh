c617d99fe332a7272505bfe2b46ef3cf966e5be5
Ryosuke Kondo <6561358+kory33@users.noreply.github.com>
2020-11-01 14:13:48 +0000

    [fix] 初回起動時のstart-server.shの挙動を修正する

--- docker/spigot/serverfiles/start-server.sh
+++ docker/spigot/serverfiles/start-server.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-mkdir -p /spigot/plugins/SeichiAssist/
-
 rm /spigot/plugins/SeichiAssist* || true
 
 cp -Rf /spigot-files/* /spigot/
+mkdir -p /spigot/plugins/SeichiAssist/
 
 cd /spigot/plugins/SeichiAssist
-rm config.yml
+
+rm config.yml || true
 jar xf ../SeichiAssist-*.jar config.yml
 
 sed -i -e "s/servernum: .*/servernum: ${SERVER_NUM}/" /spigot/plugins/SeichiAssist/config.yml
@@ -17,4 +17,6 @@ sed -i -e "s/server-id: .*/server-id: ${SERVER_ID}/" /spigot/plugins/SeichiAssis
 sed -i -e "s/host: .*/host: ${DB_HOST}/" /spigot/plugins/SeichiAssist/config.yml
 sed -i -e "s/pw: .*/pw: ${DB_PASSWORD}/" /spigot/plugins/SeichiAssist/config.yml
 
-cd /spigot/ && java -jar /spigot/spigot*.jar nogui
+cd /spigot/
+
+java -jar /spigot/spigot*.jar nogui
