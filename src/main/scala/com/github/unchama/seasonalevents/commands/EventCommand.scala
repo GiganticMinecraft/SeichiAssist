bdbb03d6ad448c33ce405501cb603d658f0da06f
Lucky3028 <lucky3028.0@gmail.com>
2020-11-04 23:37:49 +0900

    [Fix] EventCommandを新年イベント対応に

--- src/main/scala/com/github/unchama/seasonalevents/commands/EventCommand.scala
+++ src/main/scala/com/github/unchama/seasonalevents/commands/EventCommand.scala
@@ -1,21 +1,22 @@
 package com.github.unchama.seasonalevents.commands
 
 import cats.effect.IO
-import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seasonalevents.halloween.HalloweenItemData
-import com.github.unchama.seichiassist.listener.new_year_event.{NewYearBagListener, NewYearItemListener}
+import com.github.unchama.seasonalevents.newyear.NewYearItemData
+import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seichiassist.util.Util
 import com.github.unchama.targetedeffect.TargetedEffect._
 import org.bukkit.command.TabExecutor
 import org.bukkit.entity.Player
 
 object EventCommand {
+
   import com.github.unchama.targetedeffect._
 
   val newYearGrantEffect: TargetedEffect[Player] =
     Util.grantItemStacksEffect(
-      NewYearBagListener.getNewYearBag,
-      NewYearItemListener.getNewYearApple
+      NewYearItemData.newYearApple,
+      NewYearItemData.newYearBag
     )
 
   val halloweenGrantEffect: TargetedEffect[Player] =
