2801351ed301a7388118757fcf89e07db51ada07
Lucky3028 <lucky3028.0@gmail.com>
2020-11-05 13:56:39 +0900

    [Fix] 新年イベントに関するREADME

--- src/main/scala/com/github/unchama/seasonalevents/newyear/NewYear.scala
+++ src/main/scala/com/github/unchama/seasonalevents/newyear/NewYear.scala
@@ -5,16 +5,15 @@ import java.time.LocalDate
 import com.github.unchama.seasonalevents.Util.{getDateSeq, validateItemDropRate}
 
 object NewYear {
+  // お年玉袋ドロップ率（%）
+  // 旧実装：ドロップ数 1/nブロック、初期値 n=500
+  val itemDropRate: Double = validateItemDropRate(0.2)
   // 新年が何年かを西暦で入力しておくと、自動的に他の日付が設定される
   val EVENT_YEAR: Int = 2018
   val PREV_EVENT_YEAR: Int = EVENT_YEAR - 1
   val START_DATE: LocalDate = LocalDate.ofYearDay(EVENT_YEAR, 1)
   val END_DATE: LocalDate = LocalDate.ofYearDay(EVENT_YEAR, 31)
   val DISTRIBUTED_SOBA_DATE: LocalDate = LocalDate.ofYearDay(PREV_EVENT_YEAR, 365)
-
-  // お年玉袋ドロップ率（%）
-  // 旧実装：ドロップ数 1/nブロック、初期値 n=500
-  val itemDropRate: Double = validateItemDropRate(0.2)
 
   def sobaWillBeDistributed: Boolean = LocalDate.now().isEqual(DISTRIBUTED_SOBA_DATE)
 
