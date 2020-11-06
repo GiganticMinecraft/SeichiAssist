c42144af87d1ddd26363670e984d0ff34725de95
Lucky3028 <lucky3028.0@gmail.com>
2020-11-06 11:49:13 +0900

    [Fix] n周年記念に対応する

--- src/main/scala/com/github/unchama/seasonalevents/anniversary/AnniversaryListener.scala
+++ src/main/scala/com/github/unchama/seasonalevents/anniversary/AnniversaryListener.scala
@@ -2,7 +2,7 @@ package com.github.unchama.seasonalevents.anniversary
 
 import java.time.LocalDate
 
-import com.github.unchama.seasonalevents.anniversary.Anniversary.{EVENT_DATE, blogArticleUrl}
+import com.github.unchama.seasonalevents.anniversary.Anniversary.{ANNIVERSARY_COUNT, EVENT_DATE, blogArticleUrl}
 import com.github.unchama.seichiassist.SeichiAssist
 import com.github.unchama.seichiassist.util.Util.isPlayerInventoryFull
 import org.bukkit.ChatColor._
@@ -18,10 +18,10 @@ object AnniversaryListener extends Listener {
 
     if (player != null && LocalDate.now().isEqual(EVENT_DATE)) {
       List(
-        s"${BLUE}本日でギガンティック☆整地鯖は1周年を迎えます。",
+        s"${BLUE}本日でギガンティック☆整地鯖は${ANNIVERSARY_COUNT}周年を迎えます。",
         s"${BLUE}これを記念し、限定アイテムを入手可能です。詳しくは下記URLのサイトをご覧ください。",
         s"$DARK_GREEN$UNDERLINE$blogArticleUrl"
-      ).foreach(player.sendMessage)
+      ).foreach(player.sendMessage(_))
       player.playSound(player.getLocation, Sound.BLOCK_ANVIL_PLACE, 1f, 1f)
     }
   }
@@ -47,9 +47,9 @@ object AnniversaryListener extends Listener {
       player.sendMessage(s"${RED}インベントリに空きがなかったため、アイテムを配布できませんでした。")
     } else {
       // TOOD mine_chamaというMCIDのヘッドで直接指定できる
-      val command = s"""give ${player.getName} skull 1 3 {display:{Name:"まいんちゃん",Lore:["", ${YELLOW}整地サーバー1周年記念だよ！"]},SkullOwner:{Id:"fac7c46e-3e89-4249-bef6-948d5eb528c9",Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDhmNTQ0OGI0ZDg4ZTQwYjE0YzgyOGM2ZjFiNTliMzg1NDVkZGE5MzNlNzNkZmYzZjY5NWU2ZmI0Mjc4MSJ9fX0="}]}}}"""
+      val command = s"""give ${player.getName} skull 1 3 {display:{Name:"まいんちゃん",Lore:["", ${YELLOW}ギガンティック☆整地鯖${ANNIVERSARY_COUNT}周年記念だよ！"]},SkullOwner:{Id:"fac7c46e-3e89-4249-bef6-948d5eb528c9",Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDhmNTQ0OGI0ZDg4ZTQwYjE0YzgyOGM2ZjFiNTliMzg1NDVkZGE5MzNlNzNkZmYzZjY5NWU2ZmI0Mjc4MSJ9fX0="}]}}}"""
       Bukkit.dispatchCommand(Bukkit.getConsoleSender, command)
-      player.sendMessage(s"${BLUE}ギガンティック☆整地鯖1周年の記念品を入手しました。")
+      player.sendMessage(s"${BLUE}ギガンティック☆整地鯖${ANNIVERSARY_COUNT}周年の記念品を入手しました。")
 
       playerData.anniversary_$eq(false)
       SeichiAssist.databaseGateway.playerDataManipulator.setAnniversary(false, Some.apply(playerUuid))
