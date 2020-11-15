569df63ca9660794ec0de285a59b7793e3eccc9f
Lucky3028 <lucky3028.0@gmail.com>
2020-11-15 18:30:53 +0900

    Revert "[Fix] Shift+右クリックの時にもspawn保護ならProjectileを発射させない"
    This reverts commit c2f04eaa

--- src/main/scala/com/github/unchama/seichiassist/subsystems/expbottlestack/bukkit/listeners/ExpBottleStackUsageController.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/expbottlestack/bukkit/listeners/ExpBottleStackUsageController.scala
@@ -5,7 +5,6 @@ import com.github.unchama.generic.effect.ResourceScope
 import com.github.unchama.generic.effect.unsafe.EffectEnvironment
 import com.github.unchama.seichiassist.subsystems.expbottlestack.bukkit.Resources
 import com.github.unchama.seichiassist.subsystems.expbottlestack.domain.BottleCount
-import com.github.unchama.util.external.WorldGuardWrapper.getRegions
 import org.bukkit.Material
 import org.bukkit.entity.ThrownExpBottle
 import org.bukkit.event.block.Action
@@ -13,8 +12,6 @@ import org.bukkit.event.entity.ExpBottleEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.inventory.ItemStack
-
-import scala.jdk.CollectionConverters._
 
 class ExpBottleStackUsageController[
   F[_] : Effect,
@@ -47,8 +44,7 @@ class ExpBottleStackUsageController[
     if (player.isSneaking
       && playerInventory.getItemInMainHand != null
       && playerInventory.getItemInMainHand.getType == Material.EXP_BOTTLE
-      && (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)
-      && (!getRegions(player.getLocation).asScala.exists(_.getId == "spawn"))) {
+      && (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)) {
 
       val bottleCount = BottleCount(playerInventory.getItemInMainHand.getAmount)
       val bottleResource = Resources.bottleResourceSpawningAt[F](player.getLocation, bottleCount)
