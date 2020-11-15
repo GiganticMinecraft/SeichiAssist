758ff43abbf21d67f30302e8d4f91ddcc0fa120c
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-11-15 16:38:40 +0900

    Update WorldSaveTask.scala

--- src/main/scala/com/github/unchama/seichiassist/subsystems/autosave/bukkit/task/WorldSaveTask.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/autosave/bukkit/task/WorldSaveTask.scala
@@ -7,23 +7,23 @@ import org.bukkit.{Bukkit, World}
 object WorldSaveTask {
   def saveWorld(world: World): Unit = {
     // WARNを防ぐためMinecraftサーバーデフォルトの自動セーブは無効化
-    val server = getField(Bukkit.getServer.getClass, "console").get(Bukkit.getServer)
-    getField(server.getClass, "autosavePeriod").set(server, 0)
+    val server = getField(Bukkit.getServer.getClass, "console").getOrElse(return).get(Bukkit.getServer)
+    getField(server.getClass, "autosavePeriod").getOrElse(return).set(server, 0)
 
     world.save()
   }
 
-  private def getField(clazz: Class[_], name: String): Field = {
+  private def getField(clazz: Class[_], name: String): Option[Field] = {
     var c = clazz
     do for (field <- c.getDeclaredFields) {
       if (field.getName.equals(name)) {
         field.setAccessible(true)
-        return field
+        return Some(field)
       }
     } while ({
       c = c.getSuperclass
       c.getSuperclass != null
     })
-    null
+    None
   }
 }
