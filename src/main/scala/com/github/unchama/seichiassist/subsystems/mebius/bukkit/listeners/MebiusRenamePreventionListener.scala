3ec3f5d4cce53a7ebc32ae5af8605e41ef946dd1
Lucky <Lucky3028@users.noreply.github.com>
2020-11-22 22:34:09 +0900

    金床でMebiusを数字キーを使って移動できないようにする (#773)
    * [Add] 金床を開いているときの数字キーによるMebiusの移動を回避する
    
    * [Add] 金床の一番左のスロットならという条件分岐を加える
    
    * [Fix] 条件分岐を見やすく

--- src/main/scala/com/github/unchama/seichiassist/subsystems/mebius/bukkit/listeners/MebiusRenamePreventionListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/mebius/bukkit/listeners/MebiusRenamePreventionListener.scala
@@ -2,7 +2,7 @@ package com.github.unchama.seichiassist.subsystems.mebius.bukkit.listeners
 
 import com.github.unchama.seichiassist.subsystems.mebius.bukkit.codec.BukkitMebiusItemStackCodec
 import org.bukkit.ChatColor._
-import org.bukkit.event.inventory.{InventoryClickEvent, InventoryDragEvent, InventoryInteractEvent}
+import org.bukkit.event.inventory.{InventoryAction, InventoryClickEvent, InventoryDragEvent, InventoryInteractEvent}
 import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.inventory.AnvilInventory
 
@@ -42,6 +42,26 @@ class MebiusRenamePreventionListener extends Listener {
     if (!BukkitMebiusItemStackCodec.isMebius(event.getOldCursor)) return
 
     if (event.getRawSlots.contains(0) && event.getView.convertSlot(0) == 0) {
+      cancelEventAndNotifyTheAlternative(event)
+    }
+  }
+
+  // 数字キーでホットバーのアイテムを金床のスロットに移動している時
+  @EventHandler def onSwap(event: InventoryClickEvent): Unit = {
+    // 金床を開いていない場合終了
+    if (!event.getView.getTopInventory.isInstanceOf[AnvilInventory]) return
+
+    val action = event.getAction
+    // event.getActionが数字キーによるアイテムの移動ならば処理を続行
+    if (action == null || (action != InventoryAction.HOTBAR_SWAP && action != InventoryAction.HOTBAR_MOVE_AND_READD)) return
+
+    // 金床の一番左のスロットにアイテムを移動させた場合以外return
+    if (event.getView.convertSlot(0) != 0 || event.getRawSlot != 0) return
+
+    // Mebiusを移動させた場合
+    val bottomInventory = event.getView.getBottomInventory
+    val keyNum = event.getHotbarButton
+    if (BukkitMebiusItemStackCodec.isMebius(bottomInventory.getItem(keyNum))) {
       cancelEventAndNotifyTheAlternative(event)
     }
   }
