2841182a821ee3a31abe420018050f770204d770
Lucky3028 <lucky3028.0@gmail.com>
2020-12-02 22:20:37 +0900

    [Remove] 不要なコメント

--- src/main/scala/com/github/unchama/seasonalevents/newyear/NewYear.scala
+++ src/main/scala/com/github/unchama/seasonalevents/newyear/NewYear.scala
@@ -5,8 +5,6 @@ import java.time.LocalDate
 import com.github.unchama.seasonalevents.Util.{dateRangeAsSequence, validateItemDropRate}
 
 object NewYear {
-  // お年玉袋ドロップ率（%）
-  // 旧実装：ドロップ数 1/nブロック、初期値 n=500
   val itemDropRate: Double = validateItemDropRate(0.002)
   // 新年が何年かを西暦で入力しておくと、自動的に他の日付が設定される
   val EVENT_YEAR: Int = 2018
