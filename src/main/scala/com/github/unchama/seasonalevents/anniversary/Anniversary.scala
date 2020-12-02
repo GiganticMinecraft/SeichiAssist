0a5312fda53c616cf66ce889e69819a9a5047de1
Lucky3028 <lucky3028.0@gmail.com>
2020-12-02 21:07:24 +0900

    [Fix] URLがはねられるものだったので修正

--- src/main/scala/com/github/unchama/seasonalevents/anniversary/Anniversary.scala
+++ src/main/scala/com/github/unchama/seasonalevents/anniversary/Anniversary.scala
@@ -8,5 +8,5 @@ object Anniversary {
   val ANNIVERSARY_COUNT = EVENT_YEAR - 2016
   val EVENT_YEAR = 2020
   val EVENT_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 6, 29)
-  val blogArticleUrl: String = validateUrl("https://www.seichi.network/blog/categories/%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E6%83%85%E5%A0%B1")
+  val blogArticleUrl: String = validateUrl("https://www.seichi.network/post/anniversary2020")
 }
--- src/main/scala/com/github/unchama/seasonalevents/valentine/Valentine.scala
+++ src/main/scala/com/github/unchama/seasonalevents/valentine/Valentine.scala
@@ -10,5 +10,5 @@ object Valentine {
   // イベントが実際に終了する日
   val END_DATE: LocalDate = LocalDate.of(2018, 2, 27)
   val itemDropRate: Double = validateItemDropRate(30)
-  val blogArticleUrl: String = validateUrl("https://www.seichi.network/blog/categories/%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E6%83%85%E5%A0%B1")
+  val blogArticleUrl: String = validateUrl("https://www.seichi.network/post/valentine2020")
 }
