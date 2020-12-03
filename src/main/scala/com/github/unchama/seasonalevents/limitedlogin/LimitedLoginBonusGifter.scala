00734f0ddf3308f829131bbc3bcb40cffeb1619a
Lucky3028 <lucky3028.0@gmail.com>
2020-12-03 15:38:47 +0900

    [Fix] do-whileを使わないように

--- src/main/scala/com/github/unchama/seasonalevents/limitedlogin/LimitedLoginBonusGifter.scala
+++ src/main/scala/com/github/unchama/seasonalevents/limitedlogin/LimitedLoginBonusGifter.scala
@@ -4,10 +4,11 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 import com.github.unchama.seasonalevents.limitedlogin.LimitedLoginEvent.{START_DATE, isInEvent}
-import com.github.unchama.seasonalevents.limitedlogin.LimitedLoginItemData.getItemData
+import com.github.unchama.seasonalevents.limitedlogin.LoginBonusItemData.loginBonusAt
 import com.github.unchama.seichiassist.data.GachaSkullData
 import com.github.unchama.seichiassist.util.Util.grantItemStacksEffect
 import com.github.unchama.seichiassist.{DefaultEffectEnvironment, SeichiAssist}
+import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.{EventHandler, Listener}
 
@@ -16,7 +17,7 @@ object LimitedLoginBonusGifter extends Listener {
   def onPlayerJoin(event: PlayerJoinEvent): Unit = {
     if (!isInEvent) return
 
-    val player = event.getPlayer
+    implicit val player: Player = event.getPlayer
     val playerUuid = player.getUniqueId
 
     // この条件分岐がtrueになる可能性は通常ない（ログインしている限りplayerMapにはそのMCIDのデータが有るはずだ）が、なっている事例があるので念の為
@@ -24,37 +25,43 @@ object LimitedLoginBonusGifter extends Listener {
     if (!SeichiAssist.playermap.contains(playerUuid)) return
 
     val playerData = SeichiAssist.playermap(playerUuid)
-    val lastChecked = playerData.lastcheckdate
-    val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd")
-    val lastCheckedDate = LocalDate.parse(lastChecked, formatter)
-    // 開催期間内初のログイン時だったら（=lastCheckedDateがイベント開始日より前だったら）0、そうでなければ（=開催期間中ならば）playerData.LimitedLoginCount
-    var loginDays =
-      if (lastCheckedDate.isBefore(START_DATE)) 0
-      else playerData.LimitedLoginCount
+    val lastCheckedDate = {
+      val lastChecked = playerData.lastcheckdate
+      val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd")
 
-    loginDays += 1
-    var days = 0
-    do {
-      val itemNumber = getItemData(days)._1
-      val amount = getItemData(days)._2
+      LocalDate.parse(lastChecked, formatter)
+    }
 
-      itemNumber match {
-        case 1 => {
-          player.sendMessage(s"【限定ログボ：${days}日目】${amount}個のガチャ券をプレゼント！")
-          val skull = GachaSkullData.gachaSkull
-          for (_ <- 1 to amount) {
-            DefaultEffectEnvironment.runEffectAsync(
-              "ガチャ券を付与する",
-              grantItemStacksEffect(skull).run(player)
-            )
-          }
-        }
-        case _ =>
-      }
+    // 開催期間内初のログイン時だったら（=lastCheckedDateがイベント開始日より前だったら）1、そうでなければ（=開催期間中ならば）playerData.LimitedLoginCount + 1
+    val loginDays =
+      if (lastCheckedDate.isBefore(START_DATE)) 1
+      else playerData.LimitedLoginCount + 1
 
-      days += loginDays
-    } while (days == loginDays)
+    // 0日目のアイテムは毎日配布される
+    giveLoginBonus(0)
+    giveLoginBonus(loginDays)
 
     playerData.LimitedLoginCount_$eq(loginDays)
+  }
+
+  private def giveLoginBonus(day: Int)(implicit player: Player): Unit = {
+    val loginBonus = loginBonusAt(day) match {
+      case Some(loginBonus) => loginBonus
+      case None => throw new NoSuchElementException("存在しないアイテムデータが指定されました。")
+    }
+
+    loginBonus.itemId match {
+      case LoginBonusGachaTicket =>
+        val messageofDay = if (day == 0) "毎日" else s"${day}日目"
+        player.sendMessage(s"【限定ログボ：$messageofDay】${loginBonus.amount}個のガチャ券をプレゼント！")
+
+        val skull = GachaSkullData.gachaSkull
+        for (_ <- 1 to loginBonus.amount) {
+          DefaultEffectEnvironment.runEffectAsync(
+            "ガチャ券を付与する",
+            grantItemStacksEffect(skull).run(player)
+          )
+        }
+    }
   }
 }
