1082a37775799272557234cb67ef1a4d21eb85c2
Lucky3028 <lucky3028.0@gmail.com>
2020-12-05 20:16:54 +0900

    [Add] ChristmasCakeの定義・判定

---
+++ src/main/scala/com/github/unchama/seasonalevents/christmas/ChristmasItemData.scala
@@ -0,0 +1,61 @@
+package com.github.unchama.seasonalevents.christmas
+
+import java.time.LocalDate
+
+import de.tr7zw.itemnbtapi.NBTItem
+import org.bukkit.ChatColor.{AQUA, GRAY, ITALIC, RESET}
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.{ItemFlag, ItemStack}
+import org.bukkit.{Bukkit, Material}
+
+import scala.jdk.CollectionConverters._
+import scala.util.chaining._
+
+object ChristmasItemData {
+
+  //region ChristmasCake
+
+  val ChristmasCake: ItemStack = {
+    val itemFlags = Set(
+      ItemFlag.HIDE_ENCHANTS
+    )
+    val loreList = {
+      val year = LocalDate.now().getYear
+      List(
+        "",
+        s"${year}クリスマスイベント限定品",
+        "",
+        "一口で食べられます",
+        "食べると不運か幸運がランダムで付与されます"
+      ).map(str => s"$RESET$GRAY$str")
+    }.asJava
+
+    val itemMeta = Bukkit.getItemFactory.getItemMeta(Material.CAKE).tap { meta =>
+      import meta._
+      setDisplayName(s"$AQUA${ITALIC}まいんちゃん特製クリスマスケーキ")
+      addEnchant(Enchantment.MENDING, 1, true)
+      setLore(loreList)
+      itemFlags.foreach(flg => addItemFlags(flg))
+    }
+
+    val cake = new ItemStack(Material.CAKE, 1)
+    cake.setItemMeta(itemMeta)
+
+    new NBTItem(cake)
+      .tap(_.setByte(NBTTagConstants.typeIdTag, 1.toByte))
+      .pipe(_.getItem)
+  }
+
+  def isChristmasCake(itemStack: ItemStack): Boolean =
+    itemStack != null && itemStack.getType != Material.AIR && {
+      new NBTItem(itemStack)
+        .getByte(NBTTagConstants.typeIdTag) == 1
+    }
+
+  //endregion
+
+  private object NBTTagConstants {
+    val typeIdTag = "halloweenItemTypeId"
+  }
+
+}
