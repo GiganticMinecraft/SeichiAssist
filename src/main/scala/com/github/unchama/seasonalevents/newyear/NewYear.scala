2cf4dced41f805d5ec8f4d44d24ad1b0f9b24248
Lucky3028 <lucky3028.0@gmail.com>
2020-12-05 13:23:02 +0900

    [Fix] 年末年始イベントが今年から再度機能するようにする

--- src/main/scala/com/github/unchama/seasonalevents/newyear/NewYear.scala
+++ src/main/scala/com/github/unchama/seasonalevents/newyear/NewYear.scala
@@ -7,7 +7,7 @@ import com.github.unchama.seasonalevents.Util.{dateRangeAsSequence, validateItem
 object NewYear {
   val itemDropRate: Double = validateItemDropRate(0.002)
   // 新年が何年かを西暦で入力しておくと、自動的に他の日付が設定される
-  val EVENT_YEAR: Int = 2018
+  val EVENT_YEAR: Int = 2021
   val START_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 1, 1)
   val END_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 1, 31)
   val DISTRIBUTED_SOBA_DATE: LocalDate = START_DATE.minusDays(1)
