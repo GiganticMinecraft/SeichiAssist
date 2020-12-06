9814ec1ad557184a23582442ad0acb0a0cea8b23
Lucky3028 <lucky3028.0@gmail.com>
2020-12-06 19:55:52 +0900

    [Add] /eventでchristmasアイテムを入手できるように

--- src/main/scala/com/github/unchama/seasonalevents/commands/EventCommand.scala
+++ src/main/scala/com/github/unchama/seasonalevents/commands/EventCommand.scala
@@ -1,8 +1,9 @@
 package com.github.unchama.seasonalevents.commands
 
 import cats.effect.IO
-import com.github.unchama.seasonalevents.halloween.HalloweenItemData
-import com.github.unchama.seasonalevents.newyear.NewYearItemData
+import com.github.unchama.seasonalevents.christmas.ChristmasItemData._
+import com.github.unchama.seasonalevents.halloween.HalloweenItemData._
+import com.github.unchama.seasonalevents.newyear.NewYearItemData._
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seichiassist.util.Util
 import com.github.unchama.targetedeffect.TargetedEffect._
@@ -13,21 +14,32 @@ object EventCommand {
 
   import com.github.unchama.targetedeffect._
 
+  val christsmasGrantEffect: TargetedEffect[Player] =
+    Util.grantItemStacksEffect(
+      christmasCake(christmasCakeDefaultPieces),
+      christmasTurkey,
+      christmasPotion,
+      christmasChestPlate,
+      christmasPickaxe,
+      christmasSock
+    )
+
   val newYearGrantEffect: TargetedEffect[Player] =
     Util.grantItemStacksEffect(
-      NewYearItemData.newYearApple,
-      NewYearItemData.newYearBag
+      newYearApple,
+      newYearBag
     )
 
   val halloweenGrantEffect: TargetedEffect[Player] =
     Util.grantItemStacksEffect(
-      HalloweenItemData.halloweenPotion,
-      HalloweenItemData.halloweenHoe
+      halloweenPotion,
+      halloweenHoe
     )
 
   val executor: TabExecutor = playerCommandBuilder
     .execution { context =>
       val effect = context.args.yetToBeParsed match {
+        case "christmas" :: _ => christsmasGrantEffect
         case "newyear" :: _ => newYearGrantEffect
         case "halloween" :: _ => halloweenGrantEffect
         case _ => emptyEffect
