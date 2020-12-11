f6114ec16d9e6eba5e4ca875768ee9c34ce7fb46
Lucky3028 <lucky3028.0@gmail.com>
2020-12-11 11:20:29 +0900

    [Add] itemDropRateFromStray
    ストレイからのドロップ率を変更

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/christmas/Christmas.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/christmas/Christmas.scala
@@ -2,8 +2,11 @@ package com.github.unchama.seichiassist.subsystems.seasonalevents.christmas
 
 import java.time.LocalDate
 
+import com.github.unchama.seichiassist.subsystems.seasonalevents.Util.{dateRangeAsSequence, validateItemDropRate, validateUrl}
+
 object Christmas {
   val itemDropRate: Double = validateItemDropRate(0.002)
+  val itemDropRateFromStray: Double = validateItemDropRate(0.1)
   val blogArticleUrl: String = validateUrl("https://www.seichi.network/post/christmas2020")
   val EVENT_YEAR: Int = 2020
   val START_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 12, 15)
