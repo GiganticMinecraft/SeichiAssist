14f3bf6da0f443cc4ca944e076f09fb2cb86484f
Kory <6561358+kory33@users.noreply.github.com>
2020-12-12 08:52:30 +0900

    [fix] CanSaveWorlds extends AnyRef

--- src/main/scala/com/github/unchama/seichiassist/subsystems/autosave/application/CanSaveWorlds.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/autosave/application/CanSaveWorlds.scala
@@ -2,7 +2,7 @@ package com.github.unchama.seichiassist.subsystems.autosave.application
 
 import simulacrum.typeclass
 
-@typeclass trait CanSaveWorlds[F[_]] {
+@typeclass trait CanSaveWorlds[F[_]] extends AnyRef {
 
   val saveAllWorlds: F[Unit]
 
