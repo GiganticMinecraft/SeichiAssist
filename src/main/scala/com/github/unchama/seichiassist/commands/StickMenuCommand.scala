0d6e632874f5fe1d4f9acd965b67798c3c481301
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2020-12-11 15:18:06 +0900

    Update StickMenuCommand.scala
    import

--- src/main/scala/com/github/unchama/seichiassist/commands/StickMenuCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/commands/StickMenuCommand.scala
@@ -3,7 +3,7 @@ package com.github.unchama.seichiassist.commands
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import cats.effect.IO
 import org.bukkit.command.TabExecutor
-
+import com.github.unchama.seichiassist.menus.stickmenu.StickMenu
 /*
 * 棒メニューを開くコマンド
 * @author KisaragiEffective
@@ -13,7 +13,7 @@ object StickMenuCommand {
     .execution { context =>
       val sender = context.sender
       // TODO: 棒メニューを開くエフェクト
-      IO(FirstPage.open)
+      IO(StickMenu.firstPage.open)
     }
     .build()
 }
