9c71f3652247c96f443ff1adbeb8d3e4aa9c10b4
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-11 16:57:30 +0900

    Update src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/bukkit/task/DragonNightTimeTask.scala
    Co-authored-by: Lucky <Lucky3028@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/bukkit/task/DragonNightTimeTask.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/bukkit/task/DragonNightTimeTask.scala
@@ -5,7 +5,7 @@ import com.github.unchama.seichiassist.data.potioneffect.FastDiggingEffect
 
 object DragonNightTimeTask {
   def startDragonNightTime(): Unit = {
-    val effectData = new FastDiggingEffect(72000, 10, 3)
+    val effectData = new FastDiggingEffect(20 * 60 * 60, 10, 3)
     SeichiAssist.playermap.values.foreach(_.effectdatalist.addOne(effectData))
   }
 }
