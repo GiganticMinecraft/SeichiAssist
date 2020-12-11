19462f5e60e17a6980f3b1d65954527c20a2ea9c
Lucky3028 <lucky3028.0@gmail.com>
2020-12-11 10:19:44 +0900

    [Remove] OldNarutoRemakeRawProperty

--- src/main/scala/com/github/unchama/seichiassist/subsystems/itemmigration/migrations/V1_1_0_MigrateNarutoRemakeToNewCodec.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/itemmigration/migrations/V1_1_0_MigrateNarutoRemakeToNewCodec.scala
@@ -2,7 +2,6 @@ package com.github.unchama.seichiassist.subsystems.itemmigration.migrations
 
 import com.github.unchama.itemmigration.domain.{ItemMigration, ItemMigrationVersionNumber}
 import com.github.unchama.itemmigration.util.MigrationHelper
-import de.tr7zw.itemnbtapi.NBTItem
 import org.bukkit.ChatColor._
 import org.bukkit.inventory.ItemStack
 
@@ -11,41 +10,25 @@ import scala.jdk.CollectionConverters._
 object V1_1_0_MigrateNarutoRemakeToNewCodec {
 
   object OldNarutoRemakeItemStackCodec {
-    private val narutoRemake1Lore = List(s"${GRAY}2020ハロウィン討伐イベントクリア賞")
-    private val narutoRemake2Lore = List(s"${GRAY}2020ハロウィン討伐イベント特別賞")
+    private val narutoRemakeLore = List(s"${GRAY}2020ハロウィン討伐イベント")
 
-    def decodeOldNarutoRemakeProperty(itemStack: ItemStack): Option[OldNarutoRemakeRawProperty] =
-      if (isNarutoRemake1(itemStack)) Some(OldNarutoRemakeRawProperty(1))
-      else if (isNarutoRemake2(itemStack)) Some(OldNarutoRemakeRawProperty(2))
-      else None
+    def decodeOldNarutoRemake(itemStack: ItemStack): Option[ItemStack] = Some(itemStack).filter(isNarutoRemake)
 
-    def isNarutoRemake1(itemStack: ItemStack): Boolean = {
+    def isNarutoRemake(itemStack: ItemStack): Boolean = {
       if (itemStack == null || !itemStack.hasItemMeta || !itemStack.getItemMeta.hasLore) return false
       val lore = itemStack.getItemMeta.getLore.asScala
-      narutoRemake1Lore.forall(lore.contains)
+      lore.contains(narutoRemakeLore)
     }
-
-    def isNarutoRemake2(itemStack: ItemStack): Boolean = {
-      if (itemStack == null || !itemStack.hasItemMeta || !itemStack.getItemMeta.hasLore) return false
-      val lore = itemStack.getItemMeta.getLore.asScala
-      narutoRemake2Lore.forall(lore.contains)
-    }
-
-    case class OldNarutoRemakeRawProperty(itemType: Int)
-
   }
 
   import eu.timepit.refined.auto._
 
   def migrationFunction(itemStack: ItemStack): ItemStack = {
-    val property =
-      OldNarutoRemakeItemStackCodec
-        .decodeOldNarutoRemakeProperty(itemStack)
-        .getOrElse(return itemStack)
+    if (OldNarutoRemakeItemStackCodec.decodeOldNarutoRemake(itemStack).isEmpty) return itemStack
 
     import scala.util.chaining._
 
-    val clone = itemStack.clone().tap { clone =>
+    itemStack.clone().tap { clone =>
       import clone._
       setDurability(238.toShort)
       setItemMeta {
@@ -56,11 +39,6 @@ object V1_1_0_MigrateNarutoRemakeToNewCodec {
         }
       }
     }
-
-    new NBTItem(clone).tap { nbtItem =>
-
-      nbtItem.setByte("narutoRemakeTypeId", property.itemType.toByte)
-    }.pipe(_.getItem)
   }
 
   def migration: ItemMigration = ItemMigration(
