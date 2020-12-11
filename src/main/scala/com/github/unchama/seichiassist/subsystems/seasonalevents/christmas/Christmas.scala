670cc46ea161bf90c54d7ed5f2ce223ef064a064
Lucky3028 <lucky3028.0@gmail.com>
2020-12-11 11:02:14 +0900

    [Fix] イベント期間の延長

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/christmas/Christmas.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/christmas/Christmas.scala
@@ -1,7 +1,5 @@
 package com.github.unchama.seichiassist.subsystems.seasonalevents.christmas
 
-import com.github.unchama.seichiassist.subsystems.seasonalevents.Util.{dateRangeAsSequence, validateItemDropRate, validateUrl}
-
 import java.time.LocalDate
 
 object Christmas {
@@ -9,7 +7,7 @@ object Christmas {
   val blogArticleUrl: String = validateUrl("https://www.seichi.network/post/christmas2020")
   val EVENT_YEAR: Int = 2020
   val START_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 12, 15)
-  val END_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 12, 26)
+  val END_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 12, 29)
 
   def isInEvent: Boolean = dateRangeAsSequence(START_DATE, END_DATE).contains(LocalDate.now())
 }
