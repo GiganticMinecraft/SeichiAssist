f022985887246851da852e0e3a5ec1bfcf7e2747
Lucky3028 <lucky3028.0@gmail.com>
2020-12-13 21:17:10 +0900

    [Add] LimitedLoginPeriodsに期間を記述するSetとそれを扱う関数

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/limitedlogin/LimitedLoginPeriods.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/limitedlogin/LimitedLoginPeriods.scala
@@ -1,5 +1,20 @@
 package com.github.unchama.seichiassist.subsystems.seasonalevents.limitedlogin
 
 import java.time.LocalDate
+import java.time.LocalDate.of
+
+import com.github.unchama.seichiassist.subsystems.seasonalevents.Util.dateRangeAsSequence
 
 case class LimitedLoginPeriods(startDate: LocalDate, endDate: LocalDate)
+
+object LimitedLoginPeriods {
+  private val EVENT_PERIODS = Set(
+    LimitedLoginPeriods(of(2019, 3, 1), of(2019, 3, 31))
+  )
+
+  def isContainedAt: Option[LimitedLoginPeriods] = EVENT_PERIODS.find {period =>
+    dateRangeAsSequence(period.startDate, period.endDate).contains(LocalDate.now())
+  }
+
+  def isInEvent: Boolean = isContainedAt.isDefined
+}
