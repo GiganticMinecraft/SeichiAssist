5abe0a7b3735d30845e3598beadbc2b35d48ae16
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2020-12-15 01:11:44 +0900

    Update src/main/scala/com/github/unchama/seichiassist/listener/PlayerClickListener.scala
    Co-authored-by: Kory | Ryosuke Kondo <6561358+kory33@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/listener/PlayerClickListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/listener/PlayerClickListener.scala
@@ -155,8 +155,7 @@ class PlayerClickListener(implicit effectEnvironment: EffectEnvironment) extends
     Blockを右クリックしていない、または、Blockのときにチェストやトラップチェストをクリックしていれば処理を終了
     参照：https://github.com/GiganticMinecraft/SeichiAssist/issues/770
      */
-    if (!(
-      (action == Action.RIGHT_CLICK_BLOCK && clickedBlock.getType != Material.CHEST && clickedBlock.getType != Material.TRAPPED_CHEST))) return
+    if (action != Action.RIGHT_CLICK_BLOCK || clickedBlock.getType == Material.CHEST || clickedBlock.getType == Material.TRAPPED_CHEST) return
 
     val count =
       if (player.isSneaking) {
