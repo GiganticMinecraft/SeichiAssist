d95aa1e903e1389b5061860a88cd585752a1188d
Kory <6561358+kory33@users.noreply.github.com>
2020-12-15 19:38:59 +0900

    [update] SeasonalEventsAPIを追加する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/System.scala
@@ -1,7 +1,11 @@
 package com.github.unchama.seichiassist.subsystems.seasonalevents
 
+import cats.Functor
+import cats.effect.Clock
+import com.github.unchama.bungeesemaphoreresponder.domain.PlayerDataFinalizer
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 import com.github.unchama.seichiassist.subsystems.seasonalevents.anniversary.AnniversaryListener
+import com.github.unchama.seichiassist.subsystems.seasonalevents.api.SeasonalEventsAPI
 import com.github.unchama.seichiassist.subsystems.seasonalevents.christmas.ChristmasItemListener
 import com.github.unchama.seichiassist.subsystems.seasonalevents.commands.EventCommand
 import com.github.unchama.seichiassist.subsystems.seasonalevents.halloween.HalloweenItemListener
@@ -9,12 +13,23 @@ import com.github.unchama.seichiassist.subsystems.seasonalevents.limitedlogin.Li
 import com.github.unchama.seichiassist.subsystems.seasonalevents.newyear.NewYearListener
 import com.github.unchama.seichiassist.subsystems.seasonalevents.seizonsiki.SeizonsikiListener
 import com.github.unchama.seichiassist.subsystems.seasonalevents.valentine.ValentineListener
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
+class System[F[_]](override val listeners: Seq[Listener],
+                   override val managedFinalizers: Seq[PlayerDataFinalizer[F, Player]],
+                   override val commands: Map[String, TabExecutor]) extends Subsystem[F] {
+
+  def api[G[_] : Clock : Functor]: SeasonalEventsAPI[G] = SeasonalEventsAPI.withF[G]
+
+}
+
 object System {
-  def wired[F[_]](instance: JavaPlugin): Subsystem[F] = {
-    Subsystem(
-      listenersToBeRegistered = Seq(
+  def wired[F[_], G[_] : Clock : Functor](instance: JavaPlugin): System[F] = {
+    new System(
+      listeners = Seq(
         AnniversaryListener,
         new ChristmasItemListener(instance),
         HalloweenItemListener,
@@ -23,8 +38,8 @@ object System {
         ValentineListener,
         NewYearListener,
       ),
-      finalizersToBeManaged = Nil,
-      commandsToBeRegistered = Map(
+      managedFinalizers = Nil,
+      commands = Map(
         "event" -> EventCommand.executor
       )
     )
