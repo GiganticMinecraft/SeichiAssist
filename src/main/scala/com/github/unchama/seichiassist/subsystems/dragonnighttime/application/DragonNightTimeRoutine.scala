78123cc3bf395c16e54270747035d3098df5977b
Sasaki Touma <sasakitouma@yahoo.ne.jp>
2020-12-16 17:46:53 +0900

    修正

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/DragonNightTimeRoutine.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/DragonNightTimeRoutine.scala
@@ -1,10 +1,11 @@
 package com.github.unchama.seichiassist.subsystems.dragonnighttime.application
 
-import java.time.LocalTime
+import java.time.{Instant, LocalDateTime, LocalTime, ZoneId}
+import java.util.concurrent.TimeUnit
 
-import cats.Applicative
 import cats.effect.{Sync, Timer}
 import com.github.unchama.concurrent.{RepeatingRoutine, RepeatingTaskContext}
+import com.github.unchama.util.time.LocalTimeUtil
 
 object DragonNightTimeRoutine {
   def apply[F[_] : Sync : CanAddEffect : CanNotifyStart : Timer]()(implicit context: RepeatingTaskContext): F[Nothing] = {
@@ -12,17 +13,25 @@ object DragonNightTimeRoutine {
 
     import scala.concurrent.duration._
 
-    val getRepeatInterval: F[FiniteDuration] = Applicative[F].pure(1.seconds)
+    val dailyDragonNightTime = LocalTime.of(20, 0, 0)
 
-    val time = LocalTime.of(20, 0, 0)
+    val getRepeatInterval: F[FiniteDuration] = {
+      import cats.implicits._
+
+      Timer[F].clock.realTime(TimeUnit.MILLISECONDS).map { currentEpochMilli =>
+        val currentLocalTime =
+          LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(currentEpochMilli),
+            ZoneId.systemDefault()
+          ).toLocalTime
+        LocalTimeUtil.getDurationToNextTimeOfDay(currentLocalTime, dailyDragonNightTime)
+      }
+    }
 
     val routineAction: F[Unit] =
-      if (LocalTime.now().equals(time))
-        CanNotifyStart[F].notify("ドラゲナイタイム開始！") >>
-          CanNotifyStart[F].notify("採掘速度上昇Lv10のバフが1時間付与されました") >>
-          CanAddEffect[F].addEffect
-      else
-        Applicative[F].unit
+      CanNotifyStart[F].notify("ドラゲナイタイム開始！") >>
+        CanNotifyStart[F].notify("採掘速度上昇Lv10のバフが1時間付与されました") >>
+        CanAddEffect[F].addEffect
 
     RepeatingRoutine.permanentRoutine(getRepeatInterval, routineAction)
   }
