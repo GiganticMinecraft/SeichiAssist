964177c1badd9c01225a2f82d07500e39ba97c46
Lucky3028 <lucky3028.0@gmail.com>
2020-12-22 11:43:33 +0900

    [Fix] キャンセルするイベントをPlayerInteractEventに

--- src/main/scala/com/github/unchama/seichiassist/listener/SpawnRegionProjectileInterceptor.scala
+++ src/main/scala/com/github/unchama/seichiassist/listener/SpawnRegionProjectileInterceptor.scala
@@ -1,18 +1,22 @@
 package com.github.unchama.seichiassist.listener
 
-import com.github.unchama.util.external.WorldGuardWrapper.{getOneRegion, getRegions}
-import org.bukkit.entity.Player
-import org.bukkit.event.entity.ProjectileLaunchEvent
+import com.github.unchama.util.external.WorldGuardWrapper.getRegions
+import org.bukkit.Material._
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.{EventHandler, Listener}
 
 import scala.jdk.CollectionConverters._
 
 object SpawnRegionProjectileInterceptor extends Listener {
   @EventHandler
-  def onProjectileLaunch(event: ProjectileLaunchEvent): Unit = {
-    val projectile = event.getEntity
-    if (projectile == null) return
-
+  def onProjectileLaunched(event: PlayerInteractEvent): Unit = {
+    val player = event.getPlayer
+    val inventory = player.getInventory
+    val action = event.getAction
+    val projectiles = Set(
+      BOW, EGG, LINGERING_POTION, SPLASH_POTION, ENDER_PEARL, EYE_OF_ENDER, FIREBALL, SNOW_BALL, EXP_BOTTLE
+    )
     val spawnRegions = Set(
       // 基本の保護名
       "spawn",
@@ -22,10 +26,12 @@ object SpawnRegionProjectileInterceptor extends Listener {
       "world-spawn"
     )
 
-    projectile.getShooter match {
-      case player: Player =>
-        if (getRegions(player.getLocation).asScala.map(_.getId).exists(spawnRegions.contains)) event.setCancelled(true)
-      case _ =>
+    // Projectileを持った状態で右クリックし、playerがいる保護がspawn保護の中であった場合はイベントをキャンセルする
+    if (inventory.getItemInMainHand != null
+      && projectiles.contains(inventory.getItemInMainHand.getType)
+      && (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)
+      && getRegions(player.getLocation).asScala.map(_.getId).exists(spawnRegions.contains)) {
+      event.setCancelled(true)
     }
   }
 }
