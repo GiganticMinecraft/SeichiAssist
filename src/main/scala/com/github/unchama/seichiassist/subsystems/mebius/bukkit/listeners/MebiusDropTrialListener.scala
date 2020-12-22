1279d11fc28e405fc5cc0495e65b776b408edb4c
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2020-12-22 15:52:46 +0900

    [clean] ManagedWorldをimplicit class経由で利用するように
    * wipe meaningless instaniate of `implicit class`
    
    * wipe meaningless instaniate of `implicit class`
    
    * fix import

--- src/main/scala/com/github/unchama/seichiassist/subsystems/mebius/bukkit/listeners/MebiusDropTrialListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/mebius/bukkit/listeners/MebiusDropTrialListener.scala
@@ -3,7 +3,8 @@ package com.github.unchama.seichiassist.subsystems.mebius.bukkit.listeners
 import cats.effect.{IO, SyncEffect, SyncIO, Timer}
 import com.github.unchama.datarepository.bukkit.player.PlayerDataRepository
 import com.github.unchama.generic.effect.unsafe.EffectEnvironment
-import com.github.unchama.seichiassist.{ManagedWorld, MaterialSets}
+import com.github.unchama.seichiassist.MaterialSets
+import com.github.unchama.seichiassist.ManagedWorld._
 import com.github.unchama.seichiassist.subsystems.mebius.bukkit.codec.BukkitMebiusItemStackCodec
 import com.github.unchama.seichiassist.subsystems.mebius.domain.MebiusDrop
 import com.github.unchama.seichiassist.subsystems.mebius.domain.speech.{MebiusSpeech, MebiusSpeechStrength}
@@ -35,7 +36,7 @@ class MebiusDropTrialListener[
 
     val player = event.getPlayer
 
-    if (!ManagedWorld.WorldOps(player.getWorld).isSeichi) return
+    if (player.getWorld.isSeichi) return
 
     val droppedMebiusProperty = MebiusDrop
       .tryOnce[G](player.getName, player.getUniqueId.toString)
