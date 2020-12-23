f4cdf6bedeea94ad95f748fff4009c34853db446
Lucky3028 <lucky3028.0@gmail.com>
2020-12-23 23:22:10 +0900

    [Add] lastquitを取得し、それに応じたプログラムを実行する処理

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/anniversary/AnniversaryListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/anniversary/AnniversaryListener.scala
@@ -2,20 +2,26 @@ package com.github.unchama.seichiassist.subsystems.seasonalevents.anniversary
 
 import java.time.LocalDate
 
-import cats.effect.{ConcurrentEffect, LiftIO}
+import cats.effect.{ConcurrentEffect, IO, LiftIO}
 import com.github.unchama.concurrent.NonServerThreadContextShift
+import com.github.unchama.generic.effect.unsafe.EffectEnvironment
 import com.github.unchama.seichiassist.DefaultEffectEnvironment
 import com.github.unchama.seichiassist.subsystems.seasonalevents.anniversary.Anniversary.{ANNIVERSARY_COUNT, EVENT_DATE, blogArticleUrl}
 import com.github.unchama.seichiassist.subsystems.seasonalevents.anniversary.AnniversaryItemData.mineHead
 import com.github.unchama.seichiassist.subsystems.seasonalevents.service.LastQuitInquiringService
 import com.github.unchama.seichiassist.util.Util.grantItemStacksEffect
+import com.github.unchama.targetedeffect.TargetedEffect.emptyEffect
+import com.github.unchama.targetedeffect.commandsender.MessageEffect
+import com.github.unchama.targetedeffect.player.FocusedSoundEffect
 import org.bukkit.ChatColor._
 import org.bukkit.Sound
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.{EventHandler, Listener}
 
-class AnniversaryListener[F[_] : ConcurrentEffect : NonServerThreadContextShift](implicit service: LastQuitInquiringService[F]) extends Listener {
+class AnniversaryListener[F[_] : ConcurrentEffect : NonServerThreadContextShift](implicit
+    effectEnvironment: EffectEnvironment,
+    service: LastQuitInquiringService[F]) extends Listener {
   @EventHandler
   def onPlayerJoin(event: PlayerJoinEvent): Unit = {
     val player = event.getPlayer
@@ -36,20 +42,29 @@ class AnniversaryListener[F[_] : ConcurrentEffect : NonServerThreadContextShift]
   def onPlayerDeath(event: PlayerDeathEvent): Unit = {
     val player = event.getEntity
 
-    val hasNotJoinedYetBeforeEvent = for {
+    val program = for {
       _ <- NonServerThreadContextShift[F].shift
       lastQuit <- service.loadLastQuitDateTime(player.getName)
-      result <- LiftIO[F].liftIO(lastQuit match {
-        case Some(dateTime) => dateTime.isBefore(EVENT_DATE.atStartOfDay())
-        case None => false
-      }.)
-    } yield result
+      _ <- LiftIO[F].liftIO(
+        IO {
+          val hasNotJoinedInEventYet = lastQuit match {
+            case Some(dateTime) => dateTime.isBefore(EVENT_DATE.atStartOfDay())
+            case None => false
+          }
+          val effect = if (hasNotJoinedInEventYet) Set(
+            grantItemStacksEffect(mineHead),
+            MessageEffect(s"${BLUE}ギガンティック☆整地鯖${ANNIVERSARY_COUNT}周年の記念品を入手しました。"),
+            FocusedSoundEffect(Sound.BLOCK_ANVIL_PLACE, 1.0f, 1.0f)
+          ) else Set(emptyEffect)
 
-    DefaultEffectEnvironment.runEffectAsync(
+          effect.foreach(_.run(player))
+        }
+      )
+    } yield ()
+
+    effectEnvironment.runEffectAsync(
       s"${ANNIVERSARY_COUNT}周年記念ヘッドを付与する",
-      grantItemStacksEffect(mineHead).run(player)
+      program
     )
-    player.sendMessage(s"${BLUE}ギガンティック☆整地鯖${ANNIVERSARY_COUNT}周年の記念品を入手しました。")
-    player.playSound(player.getLocation, Sound.BLOCK_ANVIL_PLACE, 1.0f, 1.0f)
   }
 }
