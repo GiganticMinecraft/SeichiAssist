12b95f9413cd38ae73d91425c5a8a4139f5bc376
KisaragiEffective <kisaragi.effective@gmail.com>
2020-12-26 20:14:20 +0900

    マイグレーションファイルのべき等性を保証する

--- src/main/resources/db/migration/Vx.x.x__Create_present_tables.sql
+++ src/main/resources/db/migration/Vx.x.x__Create_present_tables.sql
@@ -1,12 +1,12 @@
 use seichiassist;
 -- プレゼントIDと実体の対応付け
-CREATE TABLE presents(
-  present_id int auto_increment PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS presents(
+  present_id int PRIMARY KEY,
   itemstack  blob
 );
 
 -- プレイヤーがプレゼントを受け取ったかどうかをモデリングする
-CREATE TABLE present_state(
+CREATE TABLE IF NOT EXISTS present_state(
   uuid       char(36) NOT NULL,
   present_id int      NOT NULL,
   claimed   boolean  NOT NULL DEFAULT FALSE,
