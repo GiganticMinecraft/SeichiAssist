fb68118df930fcfc6287b7491332b3211b8772cb
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-30 00:56:11 +0900

    Update and rename CanAddEffect.scala to AddableWithContext.scala

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/AddableWithContext.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/AddableWithContext.scala
@@ -2,6 +2,6 @@ package com.github.unchama.seichiassist.subsystems.dragonnighttime.application
 
 import simulacrum.typeclass
 
-@typeclass trait CanAddEffect[F[_]] extends AnyRef {
+@typeclass trait AddableWithContext[F[_]] extends AnyRef {
   val addEffect: F[Unit]
 }
