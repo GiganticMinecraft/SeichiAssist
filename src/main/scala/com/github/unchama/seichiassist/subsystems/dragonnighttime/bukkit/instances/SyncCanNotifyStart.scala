ec9ad653b3861537c83e266d5412cade27b184ed
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-30 00:49:59 +0900

    Update SyncCanNotifyStart.scala

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/bukkit/instances/SyncCanNotifyStart.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/bukkit/instances/SyncCanNotifyStart.scala
@@ -1,12 +1,12 @@
 package com.github.unchama.seichiassist.subsystems.dragonnighttime.bukkit.instances
 
 import cats.effect.Sync
-import com.github.unchama.seichiassist.subsystems.dragonnighttime.application.CanNotifyStart
+import com.github.unchama.seichiassist.subsystems.dragonnighttime.application.Notifiable
 import com.github.unchama.seichiassist.util.Util
 import org.bukkit.Bukkit
 
-object SyncCanNotifyStart {
-  def apply[F[_] : Sync]: CanNotifyStart[F] = (message: String) => Sync[F].delay {
+object SyncNotifiable {
+  def apply[F[_] : Sync]: Notifiable[F] = (message: String) => Sync[F].delay {
     Util.sendEveryMessage(message)
     Bukkit.getLogger.info(message)
   }
