355711caad98099d925af42bf6a6880403830470
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-30 00:45:29 +0900

    Update src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/CanNotifyStart.scala
    Co-authored-by: KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/CanNotifyStart.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/CanNotifyStart.scala
@@ -2,6 +2,6 @@ package com.github.unchama.seichiassist.subsystems.dragonnighttime.application
 
 import simulacrum.typeclass
 
-@typeclass trait CanNotifyStart[F[_]] {
+@typeclass trait Notifiable[F[_]] {
   def notify(message: String): F[Unit]
 }
