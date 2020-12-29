3234fc5beffd86cb9764a744070bb20b9a7612ed
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-30 00:55:53 +0900

    Update System.scala

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/System.scala
@@ -3,14 +3,14 @@ package com.github.unchama.seichiassist.subsystems.dragonnighttime
 import cats.effect.{Sync, Timer}
 import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts
 import com.github.unchama.seichiassist.meta.subsystem.StatefulSubsystem
-import com.github.unchama.seichiassist.subsystems.dragonnighttime.application.{CanAddEffect, Notifiable, DragonNightTimeRoutine}
-import com.github.unchama.seichiassist.subsystems.dragonnighttime.bukkit.instances.{SyncCanAddEffect, SyncNotifiable}
+import com.github.unchama.seichiassist.subsystems.dragonnighttime.application.{AddableWithContext, Notifiable, DragonNightTimeRoutine}
+import com.github.unchama.seichiassist.subsystems.dragonnighttime.bukkit.instances.{SyncAddableWithContext, SyncNotifiable}
 
 object System {
   def wired[F[_] : Sync : Timer, G[_]]: StatefulSubsystem[F, List[F[Nothing]]] = {
     import PluginExecutionContexts._
 
-    implicit val _canAddEffect: CanAddEffect[F] = SyncCanAddEffect[F]
+    implicit val _addableWithContext: AddableWithContext[F] = SyncAddableWithContext[F]
     implicit val _notifiable: Notifiable[F] = SyncNotifiable[F]
 
     val repeatedJobs = List[F[Nothing]](
