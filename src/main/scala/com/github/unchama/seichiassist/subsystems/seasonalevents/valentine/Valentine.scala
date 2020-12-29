3b995bcf628ae8e2ebc6dd5ee24841b1b9a3e376
Lucky3028 <lucky3028.0@gmail.com>
2020-12-29 16:40:04 +0900

    [Fix] 変数の宣言順

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/valentine/Valentine.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/valentine/Valentine.scala
@@ -4,11 +4,11 @@ import com.github.unchama.seichiassist.subsystems.seasonalevents.Util.{dateRange
 import java.time.LocalDate
 
 object Valentine {
-  val itemDropRate: Double = validateItemDropRate(0.3)
-  val blogArticleUrl: String = validateUrl(s"https://www.seichi.network/post/valentine$EVENT_YEAR")
   val EVENT_YEAR: Int = 2018
   val START_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 2, 13)
   val END_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 2, 27)
+  val itemDropRate: Double = validateItemDropRate(0.3)
+  val blogArticleUrl: String = validateUrl(s"https://www.seichi.network/post/valentine$EVENT_YEAR")
 
   def isInEvent: Boolean = dateRangeAsSequence(START_DATE, END_DATE).contains(LocalDate.now())
 }
