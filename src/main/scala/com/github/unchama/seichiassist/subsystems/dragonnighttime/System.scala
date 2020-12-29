2d78389bf82f9cd571f7ce747bb7e20254e2dd62
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-30 01:24:10 +0900

    Update System.scala

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/System.scala
@@ -3,8 +3,8 @@ package com.github.unchama.seichiassist.subsystems.dragonnighttime
 import cats.effect.{Sync, Timer}
 import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts
 import com.github.unchama.seichiassist.meta.subsystem.StatefulSubsystem
-import com.github.unchama.seichiassist.subsystems.dragonnighttime.application.{AddableWithContext, Notifiable, DragonNightTimeRoutine}
-import com.github.unchama.seichiassist.subsystems.dragonnighttime.bukkit.instances.{SyncAddableWithContext, SyncNotifiable}
+import com.github.unchama.seichiassist.subsystems.dragonnighttime.application._
+import com.github.unchama.seichiassist.subsystems.dragonnighttime.bukkit.instances._
 
 object System {
   def wired[F[_] : Sync : Timer, G[_]]: StatefulSubsystem[F, List[F[Nothing]]] = {
