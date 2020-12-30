512cdbb8dfce7a342f1bdccc30bda4f3ead444a4
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2020-12-30 15:19:51 +0900

    Update System.scala

--- src/main/scala/com/github/unchama/seichiassist/subsystems/webhook/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/webhook/System.scala
@@ -3,5 +3,5 @@ package com.github.unchama.seichiassist.subsystems.webhook
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 
 object System {
-  def wired[F[_]]: Subsystem[F] = Subsystem(Seq(), Map())
+  def wired[F[_]]: Subsystem[F] = Subsystem(Nil, Nil, Map())
 }
