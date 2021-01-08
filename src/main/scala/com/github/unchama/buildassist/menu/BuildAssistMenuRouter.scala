05b14c0f36ef04ed2fadf66b1d113c3622e6a5a1
Kory <6561358+kory33@users.noreply.github.com>
2021-01-09 02:30:47 +0900

    [clean] BuildAssistMenuRouterが露出するメニューを減らす

--- src/main/scala/com/github/unchama/buildassist/menu/BuildAssistMenuRouter.scala
+++ src/main/scala/com/github/unchama/buildassist/menu/BuildAssistMenuRouter.scala
@@ -7,32 +7,21 @@ import com.github.unchama.minecraft.actions.MinecraftServerThreadShift
 import com.github.unchama.seichiassist.subsystems
 
 trait BuildAssistMenuRouter[F[_]] {
-  implicit val canOpenBlockPlacementSkillMenu: F CanOpen BlockPlacementSkillMenu.type
   implicit val canOpenBuildMainMenu: F CanOpen BuildMainMenu.type
-  implicit val canOpenMineStackMassCraftMenu: F CanOpen MineStackMassCraftMenu
 }
 
 object BuildAssistMenuRouter {
-
   def apply(implicit
             flyState: subsystems.managedfly.InternalState[SyncIO],
             layoutPreparationContext: LayoutPreparationContext,
-            syncShift: MinecraftServerThreadShift[IO]): BuildAssistMenuRouter[IO] = {
-    new BuildAssistMenuRouter[IO] {
-      implicit lazy val blockPlacementSkillMenuEnvironment: BlockPlacementSkillMenu.Environment =
-        new BlockPlacementSkillMenu.Environment
-      implicit lazy val buildMainMenuEnvironment: BuildMainMenu.Environment =
-        new BuildMainMenu.Environment
-      implicit lazy val mineStackMassCraftMenuEnvironment: MineStackMassCraftMenu.Environment =
-        new MineStackMassCraftMenu.Environment
+            syncShift: MinecraftServerThreadShift[IO]): BuildAssistMenuRouter[IO] = new BuildAssistMenuRouter[IO] {
+    implicit lazy val blockPlacementSkillMenuEnvironment: BlockPlacementSkillMenu.Environment = new BlockPlacementSkillMenu.Environment
+    implicit lazy val buildMainMenuEnvironment: BuildMainMenu.Environment = new BuildMainMenu.Environment
+    implicit lazy val mineStackMassCraftMenuEnvironment: MineStackMassCraftMenu.Environment = new MineStackMassCraftMenu.Environment
 
-      override implicit lazy val canOpenBlockPlacementSkillMenu: CanOpen[IO, BlockPlacementSkillMenu.type] =
-        _.open
-      override implicit lazy val canOpenBuildMainMenu: CanOpen[IO, BuildMainMenu.type] =
-        _.open
-      override implicit lazy val canOpenMineStackMassCraftMenu: CanOpen[IO, MineStackMassCraftMenu] =
-        _.open
-    }
+    implicit lazy val canOpenBlockPlacementSkillMenu: CanOpen[IO, BlockPlacementSkillMenu.type] = _.open
+    implicit lazy val canOpenMineStackMassCraftMenu: CanOpen[IO, MineStackMassCraftMenu] = _.open
+
+    override implicit lazy val canOpenBuildMainMenu: CanOpen[IO, BuildMainMenu.type] = _.open
   }
-
 }
