64d51e7171212dc40850cee49fa3146f9251fdce
Kory <6561358+kory33@users.noreply.github.com>
2021-01-10 10:54:18 +0900

    [update] SeichiStarLevelにOrderインスタンスを追加する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/breakcount/domain/level/SeichiStarLevel.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakcount/domain/level/SeichiStarLevel.scala
@@ -1,11 +1,21 @@
 package com.github.unchama.seichiassist.subsystems.breakcount.domain.level
 
+import cats.Order
+
 /**
  * 整地スターレベル。
  */
 case class SeichiStarLevel private(level: Int) extends AnyVal
 
-object SeichiStarLevel {
+private[level] abstract class SeichiStarLevelInstances {
+
+  import cats.implicits._
+
+  implicit lazy val order: Order[SeichiStarLevel] = Order.by(_.level)
+
+}
+
+object SeichiStarLevel extends SeichiStarLevelInstances {
 
   def ofNonNegative(n: Int): SeichiStarLevel = {
     require(n >= 0, "整地スターレベルは非負である必要があります。")
