8a4b13db58d5ffaf5611cb7f6bc8d4014a075400
Kory <6561358+kory33@users.noreply.github.com>
2021-01-15 10:05:03 +0900

    [update] NotifyLevelUp にアクションの合成のためのメソッドを追加する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/breakcount/application/actions/NotifyLevelUp.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakcount/application/actions/NotifyLevelUp.scala
@@ -1,5 +1,6 @@
 package com.github.unchama.seichiassist.subsystems.breakcount.application.actions
 
+import cats.Applicative
 import com.github.unchama.generic.Diff
 import com.github.unchama.seichiassist.subsystems.breakcount.domain.level.{SeichiLevel, SeichiStarLevel}
 
@@ -8,6 +9,15 @@ trait NotifyLevelUp[F[_], Player] {
   def ofSeichiLevelTo(player: Player)(diff: Diff[SeichiLevel]): F[Unit]
 
   def ofSeichiStarLevelTo(player: Player)(diff: Diff[SeichiStarLevel]): F[Unit]
+
+  def andThenOnSeichiLevelup(action: Player => Diff[SeichiLevel] => F[Unit])
+                            (implicit F: Applicative[F]): NotifyLevelUp[F, Player] = new NotifyLevelUp[F, Player] {
+    override def ofSeichiLevelTo(player: Player)(diff: Diff[SeichiLevel]): F[Unit] =
+      F.*>(NotifyLevelUp.this.ofSeichiLevelTo(player)(diff))(action(player)(diff))
+
+    override def ofSeichiStarLevelTo(player: Player)(diff: Diff[SeichiStarLevel]): F[Unit] =
+      NotifyLevelUp.this.ofSeichiStarLevelTo(player)(diff)
+  }
 
 }
 
