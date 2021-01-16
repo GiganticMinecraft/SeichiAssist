ee720fecf104663ac0dcc63439d459904e1c26b4
KisaragiEffective <kisaragi.effective@gmail.com>
2021-01-16 16:56:55 +0900

    `ItemStack`の生成に`IconItemStackBuilder`を用いる

--- src/main/scala/com/github/unchama/seichiassist/data/ItemData.scala
+++ src/main/scala/com/github/unchama/seichiassist/data/ItemData.scala
@@ -1,71 +1,73 @@
 package com.github.unchama.seichiassist.data
 
+import com.github.unchama.itemstackbuilder.{IconItemStackBuilder, SkullItemStackBuilder}
 import com.github.unchama.seichiassist.util.StaticGachaPrizeFactory
 import org.bukkit.ChatColor._
+import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.meta.SkullMeta
-import org.bukkit.{Bukkit, Material}
-
-import java.util
 
 object ItemData {
   def getSuperPickaxe(amount: Int): ItemStack = {
-    val itemstack = new ItemStack(Material.DIAMOND_PICKAXE, amount)
-    val itemmeta = Bukkit.getItemFactory.getItemMeta(Material.DIAMOND_PICKAXE)
-    itemmeta.setDisplayName(s"$YELLOW${BOLD}Thanks for Voting!")
-    val lore = util.Arrays.asList("投票ありがとナス♡")
-    itemmeta.addEnchant(Enchantment.DIG_SPEED, 3, true)
-    itemmeta.addEnchant(Enchantment.DURABILITY, 3, true)
-    itemmeta.setLore(lore)
-    itemstack.setItemMeta(itemmeta)
-    itemstack
+    new IconItemStackBuilder(Material.DIAMOND_PICKAXE)
+      .amount(amount)
+      .title(s"$YELLOW${BOLD}Thanks for Voting!")
+      .lore(
+        "投票ありがとナス♡"
+      )
+      .addEnchant(Enchantment.DIG_SPEED, 3)
+      .addEnchant(Enchantment.DURABILITY, 3)
+      .build()
   }
 
   def getForLevelUpskull(name: String, amount: Int): ItemStack = {
-    val skull = new ItemStack(Material.SKULL_ITEM, amount)
-    val skullmeta = Bukkit.getItemFactory.getItemMeta(Material.SKULL_ITEM).asInstanceOf[SkullMeta]
-    skull.setDurability(3.toShort)
-    skullmeta.setDisplayName(s"$YELLOW${BOLD}ガチャ券")
-    val lore = util.Arrays.asList(s"$RESET${GREEN}右クリックで使えます", s"$RESET${DARK_GREEN}所有者：$name", RESET + "" + DARK_RED + "レベルアップ記念です")
-    skullmeta.setLore(lore)
-    skullmeta.setOwner("unchama")
-    skull.setItemMeta(skullmeta)
-    skull
+    new SkullItemStackBuilder("unchama")
+      .amount(amount)
+      .title(s"$YELLOW${BOLD}ガチャ券")
+      .lore(
+        s"$RESET${GREEN}右クリックで使えます",
+        s"$RESET${DARK_GREEN}所有者：$name", RESET + "" + DARK_RED + "レベルアップ記念です"
+      )
+      .build()
   }
 
   def getGachaApple(amount: Int): ItemStack = {
-    val gachaimo = new ItemStack(Material.GOLDEN_APPLE, amount)
-    val meta = Bukkit.getItemFactory.getItemMeta(Material.GOLDEN_APPLE)
-    meta.setDisplayName(StaticGachaPrizeFactory.getGachaRingoName)
-    val lore = StaticGachaPrizeFactory.getGachaRingoLore
-    meta.setLore(lore)
-    gachaimo.setItemMeta(meta)
-    gachaimo
+    import scala.jdk.CollectionConverters._
+    new IconItemStackBuilder(Material.GOLDEN_APPLE)
+      .amount(amount)
+      .title(StaticGachaPrizeFactory.getGachaRingoName)
+      .lore(StaticGachaPrizeFactory.getGachaRingoLore.asScala.toList)
+      .build()
   }
 
   def getElsa(amount: Int): ItemStack = {
-    val elsa = new ItemStack(Material.DIAMOND_BOOTS, amount)
-    val elsaMeta = Bukkit.getItemFactory.getItemMeta(Material.DIAMOND_BOOTS)
-    elsaMeta.setDisplayName(s"$AQUA${ITALIC}エルサ")
-    val lore = util.Arrays.asList("", s"${GREEN}装備中の移動速度$YELLOW(中)$GREEN", "", YELLOW + "金床" + RED + "不可", YELLOW + "修繕エンチャント" + AQUA + "可")
-    elsaMeta.setLore(lore)
-    elsaMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5, false)
-    elsaMeta.addEnchant(Enchantment.FROST_WALKER, 2, false)
-    elsaMeta.addEnchant(Enchantment.DURABILITY, 4, false)
-    elsaMeta.addEnchant(Enchantment.PROTECTION_FALL, 6, true)
-    elsaMeta.addEnchant(Enchantment.MENDING, 1, false)
-    elsa.setItemMeta(elsaMeta)
-    elsa
+    new IconItemStackBuilder(Material.DIAMOND_BOOTS)
+      .amount(amount)
+      .title(s"$AQUA${ITALIC}エルサ")
+      .lore(
+        "", 
+        s"${GREEN}装備中の移動速度$YELLOW(中)$GREEN", 
+        s"",
+        s"${YELLOW}金床${RED}不可", 
+        s"${YELLOW}修繕エンチャント${AQUA}可"
+      )
+      .addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5)
+      .addEnchant(Enchantment.FROST_WALKER, 2)
+      .addEnchant(Enchantment.DURABILITY, 4)
+      .addEnchant(Enchantment.PROTECTION_FALL, 6)
+      .addEnchant(Enchantment.MENDING, 1)
+      .build()
   }
 
   def getVotingGift(amount: Int): ItemStack = {
-    val gift = new ItemStack(Material.PAPER, amount)
-    val itemMeta = Bukkit.getItemFactory.getItemMeta(Material.PAPER)
-    itemMeta.setDisplayName(s"${AQUA}投票ギフト券")
-    val lore = util.Arrays.asList("", s"${WHITE}公共施設鯖にある", WHITE + "デパートで買い物ができます")
-    itemMeta.setLore(lore)
-    gift.setItemMeta(itemMeta)
-    gift
+    new IconItemStackBuilder(Material.PAPER)
+      .amount(amount)
+      .title(s"${AQUA}投票ギフト券")
+      .lore(
+        "",
+        s"${WHITE}公共施設鯖にある",
+        s"${WHITE}デパートで買い物ができます"
+      )
+      .build()
   }
 }
