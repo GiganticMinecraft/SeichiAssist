3b4294fbf1ca389edda8bef5fb1af412e8a06c8b
KisaragiEffective <kisaragi.effective@gmail.com>
2021-01-17 20:19:39 +0900

    ItemStackBuilderを使ってItemMetaFactoryを消す

--- src/main/scala/com/github/unchama/seichiassist/data/GachaSkullData.scala
+++ src/main/scala/com/github/unchama/seichiassist/data/GachaSkullData.scala
@@ -1,123 +1,66 @@
 package com.github.unchama.seichiassist.data
 
-import com.github.unchama.seichiassist.util.ItemMetaFactory
+import com.github.unchama.itemstackbuilder.SkullItemStackBuilder
 import org.bukkit.ChatColor._
-import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 object GachaSkullData {
-
-  import scala.jdk.CollectionConverters._
-  import scala.util.chaining._
-
   /**
    * ノーマルガチャ券
    */
   val gachaSkull: ItemStack =
-    new ItemStack(Material.SKULL_ITEM, 1).tap { skull =>
-      import skull._
-      setDurability(3.toShort)
-      setItemMeta {
-        ItemMetaFactory.SKULL.getValue.tap { skullMeta =>
-          import skullMeta._
-          setDisplayName(s"$YELLOW${BOLD}ガチャ券")
-          setLore {
-            List(
-              s"$RESET${GREEN}右クリックで使えます"
-            ).asJava
-          }
-          setOwner("unchama")
-        }
-      }
-    }
+    new SkullItemStackBuilder("unchama")
+      .title("$YELLOW${BOLD}ガチャ券")
+      .lore(
+        s"$RESET${GREEN}右クリックで使えます"
+      )
+      .build()
 
   /**
    * お詫びガチャ券
    */
   val gachaFromAdministrator: ItemStack =
-    new ItemStack(Material.SKULL_ITEM, 1).tap { itemStack =>
-      import itemStack._
-      setDurability(3)
-      setItemMeta {
-        ItemMetaFactory.SKULL.getValue.tap { meta =>
-          import meta._
-          setDisplayName(s"$YELLOW${BOLD}ガチャ券")
-          setLore {
-            List(
-              s"$RESET${GREEN}右クリックで使えます",
-              s"$RESET${DARK_RED}運営から不具合のお詫びです"
-            ).asJava
-          }
-          setOwner("unchama")
-        }
-      }
-    }
+    new SkullItemStackBuilder("unchama")
+      .title(s"$YELLOW${BOLD}ガチャ券")
+      .lore(
+        s"$RESET${GREEN}右クリックで使えます",
+        s"$RESET${DARK_RED}運営から不具合のお詫びです"
+      )
+      .build()
 
   /**
    * 投票報酬のガチャ券
    */
   val gachaForVoting: ItemStack =
-    new ItemStack(Material.SKULL_ITEM, 1).tap { itemStack =>
-      import itemStack._
-      setDurability(3)
-      setItemMeta {
-        ItemMetaFactory.SKULL.getValue.tap { meta =>
-          import meta._
-          setDisplayName(s"$YELLOW${BOLD}ガチャ券")
-          setLore {
-            List(
-              s"$RESET${GREEN}右クリックで使えます",
-              s"$RESET${LIGHT_PURPLE}投票ありがとナス♡"
-            ).asJava
-          }
-          setOwner("unchama")
-        }
-      }
-    }
+    new SkullItemStackBuilder("unchama")
+      .title(s"$YELLOW${BOLD}ガチャ券")
+      .lore(
+        s"$RESET${GREEN}右クリックで使えます",
+        s"$RESET${LIGHT_PURPLE}投票ありがとナス♡"
+      )
+      .build()
 
   /**
    * ガチャ景品（当たり・大当たり）とガチャ券の交換システムで手に入るガチャ券
    */
-  val gachaForExchanging: ItemStack = {
-    new ItemStack(Material.SKULL_ITEM, 1).tap { itemStack =>
-      import itemStack._
-      setDurability(3)
-      setItemMeta {
-        ItemMetaFactory.SKULL.getValue.tap { meta =>
-          import meta._
-          setDisplayName(s"$YELLOW${BOLD}ガチャ券")
-          setLore {
-            List(
-              s"$RESET${GREEN}右クリックで使えます",
-              s"$RESET${GRAY}ガチャ景品と交換しました。"
-            ).asJava
-          }
-          setOwner("unchama")
-        }
-      }
-    }
-  }
+  val gachaForExchanging: ItemStack =
+    new SkullItemStackBuilder("unchama")
+      .title(s"$YELLOW${BOLD}ガチャ券")
+      .lore(
+        s"$RESET${GREEN}右クリックで使えます",
+        s"$RESET${GRAY}ガチャ景品と交換しました。"
+      )
+      .build()
 
   /**
    * 整地レベルアップ時に配布されるガチャ券
    */
-  val gachaForSeichiLevelUp: ItemStack = {
-    new ItemStack(Material.SKULL_ITEM, 1).tap { itemStack =>
-      import itemStack._
-      setDurability(3)
-      setItemMeta {
-        ItemMetaFactory.SKULL.getValue.tap { meta =>
-          import meta._
-          setDisplayName(s"$YELLOW${BOLD}ガチャ券")
-          setLore {
-            List(
-              s"$RESET${GREEN}右クリックで使えます",
-              s"$RESET${GRAY}レベルアップ記念です"
-            ).asJava
-          }
-          setOwner("unchama")
-        }
-      }
-    }
-  }
+  val gachaForSeichiLevelUp: ItemStack =
+    new SkullItemStackBuilder("unchama")
+      .title(s"$YELLOW${BOLD}ガチャ券")
+      .lore(
+        s"$RESET${GREEN}右クリックで使えます",
+        s"$RESET${GRAY}レベルアップ記念です"
+      )
+      .build()
 }
