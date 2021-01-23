4b1b7e326ad7a69f5f4e007101f9c0f073d6d3fa
Kory <6561358+kory33@users.noreply.github.com>
2021-01-24 07:01:04 +0900

    [update] RepositoryFinalizationからRefDictBackedRepositoryFinalizationを切り出す

--- src/main/scala/com/github/unchama/datarepository/template/RepositoryFinalization.scala
+++ src/main/scala/com/github/unchama/datarepository/template/RepositoryFinalization.scala
@@ -1,7 +1,6 @@
 package com.github.unchama.datarepository.template
 
-import cats.{Applicative, Monad}
-import com.github.unchama.generic.RefDict
+import cats.Monad
 
 /**
  * データレポジトリの終了処理を記述するオブジェクト。
@@ -26,17 +25,6 @@ trait RepositoryFinalization[F[_], Player, R] {
         (p, s) => sFr(s).flatMap(r => RepositoryFinalization.this.persistPair(p, r))
       override val finalizeBeforeUnload: (Player, S) => F[Unit] =
         (p, s) => sFr(s).flatMap(r => RepositoryFinalization.this.finalizeBeforeUnload(p, r))
-    }
-
-}
-
-object RepositoryFinalization {
-
-  def fromRefDict[F[_] : Applicative, Player, Key, R](refDict: RefDict[F, Key, R])
-                                                     (keyExtractor: Player => Key): RepositoryFinalization[F, Player, R] =
-    new RepositoryFinalization[F, Player, R] {
-      override val persistPair: (Player, R) => F[Unit] = (player, data) => refDict.write(keyExtractor(player), data)
-      override val finalizeBeforeUnload: (Player, R) => F[Unit] = (_, _) => Applicative[F].unit
     }
 
 }
