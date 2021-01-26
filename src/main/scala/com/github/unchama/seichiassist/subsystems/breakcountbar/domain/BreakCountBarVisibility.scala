3e3b13c6fcf2a18d9fcffef50eb8003c8aea6efe
Kory <6561358+kory33@users.noreply.github.com>
2021-01-27 00:28:07 +0900

    [update] 棒メニューでI/F置き換えに対応する
    このコミット時点でコンパイルは通らない。

--- src/main/scala/com/github/unchama/seichiassist/subsystems/breakcountbar/domain/BreakCountBarVisibility.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakcountbar/domain/BreakCountBarVisibility.scala
@@ -1,11 +1,17 @@
 package com.github.unchama.seichiassist.subsystems.breakcountbar.domain
 
-sealed trait BreakCountBarVisibility
+sealed trait BreakCountBarVisibility {
+  val nextValue: BreakCountBarVisibility
+}
 
 object BreakCountBarVisibility {
 
-  case object Shown extends BreakCountBarVisibility
+  case object Shown extends BreakCountBarVisibility {
+    override lazy val nextValue: BreakCountBarVisibility = Hidden
+  }
 
-  case object Hidden extends BreakCountBarVisibility
+  case object Hidden extends BreakCountBarVisibility {
+    override lazy val nextValue: BreakCountBarVisibility = Shown
+  }
 
 }
