65f8e0b945cf4afbcb463aa8515dd6c224a7a9cc
Kory <6561358+kory33@users.noreply.github.com>
2021-02-10 01:07:05 +0900

    [update] FastDiggingEffectSuppressionStateを追加する

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/fastdiggingeffect/domain/settings/FastDiggingEffectSuppressionState.scala
@@ -0,0 +1,49 @@
+package com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.settings
+
+
+sealed trait FastDiggingEffectSuppressionState {
+
+  import FastDiggingEffectSuppressionState._
+
+  lazy val nextState: FastDiggingEffectSuppressionState =
+    this match {
+      case EnabledWithoutLimit => EnabledWithLimit.Of_127
+      case EnabledWithLimit.Of_127 => EnabledWithLimit.Of_200
+      case EnabledWithLimit.Of_200 => EnabledWithLimit.Of_400
+      case EnabledWithLimit.Of_400 => EnabledWithLimit.Of_600
+      case EnabledWithLimit.Of_600 => Disabled
+      case Disabled => EnabledWithoutLimit
+    }
+
+  lazy val effectAmplifierCap: Int =
+    this match {
+      case EnabledWithoutLimit => 65535
+      case EnabledWithLimit.Of_127 => 127
+      case EnabledWithLimit.Of_200 => 200
+      case EnabledWithLimit.Of_400 => 400
+      case EnabledWithLimit.Of_600 => 600
+      case Disabled => 0
+    }
+}
+
+object FastDiggingEffectSuppressionState {
+
+  case object EnabledWithoutLimit extends FastDiggingEffectSuppressionState
+
+  abstract sealed class EnabledWithLimit(val limit: Int) extends FastDiggingEffectSuppressionState
+
+  object EnabledWithLimit {
+
+    case object Of_127 extends EnabledWithLimit(127)
+
+    case object Of_200 extends EnabledWithLimit(200)
+
+    case object Of_400 extends EnabledWithLimit(400)
+
+    case object Of_600 extends EnabledWithLimit(600)
+
+  }
+
+  case object Disabled extends FastDiggingEffectSuppressionState
+
+}
