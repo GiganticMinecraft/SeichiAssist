e0bc3d6837cc54210344d810d6d8c9bd5f5f864c
Kory <6561358+kory33@users.noreply.github.com>
2021-02-10 09:27:29 +0900

    [update] FastDiggingEffectList.totalEffectAmplifierを実装する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/fastdiggingeffect/domain/effect/FastDiggingEffectList.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/fastdiggingeffect/domain/effect/FastDiggingEffectList.scala
@@ -1,10 +1,11 @@
 package com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.effect
 
+import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.settings.FastDiggingEffectSuppressionState
+
 import java.time.LocalDateTime
 import scala.concurrent.duration.FiniteDuration
 
 class FastDiggingEffectList(list: List[FastDiggingEffectTimings]) {
-
   def appendEffect(effect: FastDiggingEffect, duration: FiniteDuration)
                   (currentTime: LocalDateTime): FastDiggingEffectList = {
     val timings = FastDiggingEffectTimings(currentTime, duration, effect)
@@ -18,4 +19,14 @@ class FastDiggingEffectList(list: List[FastDiggingEffectTimings]) {
 
   def toFilteredList(currentTime: LocalDateTime): List[FastDiggingEffectTimings] = filterInactive(currentTime).list
 
+  def totalEffectAmplifier(suppressionSettings: FastDiggingEffectSuppressionState)
+                          (currentTime: LocalDateTime): Int = {
+    val totalAmplifier: Int =
+      toFilteredList(currentTime)
+        .map(_.effect.amplifier)
+        .sum
+        .toInt
+
+    (totalAmplifier - 1) min suppressionSettings.effectAmplifierCap
+  }
 }
