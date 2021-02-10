cb69a873dbc1b51a3c20d32b35d0fa3fb6ce8c37
Kory <6561358+kory33@users.noreply.github.com>
2021-02-10 21:26:01 +0900

    [update] 採掘速度上昇効果の同期プロセスを記述する

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/fastdiggingeffect/application/SynchronizationProcess.scala
@@ -0,0 +1,37 @@
+package com.github.unchama.seichiassist.subsystems.fastdiggingeffect.application
+
+import cats.MonadError
+import com.github.unchama.datarepository.KeyedDataRepository
+import com.github.unchama.generic.effect.concurrent.ReadOnlyRef
+import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.actions.GrantFastDiggingEffect
+import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.effect.FastDiggingEffectList
+import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.settings.FastDiggingEffectSuppressionState
+import fs2.concurrent.Topic
+import io.chrisdavenport.cats.effect.time.JavaTime
+
+object SynchronizationProcess {
+
+  import cats.implicits._
+
+  def using[
+    F[_] : GrantFastDiggingEffect[*[_], Player] : MonadError[*[_], Throwable] : JavaTime,
+    Player
+  ](suppressionState: KeyedDataRepository[Player, ReadOnlyRef[F, FastDiggingEffectSuppressionState]],
+    effectClock: Topic[F, (Player, FastDiggingEffectList)]): fs2.Stream[F, Unit] =
+    effectClock
+      .subscribe(1)
+      .evalTap { case (player, list) =>
+        // TODO ここでのレポジトリアクセスはプレーヤーが退出した後のストリーム処理により例外を吐く可能性がある。
+        //      attemptで潰せるが、そうならないように設計できないか？
+        val program = for {
+          state <- suppressionState(player).read
+          totalAmplifier <- list.totalEffectAmplifier[F](state)
+
+          _ <- GrantFastDiggingEffect[F, Player].forASecond(player)(totalAmplifier)
+        } yield ()
+
+        program.attempt
+      }
+      .as(())
+
+}
