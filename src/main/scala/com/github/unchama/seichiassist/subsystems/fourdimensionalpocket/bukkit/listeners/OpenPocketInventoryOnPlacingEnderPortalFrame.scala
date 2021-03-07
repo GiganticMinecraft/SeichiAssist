809294c9418ced6077cbd9b7334063a329985ee4
Kory <6561358+kory33@users.noreply.github.com>
2021-03-08 00:13:06 +0900

    [fix] ポケットインベントリを開く際のマテリアルフィルタを付けなおす

--- src/main/scala/com/github/unchama/seichiassist/subsystems/fourdimensionalpocket/bukkit/listeners/OpenPocketInventoryOnPlacingEnderPortalFrame.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/fourdimensionalpocket/bukkit/listeners/OpenPocketInventoryOnPlacingEnderPortalFrame.scala
@@ -3,6 +3,7 @@ package com.github.unchama.seichiassist.subsystems.fourdimensionalpocket.bukkit.
 import cats.effect.Effect
 import com.github.unchama.generic.effect.unsafe.EffectEnvironment
 import com.github.unchama.seichiassist.subsystems.fourdimensionalpocket.FourDimensionalPocketApi
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
@@ -19,6 +20,10 @@ class OpenPocketInventoryOnPlacingEnderPortalFrame[
     val player = event.getPlayer
     val action = event.getAction
     val hand = event.getHand
+
+    if (event.getMaterial != Material.ENDER_PORTAL_FRAME) {
+      return
+    }
 
     if (hand == EquipmentSlot.OFF_HAND || !(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)) {
       return
