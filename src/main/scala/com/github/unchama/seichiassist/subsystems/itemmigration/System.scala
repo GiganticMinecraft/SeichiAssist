0a7d85685efa75401face564d204937215cc2797
Kory <6561358+kory33@users.noreply.github.com>
2021-03-12 06:43:57 +0900

    [update] PlayerItemMigrationStateRepositoryを新しい定義で置き換える

--- src/main/scala/com/github/unchama/seichiassist/subsystems/itemmigration/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/itemmigration/System.scala
@@ -1,12 +1,12 @@
 package com.github.unchama.seichiassist.subsystems.itemmigration
 
-import cats.effect.concurrent.TryableDeferred
 import cats.effect.{ConcurrentEffect, ContextShift, IO, Sync, SyncEffect, SyncIO}
 import com.github.unchama.bungeesemaphoreresponder.domain.PlayerDataFinalizer
-import com.github.unchama.datarepository.bukkit.player.PlayerDataRepository
+import com.github.unchama.datarepository.bukkit.player.BukkitRepositoryControls
 import com.github.unchama.generic.ContextCoercion
 import com.github.unchama.generic.effect.unsafe.EffectEnvironment
-import com.github.unchama.itemmigration.bukkit.controllers.player.{PlayerItemMigrationController, PlayerItemMigrationStateRepository}
+import com.github.unchama.itemmigration.application.ItemMigrationStateRepositoryDefinitions
+import com.github.unchama.itemmigration.bukkit.controllers.player.PlayerItemMigrationController
 import com.github.unchama.itemmigration.service.ItemMigrationService
 import com.github.unchama.seichiassist.SeichiAssist
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
@@ -21,20 +21,22 @@ import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import org.slf4j.Logger
 
+import java.util.UUID
+
 trait System[F[_], H[_]] extends Subsystem[H] {
   val entryPoints: EntryPoints
-  val migrationStateRepository: PlayerDataRepository[TryableDeferred[F, Unit]]
 }
 
 object System {
+
+  import cats.implicits._
 
   def wired[
     F[_] : ConcurrentEffect : ContextShift,
     G[_] : SyncEffect : ContextCoercion[*[_], F],
     H[_]
-  ](implicit effectEnvironment: EffectEnvironment, logger: Logger): G[System[F, H]] = Sync[G].delay {
-
-    val migrations = {
+  ](implicit effectEnvironment: EffectEnvironment, logger: Logger): G[System[F, H]] = for {
+    migrations <- Sync[G].delay {
       implicit val syncIOUuidRepository: UuidRepository[SyncIO] = JdbcBackedUuidRepository
         .initializeStaticInstance[SyncIO]
         .unsafeRunSync()
@@ -43,29 +45,36 @@ object System {
       SeichiAssistItemMigrations.seq
     }
 
-    val service = ItemMigrationService.inContextOf[F](
+    service = ItemMigrationService.inContextOf[F](
       new PlayerItemsMigrationVersionRepository(SeichiAssist.seichiAssistConfig.getServerId),
       new PlayerItemsMigrationSlf4jLogger(logger)
     )
 
-    val repository = new PlayerItemMigrationStateRepository[G, F]
-    val playerItemMigrationController = new PlayerItemMigrationController[F, G](repository, migrations, service)
-
-    val systemEntryPoints = new EntryPoints {
-      override def runDatabaseMigration[I[_] : SyncEffect]: I[Unit] = {
-        DatabaseMigrationController[I](migrations).runDatabaseMigration
-      }
-
-      override def runWorldMigration: IO[Unit] = {
-        WorldMigrationController(migrations).runWorldMigration
-      }
-    }
+    repositoryControls <-
+      BukkitRepositoryControls.createSinglePhasedRepositoryAndHandles(
+        ItemMigrationStateRepositoryDefinitions.initialization[G],
+        ItemMigrationStateRepositoryDefinitions.finalization[G, UUID]
+      )
+  } yield {
+    val playerItemMigrationController = new PlayerItemMigrationController[F, G](
+      repositoryControls.repository, migrations, service
+    )
 
     new System[F, H] {
-      override val entryPoints: EntryPoints = systemEntryPoints
-      override val migrationStateRepository: PlayerDataRepository[TryableDeferred[F, Unit]] = repository
-      override val listeners: Seq[Listener] = Seq(repository, playerItemMigrationController)
-      override val managedFinalizers: Seq[PlayerDataFinalizer[H, Player]] = Nil
+      override val entryPoints: EntryPoints = new EntryPoints {
+        override def runDatabaseMigration[I[_] : SyncEffect]: I[Unit] = {
+          DatabaseMigrationController[I](migrations).runDatabaseMigration
+        }
+
+        override def runWorldMigration: IO[Unit] = {
+          WorldMigrationController(migrations).runWorldMigration
+        }
+      }
+      override val listeners: Seq[Listener] = Seq(
+        repositoryControls.initializer,
+        playerItemMigrationController
+      )
+      override val managedFinalizers: Seq[PlayerDataFinalizer[H, Player]] = Seq()
       override val commands: Map[String, TabExecutor] = Map()
     }
   }
