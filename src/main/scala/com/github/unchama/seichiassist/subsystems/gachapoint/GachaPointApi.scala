64d49ac7d99502d7af1debb05e6600c505d21f07
Kory <6561358+kory33@users.noreply.github.com>
2021-03-17 07:42:19 +0900

    [delete] gachapointシステムから受け取り設定に関する概念を削除する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gachapoint/GachaPointApi.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gachapoint/GachaPointApi.scala
@@ -4,21 +4,6 @@ import cats.data.Kleisli
 import com.github.unchama.datarepository.KeyedDataRepository
 import com.github.unchama.generic.effect.concurrent.ReadOnlyRef
 import com.github.unchama.seichiassist.subsystems.gachapoint.domain.gachapoint.GachaPoint
-import com.github.unchama.seichiassist.subsystems.gachapoint.domain.settings.GachaTicketReceivingSettings
-
-trait GachaPointSettingsApi[G[_], Player] {
-
-  /**
-   * プレーヤーの現在のガチャ券受け取り設定を保持するデータリポジトリ。
-   */
-  val ticketReceivingSettings: KeyedDataRepository[Player, ReadOnlyRef[G, GachaTicketReceivingSettings]]
-
-  /**
-   * プレーヤーのガチャ券受け取り設定をトグルする作用。
-   */
-  val toggleTicketReceivingSettings: Kleisli[G, Player, Unit]
-
-}
 
 trait GachaPointApi[F[_], G[_], Player] {
 
--- src/main/scala/com/github/unchama/seichiassist/subsystems/gachapoint/domain/settings/GachaTicketReceivingSettingsPersistence.scala
+++
@@ -1,7 +0,0 @@
-package com.github.unchama.seichiassist.subsystems.gachapoint.domain.settings
-
-import com.github.unchama.generic.RefDict
-
-import java.util.UUID
-
-trait GachaTicketReceivingSettingsPersistence[F[_]] extends RefDict[F, UUID, GachaTicketReceivingSettings]
