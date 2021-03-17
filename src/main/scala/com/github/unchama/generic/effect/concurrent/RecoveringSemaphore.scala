1d5823fdf0d366630cd868acbefbc8784574edb2
Kory <6561358+kory33@users.noreply.github.com>
2021-03-17 10:36:00 +0900

    [fix] RecoveringSemaphore.tryUseがセマフォの開放を待たないようにする

--- src/main/scala/com/github/unchama/generic/effect/concurrent/RecoveringSemaphore.scala
+++ src/main/scala/com/github/unchama/generic/effect/concurrent/RecoveringSemaphore.scala
@@ -9,23 +9,27 @@ import scala.concurrent.duration.FiniteDuration
 /**
  * 一定時間をおいて利用可能になるようなセマフォ。
  */
-final class RecoveringSemaphore[F[_] : Timer : Bracket[*[_], Throwable]] private(semaphore: Semaphore[F]) {
+final class RecoveringSemaphore[F[_] : Timer : Concurrent] private(semaphore: Semaphore[F]) {
 
+  import cats.effect.implicits._
   import cats.implicits._
 
   /**
    * このセマフォが利用可能であれば `action` を実行し、 `recoverTime` の間使用不能にする。
    */
-  def tryUse[U](action: F[U])(recoverTime: FiniteDuration): F[Unit] =
+  def tryUse[U](action: F[U])(recoverTime: FiniteDuration): F[Unit] = {
+    // セマフォを`recoverTime` の間使用不能にし、その後解放する作用
+    val releaseProgram: F[Unit] = Timer[F].sleep(recoverTime) >> semaphore.release
+
     semaphore.tryAcquire.flatMap { acquired =>
       if (acquired)
-        Bracket[F, Throwable].guarantee(action.as(())) {
-          Timer[F].sleep(recoverTime) >> semaphore.release
-        }
+        Bracket[F, Throwable].guarantee(action) {
+          releaseProgram.start.as(())
+        }.as(())
       else
         Monad[F].unit
     }
-
+  }
 }
 
 object RecoveringSemaphore {
