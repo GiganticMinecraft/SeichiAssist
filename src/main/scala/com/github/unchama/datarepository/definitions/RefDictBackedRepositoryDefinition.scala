20783037c88df970d56dc4288fc41177a0cb9669
Kory <6561358+kory33@users.noreply.github.com>
2021-03-19 19:46:02 +0900

    [update] RefDictBackedRepositoryDefinition.usingUuidRefDictを追加する

--- src/main/scala/com/github/unchama/datarepository/definitions/RefDictBackedRepositoryDefinition.scala
+++ src/main/scala/com/github/unchama/datarepository/definitions/RefDictBackedRepositoryDefinition.scala
@@ -1,6 +1,29 @@
 package com.github.unchama.datarepository.definitions
 
+import cats.Monad
+import com.github.unchama.datarepository.template.{PrefetchResult, RepositoryDefinition, RepositoryFinalization, SinglePhasedRepositoryInitialization}
+import com.github.unchama.generic.RefDict
+
+import java.util.UUID
+
 object RefDictBackedRepositoryDefinition {
 
+  import cats.implicits._
+
+  def usingUuidRefDict[F[_] : Monad, Player, R](refDict: RefDict[F, UUID, R])
+                                               (defaultValue: R): RepositoryDefinition[F, Player, R] = {
+    val initialization: SinglePhasedRepositoryInitialization[F, R] =
+      (uuid, _) => refDict
+        .read(uuid)
+        .map(_.getOrElse(defaultValue))
+        .map(PrefetchResult.Success.apply)
+
+    val finalization: RepositoryFinalization[F, UUID, R] = new RepositoryFinalization[F, UUID, R] {
+      override val persistPair: (UUID, R) => F[Unit] = (uuid, r) => refDict.write(uuid, r)
+      override val finalizeBeforeUnload: (UUID, R) => F[Unit] = (_, _) => Monad[F].unit
+    }
+
+    RepositoryDefinition.SinglePhased.withoutTappingAction(initialization, finalization)
+  }
 
 }
