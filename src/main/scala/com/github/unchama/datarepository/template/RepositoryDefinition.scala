f049c09d98c2d8499fd9d811dd0bcc3008e12e78
Kory <6561358+kory33@users.noreply.github.com>
2021-03-22 16:38:50 +0900

    [fix] コンパイルエラーを修正する

--- src/main/scala/com/github/unchama/datarepository/template/RepositoryDefinition.scala
+++ src/main/scala/com/github/unchama/datarepository/template/RepositoryDefinition.scala
@@ -91,6 +91,20 @@ object RepositoryDefinition {
     extends RepositoryDefinition[F, Player, R] {
     override type Self[S] = TwoPhased[F, Player, S]
 
+    def flatXmapWithIntermediateEffects[S](f: R => F[S])
+                                          (beforePersisting: S => F[R])(beforeFinalization: S => F[R])
+                                          (implicit F: Monad[F]): TwoPhased[F, Player, S] =
+      RepositoryDefinition.TwoPhased(
+        new TwoPhasedRepositoryInitialization[F, Player, S] {
+          override type IntermediateData = initialization.IntermediateData
+          override val prefetchIntermediateValue: (UUID, String) => F[PrefetchResult[IntermediateData]] =
+            initialization.prefetchIntermediateValue
+          override val prepareData: (Player, IntermediateData) => F[S] =
+            (player, i) => initialization.prepareData(player, i).flatMap(f)
+        },
+        finalization.withIntermediateEffects(beforePersisting)(beforeFinalization)
+      )
+
     def xmapWithPlayer[S](f: Player => R => F[S])(g: S => F[R])(implicit F: Monad[F]): TwoPhased[F, Player, S] =
       RepositoryDefinition.TwoPhased(
         new TwoPhasedRepositoryInitialization[F, Player, S] {
