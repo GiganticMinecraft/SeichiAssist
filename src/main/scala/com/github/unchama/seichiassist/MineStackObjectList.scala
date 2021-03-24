0023864e5d00126a5adc3aea7461ac900ef255f9
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2021-03-24 17:36:12 +0900

    Update src/main/scala/com/github/unchama/seichiassist/MineStackObjectList.scala

--- src/main/scala/com/github/unchama/seichiassist/MineStackObjectList.scala
+++ src/main/scala/com/github/unchama/seichiassist/MineStackObjectList.scala
@@ -442,7 +442,7 @@ object MineStackObjectList {
     new MineStackBuildObj("dye_14", "橙色の染料", 1, Material.INK_SACK, 14),
     new MineStackBuildObj("dye_15", "骨粉", 1, Material.INK_SACK, 15),
     new MineStackBuildObj("ink_sack0", "イカスミ", 1, Material.INK_SACK, 0),
-    new MineStackBuildObj("workbench", "作業台", 1, Material.WORKBENCH, 0),
+    new MineStackBuildObj("bed", "白色のベッド", 1, Material.BED, 0),
     new MineStackBuildObj("bed_1", "橙色のベッド", 1, Material.BED, 1),
     new MineStackBuildObj("bed_2", "赤紫色のベッド", 1, Material.BED, 2),
     new MineStackBuildObj("bed_3", "空色のベッド", 1, Material.BED, 3),
