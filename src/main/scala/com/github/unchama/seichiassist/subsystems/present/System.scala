cf52a9dafb22dd1192cdf533da1f19d813dc403b
KisaragiEffective <kisaragi.effective@gmail.com>
2021-03-26 19:15:02 +0900

    [update] fix compile error

--- src/main/scala/com/github/unchama/seichiassist/subsystems/present/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/present/System.scala
@@ -1,19 +1,24 @@
 package com.github.unchama.seichiassist.subsystems.present
 
-import cats.effect.{ConcurrentEffect, Sync}
+import cats.effect.ConcurrentEffect
 import com.github.unchama.concurrent.NonServerThreadContextShift
 import com.github.unchama.generic.effect.unsafe.EffectEnvironment
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 import com.github.unchama.seichiassist.subsystems.present.bukkit.command.PresentCommand
 import com.github.unchama.seichiassist.subsystems.present.infrastructure.JdbcBackedPresentPersistence
+import org.bukkit.command.TabExecutor
 
 object System {
   def wired[
-    AsyncContext[_] : ConcurrentEffect : NonServerThreadContextShift,
-    F[_]
-  ](implicit environment: EffectEnvironment): Subsystem[F] = {
+    AsyncContext[_] : ConcurrentEffect : NonServerThreadContextShift
+  ](implicit environment: EffectEnvironment): Subsystem[AsyncContext] = {
     implicit val repo: JdbcBackedPresentPersistence[AsyncContext] =
       new JdbcBackedPresentPersistence[AsyncContext]
-    Subsystem(Seq(), Nil, Map("present" -> PresentCommand.executor))
+
+    new Subsystem[AsyncContext] {
+      override val commands: Map[String, TabExecutor] = Map(
+        "present" -> PresentCommand.executor
+      )
+    }
   }
 }
--- src/main/scala/com/github/unchama/seichiassist/subsystems/present/bukkit/command/PresentCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/present/bukkit/command/PresentCommand.scala
@@ -17,8 +17,6 @@ import org.bukkit.{ChatColor, Material}
 import org.bukkit.command.TabExecutor
 import org.bukkit.entity.Player
 
-import java.util.UUID
-
 /**
  * `/present` コマンドを定義する。
  *
