285420137cb671a0d5b33d7e60aa67e386c3de16
KisaragiEffective <kisaragi.effective@gmail.com>
2021-04-01 21:08:54 +0900

    [update] 型安全でない`PlayerData`のプロパティを非推奨にする

--- src/main/scala/com/github/unchama/buildassist/TemporaryMutableBuildAssistPlayerData.scala
+++ src/main/scala/com/github/unchama/buildassist/TemporaryMutableBuildAssistPlayerData.scala
@@ -1,5 +1,7 @@
 package com.github.unchama.buildassist
 
+import com.github.unchama.buildassist.enums.{LineFillSlabPosition, LineFillStatusFlag}
+
 class TemporaryMutableBuildAssistPlayerData {
   var rectFillEnabled = false
   var rectFillIncludeUnderCaves = false
@@ -10,10 +12,18 @@ class TemporaryMutableBuildAssistPlayerData {
   /**
    * 直列設置設定フラグ
    */
+  @deprecated("unsafe enums backed by int.")
   var line_up_flg = 0
+  var lineFillStatus = LineFillStatusFlag.Disabled
+  @deprecated("unsafe enums backed by int.")
   var line_up_step_flg = 0
+  var lineFillSlabPosition = LineFillSlabPosition.Lower
+  @deprecated("unsafe enums backed by int. Consider migrate this to Boolean.")
   var line_up_des_flg = 0
+  var lineFillDestructWeakBlocks = false
+  @deprecated("unsafe enums backed by int. Consider migrate this to Boolean.")
   var line_up_minestack_flg = 0
+  var lineFillPrioritizeMineStack = false
 
   /**
    * ブロック範囲設置スキル設定フラグ
