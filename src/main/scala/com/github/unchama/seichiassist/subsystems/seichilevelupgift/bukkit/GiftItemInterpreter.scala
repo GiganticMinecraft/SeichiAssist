1d70977588cbb9c809a0ec13db89d845fe7e54b3
KisaragiEffective <kisaragi.effective@gmail.com>
2021-04-27 12:49:24 +0900

    [update] SequentialEffectの文脈を多相化

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/bukkit/GiftItemInterpreter.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/bukkit/GiftItemInterpreter.scala
@@ -1,8 +1,11 @@
 package com.github.unchama.seichiassist.subsystems.seichilevelupgift.bukkit
 
 import cats.data.Kleisli
+import cats.effect.{IO, LiftIO, Sync, SyncIO}
+import cats.kernel.Monoid
 import com.github.unchama.seichiassist.SeichiAssist
 import com.github.unchama.minecraft.actions.OnMinecraftServerThread
+import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.onMainThread
 import com.github.unchama.seichiassist.data.{GachaSkullData, ItemData}
 import com.github.unchama.seichiassist.subsystems.seichilevelupgift.domain.Gift
 import com.github.unchama.seichiassist.subsystems.seichilevelupgift.domain.Gift.Item
@@ -15,7 +18,7 @@ import org.bukkit.entity.Player
 /**
  * アイテムギフトの付与を実行するインタプリタ。
  */
-class GiftItemInterpreter[F[_] : OnMinecraftServerThread] extends (Gift.Item => Kleisli[F, Player, Unit]) {
+class GiftItemInterpreter[F[_] : OnMinecraftServerThread : Sync] extends (Gift.Item => Kleisli[F, Player, Unit]) {
 
   override def apply(item: Gift.Item): Kleisli[F, Player, Unit] = {
     val itemStack = item match {
@@ -26,12 +29,13 @@ class GiftItemInterpreter[F[_] : OnMinecraftServerThread] extends (Gift.Item => 
     }
 
     val message = item match {
-      case Item.GachaTicket => Some(MessageEffect("レベルアップ記念のガチャ券を配布しました。"))
+      case Item.GachaTicket => Some(MessageEffectF[F]("レベルアップ記念のガチャ券を配布しました。"))
       case _ => None
     }
 
-    // この明示的な型変数がないとビルドが通らない
-    SequentialEffect[Player](message.toList :+ grantItemStacksEffect(itemStack))
+    import cats.implicits._
+    import cats.effect.implicits._
+    SequentialEffect(message.toList :+ grantItemStacksEffect[F]())
   }
 
 }
