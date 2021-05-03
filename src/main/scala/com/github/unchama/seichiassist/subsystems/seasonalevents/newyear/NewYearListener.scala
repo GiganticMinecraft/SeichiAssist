85dd463dc02dda9280bf3bf26e90911386a18c82
Lucky <lucky3028@users.noreply.github.com>
2021-05-03 20:16:25 +0900

    [Add] イベント開催中であることを参加時に通知する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/newyear/NewYearListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/newyear/NewYearListener.scala
@@ -8,7 +8,7 @@ import com.github.unchama.seichiassist.ManagedWorld._
 import com.github.unchama.seichiassist.MaterialSets
 import com.github.unchama.seichiassist.subsystems.mana.ManaWriteApi
 import com.github.unchama.seichiassist.subsystems.seasonalevents.domain.LastQuitPersistenceRepository
-import com.github.unchama.seichiassist.subsystems.seasonalevents.newyear.NewYear.{START_DATE, isInEvent, itemDropRate}
+import com.github.unchama.seichiassist.subsystems.seasonalevents.newyear.NewYear._
 import com.github.unchama.seichiassist.subsystems.seasonalevents.newyear.NewYearItemData._
 import com.github.unchama.seichiassist.util.Util.{addItem, dropItem, grantItemStacksEffect, isPlayerInventoryFull}
 import com.github.unchama.targetedeffect.TargetedEffect.emptyEffect
@@ -34,6 +34,19 @@ class NewYearListener[
   ioOnMainThread: OnMinecraftServerThread[IO]) extends Listener {
 
   import cats.implicits._
+
+  @EventHandler
+  def onPlayerJoin(event: PlayerJoinEvent): Unit = {
+    if (isInEvent) {
+      List(
+        s"$LIGHT_PURPLE${END_DATE}までの期間限定で、新年イベントを開催しています。",
+        "詳しくは下記URLのサイトをご覧ください。",
+        s"$DARK_GREEN$UNDERLINE$blogArticleUrl"
+      ).foreach(
+        player.sendMessage(_)
+      )
+    }
+  }
 
   @EventHandler
   def giveSobaToPlayer(event: PlayerJoinEvent): Unit = {
