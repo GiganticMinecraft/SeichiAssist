c20a2fbcdbe9ac7c40556b728ed363c28122bb69
Lucky <lucky3028@users.noreply.github.com>
2021-05-04 15:06:42 +0900

    [Fix] メッセージをわかりやすく

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/System.scala
@@ -26,7 +26,7 @@ object System {
         case Gift.AutomaticGachaRun => Kleisli {
           player =>
             Sync[F].delay {
-              player.sendMessage("レベルアップ記念としてガチャを実行しました。")
+              player.sendMessage("レベルアップ記念としてガチャを回しました。")
               GachaCommand.Gachagive(player, 1, player.getName)
             }
         }
