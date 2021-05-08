6a6237999a1d380a4cd815879c37b169fe24582e
Lucky <lucky3028@users.noreply.github.com>
2021-05-08 16:34:39 +0900

    [Fix] コメントを名称変更に合わせる

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/limitedlogin/LoginBonusItemList.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/limitedlogin/LoginBonusItemList.scala
@@ -5,7 +5,7 @@ sealed trait LoginBonusDay
 object LoginBonusDay {
 
   case class TotalDay(totalDay: Int) extends LoginBonusDay {
-    require(totalDay > 0, "Login bonus count must be positive")
+    require(totalDay > 0, "Total day must be positive")
   }
 
   case object Everyday extends LoginBonusDay
