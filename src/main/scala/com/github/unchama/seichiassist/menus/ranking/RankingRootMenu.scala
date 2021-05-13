9fcdfb364fb75ed265064dd5ad2723340fc918b4
Kory <6561358+kory33@users.noreply.github.com>
2021-05-14 05:03:00 +0900

    [update] RankingRootMenuのコードを単純化する

--- src/main/scala/com/github/unchama/seichiassist/menus/ranking/RankingRootMenu.scala
+++ src/main/scala/com/github/unchama/seichiassist/menus/ranking/RankingRootMenu.scala
@@ -1,20 +1,20 @@
 package com.github.unchama.seichiassist.menus.ranking
 
 import cats.effect.IO
-import com.github.unchama.itemstackbuilder.{IconItemStackBuilder, SkullItemStackBuilder}
+import com.github.unchama.itemstackbuilder.IconItemStackBuilder
 import com.github.unchama.menuinventory.router.CanOpen
 import com.github.unchama.menuinventory.slot.button.Button
 import com.github.unchama.menuinventory.slot.button.action.LeftClickButtonEffect
 import com.github.unchama.menuinventory.syntax._
 import com.github.unchama.menuinventory.{ChestSlotRef, Menu, MenuFrame, MenuSlotLayout}
-import com.github.unchama.seichiassist.SkullOwners
 import com.github.unchama.seichiassist.effects.player.CommonSoundEffects
 import com.github.unchama.seichiassist.menus.CommonButtons
-import com.github.unchama.seichiassist.menus.stickmenu.{FirstPage, StickMenu}
+import com.github.unchama.seichiassist.menus.stickmenu.FirstPage
 import eu.timepit.refined.auto._
 import org.bukkit.ChatColor._
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 object RankingRootMenu extends Menu {
   class Environment(implicit val ioCanOpenFirstPage: IO CanOpen FirstPage.type,
@@ -34,93 +34,57 @@ object RankingRootMenu extends Menu {
   override def computeMenuLayout(player: Player)(implicit environment: Environment): IO[MenuSlotLayout] = IO {
     import environment._
 
-    val seichiGodRankingButton: Button = {
-      val iconItemStack =
-        new IconItemStackBuilder(Material.COOKIE)
-          .title(s"$YELLOW$UNDERLINE${BOLD}整地神ランキングを見る")
-          .lore(List(
-            s"$RESET$RED(整地神ランキング150位以内のプレイヤーのみ表記されます)",
-            s"$RESET$DARK_RED${UNDERLINE}クリックで開く"
-          ))
-          .build()
+    def iconOf(rankingName: String): ItemStack =
+      new IconItemStackBuilder(Material.COOKIE)
+        .title(s"$YELLOW$UNDERLINE$BOLD${rankingName}を見る")
+        .lore(List(
+          s"$RESET$RED(${rankingName}150位以内のプレイヤーのみ表記されます)",
+          s"$RESET$DARK_RED${UNDERLINE}クリックで開く"
+        ))
+        .build()
 
+    val seichiGodRankingButton: Button =
       Button(
-        iconItemStack,
+        iconOf("整地神ランキング"),
         LeftClickButtonEffect(
           CommonSoundEffects.menuTransitionFenceSound,
           ioCanOpenSeichiRankingMenu.open(SeichiRankingMenu(0)),
         )
       )
-    }
 
-    val loginGodRankingButton: Button = {
-      val iconItemStack =
-        new IconItemStackBuilder(Material.COOKIE)
-          .title(s"$YELLOW$UNDERLINE${BOLD}ログイン神ランキングを見る")
-          .lore(List(
-            s"$RESET$RED(ログイン神ランキング150位以内のプレイヤーのみ表記されます)",
-            s"$RESET$DARK_RED${UNDERLINE}クリックで開く"))
-          .build()
-
+    val loginGodRankingButton: Button =
       Button(
-        iconItemStack,
+        iconOf("ログイン神ランキング"),
         LeftClickButtonEffect(
           CommonSoundEffects.menuTransitionFenceSound,
           ioCanOpenLoginTimeRankingMenu.open(LoginTimeRankingMenu(0))
         )
       )
-    }
 
-    val voteGodRankingButton: Button = {
-      val iconItemStack =
-        new IconItemStackBuilder(Material.COOKIE)
-          .title(s"$YELLOW$UNDERLINE${BOLD}投票神ランキングを見る")
-          .lore(List(
-            s"$RESET$RED(投票神ランキング150位以内のプレイヤーのみ表記されます)",
-            s"$RESET$DARK_RED${UNDERLINE}クリックで開く"
-          ))
-          .build()
-
+    val voteGodRankingButton: Button =
       Button(
-        iconItemStack,
+        iconOf("投票神ランキング"),
         LeftClickButtonEffect(
           CommonSoundEffects.menuTransitionFenceSound,
           ioCanOpenVoteCountRankingMenu.open(VoteCountRankingMenu(0))
         )
       )
-    }
 
-    val buildGodRankingButton = {
-      val iconItemStack =
-        new IconItemStackBuilder(Material.COOKIE)
-          .title(s"$YELLOW$UNDERLINE${BOLD}建築神ランキングを見る")
-          .lore(List(
-            s"$RESET$RED(建築神ランキング150位以内のプレイヤーのみ表記されます)",
-            s"$RESET$DARK_RED${UNDERLINE}クリックで開く"
-          ))
-          .build()
-
+    val buildGodRankingButton =
       Button(
-        iconItemStack,
+        iconOf("建築神ランキング"),
         LeftClickButtonEffect(
           CommonSoundEffects.menuTransitionFenceSound,
           ioCanOpenBuildRankingMenu.open(BuildRankingMenu(0))
         )
       )
-    }
 
     MenuSlotLayout(
       ChestSlotRef(1, 1) -> seichiGodRankingButton,
       ChestSlotRef(1, 3) -> loginGodRankingButton,
       ChestSlotRef(1, 5) -> voteGodRankingButton,
       ChestSlotRef(1, 7) -> buildGodRankingButton,
-      ChestSlotRef(3, 0) -> {
-        CommonButtons.transferButton(
-          new SkullItemStackBuilder(SkullOwners.MHF_ArrowLeft),
-          "メインメニューへ",
-          StickMenu.firstPage
-        )
-      }
+      ChestSlotRef(3, 0) -> CommonButtons.openStickMenu
     )
   }
 }
