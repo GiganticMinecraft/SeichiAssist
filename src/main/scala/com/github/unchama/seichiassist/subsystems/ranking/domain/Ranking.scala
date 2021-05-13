ad51b22129fe7d98363eff1c28d4ccb7c2590a93
Kory <6561358+kory33@users.noreply.github.com>
2021-05-14 06:26:27 +0900

    [update] RankingのRに常にMonoidを要求するようにする

--- src/main/scala/com/github/unchama/seichiassist/subsystems/ranking/domain/Ranking.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/ranking/domain/Ranking.scala
@@ -6,14 +6,14 @@ import cats.{Monoid, Order}
  * ある時点でのランキング全体の情報を持つオブジェクトのクラス。
  * @tparam R 各プレーヤーのレコードが持つデータ型
  */
-class Ranking[R: Order](records: Vector[RankingRecord[R]]) {
+class Ranking[R: Order: Monoid](records: Vector[RankingRecord[R]]) {
   import cats.implicits._
 
   private val sortedRecords: Vector[RankingRecord[R]] = records.sortBy(_.value).reverse
 
   val recordCount: Int = records.size
 
-  def total(implicit ev: Monoid[R]): R = ev.combineAll(records.map(_.value))
+  val total: R = Monoid[R].combineAll(records.map(_.value))
 
   val recordsWithPositions: Vector[(Int, RankingRecord[R])] =
     sortedRecords.zipWithIndex.map { case (record, i) => (i + 1, record) }
