be47b19d665143234d55ec185ddf9899ce26acd9
Kory <6561358+kory33@users.noreply.github.com>
2021-05-18 16:50:06 +0900

    [update] テーブル名を展開する
    こうしないと静的チェックが効かない

--- src/main/scala/com/github/unchama/seichiassist/subsystems/subhome/infrastructure/SubHomePersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/subhome/infrastructure/SubHomePersistence.scala
@@ -10,13 +10,13 @@ import scalikejdbc._
 import java.util.UUID
 
 class SubHomePersistence[F[_]: Sync] extends SubHomeReadAPI[F] with SubHomeWriteAPI[F] {
-  final val table = "seichiassist.sub_home"
   private val serverId = SeichiAssist.seichiAssistConfig.getServerNum
+
   override def get(player: UUID, id: SubHome.ID): F[Option[SubHome]] = {
     Sync[F].delay {
       DB.readOnly { implicit session =>
-        sql"""SELECT id, name, location_x, location_y, location_z, world_name FROM $table
-             WHERE player = ${player.toString} AND server_id = $serverId AND id = $id"""
+        sql"""SELECT id, name, location_x, location_y, location_z, world_name FROM seichiassist.sub_home
+              WHERE name = ${player.toString} AND server_id = $serverId AND id = $id"""
           .map(extractSubHome)
           // もしかすると見つからないかもしれない
           .first()
@@ -27,7 +27,7 @@ class SubHomePersistence[F[_]: Sync] extends SubHomeReadAPI[F] with SubHomeWrite
 
   override def list(player: UUID): F[Map[SubHome.ID, SubHome]] = Sync[F].delay {
     DB.readOnly { implicit session =>
-      sql"""SELECT id, name, location_x, location_y, location_z, world_name FROM $table"""
+      sql"""SELECT id, name, location_x, location_y, location_z, world_name FROM seichiassist.sub_home"""
         .map(rs => {
           (
             rs.int("id"),
@@ -47,7 +47,7 @@ class SubHomePersistence[F[_]: Sync] extends SubHomeReadAPI[F] with SubHomeWrite
       val worldName = location.getWorld.getName
       DB.localTx { implicit session =>
         // 重複したとき、もとのエントリを残す必要はないので黙って上書きする
-        sql"""insert into $table
+        sql"""insert into seichiassist.sub_home
              |(player_uuid,server_id,id,location_x,location_y,location_z,world_name) values
              |(${player.toString},$serverId,$id,$x,$y,$z,$worldName)
              |on duplicate key update
@@ -64,8 +64,8 @@ class SubHomePersistence[F[_]: Sync] extends SubHomeReadAPI[F] with SubHomeWrite
   override def updateName(player: UUID, number: SubHome.ID, name: String): F[Unit] =
     Sync[F].delay {
       DB.localTx { implicit session =>
-        sql"""insert into $table
-             |(player_uuid,server_id,id,name) values
+        sql"""insert into seichiassist.sub_home
+             |(player_uuid, server_id, id, name) values
              |(${player.toString},$serverId,$number,$name)
              |on duplicate key update
              |name = values(name)"""
