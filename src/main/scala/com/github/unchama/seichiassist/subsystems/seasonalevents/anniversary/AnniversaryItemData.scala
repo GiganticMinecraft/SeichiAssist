cbe1f454204e400c99f48685a2579e39166d4f6b
Lucky <Lucky3028@users.noreply.github.com>
2021-06-01 13:09:41 +0900

    [Fix] SkullItemStackBuilderのloreメソッドのうち可変長引数を利用する
    Co-authored-by: Kisaragi <48310258+KisaragiEffective@users.noreply.github.com>

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/anniversary/AnniversaryItemData.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/anniversary/AnniversaryItemData.scala
@@ -21,10 +21,10 @@ object AnniversaryItemData {
   private val mineChan = SkullOwnerTextureValue("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDhmNTQ0OGI0ZDg4ZTQwYjE0YzgyOGM2ZjFiNTliMzg1NDVkZGE5MzNlNzNkZmYzZjY5NWU2ZmI0Mjc4MSJ9fX0=")
   val mineHead: ItemStack = new SkullItemStackBuilder(mineChan)
     .title("まいんちゃん")
-    .lore(List(
+    .lore(
       "",
       s"${YELLOW}ギガンティック☆整地鯖${ANNIVERSARY_COUNT}周年記念だよ！"
-    ))
+    )
     .build()
 
   //endregion
