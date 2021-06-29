f701c284b66169e8cf1cf4f1bd1b22cf8ed6eb7e
Lucky <Lucky3028@users.noreply.github.com>
2021-06-29 14:11:05 +0900

    [Fix] n周年記念企画のブログ記事のURLを修正

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/anniversary/Anniversary.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/anniversary/Anniversary.scala
@@ -8,7 +8,7 @@ object Anniversary {
   val EVENT_YEAR = 2021
   val ANNIVERSARY_COUNT = EVENT_YEAR - 2016
   val EVENT_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 6, 29)
-  val blogArticleUrl: String = validateUrl(s"https://www.seichi.network/post/anniversary$EVENT_YEAR")
+  val blogArticleUrl: String = validateUrl(s"https://www.seichi.network/post/${ANNIVERSARY_COUNT}th_anniv")
 
   def isInEvent: Boolean = EVENT_DATE.equals(LocalDate.now())
 }
