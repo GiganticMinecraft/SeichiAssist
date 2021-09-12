149b28de26709a952dd7d0edade63006a9a7e0e2
Moca3 <white@nagi.be>
2021-09-12 18:21:25 +0900

    [TEST]型の変更
    TabExecutorからContextualExecutorにしてみて、playerの型がどうなるかをみてみたい(自環境ではわからない..)

--- src/main/scala/com/github/unchama/seichiassist/commands/FourDimensionalPocketCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/commands/FourDimensionalPocketCommand.scala
@@ -1,20 +1,23 @@
 package com.github.unchama.seichiassist.commands
 
 import cats.effect.IO
+import com.github.unchama.contextualexecutor.ContextualExecutor
 import org.bukkit.entity.Player
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seichiassist.subsystems.fourdimensionalpocket.FourDimensionalPocketApi
 import com.github.unchama.targetedeffect.UnfocusedEffect
 import org.bukkit.command.TabExecutor
+
 /**
  * 四次元ポケットを開くコマンド
  * StickMenuCommand.scalaを参考に作成
  */
-object FourDimensionalPocketCommand{
-    def executor(implicit api: FourDimensionalPocketApi[IO, Player] ):TabExecutor =
+object FourDimensionalPocketCommand {
+    def executor(implicit api: FourDimensionalPocketApi[IO, Player] ):ContextualExecutor = {
     playerCommandBuilder
-      .execution {player => IO.pure(UnfocusedEffect(api.openPocketInventory(player:Player)))}
+      .execution {player=> IO.pure(UnfocusedEffect(api.openPocketInventory(player)))}
       .build()
-      .asNonBlockingTabExecutor()
+        
+    }
 }
 
