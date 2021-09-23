3448d35d5f7a773ad366fa8a1c52a000992a4e92
Tea <72652320+LargoGreenTea@users.noreply.github.com>
2021-09-23 18:22:00 +0900

    [Add]ボタンの場所

--- src/main/scala/com/github/unchama/seichiassist/menus/stickmenu/SecondPage.scala
+++ src/main/scala/com/github/unchama/seichiassist/menus/stickmenu/SecondPage.scala
@@ -57,6 +57,7 @@ object SecondPage extends Menu {
       ChestSlotRef(0, 8) -> hubCommandButton,
       ChestSlotRef(3, 0) -> CommonButtons.openStickMenu,
       ChestSlotRef(3, 3) -> recycleBinButton,
+      ChestSlotRef(3, 6) -> ItemNameRemoveButton,
       ChestSlotRef(3, 7) -> titanConversionButton,
       ChestSlotRef(3, 8) -> appleConversionButton
     )
