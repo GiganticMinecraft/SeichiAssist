20c1150b66d67e4b2be3cd9d8a493ed458b4a4b9
Lucky <lucky3028@users.noreply.github.com>
2021-09-23 21:17:45 +0900

    [Add] 21億企画報酬の一部のアイテムに耐久力5と修繕エンチャントを付与

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/itemmigration/migrations/V1_4_0_AddEnchantsTo2_1billionRewardItems.scala
@@ -0,0 +1,68 @@
+package com.github.unchama.seichiassist.subsystems.itemmigration.migrations
+
+import com.github.unchama.itemmigration.bukkit.util.MigrationHelper
+import com.github.unchama.itemmigration.domain.{ItemMigration, ItemMigrationVersionNumber}
+import org.bukkit.ChatColor._
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemStack
+
+import scala.jdk.CollectionConverters._
+
+/**
+ * 2021年に行われた5周年記念イベントの中の、21億企画の目標達成報酬として配布されたアイテムに修繕と耐久力5エンチャントを付与する
+ */
+object V1_4_0_AddEnchantsTo2_1billionRewardItems {
+
+  // 年の判別用
+  private val commonLore = s"${WHITE}21/07/22に開催された5周年記念企画"
+
+  // アイテム名だけだと、21億企画報酬ではなく以前のイベントで交換可能だった当該アイテムも含んでしまいかねないので、説明文も確認する
+  private val titanReplicaName = Seq(
+    RED -> "T",
+    GOLD -> "I",
+    YELLOW -> "T",
+    GREEN -> "A",
+    BLUE -> "N ", // スペースは故意
+    WHITE -> "Replica II"
+  ).map { case (color, str) => s"$color$BOLD$ITALIC$str" }.mkString
+  private val titanReplicaLore = s"${WHITE}30億/1日を突破した記念に配布されたものです。"
+
+  private val gaeaReplicaName = Seq(
+    RED -> "G",
+    GOLD -> "A",
+    YELLOW -> "E",
+    GREEN -> "A ", // スペースは故意
+    WHITE -> "Whiteday Replica"
+  ).map { case (color, str) => s"$color$BOLD$ITALIC$str" }.mkString
+  private val gaeaReplicaLore = s"${WHITE}35億/1日を突破した記念に配布されたものです。"
+
+  def migration: ItemMigration = ItemMigration(
+    ItemMigrationVersionNumber(1, 4, 0),
+    MigrationHelper.delegateConversionForContainers(migrationFunction)
+  )
+
+  import eu.timepit.refined.auto._
+
+  def migrationFunction(itemStack: ItemStack): ItemStack = {
+    if (!is2_1billionRewardItems(itemStack)) return itemStack
+
+    import scala.util.chaining._
+
+    itemStack.clone().addEnchantments(Map(Enchantment.MENDING -> 1, Enchantment.DURABILITY -> 5).asJava)
+  }
+
+  def is2_1billionRewardItems(itemStack: ItemStack): Boolean = {
+    if (itemStack == null || !itemStack.hasItemMeta || !itemStack.getItemMeta.hasDisplayName || !itemStack.getItemMeta.hasLore) return false
+    isRewardGaeaReplica(itemStack) || isRewardTitanReplica(itemStack)
+  }
+
+  def isRewardTitanReplica(item: ItemStack): Boolean = {
+    val lores = item.getItemMeta.getLore.asScala
+    item.getItemMeta.getDisplayName == titanReplicaName && lores.contains(titanReplicaLore) && lores.contains(commonLore)
+  }
+
+  def isRewardGaeaReplica(item: ItemStack): Boolean = {
+    val lores = item.getItemMeta.getLore.asScala
+    item.getItemMeta.getDisplayName == gaeaReplicaName && lores.contains(gaeaReplicaLore) && lores.contains(commonLore)
+  }
+}
