b3dc1405295de304f94a49b3692a85c497b035c2
Kory <6561358+kory33@users.noreply.github.com>
2021-10-03 10:50:51 +0900

    [fix] コマンド実行でexecutionの代わりにexecutionFを使うようにする

--- src/main/scala/com/github/unchama/seichiassist/subsystems/fourdimensionalpocket/bukkit/commands/FourDimensionalPocketCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/fourdimensionalpocket/bukkit/commands/FourDimensionalPocketCommand.scala
@@ -1,22 +1,19 @@
 package com.github.unchama.seichiassist.subsystems.fourdimensionalpocket.bukkit.commands
 
-import cats.effect.IO
-import org.bukkit.entity.Player
+import cats.effect.ConcurrentEffect
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seichiassist.subsystems.fourdimensionalpocket.FourDimensionalPocketApi
-import com.github.unchama.targetedeffect.UnfocusedEffect
 import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
 
 /**
  * プレイヤーが自分の四次元ポケットを開くコマンド
  * StickMenuCommand.scalaを参考に作成
  */
 object FourDimensionalPocketCommand {
-  def executor[F[_]](implicit api: FourDimensionalPocketApi[F, Player]): TabExecutor = {
+  def executor[F[_]: ConcurrentEffect](implicit api: FourDimensionalPocketApi[F, Player]): TabExecutor = {
     playerCommandBuilder
-      .execution { context =>
-        IO.pure(UnfocusedEffect(api.openPocketInventory(context.sender)))
-      }
+      .executionF(context => api.openPocketInventory(context.sender))
       .build()
       .asNonBlockingTabExecutor()
   }
