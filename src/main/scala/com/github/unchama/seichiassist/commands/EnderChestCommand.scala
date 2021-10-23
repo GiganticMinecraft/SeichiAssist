4dfdf18d0c3698674202f678c9a8b9af7ddb8fd6
KisaragiEffective <kisaragi.effective@gmail.com>
2021-10-24 02:25:43 +0900

    [fix] どこでもエンダーチェストを開くのに必要な整地レベルは25だったので合わせる

--- src/main/scala/com/github/unchama/seichiassist/commands/EnderChestCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/commands/EnderChestCommand.scala
@@ -1,24 +1,39 @@
 package com.github.unchama.seichiassist.commands
 
-import cats.effect.IO
+import cats.effect.{IO, SyncIO}
 import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
 import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.onMainThread
+import com.github.unchama.seichiassist.subsystems.breakcount.BreakCountReadAPI
+import com.github.unchama.seichiassist.subsystems.breakcount.domain.level.SeichiLevel
+import com.github.unchama.targetedeffect.commandsender.{MessageEffect, MessageEffectF}
 import com.github.unchama.targetedeffect.player.PlayerEffects
 import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
 
 /**
  * エンダーチェストを開くコマンド
  */
 object EnderChestCommand {
-  val executor: TabExecutor = playerCommandBuilder
-    .argumentsParsers(List())
-    .execution { context =>
-      val sender = context.sender
+  def executor(implicit breakAmountApi: BreakCountReadAPI[IO, SyncIO, Player]): TabExecutor =
+    playerCommandBuilder
+      .argumentsParsers(List())
+      .execution { context =>
+        val sender = context.sender
 
-      IO {
-        PlayerEffects.openInventoryEffect(sender.getEnderChest)
+        import cats.implicits._
+
+        for {
+          ref <- breakAmountApi.seichiAmountDataRepository(sender).read
+          level = ref.levelCorrespondingToExp
+        } yield {
+          if (level >= SeichiLevel(25)) {
+            // エンダーチェストは参照透明ではない
+            PlayerEffects.openInventoryEffect(sender.getEnderChest)
+          } else {
+            MessageEffect("整地レベルが25に達していないため、このコマンドは使えません。")
+          }
+        }
       }
-    }
-    .build()
-    .asNonBlockingTabExecutor()
+      .build()
+      .asNonBlockingTabExecutor()
 }
