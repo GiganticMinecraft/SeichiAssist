a66394122afed983066f0edb277002e2a4e0d6d2
Kisaragi <kisaragi.effective@gmail.com>
2022-01-05 02:37:07 +0900

    [update] プロトコルの調整

--- src/main/scala/com/github/unchama/processingstorage/ProcessingStorage.scala
+++ src/main/scala/com/github/unchama/processingstorage/ProcessingStorage.scala
@@ -2,10 +2,11 @@ package com.github.unchama.processingstorage
 
 import cats.data
 import cats.effect.IO
-import com.github.unchama.menuinventory.{LayoutPreparationContext, MenuFrame, MenuSession, MenuSlotLayout}
+import com.github.unchama.menuinventory.{LayoutPreparationContext, MenuFrame, MenuSession}
 import com.github.unchama.minecraft.actions.OnMinecraftServerThread
 import com.github.unchama.targetedeffect.TargetedEffect
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 /**
  * processing-storageのトレイト。
@@ -19,9 +20,22 @@ trait ProcessingStorage {
   val frame: MenuFrame
 
   /**
-   * @return `player`からメニューの[[MenuSlotLayout]]を計算する[[IO]]
+   * インベントリを閉じたときに発火される作用。
+   * @return `inventory`から変換されたアイテムのリストを計算する[[IO]]
    */
-  def doOperation(player: Player)(implicit environment: Environment): IO[MenuSlotLayout]
+  def doOperation(player: Player, inventory: Map[Int, ItemStack])(implicit environment: Environment): IO[List[ItemStack]]
+
+  /**
+   *
+   * @param itemStack 変換する前のアイテム
+   * @return 返す対象である場合変換後の[[ItemStack]]を包んだ[[Some]]、返さない場合[[None]]を返す[[IO]]。
+   */
+  def doMap(itemStack: ItemStack): IO[Option[ItemStack]]
+
+  /**
+   * @return 全ての変換が終わり、アイテム付与が終わったあとに発火されるプレイヤーに対するエフェクト。
+   */
+  def postEffect: IO[Unit] = IO.unit
 
   /**
    * メニューを[Player]に開かせる[TargetedEffect].
@@ -33,8 +47,8 @@ trait ProcessingStorage {
       session <- MenuSession.createNewSessionWith[IO](frame)
       _ <- session.openInventory.run(player)
       _ <- IO.shift(ctx)
-      layout <- doOperation(player)
-      _ <- session.overwriteViewWith(layout)
+      _ <- doOperation(player, session.getInventory.getContents.zipWithIndex.map(_.swap).toMap)
+      _ <- postEffect
     } yield ()
   }
 }
