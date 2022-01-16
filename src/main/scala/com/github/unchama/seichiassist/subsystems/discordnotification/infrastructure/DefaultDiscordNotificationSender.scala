3853437799bbfeeae81b9a6ae951e49bf112cc3d
Kisaragi <kisaragi.effective@gmail.com>
2022-01-16 20:36:42 +0900

    [update] WebhookのURLが指定されていなかった場合、デフォルト実装にフォールバックするように

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/discordnotification/infrastructure/DefaultDiscordNotificationSender.scala
@@ -0,0 +1,15 @@
+package com.github.unchama.seichiassist.subsystems.discordnotification.infrastructure
+
+import cats.effect.LiftIO
+import com.github.unchama.seichiassist.SeichiAssist
+import com.github.unchama.seichiassist.subsystems.discordnotification.DiscordNotificationAPI
+import io.chrisdavenport.log4cats.Logger
+
+/**
+ * この実装は[[send]]が呼ばれるたびに警告をロガーに流す以外は何もしない。
+ */
+final class DefaultDiscordNotificationSender[F[_]: Logger: LiftIO] extends DiscordNotificationAPI[F] {
+  override def send(message: String): F[Unit] = {
+    SeichiAssist.instance.loggerF.warn("Discordへの送信が試みられましたが、URLが無効、もしくは与えられていません。コンフィグを確認してください。").to
+  }
+}
