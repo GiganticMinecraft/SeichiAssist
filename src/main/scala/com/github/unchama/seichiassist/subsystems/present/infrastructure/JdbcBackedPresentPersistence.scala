4054e373ec9902db1e7aa18bf88a51678385a765
Kisaragi <kisaragi.effective@gmail.com>
2022-01-19 23:06:18 +0900

    [update] generalize internal method

--- src/main/scala/com/github/unchama/seichiassist/subsystems/present/infrastructure/JdbcBackedPresentPersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/present/infrastructure/JdbcBackedPresentPersistence.scala
@@ -2,6 +2,7 @@ package com.github.unchama.seichiassist.subsystems.present.infrastructure
 
 import cats.Applicative
 import cats.effect.Sync
+import com.github.unchama.generic.MapExtra
 import com.github.unchama.seichiassist.subsystems.present.domain.OperationResult.DeleteResult
 import com.github.unchama.seichiassist.subsystems.present.domain.{GrantRejectReason, PaginationRejectReason, PresentClaimingState, PresentPersistence, RevokeWarning}
 import eu.timepit.refined.api.Refined
@@ -167,7 +168,7 @@ class JdbcBackedPresentPersistence[F[_] : Sync] extends PresentPersistence[F, It
             .apply()
         }
 
-        Right(filledEntries(associatedEntries, idSliceWithPagination).toList)
+        Right(MapExtra.fillOnBaseSet(associatedEntries.toMap, idSliceWithPagination.toSet, PresentClaimingState.Unavailable).toList)
       }
     }
   }
@@ -185,7 +186,7 @@ class JdbcBackedPresentPersistence[F[_] : Sync] extends PresentPersistence[F, It
           .apply()
       }
 
-      filledEntries(associatedEntries, validPresentIDs).toMap
+      MapExtra.fillOnBaseSet(associatedEntries.toMap, validPresentIDs.toSet, PresentClaimingState.Unavailable).toMap
     }
   }
 
@@ -221,19 +222,6 @@ class JdbcBackedPresentPersistence[F[_] : Sync] extends PresentPersistence[F, It
 
   private def unwrapItemStack(rs: WrappedResultSet): ItemStack = {
     ItemStackBlobProxy.blobToItemStack(rs.string("itemstack"))
-  }
-
-  /**
-   * @param knownState データベース上にあるエントリ
-   * @param validGlobalId データベース上で物理削除されていないPresentID
-   * @return PresentIDとPresentClaimingStateの組
-   */
-  private def filledEntries(knownState: List[(PresentID, PresentClaimingState)], validGlobalId: Iterable[PresentID]): Map[PresentID, PresentClaimingState] = {
-    val knownId = knownState.map(_._1)
-    val outOfTarget = validGlobalId.toSeq.diff(knownId)
-    val defaultEntry = outOfTarget.map(id => (id, PresentClaimingState.Unavailable))
-    // 内容の喪失が起こらない変換
-    (defaultEntry ++ knownState).toMap
   }
 
   private def computeValidPresentCount: F[Long] = Sync[F].delay {
---
+++ src/test/scala/com/github/unchama/generic/MapExtraSpec.scala
@@ -0,0 +1,39 @@
+package com.github.unchama.generic
+
+import org.scalatest.wordspec.AnyWordSpec
+
+class MapExtraSpec extends AnyWordSpec {
+  "fillOnBaseSet" should {
+    "return empty Map" in {
+      assert(MapExtra.fillOnBaseSet[String, Int](Map(), Set(), 1).isEmpty)
+    }
+
+    "fail if map's key is not sub-set of set" in {
+      assertThrows[IllegalArgumentException] {
+        MapExtra.fillOnBaseSet[String, Int](Map("a" -> 2), Set(), 1)
+      }
+    }
+
+    "return filled Map" in {
+      assert(
+        MapExtra.fillOnBaseSet(
+          Map("a" -> 1, "b" -> 2, "c" -> 3),
+          Set("a", "b", "c", "d"),
+          4
+        ) == Map("a" -> 1, "b" -> 2, "c" -> 3, "d" -> 4)
+      )
+    }
+
+    "return filled Map when given Map is empty" in {
+      assert(
+        MapExtra.fillOnBaseSet(
+          Map(),
+          Set("A", "B", "C", "D"),
+          42
+        ) == Map("A" -> 42, "B" -> 42, "C" -> 42, "D" -> 42)
+      )
+    }
+
+
+  }
+}
