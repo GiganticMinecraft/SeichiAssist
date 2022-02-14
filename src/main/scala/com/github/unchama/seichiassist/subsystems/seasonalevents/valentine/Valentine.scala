516f4b73ae6f03b211a6abb768bc5322138782df
Lucky <Lucky3028@users.noreply.github.com>
2022-02-14 14:37:20 +0900

    fix: 脱字修正

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/valentine/Valentine.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/valentine/Valentine.scala
@@ -5,7 +5,7 @@ import java.time.LocalDate
 
 object Valentine {
   val EVENT_YEAR: Int = 2022
-  val START_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 2, 1)
+  val START_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 2, 15)
   val END_DATE: LocalDate = LocalDate.of(EVENT_YEAR, 2, 27)
   val itemDropRate: Double = validateItemDropRate(0.3)
   val blogArticleUrl: String = validateUrl(s"https://www.seichi.network/post/valentine$EVENT_YEAR")
