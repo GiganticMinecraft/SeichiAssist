27d7918edc8b1beea5f8f3fd346c406bff561d7c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-18 12:45:31 +0900

    chore: 736841216b139696158e61150cde1ba614c3891c に伴って当該プレイヤーをSetから外す

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/valentine/Valentine.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/valentine/Valentine.scala
@@ -29,12 +29,12 @@ object Valentine {
     "09f1067b-a393-43af-a9c8-289b42e77e34", "d19012e3-b6d4-4678-b339-be10a5f3e831", "f5167115-2136-46c2-8f6e-34b1b1cbc06b",
     "58fcfaf8-41b1-4a7d-b506-1e322abbe1a7", "122fd5a3-86ab-4e19-af87-80f85a2966b6", "4279e953-ffe3-4971-ac6c-3bd50265d78b",
     "91097db5-dc97-46ff-bc55-a92677bc8761", "252aaf75-fe66-42fe-8967-52d666ef2dbf", "41fb305b-b591-47a5-a467-1bb0ca333834",
-    "85dd5867-db09-4a2f-bae7-8d38d5a9c547", "424d2b92-94a3-4b57-8168-db1b001b8465", "ea6157be-28e2-4765-87c8-22f5a43567c9",
+    "424d2b92-94a3-4b57-8168-db1b001b8465", "ea6157be-28e2-4765-87c8-22f5a43567c9", "e55e5441-8d8b-4fb2-9573-d32c8a11cc46",
     "3423c69b-a1cd-41cf-a896-992f9a6f5935", "6b5a0649-e89a-4087-aff2-f6e32a983582", "6d705d18-296a-47d9-b5cf-1b557c7a35e7",
     "3d3c592b-ef28-4052-92b5-b0dcfac2a20c", "d42436db-9c85-4ac1-bef0-ec00ad201bfd", "de34061f-0f25-47f2-ad57-43510ae4060e",
     "ce3b6eea-204f-4aaa-b8f7-cd5a72a38ad3", "3dc5d243-2c85-4abb-aada-237511b410b8", "d1d2b094-d0c2-41bd-be61-b6cf38600806",
     "57da9e7b-6390-45f5-92e3-2b93596facb2", "c36e3251-e498-4558-ad5c-6099c0cc9707", "7ce8c94e-0dcc-46c9-9913-142b84fb325e",
-    "ab757137-3b3c-45ce-9dc2-49572833db89", "e55e5441-8d8b-4fb2-9573-d32c8a11cc46"
+    "ab757137-3b3c-45ce-9dc2-49572833db89"
   ).map(UUID.fromString)
   // 2022バレンタインイベントにおいて、このUUIDのプレイヤーにエラーなくクッキーが配布されなかったらしい
   val cookieUnGivenPlayer: UUID = UUID.fromString("85dd5867-db09-4a2f-bae7-8d38d5a9c547")
