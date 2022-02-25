c2250a67107933f8677986039624c7bec0489b2f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-25 12:02:18 +0900

    fix: build_count_rate_limitテーブルのカラム名がupdate_dateではなくrecord_dateになっている

--- src/main/resources/db/migration/V1.7.6__Move_player_rate_limit_table.sql
+++ src/main/resources/db/migration/V1.7.6__Move_player_rate_limit_table.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS seichiassist.build_count_rate_limit(
     uuid CHAR(36) PRIMARY KEY NOT NULL,
     -- 注: DECIMALは(全体の桁数, 小数点以下の桁数)というフォーマットである
     available_permission DECIMAL(17, 5) UNSIGNED NOT NULL,
-    updated_date DATETIME NOT NULL
+    -- update_dateではなくrecord_dateにsubsystemではなっていたので変更
+    record_date DATETIME NOT NULL
 );
