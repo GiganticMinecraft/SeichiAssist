1f636abba0244cc459fdc3b2834b54a3cf3c0d5f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-25 11:45:57 +0900

    fix: build_count_rate_limitテーブルが違うところにできてしまったので修正

---
+++ src/main/resources/db/migration/V1.7.6__Move_player_rate_limit_table.sql
@@ -0,0 +1,9 @@
+-- V1.7.5でseichiassistのDBを指定しなかったので、flyway_managed_schemaの下にテーブルができてしまった
+DROP TABLE IF EXISTS flyway_managed_schema.build_count_rate_limit;
+
+CREATE TABLE IF NOT EXISTS seichiassist.build_count_rate_limit(
+    uuid CHAR(36) PRIMARY KEY NOT NULL,
+    -- 注: DECIMALは(全体の桁数, 小数点以下の桁数)というフォーマットである
+    available_permission DECIMAL(17, 5) UNSIGNED NOT NULL,
+    updated_date DATETIME NOT NULL
+);
