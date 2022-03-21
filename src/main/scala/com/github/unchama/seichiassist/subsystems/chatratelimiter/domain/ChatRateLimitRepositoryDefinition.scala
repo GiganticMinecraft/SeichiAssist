300fccd37f6784e30a721dfe69709f1a9842c518
Kory <6561358+kory33@users.noreply.github.com>
2022-03-21 20:11:38 +0900

    refactor: ChatRateLimitRepositoryDefinitionの実装をよりシンプルに

--- src/main/scala/com/github/unchama/seichiassist/subsystems/chatratelimiter/domain/ChatRateLimitRepositoryDefinition.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/chatratelimiter/domain/ChatRateLimitRepositoryDefinition.scala
@@ -3,10 +3,7 @@ package com.github.unchama.seichiassist.subsystems.chatratelimiter.domain
 import cats.effect.concurrent.Ref
 import cats.effect.{Sync, Timer}
 import cats.implicits._
-import com.github.unchama.datarepository.template.RepositoryDefinition.Phased.{
-  SinglePhased,
-  TwoPhased
-}
+import com.github.unchama.datarepository.template.RepositoryDefinition.Phased.TwoPhased
 import com.github.unchama.datarepository.template.finalization.RepositoryFinalization
 import com.github.unchama.datarepository.template.initialization.TwoPhasedRepositoryInitialization
 import com.github.unchama.generic.ratelimiting.{FixedWindowRateLimiter, RateLimiter}
@@ -20,16 +17,15 @@ object ChatRateLimitRepositoryDefinition {
     implicit breakCountAPI: BreakCountReadAPI[F, G, Player]
   ): TwoPhased[G, Player, Ref[G, Option[RateLimiter[G, ChatCount]]]] = {
     TwoPhased(
-      TwoPhasedRepositoryInitialization.augment(SinglePhased.trivial[G, Player].initialization)(
-        (p, _) =>
-          for {
-            seichiAmount <- breakCountAPI.seichiAmountDataRepository(p).read
-            rateLimiter <- FixedWindowRateLimiter.in[G, ChatCount](ChatCount.One, 30.seconds)
-            ref <- Ref[G].of(
-              Option.when(seichiAmount.levelCorrespondingToExp.level == 1)(rateLimiter)
-            )
-          } yield ref
-      ),
+      TwoPhasedRepositoryInitialization.withoutPrefetching { p =>
+        for {
+          seichiAmount <- breakCountAPI.seichiAmountDataRepository(p).read
+          rateLimiter <- FixedWindowRateLimiter.in[G, ChatCount](ChatCount.One, 30.seconds)
+          ref <- Ref[G].of(
+            Option.when(seichiAmount.levelCorrespondingToExp.level == 1)(rateLimiter)
+          )
+        } yield ref
+      },
       // does not need any finalization
       RepositoryFinalization.trivial
     )
