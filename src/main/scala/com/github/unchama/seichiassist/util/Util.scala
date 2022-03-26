2f5e8a97ceb48d62de83586e5df81d22126b25f1
rito528 <rotoyutoriapp@gmail.com>
2022-03-26 13:56:11 +0900

    [Fix]isGachaTicketのgetOwnerからgetOwnigPlayer.getNameに修正

--- src/main/scala/com/github/unchama/seichiassist/util/Util.scala
+++ src/main/scala/com/github/unchama/seichiassist/util/Util.scala
@@ -322,7 +322,7 @@ object Util {
 
     val skullMeta = itemStack.getItemMeta.asInstanceOf[SkullMeta]
 
-    if (!(skullMeta.hasOwner && skullMeta.getOwner == "unchama")) return false
+    if (!(skullMeta.hasOwner && skullMeta.getOwningPlayer.getName == "unchama")) return false
 
     skullMeta.hasLore && skullMeta.getLore.asScala.exists(containsRightClickMessage)
   }
