c6f36e4b2314ea9b13c23f7a97c2312dfd1594ec
rito528 <rotoyutoriapp@gmail.com>
2022-03-27 23:24:32 +0900

    [Add]msCommandの動作を追加

--- src/main/scala/com/github/unchama/seichiassist/commands/MsCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/commands/MsCommand.scala
@@ -1,3 +1,54 @@
 package com.github.unchama.seichiassist.commands
 
-object MsCommand {}
+import cats.effect.IO
+import com.github.unchama.contextualexecutor.builder.ParserResponse.{failWith, succeedWith}
+import com.github.unchama.contextualexecutor.builder.Parsers
+import com.github.unchama.menuinventory.LayoutPreparationContext
+import com.github.unchama.menuinventory.router.CanOpen
+import com.github.unchama.seichiassist.commands.contextual.builder.BuilderTemplates.playerCommandBuilder
+import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.onMainThread
+import com.github.unchama.seichiassist.menus.minestack.{
+  CategorizedMineStackMenu,
+  MineStackMainMenu
+}
+import com.github.unchama.seichiassist.minestack.MineStackObjectCategory
+import com.github.unchama.targetedeffect.commandsender.MessageEffect
+import org.bukkit.command.TabExecutor
+
+object MsCommand {
+
+  def executor(
+    implicit ioCanOpenMineStackMainMenu: IO CanOpen MineStackMainMenu.type,
+    categorizedMineStackMenuEnvironment: CategorizedMineStackMenu.Environment,
+    layoutPreparationContext: LayoutPreparationContext
+  ): TabExecutor = playerCommandBuilder
+    .argumentsParsers(
+      List(
+        Parsers.identity,
+        category => {
+          category.toIntOption match {
+            case Some(categoryValue) =>
+              MineStackObjectCategory.fromSerializedValue(categoryValue) match {
+                case Some(_) => succeedWith(categoryValue)
+                case None    => failWith("指定されたカテゴリは存在しません。")
+              }
+            case None => failWith("カテゴリは数字で入力してください。")
+          }
+        }
+      )
+    )
+    .execution { context =>
+      val args = context.args.parsed
+      if (args.length != 2) {
+        IO(MessageEffect("不正な引数です。"))
+      } else if (args(1).toString.toIntOption.isEmpty) {
+        IO(MessageEffect("ページの指定が不正です。"))
+      }
+      val category =
+        MineStackObjectCategory.fromSerializedValue(args.head.asInstanceOf[Int]).get
+      IO.pure(CategorizedMineStackMenu(category, args(1).asInstanceOf[Int]).open)
+    }
+    .build()
+    .asNonBlockingTabExecutor()
+
+}
