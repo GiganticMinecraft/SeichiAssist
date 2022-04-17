8ee601e493f2aec7468482532af555dbfe620b29
rito528 <rotoyutoriapp@gmail.com>
2022-04-17 10:07:36 +0900

    [Add]建築レベル最大になった際にレベルが最大になったことを通知する文の追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/buildcount/application/actions/LevelUpNotifier.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/buildcount/application/actions/LevelUpNotifier.scala
@@ -3,11 +3,15 @@ package com.github.unchama.seichiassist.subsystems.buildcount.application.action
 import cats.{Applicative, ~>}
 import com.github.unchama.generic.Diff
 import com.github.unchama.minecraft.actions.SendMinecraftMessage
+import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.onMainThread
+import com.github.unchama.seichiassist.effects.unfocused.BroadcastSoundEffect
 import com.github.unchama.seichiassist.subsystems.buildcount.domain.explevel.{
   BuildAssistExpTable,
   BuildLevel
 }
+import com.github.unchama.seichiassist.util.Util
 import org.bukkit.ChatColor.GOLD
+import org.bukkit.Sound
 
 /**
  * 建築レベルの変化を通知する機構。
@@ -23,9 +27,13 @@ case class LevelUpNotifier[F[_], Player]()(
     import cats.implicits._
 
     val Diff(oldLevel, newLevel) = diff
-    if (newLevel eqv BuildAssistExpTable.maxLevel)
+    if (newLevel eqv BuildAssistExpTable.maxLevel) {
+      Util.sendMessageToEveryoneIgnoringPreference(
+        s"$GOLD${player.asInstanceOf[org.bukkit.entity.Player].getName}の建築レベルが最大Lvに到達したよ(`･ω･´)"
+      )
+      BroadcastSoundEffect(Sound.ENTITY_ENDERDRAGON_DEATH, 1.0f, 1.2f)
       SendMinecraftMessage[F, Player].string(player, s"${GOLD}最大Lvに到達したよ(`･ω･´)")
-    else if (oldLevel < newLevel)
+    } else if (oldLevel < newLevel)
       SendMinecraftMessage[F, Player].string(
         player,
         s"${GOLD}ﾑﾑｯﾚﾍﾞﾙｱｯﾌﾟ∩( ・ω・)∩【建築Lv(${oldLevel.level})→建築Lv(${newLevel.level})】"
