61655d5bc5556abac05dbdf66b3076f49a75483a
rito528 <rotoyutoriapp@gmail.com>
2022-04-22 22:56:36 +0900

    [Fix]マイグレーションファイルを修正

--- src/main/resources/db/migration/V1.8.0__Change_subhome_columns.sql
+++ src/main/resources/db/migration/V1.8.0__Change_subhome_columns.sql
@@ -1,6 +1,6 @@
 use seichiassist;
 
 -- sub_homeテーブルのlocationカラムの型を変更する
-ALTER TABLE sub_home alter column location_x DOUBLE
-ALTER TABLE sub_home alter column location_y DOUBLE
-ALTER TABLE sub_home alter column location_z DOUBLE
+ALTER TABLE sub_home MODIFY location_x DOUBLE;
+ALTER TABLE sub_home MODIFY location_y DOUBLE;
+ALTER TABLE sub_home MODIFY location_z DOUBLE;
