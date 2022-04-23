6cca4c9b9774b7e812a41bdce5153c86382973ea
rito528 <rotoyutoriapp@gmail.com>
2022-04-23 23:29:17 +0900

    [Chore]Scalafmt等実行

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/bukkit/GiftItemInterpreter.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/bukkit/GiftItemInterpreter.scala
@@ -7,7 +7,6 @@ import com.github.unchama.seichiassist.data.{GachaSkullData, ItemData}
 import com.github.unchama.seichiassist.subsystems.seichilevelupgift.domain.Gift
 import com.github.unchama.seichiassist.subsystems.seichilevelupgift.domain.Gift.Item
 import com.github.unchama.seichiassist.util.Util.grantItemStacksEffect
-import com.github.unchama.targetedeffect.SequentialEffect
 import org.bukkit.entity.Player
 
 /**
