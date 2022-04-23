b8cf07fabe5d8e8c8f8d962807717524b39843b2
rito528 <rotoyutoriapp@gmail.com>
2022-04-23 20:53:43 +0900

    [Fix]変数名の変更

--- src/main/scala/com/github/unchama/seichiassist/subsystems/breakcount/subsystems/notification/bukkit/actions/BukkitNotifyLevelUp.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakcount/subsystems/notification/bukkit/actions/BukkitNotifyLevelUp.scala
@@ -29,11 +29,12 @@ object BukkitNotifyLevelUp {
       override def ofSeichiAmountTo(player: Player)(diff: Diff[SeichiAmountData]): F[Unit] = {
         val Diff(oldBreakAmount, newBreakAmount) = diff
         val tenBillion = 1000000000
-        val separation = tenBillion * (oldBreakAmount.expAmount.amount / tenBillion + 1).toLong
+        val nextTenBillion =
+          tenBillion * (oldBreakAmount.expAmount.amount / tenBillion + 1).toLong
         if (
-          oldBreakAmount.expAmount.amount < separation && newBreakAmount
+          oldBreakAmount.expAmount.amount < nextTenBillion && newBreakAmount
             .expAmount
-            .amount >= separation
+            .amount >= nextTenBillion
         ) {
           OnMinecraftServerThread[F].runAction(SyncIO {
             Util.sendMessageToEveryoneIgnoringPreference(
