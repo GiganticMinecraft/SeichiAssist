5772ad44d506970cdf868be5a8721bb612ba517d
rito528 <rotoyutoriapp@gmail.com>
2022-04-28 07:51:56 +0900

    [Chore]GrantLevelUpGiftをSystemで内部的に提供するように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seichilevelupgift/System.scala
@@ -4,7 +4,7 @@ import cats.effect.Async
 import com.github.unchama.generic.effect.stream.StreamExtra
 import com.github.unchama.minecraft.actions.{OnMinecraftServerThread, SendMinecraftMessage}
 import com.github.unchama.seichiassist.subsystems.breakcount.BreakCountReadAPI
-import com.github.unchama.seichiassist.subsystems.seichilevelupgift.domain.GrantLevelUpGift
+import com.github.unchama.seichiassist.subsystems.seichilevelupgift.bukkit.BukkitGrantLevelUpGift.apply
 import com.github.unchama.seichiassist.subsystems.seichilevelupgift.usecases.GrantGiftOnSeichiLevelDiff
 import io.chrisdavenport.log4cats.ErrorLogger
 import org.bukkit.entity.Player
@@ -13,8 +13,7 @@ object System {
 
   def backGroundProcess[F[_]: OnMinecraftServerThread: ErrorLogger: Async, G[_]](
     implicit breakCountReadApi: BreakCountReadAPI[F, G, Player],
-    send: SendMinecraftMessage[F, Player],
-    grant: GrantLevelUpGift[F, Player]
+    send: SendMinecraftMessage[F, Player]
   ): F[Nothing] = {
     StreamExtra.compileToRestartingStream("[SeichiLevelUpGift]") {
       breakCountReadApi.seichiLevelUpdates.evalTap {
