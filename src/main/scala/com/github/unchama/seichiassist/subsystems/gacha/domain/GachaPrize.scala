2335eb819110d2bee1b5d20eb40321fa76b53523
rito528 <rotoyutoriapp@gmail.com>
2022-04-28 21:39:05 +0900

    [Add]GachaPrizeのコメントを詳しく書く

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaPrize.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaPrize.scala
@@ -3,6 +3,8 @@ package com.github.unchama.seichiassist.subsystems.gacha.domain
 import org.bukkit.inventory.ItemStack
 
 /**
- * @param itemStack ガチャで排出されるアイテム。数もそのまま利用されます。
+ * @param itemStack ガチャで排出されるアイテム。数もそのまま利用されます
+ * @param probability ガチャで排出される確率
+ * @param isAppendOwner 記名する場合はtrueにしてください
  */
-case class GachaPrize(itemStack: ItemStack, probability: Double, owner: Option[String])
+case class GachaPrize(itemStack: ItemStack, probability: Double, isAppendOwner: Boolean)
