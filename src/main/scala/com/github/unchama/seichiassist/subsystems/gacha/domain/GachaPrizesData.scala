ddbceea80078d91810599a2e537e5fb6c6208a3e
rito528 <rotoyutoriapp@gmail.com>
2022-04-30 23:46:35 +0900

    [Add]GachaPrizesDataについて今後の展望コメントを残す

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaPrizesData.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaPrizesData.scala
@@ -3,6 +3,7 @@ package com.github.unchama.seichiassist.subsystems.gacha.domain
 import cats.effect.Sync
 import cats.effect.concurrent.Ref
 
+//TODO このクラスがどう利用されるのかをはっきりさせて責務のあるクラスに昇華させたい
 final class GachaPrizesData[F[_]: Sync] {
 
   private val gachaPrizes: Ref[F, Vector[GachaPrize]] =
