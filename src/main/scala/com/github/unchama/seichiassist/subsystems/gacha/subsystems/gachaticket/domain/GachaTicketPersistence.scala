fd51d0272e628d006015024f0f0ff3c013e61dbb
rito528 <rotoyutoriapp@gmail.com>
2022-05-03 15:18:07 +0900

    [Add]GachaTicketPersistenceを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/subsystems/gachaticket/domain/GachaTicketPersistence.scala
@@ -0,0 +1,17 @@
+package com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.domain
+
+import java.util.UUID
+
+trait GachaTicketPersistence[F[_]] {
+
+  /**
+   * 運営からのガチャ券の枚数を全員に増加させる作用
+   */
+  def add(amount: Int): F[Boolean]
+
+  /**
+   * 運営からのガチャ券の枚数を指定UUIDのプレイヤーに増加させる作用
+   */
+  def add(amount: Int, uuid: UUID): F[Boolean]
+
+}
---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/subsystems/gachaticket/infrastructure/JdbcGachaTicketPersistence.scala
@@ -0,0 +1,41 @@
+package com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.infrastructure
+
+import cats.effect.Sync
+import com.github.unchama.concurrent.NonServerThreadContextShift
+import com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.domain.GachaTicketPersistence
+import scalikejdbc.{DB, scalikejdbcSQLInterpolationImplicitDef}
+
+import java.util.UUID
+
+class JdbcGachaTicketPersistence[F[_]: Sync: NonServerThreadContextShift]
+    extends GachaTicketPersistence[F] {
+
+  import cats.implicits._
+
+  /**
+   * 運営からのガチャ券の枚数を全員に増加させる作用
+   */
+  override def add(amount: Int): F[Boolean] = {
+    NonServerThreadContextShift[F].shift >> Sync[F].delay {
+      DB.localTx { implicit session =>
+        sql"update playerdata set numofsorryforbug = numofsorryforbug + $amount"
+          .execute()
+          .apply()
+      }
+    }
+  }
+
+  /**
+   * 運営からのガチャ券の枚数を指定UUIDのプレイヤーに増加させる作用
+   */
+  override def add(amount: Int, uuid: UUID): F[Boolean] = {
+    NonServerThreadContextShift[F].shift >> Sync[F].delay {
+      DB.localTx { implicit session =>
+        sql"update playerdata set numofsorryforbug = numofsorryforbug + $amount where uuid = ${uuid.toString}"
+          .execute()
+          .apply()
+      }
+    }
+  }
+
+}
