3d1203844d8d0540f034fa6b264489737e8f210f
rito528 <rotoyutoriapp@gmail.com>
2022-05-04 20:31:06 +0900

    [Add]toItemStack関数にアイテム数を考慮させるように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/bukkit/Wrapper/ItemStackStringWrapper.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/bukkit/Wrapper/ItemStackStringWrapper.scala
@@ -3,9 +3,11 @@ package com.github.unchama.seichiassist.subsystems.gacha.bukkit.Wrapper
 import com.github.unchama.seichiassist.util.BukkitSerialization
 import org.bukkit.inventory.ItemStack
 
+import scala.util.chaining.scalaUtilChainingOps
+
 case class ItemStackStringWrapper(itemStack: String, amount: Int) {
 
   def toItemStack: ItemStack =
-    BukkitSerialization.fromBase64(itemStack).getItem(0)
+    BukkitSerialization.fromBase64(itemStack).getItem(0).tap(_.setAmount(amount))
 
 }
