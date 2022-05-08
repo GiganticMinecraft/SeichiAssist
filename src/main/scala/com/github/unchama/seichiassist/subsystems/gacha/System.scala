cb946ea3df16b16ea951cdbc90239764da65000b
rito528 <rotoyutoriapp@gmail.com>
2022-05-08 17:33:39 +0900

    [Fix]apiの処理が変わったので修正

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/System.scala
@@ -6,11 +6,8 @@ import com.github.unchama.concurrent.NonServerThreadContextShift
 import com.github.unchama.minecraft.actions.OnMinecraftServerThread
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 import com.github.unchama.seichiassist.subsystems.gacha.bukkit.command.GachaCommand
+import com.github.unchama.seichiassist.subsystems.gacha.domain.GachaPrizesDataOperations
 import com.github.unchama.seichiassist.subsystems.gacha.domain.bukkit.GachaPrize
-import com.github.unchama.seichiassist.subsystems.gacha.domain.{
-  GachaPrizeId,
-  GachaPrizesDataOperations
-}
 import com.github.unchama.seichiassist.subsystems.gacha.infrastructure.bukkit.JdbcGachaPersistence
 import com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.infrastructure.JdbcGachaTicketPersistence
 import com.github.unchama.seichiassist.subsystems.itemmigration.domain.minecraft.UuidRepository
@@ -25,7 +22,7 @@ object System {
   def wired[F[_]: OnMinecraftServerThread: NonServerThreadContextShift: Sync: ConcurrentEffect](
     implicit syncUuidRepository: UuidRepository[SyncIO]
   ): System[F] = {
-    val gachaPersistence = new JdbcGachaPersistence[F]()
+    implicit val gachaPersistence: JdbcGachaPersistence[F] = new JdbcGachaPersistence[F]()
     implicit val gachaPrizesDataOperations: GachaPrizesDataOperations[F] =
       new GachaPrizesDataOperations[F]
     implicit val gachaTicketPersistence: JdbcGachaTicketPersistence[F] =
@@ -35,12 +32,12 @@ object System {
 
     new System[F] {
       override implicit val api: GachaAPI[F] = new GachaAPI[F] {
-        override def upsert(gachaPrize: GachaPrize): F[Unit] =
-          gachaPersistence.upsert(gachaPrize)
-
-        override def remove(id: GachaPrizeId): F[Boolean] = gachaPersistence.remove(id)
 
         override def list: F[Vector[GachaPrize]] = gachaPersistence.list
+
+        override def update(gachaPrizesList: Vector[GachaPrize]): F[Unit] =
+          gachaPersistence.update(gachaPrizesList)
+
       }
       override val commands: Map[String, TabExecutor] = Map(
         "gacha" -> new GachaCommand[F]().executor
