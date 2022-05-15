315ea44478af54036e11f3515bea1fdfc0d1ac6c
rito528 <rotoyutoriapp@gmail.com>
2022-05-15 15:56:36 +0900

    [Add]コメントをより詳しく

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaPrizesDataOperations.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaPrizesDataOperations.scala
@@ -11,33 +11,55 @@ final class GachaPrizesDataOperations[F[_]: Sync] {
   private val gachaPrizes: Ref[F, Vector[GachaPrize]] =
     Ref.unsafe[F, Vector[GachaPrize]](Vector.empty)
 
+  /**
+   * 与えられた`gachaPersistence`からガチャ景品データを読み込む
+   */
   def loadGachaPrizes(gachaPersistence: GachaPersistence[F]): F[Unit] = for {
     prizes <- gachaPersistence.list
     _ <- gachaPrizes.set(prizes)
   } yield ()
 
+  /**
+   * 指定された`GachaPrize`が存在するかどうか
+   */
   def gachaPrizeExists(gachaPrizeId: GachaPrizeId): F[Boolean] = for {
     gachaPrizes <- gachaPrizes.get
   } yield gachaPrizes.exists(_.id == gachaPrizeId)
 
+  /**
+   * `GachaPrize`を追加する。
+   * `GachaPrizeId`を与えなかった場合は最大`GachaPrizeId`の次の値が指定されます
+   */
   def addGachaPrize(gachaPrize: GachaPrizeId => GachaPrize): F[Unit] = for {
     prizes <- gachaPrizes.get
-    newList = prizes ++ Vector(gachaPrize(GachaPrizeId(prizes.size + 1)))
+    newList = prizes ++ Vector(gachaPrize(GachaPrizeId(prizes.map(_.id.id).max + 1)))
     _ <- gachaPrizes.set(newList)
   } yield ()
 
+  /**
+   * `GachaPrizeId`から`GachaPrize`を取得する
+   */
   def getGachaPrize(gachaPrizeId: GachaPrizeId): F[Option[GachaPrize]] = for {
     prizes <- gachaPrizes.get
   } yield prizes.find(_.id == gachaPrizeId)
 
+  /**
+   * `gachaPrizeId`を利用して`GachaPrize`を削除する
+   */
   def removeByGachaPrizeId(gachaPrizeId: GachaPrizeId): F[Unit] = for {
     prizes <- gachaPrizes.get
     targetPrize = prizes.filter(_.id == gachaPrizeId)
     _ <- gachaPrizes.set(prizes.diff(targetPrize))
   } yield ()
 
+  /**
+   * すべてのガチャ景品データを返す
+   */
   def getGachaPrizesList: F[Vector[GachaPrize]] = gachaPrizes.get
 
+  /**
+   * すべてのガチャ景品データを削除する
+   */
   def clear(): F[Unit] = for {
     _ <- gachaPrizes.set(Vector.empty)
   } yield ()
