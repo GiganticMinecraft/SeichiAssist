95d8c12c7aee182833a516f2ea8c4484fcf6e1c7
rito528 <rotoyutoriapp@gmail.com>
2022-07-09 22:51:54 +0900

    [Add]GropedMineStackObjのドメイン成約を追加

--- src/main/scala/com/github/unchama/seichiassist/minestack/GroupedMineStackObj.scala
+++ src/main/scala/com/github/unchama/seichiassist/minestack/GroupedMineStackObj.scala
@@ -3,4 +3,8 @@ package com.github.unchama.seichiassist.minestack
 case class GroupedMineStackObj(
   representative: MineStackObject,
   coloredVariants: List[MineStackObject]
-)
+) {
+  require(coloredVariants.forall(_.category == representative.category))
+
+  def category: MineStackObjectCategory = representative.category
+}
