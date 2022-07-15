f0638d922f0f7821239ad04327955c442c07e686
rito528 <rotoyutoriapp@gmail.com>
2022-07-16 00:28:21 +0900

    [Fix]name

--- src/main/scala/com/github/unchama/seichiassist/minestack/MineStackUsageHistory.scala
+++ src/main/scala/com/github/unchama/seichiassist/minestack/MineStackUsageHistory.scala
@@ -9,9 +9,9 @@ class MineStackUsageHistory {
   /**
    * 履歴に追加します。ただし、データの保存可能な最大値を超えていた場合、先頭から削除されます。
    */
-  def addHistory(mineStackObjectGroup: MineStackObject): Unit = {
-    _usageHistory = _usageHistory.filterNot(_ == mineStackObjectGroup)
-    _usageHistory :+= mineStackObjectGroup
+  def addHistory(mineStackObject: MineStackObject): Unit = {
+    _usageHistory = _usageHistory.filterNot(_ == mineStackObject)
+    _usageHistory :+= mineStackObject
     if (_usageHistory.size > maxListSize) _usageHistory.drop(1)
   }
 
