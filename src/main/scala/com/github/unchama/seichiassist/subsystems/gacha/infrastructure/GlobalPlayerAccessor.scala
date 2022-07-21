c47d70fcf7d4666973d6e8336442514c13263425
rito528 <rotoyutoriapp@gmail.com>
2022-07-21 19:43:32 +0900

    [Add]gachaパッケージにGlobalPlayerAccessorを追加

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/infrastructure/GlobalPlayerAccessor.scala
@@ -0,0 +1,27 @@
+package com.github.unchama.seichiassist.subsystems.gacha.infrastructure
+
+import cats.effect.Sync
+import com.github.unchama.seichiassist.domain.actions.UuidToLastSeenName
+import scalikejdbc._
+
+import java.util.UUID
+
+/**
+ * Bukkitの[[org.bukkit.Server]]の枠組みを超えて、全てのプレイヤーの情報についてアクセスするオブジェクト
+ */
+class GlobalPlayerAccessor[F[_]: Sync] extends UuidToLastSeenName[F] {
+
+  /**
+   * @return
+   *   全てのUUIDとそれに紐付けられた最終的な名前
+   */
+  override def entries: F[Map[UUID, String]] = Sync[F].delay {
+    DB.readOnly { implicit session =>
+      sql"""SELECT name, uuid from seichiassist.playerdata"""
+        .map { rs => (UUID.fromString(rs.string("uuid")), rs.string("name")) }
+        .list()
+        .apply()
+        .toMap
+    }
+  }
+}
