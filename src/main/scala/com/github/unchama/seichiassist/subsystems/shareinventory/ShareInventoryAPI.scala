547639ba6f47b9481e55e8880dbbf294d4d2bd49
rito528 <rotoyutoriapp@gmail.com>
2022-07-21 22:21:56 +0900

    [Add]ShareInventoryAPIを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/shareinventory/ShareInventoryAPI.scala
@@ -0,0 +1,33 @@
+package com.github.unchama.seichiassist.subsystems.shareinventory
+
+import com.github.unchama.seichiassist.subsystems.shareinventory.domain.InventoryContents
+
+import java.util.UUID
+
+trait ShareInventoryWriteAPI[F[_]] {
+
+  def save(targetUuid: UUID, inventoryContents: InventoryContents): F[Unit]
+
+  def clear(targetUuid: UUID): F[Unit]
+
+}
+
+object ShareInventoryWriteAPI {
+
+  def apply[F[_]](implicit ev: ShareInventoryWriteAPI[F]): ShareInventoryWriteAPI[F] = ev
+
+}
+
+trait ShareInventoryReadAPI[F[_]] {
+
+  def load(targetUuid: UUID): F[InventoryContents]
+
+}
+
+object ShareInventoryReadAPI {
+
+  def apply[F[_]](implicit ev: ShareInventoryReadAPI[F]): ShareInventoryReadAPI[F] = ev
+
+}
+
+trait ShareInventoryAPI[F[_]] extends ShareInventoryReadAPI[F] with ShareInventoryWriteAPI[F]
