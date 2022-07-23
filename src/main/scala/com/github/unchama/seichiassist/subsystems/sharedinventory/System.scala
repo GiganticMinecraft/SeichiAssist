4ef05d2bb03762c2dbf4b12fa661047119949604
rito528 <rotoyutoriapp@gmail.com>
2022-07-23 15:49:24 +0900

    [Remove]Repositoryを削除し、直接DBを参照するように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/sharedinventory/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/sharedinventory/System.scala
@@ -1,13 +1,8 @@
 package com.github.unchama.seichiassist.subsystems.sharedinventory
 
-import cats.effect.concurrent.Ref
-import cats.effect.{ConcurrentEffect, SyncEffect}
-import com.github.unchama.datarepository.KeyedDataRepository
-import com.github.unchama.datarepository.bukkit.player.BukkitRepositoryControls
+import cats.effect.ConcurrentEffect
 import com.github.unchama.generic.ContextCoercion
-import com.github.unchama.minecraft.bukkit.algebra.BukkitPlayerHasUuid.instance
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
-import com.github.unchama.seichiassist.subsystems.sharedinventory.application.repository.SharedInventoryRepositoryDefinition
 import com.github.unchama.seichiassist.subsystems.sharedinventory.bukkit.command.ShareInventoryCommand
 import com.github.unchama.seichiassist.subsystems.sharedinventory.domain.SharedFlag
 import com.github.unchama.seichiassist.subsystems.sharedinventory.domain.bukkit.InventoryContents
@@ -25,51 +20,30 @@ object System {
 
   import cats.implicits._
 
-  def wired[F[_]: SyncEffect: ContextCoercion[*[_], G], G[_]: ConcurrentEffect]
-    : G[System[G]] = {
+  def wired[F[_]: ConcurrentEffect]: System[F] = {
     val persistence = new JdbcSharedInventoryPersistence[F]
 
-    for {
-      sharedFlagRepositoryControls <-
-        ContextCoercion(
-          BukkitRepositoryControls.createHandles(
-            SharedInventoryRepositoryDefinition.withContext[F, G, Player](persistence)
-          )
-        )
-    } yield {
-      new System[G] {
-        override implicit val api: SharedInventoryAPI[G, Player] =
-          new SharedInventoryAPI[G, Player] {
-            override def save(targetUuid: UUID, inventoryContents: InventoryContents): G[Unit] =
-              ContextCoercion(persistence.write(targetUuid, inventoryContents))
+    new System[F] {
+      override implicit val api: SharedInventoryAPI[F, Player] =
+        new SharedInventoryAPI[F, Player] {
+          override def save(targetUuid: UUID, inventoryContents: InventoryContents): F[Unit] =
+            ContextCoercion(persistence.save(targetUuid, inventoryContents))
 
-            override def clear(targetUuid: UUID): G[Unit] =
-              ContextCoercion(persistence.clear(targetUuid))
+          override def clear(targetUuid: UUID): F[Unit] =
+            ContextCoercion(persistence.clear(targetUuid))
 
-            override def load(targetUuid: UUID): G[Option[InventoryContents]] =
-              ContextCoercion(persistence.read(targetUuid))
+          override def load(targetUuid: UUID): F[Option[InventoryContents]] =
+            ContextCoercion(persistence.load(targetUuid))
 
-            override val inventoryContentsRepository
-              : KeyedDataRepository[Player, Ref[G, InventoryContents]] =
-              sharedFlagRepositoryControls
-                .repository
-                .map(value => value.sharedFlag.mapK(ContextCoercion.asFunctionK))
-
-            override def sharedFlag(player: Player): G[SharedFlag] = for {
-              inventoryContents <- inventoryContentsRepository(player).get
-            } yield {
-              if (inventoryContents == InventoryContents.initial) SharedFlag.Sharing
-              else SharedFlag.NotSharing
+          override def sharedFlag(player: Player): F[SharedFlag] =
+            load(player.getUniqueId).map {
+              case Some(_) => SharedFlag.Sharing
+              case None    => SharedFlag.NotSharing
             }
 
-          }
-
-        override val managedRepositoryControls: Seq[BukkitRepositoryControls[G, _]] =
-          Seq(sharedFlagRepositoryControls.coerceFinalizationContextTo[G])
-
-        override val commands: Map[String, TabExecutor] = {
-          Map("shareinv" -> new ShareInventoryCommand[G].executor)
         }
+      override val commands: Map[String, TabExecutor] = {
+        Map("shareinv" -> new ShareInventoryCommand[F].executor)
       }
     }
   }
