e98f3be9377b99121e044907099aafbea2cc03f8
rito528 <rotoyutoriapp@gmail.com>
2022-07-23 19:24:25 +0900

    [Add]Systemにvoteコマンドを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/System.scala
@@ -1,8 +1,9 @@
 package com.github.unchama.seichiassist.subsystems.vote
 
-import cats.effect.Sync
+import cats.effect.ConcurrentEffect
 import com.github.unchama.minecraft.actions.OnMinecraftServerThread
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
+import com.github.unchama.seichiassist.subsystems.vote.bukkit.command.VoteCommand
 import com.github.unchama.seichiassist.subsystems.vote.domain.{
   ChainVoteDayNumber,
   PlayerName,
@@ -12,6 +13,7 @@ import com.github.unchama.seichiassist.subsystems.vote.infrastructure.{
   JdbcChainVotePersistence,
   JdbcVotePointPersistence
 }
+import org.bukkit.command.TabExecutor
 
 import java.util.UUID
 
@@ -21,12 +23,12 @@ trait System[F[_]] extends Subsystem[F] {
 
 object System {
 
-  def wired[F[_]: Sync: OnMinecraftServerThread]: System[F] = {
+  def wired[F[_]: ConcurrentEffect: OnMinecraftServerThread]: System[F] = {
     val chainVotePersistence = new JdbcChainVotePersistence[F]
     val votePointPersistence = new JdbcVotePointPersistence[F]
 
     new System[F] {
-      override val api: VoteAPI[F] = new VoteAPI[F] {
+      override implicit val api: VoteAPI[F] = new VoteAPI[F] {
         override def incrementVotePoint(playerName: PlayerName): F[Unit] =
           votePointPersistence.increment(playerName)
 
@@ -39,6 +41,10 @@ object System {
         override def chainVoteDayNumber(uuid: UUID): F[ChainVoteDayNumber] =
           chainVotePersistence.getChainVoteDays(uuid)
       }
+
+      override val commands: Map[String, TabExecutor] = Map(
+        "vote" -> new VoteCommand[F].executor
+      )
     }
   }
 
