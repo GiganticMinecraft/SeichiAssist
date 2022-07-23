1303d39e8f08b998b54d50cb80b9dcbcf239970d
rito528 <rotoyutoriapp@gmail.com>
2022-07-23 14:18:44 +0900

    [Fix]List -> NonEmptyList

--- src/main/scala/com/github/unchama/seichiassist/subsystems/sharedinventory/domain/bukkit/InventoryContents.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/sharedinventory/domain/bukkit/InventoryContents.scala
@@ -1,5 +1,6 @@
 package com.github.unchama.seichiassist.subsystems.sharedinventory.domain.bukkit
 
+import cats.data.NonEmptyList
 import org.bukkit.inventory.ItemStack
 
 case class InventoryContents private (inventoryContents: List[ItemStack])
@@ -7,8 +8,6 @@ case class InventoryContents private (inventoryContents: List[ItemStack])
 object InventoryContents {
   val initial: InventoryContents = InventoryContents(List.empty)
 
-  def ofNonEmpty(inventoryContents: List[ItemStack]): InventoryContents = {
-    require(inventoryContents.nonEmpty)
-    InventoryContents(inventoryContents)
-  }
+  def ofNonEmpty(inventoryContents: NonEmptyList[ItemStack]): InventoryContents =
+    InventoryContents(inventoryContents.toList)
 }
