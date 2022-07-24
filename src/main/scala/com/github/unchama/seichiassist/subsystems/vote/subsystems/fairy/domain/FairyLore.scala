3601e32f44a989467c30d903eb248ec206b5d349
rito528 <rotoyutoriapp@gmail.com>
2022-07-24 13:58:34 +0900

    [Remove]不要なカッコを削除

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairyLore.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairyLore.scala
@@ -2,4 +2,4 @@ package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain
 
 import cats.data.NonEmptyList
 
-case class FairyLore(lore: NonEmptyList[String]) {}
+case class FairyLore(lore: NonEmptyList[String])
