00dde8e8021e4ed5cd08768609907ca4227da706
rito528 <rotoyutoriapp@gmail.com>
2022-07-25 11:14:35 +0900

    [Add]validTimeを定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairyValidTime.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairyValidTime.scala
@@ -2,4 +2,4 @@ package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain
 
 import java.time.LocalDateTime
 
-case class FairyValidTime(validTIme: LocalDateTime)
+case class FairyValidTime(startTime: LocalDateTime, endTime: LocalDateTime)
--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairyValidTimeState.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairyValidTimeState.scala
@@ -1,5 +1,26 @@
 package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain
 
+import java.time.LocalDateTime
+
 case class FairyValidTimeState(value: Int) {
   require(1 <= value && value <= 4, "FairySummonValidTimeStateは1~4の間で指定してください。")
+
+  /**
+   * @return 妖精が有効な時間
+   */
+  def validTime: FairyValidTime = {
+    val now = LocalDateTime.now()
+    val validTime = value match {
+      case 1 =>
+        FairyValidTime(_, now.plusMonths(30))
+      case 2 =>
+        FairyValidTime(_, now.plusHours(1))
+      case 3 =>
+        FairyValidTime(_, now.plusHours(1).plusMinutes(30))
+      case 4 =>
+        FairyValidTime(_, now.plusHours(2))
+    }
+    validTime(now)
+  }
+
 }
