45eabe6fc532f23db2608ba533683244cbcfa84f
rito528 <rotoyutoriapp@gmail.com>
2022-07-27 00:29:39 +0900

    [Add]妖精がランダムで発言するテーブルを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/resources/FairyMessageTable.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/resources/FairyMessageTable.scala
@@ -8,6 +8,7 @@ import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.p
 
 object FairyMessageTable {
 
+  // 朝に妖精を召喚したときに表示されるメッセージ
   val morningMessages: NameCalledByFairy => FairyMessages = (name: NameCalledByFairy) =>
     FairyMessages(
       FairyMessage(s"おはよ！${name.value}"),
@@ -17,6 +18,7 @@ object FairyMessageTable {
       FairyMessage(s"今日は整地日和だね！${name.value}")
     )
 
+  // 昼に妖精を召喚したときに表示されるメッセージ
   val dayMessages: NameCalledByFairy => FairyMessages = (name: NameCalledByFairy) =>
     FairyMessages(
       FairyMessage(s"やあ！${name.value}"),
@@ -26,6 +28,7 @@ object FairyMessageTable {
       FairyMessage("今日のおやつはがちゃりんごいっぱいだ！")
     )
 
+  // 夜に妖精を召喚したときに表示されるメッセージ
   val nightMessages: NameCalledByFairy => FairyMessages = (name: NameCalledByFairy) =>
     FairyMessages(
       FairyMessage(s"やあ！${name.value}"),
@@ -33,6 +36,37 @@ object FairyMessageTable {
       FairyMessage(s"ふわぁ。。。${name.value}は夜も元気だね。"),
       FairyMessage(s"もう寝ようと思ってたのにー。${name.value}はしょうがないなぁ"),
       FairyMessage("こんな時間に呼ぶなんて…りんごははずんでもらうよ？")
+    )
+
+  // マナが満タンだったときに表示されるメッセージ
+  val manaFullMessages: NameCalledByFairy => FairyMessages = (name: NameCalledByFairy) =>
+    FairyMessages(
+      FairyMessage("整地しないのー？"),
+      FairyMessage("たくさん働いて、たくさんりんごを食べようね！"),
+      FairyMessage("僕はいつか大きながちゃりんごを食べ尽して見せるっ！"),
+      FairyMessage("ちょっと食べ疲れちゃった"),
+      FairyMessage(s"${name.value}はどのりんごが好き？僕はがちゃりんご！"),
+      FairyMessage("動いてお腹を空かしていっぱい食べるぞー！")
+    )
+
+  // 妖精にりんごが消費されたときに表示されるメッセージ
+  val consumed: NameCalledByFairy => FairyMessages = (name: NameCalledByFairy) =>
+    FairyMessages(
+      FairyMessage("(´～｀)ﾓｸﾞﾓｸﾞ…"),
+      FairyMessage("がちゃりんごって美味しいよね！"),
+      FairyMessage("あぁ！幸せ！"),
+      FairyMessage(s"${name.value}のりんごはおいしいなぁ"),
+      FairyMessage("いつもりんごをありがとう！")
+    )
+
+  // 妖精がりんごを消費しなかったときに表示されるメッセージ
+  val notConsumed: NameCalledByFairy => FairyMessages = (name: NameCalledByFairy) =>
+    FairyMessages(
+      FairyMessage("お腹空いたなぁー。"),
+      FairyMessage("がちゃりんごがっ！食べたいっ！"),
+      FairyMessage("(´；ω；`)ｳｩｩ ﾋﾓｼﾞｲ..."),
+      FairyMessage(s"＠うんちゃま ${name.value}が意地悪するんだっ！"),
+      FairyMessage("うわーん！お腹空いたよー！")
     )
 
 }
