60c665bcb70f898f46aa9b6da3e871b032d80fc0
rito528 <rotoyutoriapp@gmail.com>
2022-07-26 21:42:06 +0900

    [Fix]migrationファイルの型変換文のミスを修正

--- src/main/resources/db/migration/V2.0.0__Change_new_fairy_time_type.sql
+++ src/main/resources/db/migration/V2.0.0__Change_new_fairy_time_type.sql
@@ -2,4 +2,4 @@ use seichiassist;
 
 -- 型を変えるので一度内容をリセットする
 UPDATE playerdata SET newVotingFairyTime = NULL;
-ALTER TABLE playerdata ALTER COLUMN newVotingFairyTime DATETIME;
+ALTER TABLE playerdata MODIFY newVotingFairyTime DATETIME;
