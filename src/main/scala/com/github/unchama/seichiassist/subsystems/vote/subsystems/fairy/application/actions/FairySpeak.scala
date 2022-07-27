62152b940c6cc51fff50fe7060e7c77c2fe706ad
rito528 <rotoyutoriapp@gmail.com>
2022-07-27 09:46:57 +0900

    [Add]ドキュメントの追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/application/actions/FairySpeak.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/application/actions/FairySpeak.scala
@@ -5,12 +5,21 @@ import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.p
 
 trait FairySpeak[F[_], Player] {
 
+  /**
+   * 単に妖精からのメッセージを発言する作用
+   */
   def speak(player: Player, fairyMessage: FairyMessage)(
     implicit fairyAPI: FairyAPI[F, Player]
   ): F[Unit]
 
+  /**
+   * 妖精からのランダムなメッセージを発言する作用
+   */
   def speakRandomly(player: Player)(implicit fairyAPI: FairyAPI[F, Player]): F[Unit]
 
+  /**
+   * 妖精からの召喚時メッセージを発言する作用
+   */
   def speakStartMessage(player: Player)(implicit fairyAPI: FairyAPI[F, Player]): F[Unit]
 
 }
