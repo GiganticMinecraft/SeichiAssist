acb8288d3a91930e999d4d31ed8e55b6a645592f
rito528 <rotoyutoriapp@gmail.com>
2022-07-27 11:16:08 +0900

    [Add]FairySpeechServiceを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/service/FairySpeechService.scala
@@ -0,0 +1,20 @@
+package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.service
+
+import cats.effect.Sync
+import cats.implicits.catsSyntaxFlatMapOps
+import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.FairySpeechGateway
+import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.property.{
+  FairyMessage,
+  FairyPlaySound
+}
+
+class FairySpeechService[F[_]: Sync](gateway: FairySpeechGateway[F]) {
+
+  def makeSpeech(fairyMessage: FairyMessage, fairyPlaySound: FairyPlaySound): F[Unit] = {
+    if (fairyPlaySound == FairyPlaySound.on)
+      gateway.sendMessage(fairyMessage) >> gateway.playSpeechSound
+    else
+      gateway.sendMessage(fairyMessage)
+  }
+
+}
