57968442e23e3bbc35280c5a506b41beea5936c9
rito528 <rotoyutoriapp@gmail.com>
2022-07-31 17:33:33 +0900

    fix: bukkitFairySpeechGatewayでGとIOを往復しないように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/bukkit/gateway/BukkitFairySpeechGateway.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/bukkit/gateway/BukkitFairySpeechGateway.scala
@@ -1,22 +1,23 @@
 package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.bukkit.gateway
 
-import cats.effect.Sync
+import cats.effect.{LiftIO, Sync}
 import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.FairySpeechGateway
 import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.property.FairyMessage
-import com.github.unchama.targetedeffect.commandsender.MessageEffect
+import com.github.unchama.targetedeffect.commandsender.MessageEffectF
 import com.github.unchama.targetedeffect.player.FocusedSoundEffect
 import org.bukkit.ChatColor.{AQUA, BOLD, RESET}
 import org.bukkit.Sound
 import org.bukkit.entity.Player
 
-class BukkitFairySpeechGateway[G[_]: Sync](player: Player) extends FairySpeechGateway[G] {
+class BukkitFairySpeechGateway[G[_]: Sync: LiftIO](player: Player)
+    extends FairySpeechGateway[G] {
 
-  override def sendMessage(fairyMessage: FairyMessage): G[Unit] = Sync[G].delay {
-    MessageEffect(s"$AQUA$BOLD<マナ妖精>$RESET${fairyMessage.message}").run(player).unsafeRunSync()
-  }
+  override def sendMessage(fairyMessage: FairyMessage): G[Unit] =
+    MessageEffectF[G](s"$AQUA$BOLD<マナ妖精>$RESET${fairyMessage.message}").run(player)
 
-  override def playSpeechSound: G[Unit] = Sync[G].delay {
-    FocusedSoundEffect(Sound.BLOCK_NOTE_PLING, 2.0f, 1.0f).run(player).unsafeRunSync()
-  }
+  override def playSpeechSound: G[Unit] =
+    LiftIO[G].liftIO {
+      FocusedSoundEffect(Sound.BLOCK_NOTE_PLING, 2.0f, 1.0f).run(player)
+    }
 
 }
