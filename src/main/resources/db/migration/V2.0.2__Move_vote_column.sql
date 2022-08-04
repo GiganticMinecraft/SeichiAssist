1741245f7dffc1fd0e85b341816c5560562fc4ac
rito528 <rotoyutoriapp@gmail.com>
2022-08-04 11:35:29 +0900

    add: migrationファイル V2.0.2の追加

---
+++ src/main/resources/db/migration/V2.0.2__Move_vote_column.sql
@@ -0,0 +1,22 @@
+use seichiassist;
+
+CREATE TABLE vote(
+    uuid CHAR(36) PRIMARY KEY,
+    vote_number INT,
+    chain_vote_number INT,
+    effect_point INT,
+    given_effect_point INT,
+    last_vote DATETIME
+);
+
+INSERT INTO vote(
+    uuid,
+    vote_number,
+    chain_vote_number,
+    effect_point,
+    given_effect_point,
+    last_vote
+)
+SELECT uuid,p_vote,chainvote,effectpoint,p_givenvote,lastvote FROM playerdata;
+
+ALTER TABLE playerdata DROP p_vote,chainvote,effectpoint,p_givenvote,lastvote;
