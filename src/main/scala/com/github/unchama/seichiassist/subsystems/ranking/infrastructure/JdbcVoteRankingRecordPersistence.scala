72a5cf6d4e17aa27ff85f2b3bc515ad9d53e563b
rito528 <rotoyutoriapp@gmail.com>
2022-08-04 15:27:12 +0900

    fix: p_vote -> vote_number

--- src/main/scala/com/github/unchama/seichiassist/subsystems/ranking/infrastructure/JdbcVoteRankingRecordPersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/ranking/infrastructure/JdbcVoteRankingRecordPersistence.scala
@@ -13,7 +13,7 @@ class JdbcVoteRankingRecordPersistence[F[_]: Sync]
   override def getAllRankingRecords: F[Vector[RankingRecord[VoteCount]]] = Sync[F].delay {
     DB.readOnly { implicit session =>
       sql"SELECT playerdata.name,vote_number FROM vote INNER JOIN playerdata ON vote.uuid = playerdata.uuid"
-        .map { rs => RankingRecord(rs.string("name"), VoteCount(rs.int("p_vote"))) }
+        .map { rs => RankingRecord(rs.string("name"), VoteCount(rs.int("vote_number"))) }
         .list()
         .apply()
         .toVector
