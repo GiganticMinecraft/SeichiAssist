6cdb18275a01d115da93ae9708ef87a1528f7ab9
rito528 <rotoyutoriapp@gmail.com>
2022-08-08 23:23:27 +0900

    feat: Move_donate_data.sqlを追加

---
+++ src/main/resources/db/migration/V2.0.0__Move_donate_data.sql
@@ -0,0 +1,48 @@
+USE seichiassist;
+
+CREATE TABLE donate_usage_history(
+    id INT AUTO_INCREMENT NOT NULL,
+    uuid CHAR(36) NOT NULL,
+    effect_name VARCHAR(20) NOT NULL,
+    use_points INT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY(id, uuid)
+);
+
+INSERT INTO donate_usage_history(
+    uuid,
+    effect_name,
+    use_points,
+    timestamp
+)
+SELECT
+    playeruuid,
+    effectname,
+    usepoint,
+    date
+FROM
+    donatedata
+WHERE
+    usepoint > 0;
+
+CREATE TABLE donate_purchase_history(
+    id INT AUTO_INCREMENT NOT NULL,
+    uuid CHAR(36) NOT NULL,
+    get_points INT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY(id, uuid)
+);
+
+INSERT INTO donate_purchase_history(
+    uuid,
+    get_points,
+    timestamp
+)
+SELECT
+    playeruuid,
+    getpoint,
+    date
+FROM
+    donatedata
+WHERE
+    getpoint > 0;
