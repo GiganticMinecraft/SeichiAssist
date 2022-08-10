7ed8185ecb190a68e517979f72420f32af65ffb0
rito528 <rotoyutoriapp@gmail.com>
2022-08-10 18:54:05 +0900

    feat: donateReadAPIの定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/donate/DonateAPI.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/donate/DonateAPI.scala
@@ -1,6 +1,10 @@
 package com.github.unchama.seichiassist.subsystems.donate
 
 import com.github.unchama.seichiassist.seichiskill.effect.ActiveSkillPremiumEffect
+import com.github.unchama.seichiassist.subsystems.donate.domain.{
+  DonatePremiumEffectPoint,
+  PremiumEffectPurchaseData
+}
 
 import java.util.UUID
 
@@ -22,4 +26,29 @@ object DonateWriteAPI {
 
 }
 
-trait DonateAPI[F[_]] extends DonateWriteAPI[F]
+trait DonateReadAPI[F[_]] {
+
+  /**
+   * 現在のプレミアムエフェクトポイントの合計を取得する作用
+   */
+  def currentPremiumEffectPoints(uuid: UUID): F[DonatePremiumEffectPoint]
+
+  /**
+   * プレミアムエフェクトの購入履歴を取得する作用
+   */
+  def donatePremiumEffectPointPurchaseHistory(uuid: UUID): F[Vector[PremiumEffectPurchaseData]]
+
+  /**
+   * プレミアムエフェクトの使用履歴を取得する作用
+   */
+  def donatePremiumEffectPointUsageHistory(uuid: UUID): F[Vector[PremiumEffectPurchaseData]]
+
+}
+
+object DonateReadAPI {
+
+  def apply[F[_]](implicit ev: DonateReadAPI[F]): DonateReadAPI[F] = ev
+
+}
+
+trait DonateAPI[F[_]] extends DonateWriteAPI[F] with DonateReadAPI[F]
