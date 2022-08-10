7b8ba76b0f8a8a60f7c99bac310fd744ea740384
rito528 <rotoyutoriapp@gmail.com>
2022-08-10 18:55:17 +0900

    feat: systemにapiを定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/donate/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/donate/System.scala
@@ -2,14 +2,17 @@ package com.github.unchama.seichiassist.subsystems.donate
 
 import cats.effect.ConcurrentEffect
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
+import com.github.unchama.seichiassist.seichiskill.effect.ActiveSkillPremiumEffect
 import com.github.unchama.seichiassist.subsystems.donate.bukkit.commands.DonationCommand
 import com.github.unchama.seichiassist.subsystems.donate.domain.{
   DonatePersistence,
   DonatePremiumEffectPoint,
-  PlayerName
+  PremiumEffectPurchaseData
 }
 import com.github.unchama.seichiassist.subsystems.donate.infrastructure.JdbcDonatePersistence
 import org.bukkit.command.TabExecutor
+
+import java.util.UUID
 
 trait System[F[_]] extends Subsystem[F] {
 
@@ -23,7 +26,26 @@ object System {
     implicit val persistence: DonatePersistence[F] = new JdbcDonatePersistence[F]
 
     new System[F] {
-      override val api: DonateAPI[F] = new DonateAPI[F] {}
+      override val api: DonateAPI[F] = new DonateAPI[F] {
+        override def currentPremiumEffectPoints(uuid: UUID): F[DonatePremiumEffectPoint] =
+          persistence.currentPremiumEffectPoints(uuid)
+
+        override def donatePremiumEffectPointPurchaseHistory(
+          uuid: UUID
+        ): F[Vector[PremiumEffectPurchaseData]] =
+          persistence.donatePremiumEffectPointPurchaseHistory(uuid)
+
+        override def donatePremiumEffectPointUsageHistory(
+          uuid: UUID
+        ): F[Vector[PremiumEffectPurchaseData]] =
+          persistence.donatePremiumEffectPointUsageHistory(uuid)
+
+        override def useDonatePremiumEffectPoint(
+          uuid: UUID,
+          activeSkillPremiumEffect: ActiveSkillPremiumEffect
+        ): F[Unit] =
+          persistence.useDonatePremiumEffectPoint(uuid, activeSkillPremiumEffect)
+      }
 
       override val commands: Map[String, TabExecutor] = Map(
         "donation" -> new DonationCommand[F].executor
