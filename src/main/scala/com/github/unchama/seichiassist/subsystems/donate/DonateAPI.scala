c0784d2eecdeb9aee2413cf120465723ac3c1940
rito528 <rotoyutoriapp@gmail.com>
2022-08-10 18:51:55 +0900

    feat: apiにuseDonatePremiumEffectPointを定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/donate/DonateAPI.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/donate/DonateAPI.scala
@@ -1,8 +1,18 @@
 package com.github.unchama.seichiassist.subsystems.donate
 
+import com.github.unchama.seichiassist.seichiskill.effect.ActiveSkillPremiumEffect
+
+import java.util.UUID
+
 trait DonateWriteAPI[F[_]] {
 
-
+  /**
+   * プレミアムエフェクトポイントを使用する作用
+   */
+  def useDonatePremiumEffectPoint(
+    uuid: UUID,
+    activeSkillPremiumEffect: ActiveSkillPremiumEffect
+  ): F[Unit]
 
 }
 
