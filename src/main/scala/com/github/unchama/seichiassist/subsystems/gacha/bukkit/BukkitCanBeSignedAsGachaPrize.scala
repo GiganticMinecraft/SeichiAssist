0da6bd94ad2407ea6ab45d7fabdc42bbc3f9ed11
rito528 <rotoyutoriapp@gmail.com>
2022-08-13 00:40:18 +0900

    fix: BukkitCanBeSignedAsGachaPrizeにItemStackUtilのものを移動する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/bukkit/BukkitCanBeSignedAsGachaPrize.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/bukkit/BukkitCanBeSignedAsGachaPrize.scala
@@ -1,11 +1,41 @@
 package com.github.unchama.seichiassist.subsystems.gacha.bukkit
 
 import com.github.unchama.seichiassist.subsystems.gacha.domain.CanBeSignedAsGachaPrize
-import com.github.unchama.util.bukkit.ItemStackUtil.appendOwnerInformation
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 
-class BukkitCanBeSignedAsGachaPrize extends CanBeSignedAsGachaPrize[ItemStack] {
+import org.bukkit.ChatColor._
+
+object BukkitCanBeSignedAsGachaPrize extends CanBeSignedAsGachaPrize[ItemStack] {
   override def signWith(ownerName: String): ItemStack => ItemStack = { itemStack =>
     appendOwnerInformation(ownerName)(itemStack)
+  }
+
+  /**
+   * メタが `f` によって変更されたような新たな `ItemStack` を作成する
+   */
+  private def modifyMeta(f: ItemMeta => Unit)(stack: ItemStack): ItemStack = {
+    val itemMeta = stack.getItemMeta
+    val newItem = stack.clone()
+    f(itemMeta)
+    newItem.setItemMeta(itemMeta)
+    newItem
+  }
+
+  /**
+   * `owner` のloreが追加されたような新たな `ItemStack` を作成する
+   */
+  private def appendOwnerInformation(ownerName: String)(itemStack: ItemStack): ItemStack = {
+    import scala.jdk.CollectionConverters._
+
+    modifyMeta { m =>
+      import m._
+      setLore {
+        val originalLore = if (itemStack.getItemMeta.hasLore) getLore.asScala else Nil
+        val appended = originalLore ++ List(s"$RESET${DARK_GREEN}所有者：${ownerName}")
+
+        appended.asJava
+      }
+    }(itemStack)
   }
 }
