fd021a75bf0aea8d92aafa81a0432d1a0406de43
rito528 <rotoyutoriapp@gmail.com>
2022-08-15 18:46:03 +0900

    feat: GachaRarity.ofを実装

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaRarity.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/domain/GachaRarity.scala
@@ -14,7 +14,20 @@ object GachaRarity {
 
     case object Regular extends GachaRarity(GachaProbability(0.1))
 
+    case object GachaRingoOrExpBottle extends GachaRarity(GachaProbability(1.0))
+
     override def values: IndexedSeq[GachaRarity] = findValues
+
+    def of[ItemStack](gachaPrize: GachaPrize[ItemStack]): GachaRarity = {
+      val gachaPrizeProbability = gachaPrize.probability.value
+      if (gachaPrizeProbability < GachaRarity.Gigantic.maxProbability.value)
+        GachaRarity.Gigantic
+      else if (gachaPrizeProbability < GachaRarity.Big.maxProbability.value)
+        GachaRarity.Big
+      else if (gachaPrizeProbability < GachaRarity.Regular.maxProbability.value)
+        GachaRarity.Regular
+      else GachaRarity.GachaRingoOrExpBottle
+    }
 
   }
 
