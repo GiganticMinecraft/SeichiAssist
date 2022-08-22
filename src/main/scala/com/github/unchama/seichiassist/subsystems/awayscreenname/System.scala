ea674e6a8d122f717ec1e19d6e5d207092b01bbb
rito528 <rotoyutoriapp@gmail.com>
2022-08-22 21:24:44 +0900

    feat: SystemにidleMinuteRepositoryControlsを定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/awayscreenname/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/awayscreenname/System.scala
@@ -1,7 +1,12 @@
 package com.github.unchama.seichiassist.subsystems.awayscreenname
 
+import cats.effect.SyncIO
+import com.github.unchama.datarepository.bukkit.player.BukkitRepositoryControls
+import com.github.unchama.datarepository.template.RepositoryDefinition
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
+import com.github.unchama.seichiassist.subsystems.awayscreenname.application.repository.IdleMinuteRepositoryDefinitions
 import com.github.unchama.seichiassist.subsystems.awayscreenname.domain.IdleMinute
+import org.bukkit.entity.Player
 
 import java.util.UUID
 
@@ -13,14 +18,26 @@ trait System[F[_]] extends Subsystem[F] {
 
 object System {
 
-  def wired[F[_]]: System[F] = {
-    new System[F] {
-      override val api: AwayScreenNameAPI[F] = new AwayScreenNameAPI[F] {
+  def wired[F[_]]: SyncIO[System[F]] = {
+    for {
+      idleMinuteRepositoryControls <- BukkitRepositoryControls.createHandles(
+        RepositoryDefinition
+          .Phased
+          .TwoPhased(
+            IdleMinuteRepositoryDefinitions.initialization[SyncIO, Player],
+            IdleMinuteRepositoryDefinitions.finalization[SyncIO, Player]
+          )
+      )
+    } yield {
+      idleMinuteRepositoryControls.repository
+      new System[F] {
+        override val api: AwayScreenNameAPI[F] = new AwayScreenNameAPI[F] {
 
-        /**
-         * @return 指定したUUIDプレイヤーの[[IdleMinute]]を返す作用
-         */
-        override def idleTime(uuid: UUID): F[IdleMinute] = ???
+          /**
+           * @return 指定したUUIDプレイヤーの[[IdleMinute]]を返す作用
+           */
+          override def idleTime(uuid: UUID): F[IdleMinute] = ???
+        }
       }
     }
   }
