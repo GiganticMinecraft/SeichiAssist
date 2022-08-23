48b4bd402bc4e034c6b2f4466e199157001a265d
rito528 <rotoyutoriapp@gmail.com>
2022-08-23 21:09:24 +0900

    feat: PlayerLocationRepositoryDefinitionsの実装

--- src/main/scala/com/github/unchama/seichiassist/subsystems/awayscreenname/application/repository/PlayerLocationRepositoryDefinitions.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/awayscreenname/application/repository/PlayerLocationRepositoryDefinitions.scala
@@ -1,5 +1,21 @@
 package com.github.unchama.seichiassist.subsystems.awayscreenname.application.repository
 
+import cats.Applicative
+import com.github.unchama.datarepository.template.finalization.RepositoryFinalization
+import com.github.unchama.datarepository.template.initialization.TwoPhasedRepositoryInitialization
+import com.github.unchama.seichiassist.subsystems.awayscreenname.domain.PlayerLocationRepository
+
 object PlayerLocationRepositoryDefinitions {
+
+  def initialization[F[_]: Applicative, Location, Player](
+    playerLocationRepository: Player => PlayerLocationRepository[F, Location, Player]
+  ): TwoPhasedRepositoryInitialization[F, Player, PlayerLocationRepository] =
+    TwoPhasedRepositoryInitialization.withoutPrefetching[F, Player, PlayerLocationRepository] {
+      player => Applicative[F].pure(playerLocationRepository(player))
+    }
+
+  def finalization[F[_]: Applicative, Player]
+    : RepositoryFinalization[F, Player, PlayerLocationRepository] =
+    RepositoryFinalization.trivial
 
 }
