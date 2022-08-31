455354f64a9bde0e97f27742c17b458caf130ef4
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-31 22:58:31 +0900

    feat: add SubHome#maxSubHomePerPlayer

--- src/main/scala/com/github/unchama/seichiassist/subsystems/subhome/domain/SubHome.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/subhome/domain/SubHome.scala
@@ -13,3 +13,10 @@ case class SubHomeLocation(
  * サブホームオブジェクトのクラス
  */
 case class SubHome(name: Option[String], location: SubHomeLocation)
+
+object SubHome {
+  /**
+   * プレイヤーが持つことができるサブホームの最大個数
+   */
+  val maxSubHomePerPlayer: 9 = 9
+}
