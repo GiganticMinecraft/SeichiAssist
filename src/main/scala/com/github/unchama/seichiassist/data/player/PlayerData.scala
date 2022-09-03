12b231d39aa01ad4408c08f5c722cd650f842ff2
rito528 <rotoyutoriapp@gmail.com>
2022-09-03 20:55:20 +0900

    refactor: PlayerDataからlocationを削除

--- src/main/scala/com/github/unchama/seichiassist/data/player/PlayerData.scala
+++ src/main/scala/com/github/unchama/seichiassist/data/player/PlayerData.scala
@@ -36,9 +36,6 @@ class PlayerData(@Deprecated() val uuid: UUID, val name: String) {
   // MineStackの履歴
   val hisotryData: MineStackUsageHistory = new MineStackUsageHistory()
 
-  // 現在座標
-  var loc: Option[Location] = None
-
   // 放置時間
   var idleMinute = 0
 
@@ -183,12 +180,6 @@ class PlayerData(@Deprecated() val uuid: UUID, val name: String) {
   def setDisplayName(): Unit = {
     val playerName = player.getName
 
-    // 放置時に色を変える
-    val idleColor: String =
-      if (idleMinute >= 10) s"$DARK_GRAY"
-      else if (idleMinute >= 3) s"$GRAY"
-      else ""
-
     val amountData =
       SeichiAssist
         .instance
@@ -201,7 +192,7 @@ class PlayerData(@Deprecated() val uuid: UUID, val name: String) {
     val level = amountData.levelCorrespondingToExp.level
     val starLevel = amountData.starLevelCorrespondingToExp
 
-    val newDisplayName = idleColor + {
+    val newDisplayName =  {
       val nicknameSettings = settings.nickname
       val currentNickname =
         Option
