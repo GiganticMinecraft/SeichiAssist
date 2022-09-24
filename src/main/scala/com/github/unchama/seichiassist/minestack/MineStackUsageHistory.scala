fb2760128f21d4b9e2458b2c853abcfeead3ec95
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-09-24 22:18:22 +0900

    fix: immutableなコレクションを更新したビューを再代入していない問題を修正 closes #1613

--- src/main/scala/com/github/unchama/seichiassist/minestack/MineStackUsageHistory.scala
+++ src/main/scala/com/github/unchama/seichiassist/minestack/MineStackUsageHistory.scala
@@ -7,12 +7,15 @@ class MineStackUsageHistory {
   private var _usageHistory: Vector[MineStackObject] = Vector.empty
 
   /**
-   * 履歴に追加します。ただし、データの保存可能な最大値を超えていた場合、先頭から削除されます。
+   * アイテムを履歴に追加します。
+   * ただし、追加したことで履歴の最大件数を超えた場合、時系列順で見て最も最初に追加された履歴が削除されます。
    */
   def addHistory(mineStackObject: MineStackObject): Unit = {
     _usageHistory = _usageHistory.filterNot(_ == mineStackObject)
     _usageHistory :+= mineStackObject
-    if (_usageHistory.size > maxListSize) _usageHistory.drop(1)
+    if (_usageHistory.size > maxListSize) {
+      _usageHistory = _usageHistory.drop(1)
+    }
   }
 
   def usageHistory: Vector[MineStackObject] = _usageHistory
