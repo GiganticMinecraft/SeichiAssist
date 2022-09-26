a0f1c3ace959b4a209c834881d782094f1638830
rito528 <rotoyutoriapp@gmail.com>
2022-09-26 22:17:50 +0900

    feat: MineStackObjectRepositoryDefinitionを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/application/repository/MineStackObjectRepositoryDefinition.scala
@@ -0,0 +1,21 @@
+package com.github.unchama.seichiassist.subsystems.minestack.application.repository
+
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import com.github.unchama.datarepository.definitions.RefDictBackedRepositoryDefinition
+import com.github.unchama.datarepository.template.RepositoryDefinition
+import com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobject.{
+  MineStackObjectPersistence,
+  MineStackObjectWithAmount
+}
+
+object MineStackObjectRepositoryDefinition {
+
+  def withContext[F[_]: Sync, Player](
+    persistence: MineStackObjectPersistence[F]
+  ): RepositoryDefinition[F, Player, Ref[F, List[MineStackObjectWithAmount]]] =
+    RefDictBackedRepositoryDefinition
+      .usingUuidRefDict[F, Player, List[MineStackObjectWithAmount]](persistence)(List.empty)
+      .toRefRepository
+
+}
