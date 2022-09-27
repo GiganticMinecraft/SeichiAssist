ebb0fb96c920ea2d4ce29b7a6bedf43474e888da
rito528 <rotoyutoriapp@gmail.com>
2022-09-27 22:34:17 +0900

    feat: MineStackObjectWithAmountにIncreaseとDecreaseを定義する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/minestackobject/MineStackObjectWithAmount.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/minestackobject/MineStackObjectWithAmount.scala
@@ -2,4 +2,19 @@ package com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobj
 
 case class MineStackObjectWithAmount(mineStackObject: MineStackObject, amount: Long) {
   require(amount >= 0)
+
+  /**
+   * @return `amount`を指定分だけ増加させる作用
+   */
+  def increase(value: Long): MineStackObjectWithAmount = {
+    this.copy(amount = amount + value)
+  }
+
+  /**
+   * @return `amount`を指定分だけ減少させる作用
+   */
+  def decrease(value: Long): MineStackObjectWithAmount = {
+    this.copy(amount = amount - value)
+  }
+
 }
