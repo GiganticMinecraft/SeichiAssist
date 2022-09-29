5479e8b41ad76cb75bbd9b845877a2ae7ad799bd
rito528 <rotoyutoriapp@gmail.com>
2022-09-29 22:22:21 +0900

    feat: MineStackUsageHistoryRepositoryDefinitionsを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/application/repository/MineStackUsageHistoryRepositoryDefinitions.scala
@@ -0,0 +1,21 @@
+package com.github.unchama.seichiassist.subsystems.minestack.application.repository
+
+import cats.Applicative
+import com.github.unchama.datarepository.template.finalization.RepositoryFinalization
+import com.github.unchama.datarepository.template.initialization.TwoPhasedRepositoryInitialization
+import com.github.unchama.seichiassist.subsystems.minestack.domain.MineStackUsageHistory
+
+object MineStackUsageHistoryRepositoryDefinitions {
+
+  def initialization[F[_]: Applicative, Player, ItemStack]
+    : TwoPhasedRepositoryInitialization[F, Player, MineStackUsageHistory[ItemStack]] =
+    TwoPhasedRepositoryInitialization
+      .withoutPrefetching[F, Player, MineStackUsageHistory[ItemStack]] { player =>
+        Applicative[F].pure(new MineStackUsageHistory[ItemStack])
+      }
+
+  def finalization[F[_]: Applicative, Player, ItemStack]
+    : RepositoryFinalization[F, Player, MineStackUsageHistory[ItemStack]] =
+    RepositoryFinalization.trivial
+
+}
