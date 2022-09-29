b2e5d8123298d52692ee332d4dc5f670086ba8ff
rito528 <rotoyutoriapp@gmail.com>
2022-09-29 23:17:31 +0900

    feat: MineStackSettingsを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/MineStackSettings.scala
@@ -0,0 +1,20 @@
+package com.github.unchama.seichiassist.subsystems.minestack.domain
+
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+
+class MineStackSettings[F[_]: Sync] {
+
+  private val autoMineStack: Ref[F, Boolean] = Ref.unsafe(false)
+
+  /**
+   * @return AutoMineStackをonに切り替えます
+   */
+  def toggleAutoMineStackTurnOn: F[Unit] = autoMineStack.set(true)
+
+  /**
+   * @return AutoMineStackをoffに切り替えます
+   */
+  def toggleAutoMineStackTurnOff: F[Unit] = autoMineStack.set(false)
+
+}
