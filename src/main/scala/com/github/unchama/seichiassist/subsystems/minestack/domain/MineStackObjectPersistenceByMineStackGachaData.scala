c0253532aa58b3aeee0e9e44f3c014974a7cc73b
rito528 <rotoyutoriapp@gmail.com>
2022-09-29 17:00:29 +0900

    feat: MineStackObjectPersistenceByMineStackGachaDataを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/MineStackObjectPersistenceByMineStackGachaData.scala
@@ -0,0 +1,11 @@
+package com.github.unchama.seichiassist.subsystems.minestack.domain
+
+import com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobject.MineStackObject
+
+trait MineStackObjectPersistenceByMineStackGachaData[F[_]] {
+
+  /**
+   * @return MineStackGachaDataから[[MineStackObject]]のListを取得する
+   */
+  def load: F[List[MineStackObject]]
+}
