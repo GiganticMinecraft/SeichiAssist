333d1f8ce41bfc303288f3328a97fdf71790eb7a
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-10-14 20:58:43 +0900

    docs: publicメソッドに説明を追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/buildcount/subsystems/notification/application/actions/NotifyBuildAmountThreshold.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/buildcount/subsystems/notification/application/actions/NotifyBuildAmountThreshold.scala
@@ -5,6 +5,9 @@ import com.github.unchama.seichiassist.subsystems.buildcount.domain.playerdata.B
 
 trait NotifyBuildAmountThreshold[F[_], Player] {
 
+  /**
+   * @return 建築量の100万の位が増えるごとに全体通知を出す作用を返す
+   */
   def ofBuildAmountTo(player: Player)(diff: Diff[BuildAmountData]): F[Unit]
 
 }
