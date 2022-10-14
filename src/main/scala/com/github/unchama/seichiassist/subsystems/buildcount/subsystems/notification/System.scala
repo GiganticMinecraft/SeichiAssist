145c872a80d25e11df7492060d230fc0d90a732d
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-10-15 01:35:37 +0900

    refactor: 型引数から不要になったOnMinecraftServerThreadを除去し、Syncを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/buildcount/subsystems/notification/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/buildcount/subsystems/notification/System.scala
@@ -1,28 +1,19 @@
 package com.github.unchama.seichiassist.subsystems.buildcount.subsystems.notification
 
-import cats.effect.ConcurrentEffect
+import cats.effect.{ConcurrentEffect, Sync}
 import com.github.unchama.generic.effect.stream.StreamExtra
-import com.github.unchama.minecraft.actions.OnMinecraftServerThread
 import com.github.unchama.seichiassist.subsystems.buildcount.BuildCountAPI
-import com.github.unchama.seichiassist.subsystems.buildcount.subsystems.notification.application.actions.{
-  NotifyBuildAmountThreshold,
-  NotifyLevelUp
-}
-import com.github.unchama.seichiassist.subsystems.buildcount.subsystems.notification.bukkit.actions.{
-  BukkitNotifyBuildAmountThreshold,
-  BukkitNotifyLevelUp
-}
+import com.github.unchama.seichiassist.subsystems.buildcount.subsystems.notification.application.actions.{NotifyBuildAmountThreshold, NotifyLevelUp}
+import com.github.unchama.seichiassist.subsystems.buildcount.subsystems.notification.bukkit.actions.{BukkitNotifyBuildAmountThreshold, BukkitNotifyLevelUp}
 import com.github.unchama.seichiassist.subsystems.discordnotification.DiscordNotificationAPI
 import io.chrisdavenport.log4cats.ErrorLogger
 import org.bukkit.entity.Player
 
 object System {
 
-  def backgroundProcess[F[
+  def backgroundProcess[F[_]: Sync: ConcurrentEffect: ErrorLogger: DiscordNotificationAPI, G[
     _
-  ]: ConcurrentEffect: OnMinecraftServerThread: ErrorLogger: DiscordNotificationAPI, G[_], A](
-    buildCountReadAPI: BuildCountAPI[F, G, Player]
-  ): F[A] = {
+  ], A](buildCountReadAPI: BuildCountAPI[F, G, Player]): F[A] = {
     val notifyLevelUp: NotifyLevelUp[F, Player] = BukkitNotifyLevelUp[F]
     val notifyBuildAmountThreshold: NotifyBuildAmountThreshold[F, Player] =
       BukkitNotifyBuildAmountThreshold[F]
