19c210917c13186a9cdd53c9f2770c33e22debed
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-17 21:13:26 +0900

    chore: fix typo

--- src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
+++ src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
@@ -3,4 +3,4 @@ use seichiassist;
 -- 接続時間がオーバーフローしているプレイヤーがいるのでbigintに
 alter table playerdata modify playtick bigint default 0;
 -- ref. https://discord.com/channels/288275544234131456/324180473649692682/1031533117078511776
-update playerdata set playtick = 2147483648 - playtick + 2147483648 WHERE playtick < 0;
+update playerdata set playtick = 2147483648 - playtick + 2147483647 WHERE playtick < 0;
