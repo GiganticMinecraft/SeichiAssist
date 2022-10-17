a0fe8873ff38abdf2533c27d5b5e8d27bfc086c2
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-17 21:12:08 +0900

    chore: fix overflowed `playtick` in SQL

--- src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
+++ src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
@@ -2,3 +2,5 @@ use seichiassist;
 
 -- 接続時間がオーバーフローしているプレイヤーがいるのでbigintに
 alter table playerdata modify playtick bigint default 0;
+-- ref. https://discord.com/channels/288275544234131456/324180473649692682/1031533117078511776
+update playerdata set playtick = 2147483648 - playtick + 2147483648 WHERE playtick < 0;
