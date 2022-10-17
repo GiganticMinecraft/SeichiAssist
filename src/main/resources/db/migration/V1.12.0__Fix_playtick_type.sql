ef39aa1b5f5baceb769adfd4de43cff374a0ba35
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-17 21:43:30 +0900

    chore: reserved words in SQL should be UPPER_CASE

--- src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
+++ src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
@@ -1,7 +1,7 @@
 use seichiassist;
 
 -- 接続時間がオーバーフローしているプレイヤーがいるのでbigintに
-alter table playerdata modify playtick bigint default 0;
+ALTER TABLE playerdata MODIFY playtick BIGINT DEFAULT 0;
 -- ref. https://discord.com/channels/288275544234131456/324180473649692682/1031533117078511776
 -- 「4294967295」は、上記計算式中の定数を足し合わせたもの
-update playerdata set playtick = 4294967295 - playtick WHERE playtick < 0;
+UPDATE playerdata SET playtick = 4294967295 - playtick WHERE playtick < 0;
