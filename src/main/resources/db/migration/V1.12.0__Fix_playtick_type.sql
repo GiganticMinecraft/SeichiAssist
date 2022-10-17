4d6fb64ff52c7b0cf01cbbb705f76ab5d4904b25
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-17 21:17:42 +0900

    chore: refactor calculation

--- src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
+++ src/main/resources/db/migration/V1.12.0__Fix_playtick_type.sql
@@ -3,4 +3,5 @@ use seichiassist;
 -- 接続時間がオーバーフローしているプレイヤーがいるのでbigintに
 alter table playerdata modify playtick bigint default 0;
 -- ref. https://discord.com/channels/288275544234131456/324180473649692682/1031533117078511776
-update playerdata set playtick = 2147483648 - playtick + 2147483647 WHERE playtick < 0;
+-- 「4294967295」は、上記計算式中の定数を足し合わせたもの
+update playerdata set playtick = 4294967295 - playtick WHERE playtick < 0;
