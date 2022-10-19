9f8408acde3fce088b61e928f724430ad46034c0
rito528 <rotoyutoriapp@gmail.com>
2022-10-19 22:02:48 +0900

    feat: GachaTicketのSystemを実装

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/subsystems/gachaticket/System.scala
@@ -0,0 +1,50 @@
+package com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket
+
+import cats.effect.Sync
+import com.github.unchama.concurrent.NonServerThreadContextShift
+import com.github.unchama.seichiassist.meta.subsystem.Subsystem
+import com.github.unchama.seichiassist.subsystems.gacha.domain.PlayerName
+import com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.domain.{
+  GachaTicketAmount,
+  ReceiptResultOfGachaTicketFromAdminTeam
+}
+import com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.infrastructure.JdbcGachaTicketFromAdminTeamRepository
+
+import java.util.UUID
+
+trait System[F[_]] extends Subsystem[F] {
+
+  val api: GachaTicketAPI[F]
+
+}
+
+object System {
+
+  def wired[F[_]: Sync: NonServerThreadContextShift]: System[F] = {
+    val gachaTicketFromAdminTeamRepository = new JdbcGachaTicketFromAdminTeamRepository[F]
+
+    new System[F] {
+      override val api: GachaTicketAPI[F] = new GachaTicketAPI[F] {
+
+        override def addToAllKnownPlayers(amount: GachaTicketAmount): F[Unit] =
+          gachaTicketFromAdminTeamRepository.addToAllKnownPlayers(amount)
+
+        override def addByPlayerName(
+          amount: GachaTicketAmount,
+          playerName: PlayerName
+        ): F[ReceiptResultOfGachaTicketFromAdminTeam] =
+          gachaTicketFromAdminTeamRepository.addByPlayerName(amount, playerName)
+
+        override def addByUUID(
+          amount: GachaTicketAmount,
+          uuid: UUID
+        ): F[ReceiptResultOfGachaTicketFromAdminTeam] =
+          gachaTicketFromAdminTeamRepository.addByUUID(amount, uuid)
+
+        override def receive(uuid: UUID): F[GachaTicketAmount] =
+          gachaTicketFromAdminTeamRepository.receive(uuid)
+      }
+    }
+  }
+
+}
