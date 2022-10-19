01758f50b353b13e1d9119cba2e870e329d46170
rito528 <rotoyutoriapp@gmail.com>
2022-10-19 21:59:34 +0900

    feat: GachaTicketAPIを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/subsystems/gachaticket/GachaTicketAPI.scala
@@ -0,0 +1,45 @@
+package com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket
+
+import com.github.unchama.seichiassist.subsystems.gacha.domain.PlayerName
+import com.github.unchama.seichiassist.subsystems.gacha.subsystems.gachaticket.domain.{
+  GachaTicketAmount,
+  ReceiptResultOfGachaTicketFromAdminTeam
+}
+
+import java.util.UUID
+
+trait GachaTicketAPI[F[_]] {
+
+  /**
+   * @return 呼び出された時点で永続化バックエンド中にある全プレイヤーの「運営からのガチャ券」を増加させる作用
+   */
+  def addToAllKnownPlayers(amount: GachaTicketAmount): F[Unit]
+
+  /**
+   * @return 指定されたプレイヤー名の「運営からのガチャ券」の枚数を増加させる作用
+   */
+  def addByPlayerName(
+    amount: GachaTicketAmount,
+    playerName: PlayerName
+  ): F[ReceiptResultOfGachaTicketFromAdminTeam]
+
+  /**
+   * @return 指定されたUUIDの「運営からのガチャ券」の枚数を増加させる作用
+   */
+  def addByUUID(
+    amount: GachaTicketAmount,
+    uuid: UUID
+  ): F[ReceiptResultOfGachaTicketFromAdminTeam]
+
+  /**
+   * @return 運営からのガチャ券を受け取った枚数
+   */
+  def receive(uuid: UUID): F[GachaTicketAmount]
+
+}
+
+object GachaTicketAPI {
+
+  def apply[F[_]](implicit ev: GachaTicketAPI[F]): GachaTicketAPI[F] = ev
+
+}
