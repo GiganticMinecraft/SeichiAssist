0ba19238cc0607aab82151e79b0c7dcca9110d30
rito528 <rotoyutoriapp@gmail.com>
2022-10-20 22:50:34 +0900

    feat: TradeResultにTransactionInfoを追加し、GtToSiinaシステムをそれに追従させる

--- src/main/scala/com/github/unchama/seichiassist/subsystems/tradesystems/subsystems/gttosiina/bukkit/traderules/BukkitTrade.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/tradesystems/subsystems/gttosiina/bukkit/traderules/BukkitTrade.scala
@@ -5,19 +5,25 @@ import com.github.unchama.seichiassist.subsystems.gacha.domain.CanBeSignedAsGach
 import com.github.unchama.seichiassist.subsystems.gacha.domain.GachaRarity.GachaRarity
 import com.github.unchama.seichiassist.subsystems.gacha.domain.GachaRarity.GachaRarity.Gigantic
 import com.github.unchama.seichiassist.subsystems.gacha.domain.gachaprize.GachaPrize
-import com.github.unchama.seichiassist.subsystems.tradesystems.domain.{TradeResult, TradeRule, TradeSuccessResult}
+import com.github.unchama.seichiassist.subsystems.tradesystems.domain.{
+  TradeResult,
+  TradeRule,
+  TradeSuccessResult
+}
 import com.github.unchama.seichiassist.subsystems.tradesystems.subsystems.gttosiina.domain.StaticTradeItemFactory
 import org.bukkit.inventory.ItemStack
+
+import scala.reflect.ClassTag.Nothing
 
 class BukkitTrade(owner: String, gachaPrizeTable: Vector[GachaPrize[ItemStack]])(
   implicit canBeSignedAsGachaPrize: CanBeSignedAsGachaPrize[ItemStack],
   tradeItemFactory: StaticTradeItemFactory[ItemStack]
-) extends TradeRule[ItemStack] {
+) extends TradeRule[ItemStack, Nothing] {
 
   /**
    * プレーヤーが入力したアイテムから、交換結果を計算する
    */
-  override def trade(contents: List[ItemStack]): TradeResult[ItemStack] = {
+  override def trade(contents: List[ItemStack]): TradeResult[ItemStack, Nothing] = {
     val giganticItemStacks = gachaPrizeTable
       .filter(GachaRarity.of[ItemStack](_) == Gigantic)
       .map(gachaPrize => canBeSignedAsGachaPrize.signWith(owner)(gachaPrize))
@@ -25,7 +31,7 @@ class BukkitTrade(owner: String, gachaPrizeTable: Vector[GachaPrize[ItemStack]])
     val (tradableItems, nonTradableItems) =
       contents.partition(itemStack => giganticItemStacks.exists(_.isSimilar(itemStack)))
 
-    TradeResult[ItemStack](
+    TradeResult[ItemStack, Nothing](
       tradableItems.map(_ =>
         /* NOTE: 2022/10/19現在、交換できるギガンティックアイテムは
               スタックできるアイテムではない。
@@ -34,7 +40,8 @@ class BukkitTrade(owner: String, gachaPrizeTable: Vector[GachaPrize[ItemStack]])
          */
         TradeSuccessResult(
           tradeItemFactory.getMaxRingo(owner),
-          SeichiAssist.seichiAssistConfig.rateGiganticToRingo
+          SeichiAssist.seichiAssistConfig.rateGiganticToRingo,
+          Nothing
         )
       ),
       nonTradableItems
