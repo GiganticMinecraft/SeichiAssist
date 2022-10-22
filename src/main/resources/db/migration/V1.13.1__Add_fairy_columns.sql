cbb7bc8fbfc8c3866da5d430e4b3b8a245cd8a8f
rito528 <rotoyutoriapp@gmail.com>
2022-10-22 21:44:55 +0900

    style: V1.13.1のファイルを規則にそわせる

--- src/main/resources/db/migration/V1.13.1__Add_fairy_columns.sql
+++ src/main/resources/db/migration/V1.13.1__Add_fairy_columns.sql
@@ -1,3 +1,3 @@
-use seichiassist;
+USE seichiassist;
 
-ALTER TABLE playerdata ADD is_fairy_speech_play_sound BOOLEAN DEFAULT TRUE AFTER toggleVotingFairy ;
+ALTER TABLE playerdata ADD is_fairy_speech_play_sound BOOLEAN DEFAULT TRUE AFTER toggleVotingFairy;
