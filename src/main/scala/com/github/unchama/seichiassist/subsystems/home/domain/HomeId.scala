e3b3872e56e38701ab88d650ab37716d0c925779
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-10-22 12:55:10 +0900

    feat: プレイヤー各種レベルに応じて追加ホームポイント数を計算する関数を追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/home/domain/HomeId.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/home/domain/HomeId.scala
@@ -1,5 +1,9 @@
 package com.github.unchama.seichiassist.subsystems.home.domain
 
+import com.github.unchama.seichiassist.subsystems.breakcount.domain.SeichiAmountData
+import com.github.unchama.seichiassist.subsystems.buildcount.domain.playerdata.BuildAmountData
+import org.bukkit.entity.Player
+
 case class HomeId(value: Int) {
   require(
     HomeId.minimumNumber <= value && value <= HomeId.maxNumber,
@@ -20,5 +24,31 @@ object HomeId {
   /**
    * [[HomeId]]が持つことができる最大値
    */
-  val maxNumber: 9 = Home.maxHomePerPlayer
+  val maxNumber = Home.maxHomePerPlayer + maxNumberExtra
+
+  /**
+   * プレイヤーのレベルに応じて追加で持つことができる[[HomeId]]の数
+   */
+  val maxNumberExtra: 9 = 9
+
+  /**
+   * 追加ホームポイント
+   * - 無条件で全員に1つ
+   * - 整地レベルが増加することに1つ（50、100、200、星5）、計4つ
+   * - 建築レベルが増加することに1つ（20、40、60、100）、計4つ
+   *
+   * @return そのプレイヤーが現在扱えるHomeIdの最大値
+   */
+  def maxNumberByPlayerOf(seichiAmount: SeichiAmountData, buildAmount: BuildAmountData): Int = {
+    val seichiLevel = seichiAmount.levelCorrespondingToExp.level
+    val additionalHomePointBySeichiLevel =
+      Seq(50, 100, 200).foldLeft(0)((acm, elem) => if (seichiLevel >= elem) acm + 1 else acm)
+    val additionalHomePointByStarLevel =
+      if (seichiAmount.starLevelCorrespondingToExp.level >= 5) 1 else 0
+    val buildLevel = buildAmount.levelCorrespondingToExp.level
+    val additionalHomePointByBuildLevel =
+      Seq(20, 40, 60, 100).foldLeft(0)((acm, elem) => if (buildLevel >= elem) acm + 1 else acm)
+
+    1 + additionalHomePointBySeichiLevel + additionalHomePointByStarLevel + additionalHomePointByBuildLevel
+  }
 }
