839fa87617f0166b67f7503dbc20187ed0660e42
rito528 <rotoyutoriapp@gmail.com>
2022-10-23 22:02:43 +0900

    docs: FairySpeechGatewayにドキュメントを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/speech/FairySpeechGateway.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/speech/FairySpeechGateway.scala
@@ -4,8 +4,14 @@ import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.p
 
 trait FairySpeechGateway[F[_]] {
 
+  /**
+   * @return 妖精からメッセージを送信する作用
+   */
   def sendMessage(fairyMessage: FairyMessage): F[Unit]
 
+  /**
+   * @return 妖精がメッセージを送信した時の音を再生する作用
+   */
   def playSpeechSound: F[Unit]
 
 }
