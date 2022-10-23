1c94a51297e4a8aa6f536d6f5eebba3a7d222241
rito528 <rotoyutoriapp@gmail.com>
2022-10-23 18:22:40 +0900

    feat: AppleAteByFairyRankのrequireにエラーメッセージを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/property/AppleAteByFairyRank.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/property/AppleAteByFairyRank.scala
@@ -1,5 +1,5 @@
 package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.property
 
 case class AppleAteByFairyRank(name: String, rank: Int, appleAmount: AppleAmount) {
-  require(rank >= 1)
+  require(rank >= 1, "rankは1以上の値で指定してください。")
 }
