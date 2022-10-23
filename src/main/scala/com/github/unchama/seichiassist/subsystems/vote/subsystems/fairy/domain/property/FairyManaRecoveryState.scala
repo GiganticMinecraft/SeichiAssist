17a390c46d5457db45a4c9c1e50f27b2f0947ebb
rito528 <rotoyutoriapp@gmail.com>
2022-10-23 22:05:19 +0900

    docs: FairyManaRecoveryStateにドキュメントを定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/property/FairyManaRecoveryState.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/property/FairyManaRecoveryState.scala
@@ -2,12 +2,24 @@ package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.
 
 sealed trait FairyManaRecoveryState
 
+/**
+ * 妖精がマナを回復する際に考えられるステータスを定義したオブジェクト
+ */
 object FairyManaRecoveryState {
 
+  /**
+   * マナが完全に回復している
+   */
   case object Full extends FairyManaRecoveryState
 
+  /**
+   * マナを回復したがりんごを消費しなかった
+   */
   case object NotConsumptionApple extends FairyManaRecoveryState
 
+  /**
+   * りんごを消費してマナを回復した
+   */
   case object ConsumptionApple extends FairyManaRecoveryState
 
 }
