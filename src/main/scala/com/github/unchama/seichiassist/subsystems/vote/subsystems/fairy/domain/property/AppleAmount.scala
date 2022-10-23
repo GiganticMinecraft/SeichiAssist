43ad6a371ba12312f6e6c2107306415303a22e5f
rito528 <rotoyutoriapp@gmail.com>
2022-10-23 18:22:00 +0900

    feat: AppleAmountのrequireにエラーメッセージを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/property/AppleAmount.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/property/AppleAmount.scala
@@ -1,5 +1,5 @@
 package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.property
 
 case class AppleAmount(amount: Int) {
-  require(amount >= 0)
+  require(amount >= 0, "amountは非負の値で指定してください。")
 }
