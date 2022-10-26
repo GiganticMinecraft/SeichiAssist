e19fb347f5a032485ab159ca8dbf909862f22e5c
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-10-26 14:08:24 +0900

    chore: エデンサーバー入場時の警告を追加

--- src/main/scala/com/github/unchama/seichiassist/listener/PlayerJoinListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/listener/PlayerJoinListener.scala
@@ -202,6 +202,14 @@ class PlayerJoinListener extends Listener {
         70,
         20
       )
+
+    // エデンサーバーへ入場する際に警告を行う
+    // TODO: エデンサーバーの不具合が解消されたら削除すること
+    if (SeichiAssist.seichiAssistConfig.getServerNum == 2) {
+      player.sendMessage(
+        s"${BOLD}${RED}【警告】${RESET}\n${YELLOW}エデンサーバーは時々予定されておらず、かつ意図されないシャットダウン（いわゆる「鯖落ち」）が起こることがあります。\n鯖落ちによりアイテム等の消失が発生しても補償はできかねます。\n当サーバーは以上の内容をご理解の上ご利用ください。"
+      )
+    }
   }
 
   // プレイヤーがワールドを移動したとき
