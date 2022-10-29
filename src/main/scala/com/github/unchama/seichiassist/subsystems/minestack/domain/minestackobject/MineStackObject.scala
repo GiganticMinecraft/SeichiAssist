c887d5b612afa640fbbbdd6f3bda121b435b99c9
rito528 <rotoyutoriapp@gmail.com>
2022-10-29 23:48:24 +0900

    docs: tryToSignedItemStackをTODOを書きつつFIXMEをけす

--- src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/minestackobject/MineStackObject.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/minestackobject/MineStackObject.scala
@@ -1,6 +1,8 @@
 package com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobject
 
 import com.github.unchama.minecraft.objects.MinecraftMaterial
+import com.github.unchama.seichiassist.subsystems.gacha.GachaAPI
+import com.github.unchama.seichiassist.subsystems.gacha.domain.GachaPrize
 
 case class MineStackObject[ItemStack <: Cloneable](
   mineStackObjectName: String,
@@ -12,41 +14,19 @@ case class MineStackObject[ItemStack <: Cloneable](
 
   def itemStack: ItemStack = _itemStack.clone[ItemStack]
 
-//  /**
-//   * [[GachaPrize]]への変換した後、記名済みの[[ItemStack]]へ変換することを試みます
-//   *
-//   * FIXME: この実装は[[GachaProbability]]が0.0(つまり通常では排出されない)
-//   *  になっていたり、[[GachaPrizeId]]が[[Int]]のMaxValueを取っていたりしているが、
-//   *  [[GachaProbability]]は今後の処理に一切影響を与えないという
-//   *  前提があり(これが直接ガチャで排出されるわけではないので)このようにしている。
-//   *  また、[[GachaPrizeId]]でIntのMaxValueを取っているのは、[[GachaPrizeId]]が[[GachaPrize]]のIdと
-//   *  重複しないようにしているためである。
-//   *  そしてこの実装は、ガチャシステム側で通常排出アイテムと
-//   *  イベント時に排出されるアイテムが常にSeichiAssist側で取得できる機構
-//   *  see: https://github.com/GiganticMinecraft/SeichiAssist/issues/1657
-//   *  を実装した後に、そちら側に合わせる実装にするべきである。
-//   *  ※現在イベント時に期間限定で排出されるGTアイテムなどを登録する際に、
-//   *  一度通常GTアイテムを削除しているようなので、現在の仕様のままガチャシステムから[[GachaPrize]]を取得
-//   *  しようとすると期間限定GTアイテムが排出されている期間は、MineStackからGTアイテムを取り出すことができない
-//   *  (期間中は[[GachaPrize]]が存在しないので)ということになるのを防ぐため、確実に[[GachaPrize]]を取得するために
-//   *  やむを得ず実装したものである。
-//   */
-//  def tryToSignedItemStack(
-//    name: String
-//  )(implicit signedAsGachaPrize: CanBeSignedAsGachaPrize[ItemStack]): Option[ItemStack] = {
-//    if (
-//      category != MineStackObjectCategory.GACHA_PRIZES || category == MineStackObjectCategory.BUILTIN_GACHA_PRIZES
-//    ) return None
-//
-//    val gachaPrize = GachaPrize[ItemStack](
-//      _itemStack,
-//      GachaProbability(0.0),
-//      signOwner = true,
-//      GachaPrizeId(Int.MaxValue)
-//    )
-//
-//    Some(signedAsGachaPrize.signWith(name)(gachaPrize))
-//  }
+  /**
+   * [[GachaPrize]]への変換した後、記名済みの[[ItemStack]]へ変換することを試みます
+   */
+  def tryToSignedItemStack[F[_], Player](
+    name: String
+  )(implicit gachaAPI: GachaAPI[F, ItemStack, Player]): Option[ItemStack] = {
+    if (
+      category != MineStackObjectCategory.GACHA_PRIZES || category == MineStackObjectCategory.BUILTIN_GACHA_PRIZES
+    ) return None
+
+    // TODO ガチャAPIからcanBeSigned..を取得して記名して返す
+    None
+  }
 
 }
 
