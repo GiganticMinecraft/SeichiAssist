fb1ad6f2afd416092f62e0bdb3e09d41697eb4f4
rito528 <rotoyutoriapp@gmail.com>
2022-10-30 18:08:59 +0900

    style: PlayerPickupItemListenerの定義をtryIntoMineStackを利用したものに

--- src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/bukkit/PlayerPickupItemListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/bukkit/PlayerPickupItemListener.scala
@@ -4,9 +4,7 @@ import cats.effect.ConcurrentEffect.ops.toAllConcurrentEffectOps
 import cats.effect.{ConcurrentEffect, Sync}
 import com.github.unchama.datarepository.bukkit.player.PlayerDataRepository
 import com.github.unchama.seichiassist.SeichiAssist
-import com.github.unchama.seichiassist.subsystems.minestack.MineStackAPI
-import com.github.unchama.seichiassist.subsystems.minestack.domain.MineStackSettings
-import com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobject.MineStackObjectList
+import com.github.unchama.seichiassist.subsystems.minestack.domain.{MineStackSettings, TryIntoMineStack}
 import org.bukkit.ChatColor._
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityPickupItemEvent
@@ -16,8 +14,7 @@ import org.bukkit.{GameMode, Sound}
 
 class PlayerPickupItemListener[F[_]: ConcurrentEffect](
   implicit mineStackSettingRepository: PlayerDataRepository[MineStackSettings[F]],
-  mineStackObjectList: MineStackObjectList[F, ItemStack],
-  mineStackAPI: MineStackAPI[F, Player, ItemStack]
+  tryIntoMineStack: TryIntoMineStack[F, Player, ItemStack]
 ) extends Listener {
 
   import cats.implicits._
@@ -31,28 +28,23 @@ class PlayerPickupItemListener[F[_]: ConcurrentEffect](
         val item = event.getItem
         val itemStack = item.getItemStack
 
-        val tryIntoMineStackProgram = for {
-          foundMineStackObject <- mineStackObjectList.findByItemStack(itemStack)
-          _ <- {
-            mineStackAPI.addStackedAmountOf(
-              player,
-              foundMineStackObject.get,
-              itemStack.getAmount
-            ) >> Sync[F].delay {
-              event.setCancelled(true)
-              player.playSound(player.getLocation, Sound.ENTITY_ITEM_PICKUP, 1f, 1f)
-              item.remove()
-              if (SeichiAssist.DEBUG) {
-                player.sendMessage(RED.toString + "pick:" + itemStack.toString)
-                player.sendMessage(RED.toString + "pickDurability:" + itemStack.getDurability)
-              }
-            }
-          }.whenA(foundMineStackObject.nonEmpty)
-        } yield ()
-
         val program = for {
           currentAutoMineStackState <- mineStackSettingRepository(player).currentState
-          _ <- tryIntoMineStackProgram.whenA(currentAutoMineStackState)
+          isSucceedTryIntoMineStack <- {
+            if (currentAutoMineStackState)
+              tryIntoMineStack(player, itemStack, itemStack.getAmount)
+            else
+              Sync[F].pure(false)
+          }
+          _ <- Sync[F].delay {
+            event.setCancelled(true)
+            player.playSound(player.getLocation, Sound.ENTITY_ITEM_PICKUP, 1f, 1f)
+            item.remove()
+            if (SeichiAssist.DEBUG) {
+              player.sendMessage(RED.toString + "pick:" + itemStack.toString)
+              player.sendMessage(RED.toString + "pickDurability:" + itemStack.getDurability)
+            }
+          }.whenA(isSucceedTryIntoMineStack)
         } yield ()
 
         program.toIO.unsafeRunAsyncAndForget()
