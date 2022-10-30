eb1210916ba01573cfeba54c8baa2f645fc24021
rito528 <rotoyutoriapp@gmail.com>
2022-10-30 21:36:07 +0900

    refactor: updatePlayerMineStackを削除

--- src/main/scala/com/github/unchama/seichiassist/task/PlayerDataSaveTask.scala
+++ src/main/scala/com/github/unchama/seichiassist/task/PlayerDataSaveTask.scala
@@ -27,19 +27,6 @@ object PlayerDataSaveTask {
   def savePlayerData[F[_]: Sync](player: Player, playerdata: PlayerData): F[Unit] = {
     val databaseGateway = SeichiAssist.databaseGateway
 
-    def updatePlayerMineStack(stmt: Statement): Unit = {
-      val playerUuid = player.getUniqueId.toString
-      playerdata.minestack.getObjectCounts.foreach {
-        case (mineStackObj, amount) =>
-          val updateCommand = ("insert into seichiassist.mine_stack"
-            + "(player_uuid, object_name, amount) values "
-            + "('" + playerUuid + "', '" + mineStackObj.mineStackObjectName + "', '" + amount + "') "
-            + "on duplicate key update amount = values(amount)")
-
-          stmt.executeUpdate(updateCommand)
-      }
-    }
-
     def updateGridTemplate(stmt: Statement): Unit = {
       val playerUuid = player.getUniqueId.toString
 
@@ -197,7 +184,6 @@ object PlayerDataSaveTask {
         updateActiveSkillEffectUnlockState(localStatement)
         updateSeichiSkillUnlockState(localStatement)
         updatePlayerDataColumns(localStatement)
-        updatePlayerMineStack(localStatement)
         updateGridTemplate(localStatement)
         ActionStatus.Ok
       } catch {
