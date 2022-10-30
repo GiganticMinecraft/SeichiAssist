6d681efab7b08ca90df771510993f300c9fa845b
rito528 <rotoyutoriapp@gmail.com>
2022-10-30 17:56:19 +0900

    feat: TryIntoMineStackを実装

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/TryIntoMineStack.scala
@@ -0,0 +1,49 @@
+package com.github.unchama.seichiassist.subsystems.minestack.domain
+
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import com.github.unchama.datarepository.KeyedDataRepository
+import com.github.unchama.generic.ListExtra
+import com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobject.{
+  MineStackObject,
+  MineStackObjectList,
+  MineStackObjectWithAmount
+}
+
+class TryIntoMineStack[F[_]: Sync, Player, ItemStack <: Cloneable](
+  implicit mineStackObjectList: MineStackObjectList[F, ItemStack],
+  mineStackObjectRepository: KeyedDataRepository[Player, Ref[F, List[
+    MineStackObjectWithAmount[ItemStack]
+  ]]]
+) {
+
+  import cats.implicits._
+
+  /**
+   * [[Player]]のMineStackリポジトリに[[ItemStack]]を格納することを試みます。
+   * @return 格納ができたかどうか
+   */
+  def apply(player: Player, itemStack: ItemStack, amount: Int): F[Boolean] = for {
+    foundMineStackObject <- mineStackObjectList.findByItemStack(itemStack)
+    _ <- addStackedAmountOf(player, foundMineStackObject.get, amount)
+      .whenA(foundMineStackObject.nonEmpty)
+  } yield foundMineStackObject.nonEmpty
+
+  /**
+   * @return MineStackに指定した[[MineStackObject]]の量を加算する作用
+   */
+  private def addStackedAmountOf(
+    player: Player,
+    mineStackObject: MineStackObject[ItemStack],
+    amount: Int
+  ): F[Unit] = mineStackObjectRepository(player).update { mineStackObjects =>
+    ListExtra.rePrependOrAdd(mineStackObjects)(
+      _.mineStackObject == mineStackObject,
+      {
+        case Some(value) => value.increase(amount)
+        case None        => MineStackObjectWithAmount(mineStackObject, amount)
+      }
+    )
+  }
+
+}
