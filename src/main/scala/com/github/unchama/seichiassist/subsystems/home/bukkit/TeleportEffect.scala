91ac9a444674a84ea9dd42cd0c3f8ad3847a3e68
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-11-02 22:12:43 +0900

    refactor: MinecraftServerThreadに行わせる処理を必要最低限に

--- src/main/scala/com/github/unchama/seichiassist/subsystems/home/bukkit/TeleportEffect.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/home/bukkit/TeleportEffect.scala
@@ -1,22 +1,23 @@
 package com.github.unchama.seichiassist.subsystems.home.bukkit
 
 import cats.data.Kleisli
-import cats.effect.SyncIO
+import cats.effect.{Sync, SyncIO}
 import com.github.unchama.minecraft.actions.OnMinecraftServerThread
+import com.github.unchama.targetedeffect.TargetedEffect
 import org.bukkit.Location
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 object TeleportEffect {
-  def to[F[_]: OnMinecraftServerThread](location: Location): Kleisli[F, CommandSender, Unit] = {
-    Kleisli { player =>
-      OnMinecraftServerThread[F].runAction(SyncIO {
-        player match {
-          case p: Player => p.teleport(location)
-          case _         => throw new RuntimeException("This branch should not be reached.")
-        }
-        ()
-      })
+  def to[F[_]: OnMinecraftServerThread: Sync](
+    location: Location
+  ): Kleisli[F, CommandSender, Unit] = {
+    TargetedEffect.delay {
+      case player: Player =>
+        OnMinecraftServerThread[F].runAction(SyncIO {
+          player.teleport(location)
+        })
+      case _ =>
     }
   }
 }
