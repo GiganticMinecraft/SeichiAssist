cc28a25e73d773ddf9d61175035446ce18066b06
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-11-03 00:19:41 +0900

    fix: テレポートしないバグを修正

--- src/main/scala/com/github/unchama/seichiassist/subsystems/home/bukkit/TeleportEffect.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/home/bukkit/TeleportEffect.scala
@@ -1,23 +1,22 @@
 package com.github.unchama.seichiassist.subsystems.home.bukkit
 
 import cats.data.Kleisli
-import cats.effect.{Sync, SyncIO}
+import cats.effect.SyncIO
 import com.github.unchama.minecraft.actions.OnMinecraftServerThread
-import com.github.unchama.targetedeffect.TargetedEffect
 import org.bukkit.Location
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 object TeleportEffect {
-  def to[F[_]: OnMinecraftServerThread: Sync](
-    location: Location
-  ): Kleisli[F, CommandSender, Unit] = {
-    TargetedEffect.delay {
-      case player: Player =>
-        OnMinecraftServerThread[F].runAction(SyncIO {
-          player.teleport(location)
-        })
-      case _ =>
+  def to[F[_]: OnMinecraftServerThread](location: Location): Kleisli[F, CommandSender, Unit] = {
+    Kleisli { commandSender =>
+      OnMinecraftServerThread[F].runAction(SyncIO {
+        commandSender match {
+          case player: Player => player.teleport(location)
+          case _              =>
+        }
+        ()
+      })
     }
   }
 }
