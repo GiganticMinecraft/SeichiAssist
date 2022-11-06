3342a50ffb6fb94a9b40a39e6e6fba3e6b8c19b6
rito528 <rotoyutoriapp@gmail.com>
2022-11-06 17:08:11 +0900

    style: MINESTACK_GACHADATA_TABLENAMEを消す

--- src/main/scala/com/github/unchama/seichiassist/database/DatabaseConstants.java
+++ src/main/scala/com/github/unchama/seichiassist/database/DatabaseConstants.java
@@ -4,7 +4,6 @@ public class DatabaseConstants {
 
     public static final String PLAYERDATA_TABLENAME = "playerdata";
     public static final String DONATEDATA_TABLENAME = "donatedata";
-    public static final String MINESTACK_GACHADATA_TABLENAME = "msgachadata";
 
     public static final String GRID_TEMPLATE_TABLENAME = "grid_template";
     public static final String SKILL_EFFECT_TABLENAME = "unlocked_active_skill_effect";
