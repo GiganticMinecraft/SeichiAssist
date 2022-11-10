bcdd901e5a458df74a1630cf3f9fd5b253e441f0
rito528 <rotoyutoriapp@gmail.com>
2022-11-10 18:47:07 +0900

    feat: RegionUnitPerClickSettingRepositoryDefinitionを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/application/repository/RegionUnitPerClickSettingRepositoryDefinition.scala
@@ -0,0 +1,22 @@
+package com.github.unchama.seichiassist.subsystems.gridregion.application.repository
+
+import cats.Applicative
+import cats.effect.Sync
+import com.github.unchama.datarepository.template.finalization.RepositoryFinalization
+import com.github.unchama.datarepository.template.initialization.TwoPhasedRepositoryInitialization
+import com.github.unchama.seichiassist.subsystems.gridregion.domain.RegionUnitPerClickSetting
+
+object RegionUnitPerClickSettingRepositoryDefinition {
+
+  def initialization[F[_]: Sync, Player]
+    : TwoPhasedRepositoryInitialization[F, Player, RegionUnitPerClickSetting[F]] =
+    TwoPhasedRepositoryInitialization
+      .withoutPrefetching[F, Player, RegionUnitPerClickSetting[F]] { _ =>
+        Sync[F].pure(new RegionUnitPerClickSetting[F])
+      }
+
+  def finalization[F[_]: Applicative, Player]
+    : RepositoryFinalization[F, Player, RegionUnitPerClickSetting[F]] =
+    RepositoryFinalization.trivial
+
+}
