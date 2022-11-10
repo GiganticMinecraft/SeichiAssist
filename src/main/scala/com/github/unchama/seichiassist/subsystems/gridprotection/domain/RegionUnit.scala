9a218b55692761b3d3e69b9b4970d8697f9c5c18
rito528 <rotoyutoriapp@gmail.com>
2022-11-10 18:17:45 +0900

    feat: add関数を実装する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridprotection/domain/RegionUnit.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridprotection/domain/RegionUnit.scala
@@ -3,8 +3,14 @@ package com.github.unchama.seichiassist.subsystems.gridprotection.domain
 /**
  * 1ユニット <=> 15 * 15の保護領域
  */
-case class RegionUnit(value: Int) {
-  require(value >= 0)
+case class RegionUnit(units: Int) {
+  require(units >= 0)
+
+  /**
+   * 指定[[RegionUnit]]分だけ加算する
+   */
+  def add(_units: RegionUnit): RegionUnit = RegionUnit(units + _units.units)
+
 }
 
 object RegionUnit {
