cf6d3b33dd9694878bfb2ed0fe9ef571620d3b72
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2022-11-11 00:18:17 +0900

    style: scalafmt等

--- src/main/scala/com/github/unchama/seichiassist/menus/stickmenu/SecondPage.scala
+++ src/main/scala/com/github/unchama/seichiassist/menus/stickmenu/SecondPage.scala
@@ -1,6 +1,5 @@
 package com.github.unchama.seichiassist.menus.stickmenu
 
-import cats.effect.implicits._
 import cats.effect.{IO, SyncIO}
 import com.github.unchama.itemstackbuilder.{IconItemStackBuilder, SkullItemStackBuilder}
 import com.github.unchama.menuinventory
