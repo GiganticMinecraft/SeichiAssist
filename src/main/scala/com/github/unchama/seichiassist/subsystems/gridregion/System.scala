1cce0958e78043621c4ebf0672e60449c162194f
rito528 <rotoyutoriapp@gmail.com>
2022-11-10 21:16:36 +0900

    feat: Systemの実装

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/System.scala
@@ -1,6 +1,12 @@
 package com.github.unchama.seichiassist.subsystems.gridregion
 
+import cats.data.Kleisli
+import cats.effect.SyncEffect
+import com.github.unchama.datarepository.bukkit.player.BukkitRepositoryControls
+import com.github.unchama.datarepository.template.RepositoryDefinition
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
+import com.github.unchama.seichiassist.subsystems.gridregion.application.repository.RegionUnitPerClickSettingRepositoryDefinition
+import com.github.unchama.seichiassist.subsystems.gridregion.domain.RegionUnit
 import org.bukkit.entity.Player
 
 trait System[F[_], Player] extends Subsystem[F] {
@@ -11,8 +17,34 @@ trait System[F[_], Player] extends Subsystem[F] {
 
 object System {
 
-  def wired[F[_]]: System[F, Player] = {
+  import cats.implicits._
 
+  def wired[F[_]: SyncEffect]: F[System[F, Player]] = {
+    for {
+      regionUnitPerClickSettingRepositoryControls <- BukkitRepositoryControls.createHandles(
+        RepositoryDefinition
+          .Phased
+          .TwoPhased(
+            RegionUnitPerClickSettingRepositoryDefinition.initialization[F, Player],
+            RegionUnitPerClickSettingRepositoryDefinition.finalization[F, Player]
+          )
+      )
+    } yield {
+      val regionUnitPerClickSettingRepository =
+        regionUnitPerClickSettingRepositoryControls.repository
+
+      new System[F, Player] {
+        override val api: GridRegionAPI[F, Player] = new GridRegionAPI[F, Player] {
+
+          override def toggleUnitPerClick: Kleisli[F, Player, Unit] = Kleisli { player =>
+            regionUnitPerClickSettingRepository(player).toggleUnitPerClick
+          }
+
+          override def unitPerClick(player: Player): F[RegionUnit] =
+            regionUnitPerClickSettingRepository(player).unitPerClick
+        }
+      }
+    }
   }
 
 }
