128c309013b0fed4072aef011796a8ede366c5d6
rito528 <rotoyutoriapp@gmail.com>
2022-11-10 21:46:55 +0900

    feat: toggleUnitPerClickを実装

--- src/main/scala/com/github/unchama/seichiassist/menus/gridregion/GridRegionMenu.scala
+++ src/main/scala/com/github/unchama/seichiassist/menus/gridregion/GridRegionMenu.scala
@@ -1,14 +1,26 @@
 package com.github.unchama.seichiassist.menus.gridregion
 
 import cats.effect.IO
-import com.github.unchama.menuinventory.{Menu, MenuFrame, MenuSlotLayout}
+import com.github.unchama.itemstackbuilder.IconItemStackBuilder
+import com.github.unchama.menuinventory.slot.button.action.LeftClickButtonEffect
+import com.github.unchama.menuinventory.slot.button.{Button, RecomputedButton}
+import com.github.unchama.menuinventory.{LayoutPreparationContext, Menu, MenuFrame, MenuSlotLayout}
+import com.github.unchama.minecraft.actions.OnMinecraftServerThread
+import com.github.unchama.seichiassist.subsystems.gridregion.GridRegionAPI
+import com.github.unchama.targetedeffect.player.FocusedSoundEffect
+import com.github.unchama.targetedeffect.{DeferredEffect, SequentialEffect}
+import org.bukkit.ChatColor._
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryType
-import org.bukkit.ChatColor._
+import org.bukkit.{Material, Sound}
 
 object GridRegionMenu extends Menu {
 
-  class Environment()
+  class Environment(
+    implicit val onMainThread: OnMinecraftServerThread[IO],
+    implicit val layoutPreparationContext: LayoutPreparationContext,
+    val gridRegionAPI: GridRegionAPI[IO, Player]
+  )
 
   override val frame: MenuFrame =
     MenuFrame(Right(InventoryType.DISPENSER), s"${LIGHT_PURPLE}グリッド式保護設定メニュー")
@@ -17,9 +29,34 @@ object GridRegionMenu extends Menu {
     implicit environment: Environment
   ): IO[MenuSlotLayout] = ???
 
-  case class computeButtons() {
+  case class computeButtons(player: Player)(implicit environment: Environment) {
+    import environment._
 
-    val toggleUnitPerClick
+    def toggleUnitPerClick(): IO[Button] = RecomputedButton {
+      for {
+        currentRegionUnit <- gridRegionAPI.unitPerClick(player)
+      } yield {
+        val iconItemStack = new IconItemStackBuilder(Material.STAINED_GLASS_PANE, 1)
+          .title(s"${GREEN}拡張単位の変更")
+          .lore(
+            List(
+              s"${GREEN}現在のユニット指定量",
+              s"$AQUA${currentRegionUnit.units}${GREEN}ユニット($AQUA${currentRegionUnit.computeBlockAmount}${GREEN}ブロック)/1クリック",
+              s"$RED${UNDERLINE}クリックで変更"
+            )
+          )
+          .build()
+
+        val leftClickEffect = LeftClickButtonEffect {
+          SequentialEffect(
+            DeferredEffect(IO(gridRegionAPI.toggleUnitPerClick)),
+            FocusedSoundEffect(Sound.BLOCK_STONE_BUTTON_CLICK_ON, 1.0f, 1.0f)
+          )
+        }
+
+        Button(iconItemStack, leftClickEffect)
+      }
+    }
 
   }
 
--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionUnit.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionUnit.scala
@@ -1,7 +1,7 @@
 package com.github.unchama.seichiassist.subsystems.gridregion.domain
 
 /**
- * 1ユニット <=> 15 * 15の保護領域
+ * 1ユニット <=> 15 * 15ブロックの保護領域
  */
 case class RegionUnit(units: Int) {
   require(units >= 0)
@@ -11,12 +11,22 @@ case class RegionUnit(units: Int) {
    */
   def add(_units: RegionUnit): RegionUnit = RegionUnit(units + _units.units)
 
+  /**
+   * 1[[RegionUnit]]あたりのブロック数
+   */
+  private val blockPerBlockAmount = 15
+
+  /**
+   * [[RegionUnit]]からブロック数を計算する
+   */
+  def computeBlockAmount: Int = units * blockPerBlockAmount
+
 }
 
 object RegionUnit {
 
   /**
-   * Unitの初期値
+   * [[RegionUnit]]の初期値
    */
   val initial: RegionUnit = RegionUnit(0)
 
