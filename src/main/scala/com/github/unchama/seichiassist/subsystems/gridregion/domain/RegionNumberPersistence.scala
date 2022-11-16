8d7ef1766cb4bdd3764e2967ff94943ce7defe4b
rito528 <rotoyutoriapp@gmail.com>
2022-11-16 20:09:03 +0900

    feat: RegionNumberPersistenceを定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionNumberPersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionNumberPersistence.scala
@@ -1,7 +1,15 @@
 package com.github.unchama.seichiassist.subsystems.gridregion.domain
 
-trait RegionNumberPersistence[F[_]] {
+trait RegionNumberPersistence[F[_], Player] {
 
-  def
+  /**
+   * @return `player`の[[RegionNumber]]を`regionNumber`で更新する作用
+   */
+  def setRegionNumber(player: Player, regionNumber: RegionNumber): F[Unit]
+
+  /**
+   * @return `player`の[[RegionNumber]]を取得する作用
+   */
+  def fetchRegionNumber(player: Player): F[Unit]
 
 }
