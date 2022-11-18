3c1d3764b48661149cd2e352fa6ccc94f9af5989
rito528 <rotoyutoriapp@gmail.com>
2022-11-18 23:28:45 +0900

    style: RegionTemplatePersistenceのPlayer依存を削除

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionTemplatePersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionTemplatePersistence.scala
@@ -1,6 +1,5 @@
 package com.github.unchama.seichiassist.subsystems.gridregion.domain
 
 import com.github.unchama.generic.RefDict
-import org.bukkit.entity.Player
 
-trait RegionTemplatePersistence[F[_]] extends RefDict[F, Player, Vector[RegionUnits]]
+trait RegionTemplatePersistence[F[_], Player] extends RefDict[F, Player, Vector[RegionUnits]]
