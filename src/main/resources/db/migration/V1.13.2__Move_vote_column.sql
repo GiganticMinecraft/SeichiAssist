c3157ee169f28ccc10e0d86360d36d61064ba729
rito528 <rotoyutoriapp@gmail.com>
2022-12-06 15:50:59 +0900

    style: tab -> space

--- src/main/resources/db/migration/V1.13.2__Move_vote_column.sql
+++ src/main/resources/db/migration/V1.13.2__Move_vote_column.sql
@@ -1,36 +1,36 @@
 USE seichiassist;
 
 CREATE TABLE vote(
-	uuid CHAR(36) PRIMARY KEY,
-	vote_number INT,
-	chain_vote_number INT,
-	effect_point INT,
-	given_effect_point INT,
-	last_vote DATETIME
+ uuid CHAR(36) PRIMARY KEY,
+ vote_number INT,
+ chain_vote_number INT,
+ effect_point INT,
+ given_effect_point INT,
+ last_vote DATETIME
 );
 
 INSERT INTO
-	vote(
-		uuid,
-		vote_number,
-		chain_vote_number,
-		effect_point,
-		given_effect_point,
-		last_vote
-	)
+ vote(
+  uuid,
+  vote_number,
+  chain_vote_number,
+  effect_point,
+  given_effect_point,
+  last_vote
+ )
 SELECT
-	uuid,
-	p_vote,
-	chainvote,
-	effectpoint,
-	p_givenvote,
-	CONVERT(lastvote, DATE)
+ uuid,
+ p_vote,
+ chainvote,
+ effectpoint,
+ p_givenvote,
+ CONVERT(lastvote, DATE)
 FROM
-	playerdata;
+ playerdata;
 
 ALTER TABLE
-	playerdata DROP p_vote,
-	DROP chainvote,
-	DROP effectpoint,
-	DROP p_givenvote,
-	DROP lastvote;
+ playerdata DROP p_vote,
+ DROP chainvote,
+ DROP effectpoint,
+ DROP p_givenvote,
+ DROP lastvote;
--- src/main/resources/db/migration/V1.13.3__Move_vote_fairy_column.sql
+++ src/main/resources/db/migration/V1.13.3__Move_vote_fairy_column.sql
@@ -1,44 +1,44 @@
 USE seichiassist;
 
 CREATE TABLE vote_fairy(
-	uuid CHAR(36) PRIMARY KEY,
-	apple_open_state INT DEFAULT 1,
-	fairy_summon_cost INT DEFAULT 1,
-	is_fairy_using BOOLEAN DEFAULT false,
-	fairy_recovery_mana_value INT DEFAULT 0,
-	fairy_end_time DATETIME DEFAULT NULL,
-	given_apple_amount BIGINT DEFAULT 0,
-	is_play_fairy_speech_sound BOOLEAN DEFAULT true
+ uuid CHAR(36) PRIMARY KEY,
+ apple_open_state INT DEFAULT 1,
+ fairy_summon_cost INT DEFAULT 1,
+ is_fairy_using BOOLEAN DEFAULT false,
+ fairy_recovery_mana_value INT DEFAULT 0,
+ fairy_end_time DATETIME DEFAULT NULL,
+ given_apple_amount BIGINT DEFAULT 0,
+ is_play_fairy_speech_sound BOOLEAN DEFAULT true
 );
 
 INSERT INTO
-	vote_fairy(
-		uuid,
-		apple_open_state,
-		fairy_summon_cost,
-		is_fairy_using,
-		fairy_recovery_mana_value,
-		fairy_end_time,
-		given_apple_amount,
-		is_play_fairy_speech_sound
-	)
+ vote_fairy(
+  uuid,
+  apple_open_state,
+  fairy_summon_cost,
+  is_fairy_using,
+  fairy_recovery_mana_value,
+  fairy_end_time,
+  given_apple_amount,
+  is_play_fairy_speech_sound
+ )
 SELECT
-	uuid,
-	toggleGiveApple,
-	toggleVotingFairy,
-	canVotingFairyUse,
-	VotingFairyRecoveryValue,
-	newVotingFairyTime,
-	p_apple,
-	is_fairy_speech_play_sound
+ uuid,
+ toggleGiveApple,
+ toggleVotingFairy,
+ canVotingFairyUse,
+ VotingFairyRecoveryValue,
+ newVotingFairyTime,
+ p_apple,
+ is_fairy_speech_play_sound
 FROM
-	playerdata;
+ playerdata;
 
 ALTER TABLE
-	playerdata DROP toggleGiveApple,
-	DROP toggleVotingFairy,
-	DROP canVotingFairyUse,
-	DROP VotingFairyRecoveryValue,
-	DROP newVotingFairyTime,
-	DROP p_apple,
-	DROP is_fairy_speech_play_sound
+ playerdata DROP toggleGiveApple,
+ DROP toggleVotingFairy,
+ DROP canVotingFairyUse,
+ DROP VotingFairyRecoveryValue,
+ DROP newVotingFairyTime,
+ DROP p_apple,
+ DROP is_fairy_speech_play_sound
