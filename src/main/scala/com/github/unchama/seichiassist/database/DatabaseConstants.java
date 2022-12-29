d8a677c6d3591d9d04729c6f72218dd6f1b7a0af
rito528 <rotoyutoriapp@gmail.com>
2022-12-29 19:10:18 +0900

    refactor: DatabaseConstantsで使っていないものを削除する

--- src/main/scala/com/github/unchama/seichiassist/database/DatabaseConstants.java
+++ src/main/scala/com/github/unchama/seichiassist/database/DatabaseConstants.java
@@ -3,8 +3,6 @@ package com.github.unchama.seichiassist.database;
 public class DatabaseConstants {
 
     public static final String PLAYERDATA_TABLENAME = "playerdata";
-    public static final String GACHADATA_TABLENAME = "gachadata";
-    public static final String DONATEDATA_TABLENAME = "donatedata";
     public static final String MINESTACK_GACHADATA_TABLENAME = "msgachadata";
 
     public static final String MINESTACK_TABLENAME = "mine_stack";
