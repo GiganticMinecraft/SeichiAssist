c05822d664196febbc3c5f65cf13a195cf07db06
rito528 <rotoyutoriapp@gmail.com>
2022-12-30 22:57:42 +0900

    feat: デバッグ環境でitzgさんのDockerイメージを使う

--- prepare-docker.bat
+++ prepare-docker.bat
@@ -2,9 +2,6 @@ call rd /s /q target\build
 
 call sbt assembly || goto :onerror
 
-REM https://stackoverflow.com/a/23812179
-call echo n | copy /-y docker\spigot\eula.txt docker\spigot\serverfiles\eula.txt
-
 call docker-compose build -m 2g || goto :onerror
 
 call docker-compose down
