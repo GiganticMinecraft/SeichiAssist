b5ac24a6fc4168a1b660fb51862eee1ee17521a4
rito528 <rotoyutoriapp@gmail.com>
2022-12-30 18:11:57 +0900

    style: apply scalafmt

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/application/repository/RegionCountRepositoryDefinition.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/application/repository/RegionCountRepositoryDefinition.scala
@@ -5,7 +5,10 @@ import cats.effect.concurrent.Ref
 import com.github.unchama.datarepository.definitions.RefDictBackedRepositoryDefinition
 import com.github.unchama.datarepository.template.RepositoryDefinition
 import com.github.unchama.minecraft.algebra.HasUuid
-import com.github.unchama.seichiassist.subsystems.gridregion.domain.{RegionCount, RegionCountPersistence}
+import com.github.unchama.seichiassist.subsystems.gridregion.domain.{
+  RegionCount,
+  RegionCountPersistence
+}
 
 object RegionCountRepositoryDefinition {
 
--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionTemplatePersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/domain/RegionTemplatePersistence.scala
@@ -4,4 +4,5 @@ import com.github.unchama.generic.RefDict
 
 import java.util.UUID
 
-trait RegionTemplatePersistence[F[_]] extends RefDict[F, UUID, Map[RegionTemplateId, RegionUnits]]
+trait RegionTemplatePersistence[F[_]]
+    extends RefDict[F, UUID, Map[RegionTemplateId, RegionUnits]]
