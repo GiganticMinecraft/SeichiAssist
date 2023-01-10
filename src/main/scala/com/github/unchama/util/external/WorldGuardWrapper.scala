28ef893fb8a96c9cd118d5a528f46ce16c225e05
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-01-10 23:47:16 +0900

    refactor: WorldGuardWrapper.javaをScala化

---
+++ src/main/scala/com/github/unchama/util/external/WorldGuardWrapper.scala
@@ -0,0 +1,85 @@
+package com.github.unchama.util.external;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.managers.RegionManager
+import com.sk89q.worldguard.protection.regions.ProtectedRegion
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import scala.jdk.CollectionConverters._
+
+/**
+ * WorldGuardの各種関数を集めたクラスです.
+ *
+ * @author karayuu
+ */
+object WorldGuardWrapper {
+
+  /**
+   * WorldGuardのインスタンス
+   */
+  private val plugin = ExternalPlugins.getWorldGuard;
+
+  /**
+   * [[RegionManager]]を返す
+   *
+   * The expression is nullable: WorldConfiguration#useRegions is false => null
+   */
+  def getRegionManager(world: World): Option[RegionManager] = Option(
+    plugin.getRegionManager(world)
+  )
+
+  /**
+   * 与えられた [[World]] の [[Player]] の最大保護可能数を取得します.
+   *
+   * @param player 最大保護可能数を取得したい [[Player]] (`null`は許容されない)
+   * @param world  最大保護可能数を取得したい [[World]] (`null`は許容されない)
+   * @return [[Player]] の [[World]] における最大保護可能数
+   */
+  def getMaxRegionCount(player: Player, world: World): Int =
+    // TODO: migrate this to OptionalInt
+    plugin.getGlobalStateManager.get(world).getMaxRegionCount(player)
+
+  /**
+   * 現在[[Player]]が[[World]]でオーナーになっている保護の数を返す。
+   * @param who 誰か
+   * @param where どのワールドか
+   * @return オーナーになっている保護の数。どこのオーナーでもない場合は0
+   */
+  def getNumberOfRegions(who: Player, where: World): Int =
+    // TODO: migrate this to OptionalInt
+    WorldGuardPlugin
+      .inst()
+      .getRegionContainer
+      .get(where)
+      .getRegionCountOfPlayer(WorldGuardPlugin.inst().wrapPlayer(who))
+
+  /**
+   * 現在[[Player]]が[[Location]]の座標でOwnerになっている保護があるかどうかを返す。
+   * @param player 調べる対象であるPlayer
+   * @param location どの座標か
+   * @return Ownerである保護が1つだけあればtrue、ないか保護が2個以上重なっていて判定できなければfalse
+   */
+  def isRegionOwner(player: Player, location: Location): Boolean =
+    getOneRegion(location).exists(rg => rg.isOwner(plugin.wrapPlayer(player)))
+
+  /**
+   * 現在[[Player]]が[[Location]]の座標でMemberになっている保護があるかどうかを返す。
+   * ※Ownerでもある場合も含まれる。
+   * @param player 調べる対象であるPlayer
+   * @param location どの座標か
+   * @return Memberである保護が1つだけあればtrue、ないか保護が2個以上重なっていて判定できなければfalse
+   */
+  def isRegionMember(player: Player, location: Location): Boolean =
+    getOneRegion(location).exists(rg => rg.isMember(plugin.wrapPlayer(player)))
+
+  def getOneRegion(location: Location): Option[ProtectedRegion] = getRegions(
+    location
+  ).headOption
+
+  def getRegions(location: Location): Set[ProtectedRegion] =
+    getRegionManager(location.getWorld)
+      .map(manager => manager.getApplicableRegions(location).getRegions.asScala.toSet)
+      .getOrElse(Set.empty)
+}
