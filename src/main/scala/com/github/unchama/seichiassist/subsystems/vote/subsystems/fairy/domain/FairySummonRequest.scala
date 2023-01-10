7455a527dc25ca01f6863aa7b126da71b6ea3224
rito528 <rotoyutoriapp@gmail.com>
2023-01-10 19:59:20 +0900

    docs: summonRequestのドキュメントを適切なものにする

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairySummonRequest.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/domain/FairySummonRequest.scala
@@ -3,9 +3,8 @@ package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain
 trait FairySummonRequest[F[_], Player] {
 
   /**
-   * 妖精の召喚をリクエストする
-   * 召喚に失敗した場合は[[FairySpawnRequestError]]を返す
-   * 成功した場合は召喚する作用を返す
+   * @return 妖精の召喚をリクエストする作用
+   *         失敗した場合は失敗理由
    */
   def summonRequest(player: Player): F[FairySpawnRequestErrorOrSpawn[F]]
 
