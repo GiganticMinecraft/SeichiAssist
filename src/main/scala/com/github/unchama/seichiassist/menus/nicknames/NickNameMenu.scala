267f7d18aff5c02d2d7ba7d875b40ffef6f74c8a
rito528 <rotoyutoriapp@gmail.com>
2023-02-14 23:57:51 +0900

    feat: achievementPointsInformationをクリックで更新されるボタンに

--- src/main/scala/com/github/unchama/seichiassist/menus/nicknames/NickNameMenu.scala
+++ src/main/scala/com/github/unchama/seichiassist/menus/nicknames/NickNameMenu.scala
@@ -3,10 +3,16 @@ package com.github.unchama.seichiassist.menus.nicknames
 import cats.effect.IO
 import com.github.unchama.itemstackbuilder.{IconItemStackBuilder, SkullItemStackBuilder}
 import com.github.unchama.menuinventory.router.CanOpen
-import com.github.unchama.menuinventory.slot.button.Button
+import com.github.unchama.menuinventory.slot.button.{Button, RecomputedButton}
 import com.github.unchama.menuinventory.slot.button.action.LeftClickButtonEffect
 import com.github.unchama.menuinventory.syntax.IntInventorySizeOps
-import com.github.unchama.menuinventory.{ChestSlotRef, Menu, MenuFrame, MenuSlotLayout}
+import com.github.unchama.menuinventory.{
+  ChestSlotRef,
+  LayoutPreparationContext,
+  Menu,
+  MenuFrame,
+  MenuSlotLayout
+}
 import com.github.unchama.seichiassist.{SeichiAssist, SkullOwners}
 import com.github.unchama.seichiassist.achievement.Nicknames
 import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.onMainThread
@@ -22,7 +28,10 @@ import org.bukkit.ChatColor._
 
 object NickNameMenu extends Menu {
 
-  class Environment(implicit val ioCanOpenAchievementMenu: IO CanOpen AchievementMenu.type)
+  class Environment(
+    implicit val ioCanOpenAchievementMenu: IO CanOpen AchievementMenu.type,
+    implicit val layoutPreparationContext: LayoutPreparationContext
+  )
 
   override val frame: MenuFrame = MenuFrame(4.chestRows, s"$DARK_PURPLE${BOLD}二つ名組み合わせシステム")
 
@@ -42,7 +51,10 @@ object NickNameMenu extends Menu {
     val buttons = NickNameMenuButtons(player)
     import buttons._
 
-    IO(
+    for {
+      achievementPointsInformation <- achievementPointsInformation
+      pointConvertButton <- pointConvertButton
+    } yield {
       MenuSlotLayout(
         ChestSlotRef(0, 0) -> achievementPointsInformation,
         ChestSlotRef(0, 1) -> pointConvertButton,
@@ -53,61 +65,67 @@ object NickNameMenu extends Menu {
         ChestSlotRef(1, 6) -> tailPartsSelect,
         ChestSlotRef(3, 0) -> backToAchievementMenu
       )
-    )
+    }
   }
 
-  private case class NickNameMenuButtons(player: Player) {
+  private case class NickNameMenuButtons(player: Player)(implicit environment: Environment) {
+
+    import environment._
 
     private val playerData = SeichiAssist.playermap.apply(player.getUniqueId)
 
-    val achievementPointsInformation: Button = {
-      val itemStack = new IconItemStackBuilder(Material.EMERALD_ORE)
-        .title(s"$YELLOW$UNDERLINE${BOLD}実績ポイント情報")
-        .lore(
-          List(
-            s"${GREEN}クリックで情報を最新化",
-            s"${RED}累計獲得量：${playerData.achievePoint.cumulativeTotal}",
-            s"${RED}累計消費量：${playerData.achievePoint.used}",
-            s"${RED}使用可能量：${playerData.achievePoint.left}"
+    val achievementPointsInformation: IO[Button] = RecomputedButton {
+      IO {
+        val itemStack = new IconItemStackBuilder(Material.EMERALD_ORE)
+          .title(s"$YELLOW$UNDERLINE${BOLD}実績ポイント情報")
+          .lore(
+            List(
+              s"${GREEN}クリックで情報を最新化",
+              s"${RED}累計獲得量：${playerData.achievePoint.cumulativeTotal}",
+              s"${RED}累計消費量：${playerData.achievePoint.used}",
+              s"${RED}使用可能量：${playerData.achievePoint.left}"
+            )
           )
-        )
-        .build()
+          .build()
 
-      Button(
-        itemStack,
-        LeftClickButtonEffect {
-          FocusedSoundEffect(Sound.BLOCK_STONE_BUTTON_CLICK_ON, 1.0f, 1.0f)
-        }
-      )
+        Button(
+          itemStack,
+          LeftClickButtonEffect {
+            FocusedSoundEffect(Sound.BLOCK_STONE_BUTTON_CLICK_ON, 1.0f, 1.0f)
+          }
+        )
+      }
     }
 
-    val pointConvertButton: Button = {
-      val itemStack = new IconItemStackBuilder(Material.EMERALD)
-        .title(s"$YELLOW$UNDERLINE${BOLD}ポイント変換ボタン")
-        .lore(
-          List(
-            s"${RED}JMS投票で手に入るポイントを",
-            s"${RED}実績ポイントに変換できます。",
-            s"$YELLOW${BOLD}投票pt 10pt → 実績pt 3pt",
-            s"${AQUA}クリックで変換を一回行います。",
-            s"${GREEN}所有投票pt：${playerData.effectPoint}",
-            s"${GREEN}所有実績pt；${playerData.achievePoint.left}"
+    val pointConvertButton: IO[Button] = RecomputedButton {
+      IO {
+        val itemStack = new IconItemStackBuilder(Material.EMERALD)
+          .title(s"$YELLOW$UNDERLINE${BOLD}ポイント変換ボタン")
+          .lore(
+            List(
+              s"${RED}JMS投票で手に入るポイントを",
+              s"${RED}実績ポイントに変換できます。",
+              s"$YELLOW${BOLD}投票pt 10pt → 実績pt 3pt",
+              s"${AQUA}クリックで変換を一回行います。",
+              s"${GREEN}所有投票pt：${playerData.effectPoint}",
+              s"${GREEN}所有実績pt；${playerData.achievePoint.left}"
+            )
           )
-        )
-        .build()
+          .build()
 
-      Button(
-        itemStack,
-        LeftClickButtonEffect {
-          UnfocusedEffect {
-            if (playerData.effectPoint >= 10) {
-              playerData.convertEffectPointToAchievePoint()
-            } else {
-              player.sendMessage("エフェクトポイントが不足しています。")
+        Button(
+          itemStack,
+          LeftClickButtonEffect {
+            UnfocusedEffect {
+              if (playerData.effectPoint >= 10) {
+                playerData.convertEffectPointToAchievePoint()
+              } else {
+                player.sendMessage("エフェクトポイントが不足しています。")
+              }
             }
           }
-        }
-      )
+        )
+      }
     }
 
     val currentNickName: Button = {
