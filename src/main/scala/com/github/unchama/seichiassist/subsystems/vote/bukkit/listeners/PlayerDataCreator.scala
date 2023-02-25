e796b7b308c2f15377a8dfb7d53d3cf900684cea
rito528 <rotoyutoriapp@gmail.com>
2023-02-26 00:18:06 +0900

    fix: 初参加プレイヤーの投票データが作成されない不具合を修正

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/bukkit/listeners/PlayerDataCreator.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/bukkit/listeners/PlayerDataCreator.scala
@@ -3,15 +3,15 @@ package com.github.unchama.seichiassist.subsystems.vote.bukkit.listeners
 import cats.effect.ConcurrentEffect
 import cats.effect.ConcurrentEffect.ops.toAllConcurrentEffectOps
 import com.github.unchama.seichiassist.subsystems.vote.domain.VotePersistence
-import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.event.{EventHandler, Listener}
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent
+import org.bukkit.event.{EventHandler, EventPriority, Listener}
 
 class PlayerDataCreator[F[_]: ConcurrentEffect](implicit votePersistence: VotePersistence[F])
     extends Listener {
 
-  @EventHandler
-  def onJoin(e: PlayerJoinEvent): Unit = {
-    votePersistence.createPlayerData(e.getPlayer.getUniqueId).toIO.unsafeRunSync()
+  @EventHandler(priority = EventPriority.HIGHEST)
+  def onPlayerPreLoginEvent(e: AsyncPlayerPreLoginEvent): Unit = {
+    votePersistence.createPlayerData(e.getUniqueId).toIO.unsafeRunAsyncAndForget()
   }
 
 }
