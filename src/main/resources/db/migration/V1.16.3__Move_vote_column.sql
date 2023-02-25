33b2f7590c3f55f58a97abb5d5a663a40d1b0e14
rito528 <rotoyutoriapp@gmail.com>
2023-02-25 20:28:03 +0900

    chore: playerdataテーブルからuuidカラムが重複しているデータを取り除くマイグレーションを追加

--- src/main/resources/db/migration/V1.16.3__Move_vote_column.sql
+++ src/main/resources/db/migration/V1.16.3__Move_vote_column.sql
@@ -1,7 +1,7 @@
 USE seichiassist;
 
 CREATE TABLE IF NOT EXISTS vote(
- uuid CHAR(36) NOT NULL,
+ uuid CHAR(36) NOT NULL PRIMARY KEY,
  vote_number INT NOT NULL,
  chain_vote_number INT NOT NULL,
  effect_point INT NOT NULL,
