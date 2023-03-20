3343e8732f42eda8a259ee79b79beb4d39c432f6
lis2a <sk92en@gmail.com>
2023-03-21 01:37:04 +0900

    feat: add other metarial options

--- src/main/scala/com/github/unchama/seichiassist/subsystems/mebius/domain/property/MebiusForcedMaterial.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/mebius/domain/property/MebiusForcedMaterial.scala
@@ -16,13 +16,36 @@ sealed trait MebiusForcedMaterial {
 object MebiusForcedMaterial {
   import cats.implicits._
 
+  // ダイヤモンド
   case object None extends MebiusForcedMaterial {
     override def allowedAt(level: MebiusLevel): Boolean = true
     override def next: MebiusForcedMaterial = Leather
   }
 
+  // 革
   case object Leather extends MebiusForcedMaterial {
     override def allowedAt(level: MebiusLevel): Boolean = level >= MebiusLevel(30)
     override def next: MebiusForcedMaterial = None
+  }
+
+  // 金
+  case object Gold extends MebiusForcedMaterial {
+    override def allowedAt(level: MebiusLevel): Boolean = level >= MebiusLevel(30)
+
+    override def next: MebiusForcedMaterial = Gold
+  }
+
+  // 鉄
+  case object Iron extends MebiusForcedMaterial {
+    override def allowedAt(level: MebiusLevel): Boolean = level >= MebiusLevel(30)
+
+    override def next: MebiusForcedMaterial = Iron
+  }
+
+  // チェーン
+  case object Chain extends MebiusForcedMaterial {
+    override def allowedAt(level: MebiusLevel): Boolean = level >= MebiusLevel(30)
+
+    override def next: MebiusForcedMaterial = Chain
   }
 }
