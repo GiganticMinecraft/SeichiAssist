0cd74162ec2e3c8552974f6e5e0f7351ccbe86da
rito528 <rotoyutoriapp@gmail.com>
2023-04-16 16:13:00 +0900

    feat: 音を再生するかどうかを永続化データから取得できるようにする

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairyspeech/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairyspeech/System.scala
@@ -4,7 +4,11 @@ import cats.effect.Sync
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.property.FairyMessage
 import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.bukkit.BukkitFairySpeechGateway
-import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.domain.FairySpeechGateway
+import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.domain.{
+  FairySpeechGateway,
+  FairySpeechPersistence
+}
+import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.infrastructure.JdbcFairySpeechPersistence
 import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.service.FairySpeechService
 import org.bukkit.entity.Player
 
@@ -16,19 +20,22 @@ trait System[F[_], Player] extends Subsystem[F] {
 
 object System {
 
+  import cats.implicits._
+
   def wired[F[_]: Sync](): System[F, Player] = {
     val speechGateway: Player => FairySpeechGateway[F] = player =>
       new BukkitFairySpeechGateway[F](player)
     val speechService: Player => FairySpeechService[F] = player =>
       new FairySpeechService[F](speechGateway(player))
+    val persistence: FairySpeechPersistence[F] = new JdbcFairySpeechPersistence[F]
 
     new System[F, Player] {
       override val api: FairySpeechAPI[F, Player] = new FairySpeechAPI[F, Player] {
 
-        override def speech(player: Player, messages: Seq[FairyMessage]): F[Unit] = {
-          // todo: fairyPlaySoundをインフラ層から取得するようにする
-          speechService(player).makeSpeech(messages, true)
-        }
+        override def speech(player: Player, messages: Seq[FairyMessage]): F[Unit] = for {
+          fairyPlaySound <- persistence.playSoundOnFairySpeech(player.getUniqueId)
+          _ <- speechService(player).makeSpeech(messages, fairyPlaySound)
+        } yield ()
 
       }
     }
