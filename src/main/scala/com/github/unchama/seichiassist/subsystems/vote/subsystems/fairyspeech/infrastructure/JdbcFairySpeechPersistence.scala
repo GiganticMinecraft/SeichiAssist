51d4e0ae321205acbaf08da0a0a21ed3aab4d808
rito528 <rotoyutoriapp@gmail.com>
2023-04-16 16:08:56 +0900

    refactor: FairySpeechPersistenceを作成してSpeechに関する永続化処理を記載する

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairyspeech/infrastructure/JdbcFairySpeechPersistence.scala
@@ -0,0 +1,30 @@
+package com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.infrastructure
+
+import cats.effect.Sync
+import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairyspeech.domain.FairySpeechPersistence
+import scalikejdbc.{DB, scalikejdbcSQLInterpolationImplicitDef}
+
+import java.util.UUID
+
+class JdbcFairySpeechPersistence[F[_]: Sync] extends FairySpeechPersistence[F] {
+
+  override def setPlaySoundOnSpeech(player: UUID, playOnSpeech: Boolean): F[Unit] =
+    Sync[F].delay {
+      DB.localTx { implicit session =>
+        sql"UPDATE vote_fairy SET is_play_fairy_speech_sound = $playOnSpeech WHERE uuid = ${player.toString}"
+          .execute()
+          .apply()
+      }
+    }
+
+  override def playSoundOnFairySpeech(player: UUID): F[Boolean] = Sync[F].delay {
+    DB.readOnly { implicit session =>
+      sql"SELECT is_play_fairy_speech_sound FROM vote_fairy WHERE uuid=${player.toString}"
+        .map(_.boolean("is_play_fairy_speech_sound"))
+        .single()
+        .apply()
+        .get
+    }
+  }
+
+}
