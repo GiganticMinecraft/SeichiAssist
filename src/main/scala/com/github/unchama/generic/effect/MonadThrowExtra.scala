2775b4363a32aea0e08a39b62e157f79aa93b6f4
rito528 <rotoyutoriapp@gmail.com>
2023-04-27 15:00:23 +0900

    feat: LimitReachedに最後に発生した例外を含むように

--- src/main/scala/com/github/unchama/generic/effect/MonadThrowExtra.scala
+++ src/main/scala/com/github/unchama/generic/effect/MonadThrowExtra.scala
@@ -8,24 +8,25 @@ object MonadThrowExtra {
   import cats.implicits._
 
   def retryUntilSucceeds[F[_]: MonadThrow, A](fa: F[A])(limit: Int): F[A] = {
-    def go(currentIterationCount: Int): F[A] = {
+    def go(currentIterationCount: Int, lastException: Option[Throwable]): F[A] = {
       if (currentIterationCount <= limit) {
         fa.attempt.flatMap {
           case Right(a) => a.pure[F]
           case Left(error) =>
-            go(currentIterationCount + 1).map { a =>
+            go(currentIterationCount + 1, Some(error)).map { a =>
               error.printStackTrace()
               a
             }
         }
       } else {
-        case object LimitReached extends Exception(s"Limit $limit reached!")
+        // このelse節に入っている時点で1度は失敗しているので、`lastException`が`None`であることはありえない。
+        case object LimitReached extends Exception(s"Limit $limit reached!", lastException.get)
 
         MonadError[F, Throwable].raiseError(LimitReached)
       }
     }
 
-    go(0)
+    go(0, None)
   }
 
 }
