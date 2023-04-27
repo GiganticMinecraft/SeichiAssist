54e7988de41ad2d77520463715405bf49d72c0c4
rito528 <rotoyutoriapp@gmail.com>
2023-04-27 14:53:49 +0900

    style: retryUntilSucceedsに対してnowを指定できないように

--- src/main/scala/com/github/unchama/generic/effect/MonadThrowExtra.scala
+++ src/main/scala/com/github/unchama/generic/effect/MonadThrowExtra.scala
@@ -7,21 +7,25 @@ object MonadThrowExtra {
 
   import cats.implicits._
 
-  def retryUntilSucceeds[F[_]: MonadThrow, A](fa: F[A])(limit: Int, now: Int = 0): F[A] = {
-    if (now <= limit) {
-      fa.attempt.flatMap {
-        case Right(a) => a.pure[F]
-        case Left(error) =>
-          retryUntilSucceeds(fa)(limit, now + 1).map { a =>
-            error.printStackTrace()
-            a
-          }
-      }
-    } else {
-      case object LimitReached extends Exception(s"Limit $limit reached!")
+  def retryUntilSucceeds[F[_]: MonadThrow, A](fa: F[A])(limit: Int): F[A] = {
+    def go(currentIterationCount: Int): F[A] = {
+      if (currentIterationCount <= limit) {
+        fa.attempt.flatMap {
+          case Right(a) => a.pure[F]
+          case Left(error) =>
+            go(currentIterationCount + 1).map { a =>
+              error.printStackTrace()
+              a
+            }
+        }
+      } else {
+        case object LimitReached extends Exception(s"Limit $limit reached!")
 
-      MonadError[F, Throwable].raiseError(LimitReached)
+        MonadError[F, Throwable].raiseError(LimitReached)
+      }
     }
+
+    go(0)
   }
 
 }
