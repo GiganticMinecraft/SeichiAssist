e7560b613bcc15365b8b07ec70f7e23da1d3c22f
rito528 <rotoyutoriapp@gmail.com>
2023-05-07 15:57:59 +0900

    fix: occurredExceptionsの型シグネチャをOptionにする

--- src/main/scala/com/github/unchama/generic/effect/MonadThrowExtra.scala
+++ src/main/scala/com/github/unchama/generic/effect/MonadThrowExtra.scala
@@ -9,24 +9,24 @@ object MonadThrowExtra {
 
   def retryUntilSucceeds[F[_]: Sync, A](fa: F[A])(limit: Int): F[A] = {
     require(limit >= 1)
-    def go(currentIterationCount: Int, occurredExceptions: List[Throwable]): F[A] = {
+    def go(currentIterationCount: Int, occurredException: Option[Throwable]): F[A] = {
       if (currentIterationCount <= limit) {
         fa.attempt.flatMap {
           case Right(a) =>
-            Sync[F].delay(occurredExceptions.foreach(_.printStackTrace())).as(a)
+            Sync[F].delay(occurredException.foreach(_.printStackTrace())).as(a)
           case Left(error) =>
-            go(currentIterationCount + 1, occurredExceptions :+ error)
+            go(currentIterationCount + 1, Some(error))
         }
       } else {
-        // このelse節に入っている時点で1度は失敗しているので、`occurredExceptions`が`empty`であることはありえない。
+        // このelse節に入っている時点で1度は失敗しているので、`occurredExceptions`が`None`であることはありえない。
         case object LimitReached
-            extends Exception(s"Limit $limit reached!", occurredExceptions.last)
+            extends Exception(s"Limit $limit reached!", occurredException.last)
 
         MonadError[F, Throwable].raiseError(LimitReached)
       }
     }
 
-    go(0, Nil)
+    go(0, None)
   }
 
 }
