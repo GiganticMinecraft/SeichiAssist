44e1873e33c581ffe667cb372b410d785bfbcfca
rito528 <rotoyutoriapp@gmail.com>
2023-05-13 16:42:38 +0900

    chore: serverIdがlocal-から始まったときにSentryのセットアップしないように

--- src/main/scala/com/github/unchama/seichiassist/SeichiAssist.scala
+++ src/main/scala/com/github/unchama/seichiassist/SeichiAssist.scala
@@ -596,17 +596,19 @@ class SeichiAssist extends JavaPlugin() {
     // コンフィグ系の設定は全てConfig.javaに移動
     SeichiAssist.seichiAssistConfig = Config.loadFrom(this)
 
-    Sentry.init { options =>
-      options.setDsn("https://7f241763b17c49db982ea29ad64b0264@sentry.onp.admin.seichi.click/2")
-      // パフォーマンスモニタリングに使うトレースサンプルの送信割合
-      // tracesSampleRateを1.0にすると全てのイベントが送られるため、送りすぎないように調整する必要がある
-      options.setTracesSampleRate(0.25)
+    if (!SeichiAssist.seichiAssistConfig.getServerId.startsWith("local-")) {
+      Sentry.init { options =>
+        options.setDsn("https://7f241763b17c49db982ea29ad64b0264@sentry.onp.admin.seichi.click/2")
+        // パフォーマンスモニタリングに使うトレースサンプルの送信割合
+        // tracesSampleRateを1.0にすると全てのイベントが送られるため、送りすぎないように調整する必要がある
+        options.setTracesSampleRate(0.25)
 
-      // どのサーバーからイベントが送られているのかを判別する識別子
-      options.setEnvironment(SeichiAssist.seichiAssistConfig.getServerId)
+        // どのサーバーからイベントが送られているのかを判別する識別子
+        options.setEnvironment(SeichiAssist.seichiAssistConfig.getServerId)
+      }
+
+      Sentry.configureScope(_.setLevel(SentryLevel.WARNING))
     }
-
-    Sentry.configureScope(_.setLevel(SentryLevel.WARNING))
 
     if (SeichiAssist.seichiAssistConfig.getDebugMode == 1) {
       // debugmode=1の時は最初からデバッグモードで鯖を起動
