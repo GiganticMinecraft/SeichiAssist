57ca0a831c2bb976658a82f7fd836f184da41cac
rito528 <rotoyutoriapp@gmail.com>
2023-05-14 18:08:59 +0900

    fix: HalloweenItemListenerのえらーを治す

--- src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/halloween/HalloweenItemListener.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/seasonalevents/halloween/HalloweenItemListener.scala
@@ -1,14 +1,8 @@
 package com.github.unchama.seichiassist.subsystems.seasonalevents.halloween
 
-import com.github.unchama.seichiassist.subsystems.seasonalevents.halloween.Halloween.{
-  END_DATE,
-  blogArticleUrl,
-  isInEvent
-}
-import com.github.unchama.seichiassist.subsystems.seasonalevents.halloween.HalloweenItemData.{
-  isHalloweenHoe,
-  isHalloweenPotion
-}
+import com.github.unchama.seichiassist.subsystems.seasonalevents.halloween.Halloween.{END_DATE, blogArticleUrl, isInEvent}
+import com.github.unchama.seichiassist.subsystems.seasonalevents.halloween.HalloweenItemData.{isHalloweenHoe, isHalloweenPotion}
+import com.github.unchama.util.external.WorldGuardWrapper
 import com.github.unchama.util.external.WorldGuardWrapper.isRegionMember
 import org.bukkit.ChatColor.{DARK_GREEN, LIGHT_PURPLE, UNDERLINE}
 import org.bukkit.Material
@@ -73,7 +67,7 @@ object HalloweenItemListener extends Listener {
   }
 
   private def canBeReplacedWithSoil(player: Player, block: Block) = {
-    (block.getType == Material.FARMLAND || block.getType == Material.GRASS) && isRegionMember(
+    (block.getType == Material.FARMLAND || block.getType == Material.GRASS) && WorldGuardWrapper.isRegionMember(
       player,
       block.getLocation
     )
