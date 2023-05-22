52df348c3580362ca5f7ce7b4d70d314ec77ee55
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-05-22 23:13:30 +0900

    refactor: パース済みの引数がListではなくHListを受け取るように

--- src/main/scala/com/github/unchama/contextualexecutor/Contexts.scala
+++ src/main/scala/com/github/unchama/contextualexecutor/Contexts.scala
@@ -24,7 +24,7 @@ case class RawCommandContext(
  * @param yetToBeParsed
  *   コマンド引数のうち, [parsed]へと変換されていない文字列.
  */
-case class PartiallyParsedArgs[+Args](parsed: List[Args], yetToBeParsed: List[String])
+case class PartiallyParsedArgs[+Args <: HList](parsed: Args, yetToBeParsed: List[String])
 
 /**
  * コマンドの実行時のコマンド引数や実行者などの情報を変換, 加工したデータ.
