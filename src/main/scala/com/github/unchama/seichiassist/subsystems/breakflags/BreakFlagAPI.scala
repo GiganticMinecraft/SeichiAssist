ac85894ecd2cd91a227c8be06f50741df06b5755
rito528 <rotoyutoriapp@gmail.com>
2023-05-31 23:35:04 +0900

    feat: Systemの定義

--- src/main/scala/com/github/unchama/seichiassist/subsystems/breakflags/BreakFlagAPI.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakflags/BreakFlagAPI.scala
@@ -1,30 +1,28 @@
 package com.github.unchama.seichiassist.subsystems.breakflags
 
-import com.github.unchama.seichiassist.subsystems.breakflags.domain.BreakFlag
+import com.github.unchama.seichiassist.subsystems.breakflags.domain.{BreakFlag, BreakFlagName}
 
-import java.util.UUID
-
-trait BreakFlagAPI[F[_]] {
+trait BreakFlagAPI[F[_], Player] {
 
   /**
    * @return 破壊フラグを有効にする作用
    */
-  def turnOnBreakFlag(player: UUID, breakFlag: BreakFlag): F[Unit]
+  def turnOnBreakFlag(player: Player, breakFlagName: BreakFlagName): F[Unit]
 
   /**
    * @return 破壊フラグを無効にする作用
    */
-  def turnOffBreakFlag(player: UUID, breakFlag: BreakFlag): F[Unit]
+  def turnOffBreakFlag(player: Player, breakFlagName: BreakFlagName): F[Unit]
 
   /**
    * @return 現在の破壊フラグを取得する作用
    */
-  def breakFlag(player: UUID): F[BreakFlag]
+  def breakFlag(player: Player, breakFlagName: BreakFlagName): F[Boolean]
 
 }
 
 object BreakFlagAPI {
 
-  def apply[F[_]](implicit ev: BreakFlagAPI[F]): BreakFlagAPI[F] = ev
+  def apply[F[_], Player](implicit ev: BreakFlagAPI[F, Player]): BreakFlagAPI[F, Player] = ev
 
 }
--- src/main/scala/com/github/unchama/seichiassist/subsystems/breakflags/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakflags/System.scala
@@ -1,21 +1,22 @@
 package com.github.unchama.seichiassist.subsystems.breakflags
 
-import cats.effect.{Sync, SyncEffect}
+import cats.effect.SyncEffect
 import com.github.unchama.datarepository.bukkit.player.BukkitRepositoryControls
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
 import com.github.unchama.seichiassist.subsystems.breakflags.application.repository.BreakFlagRepositoryDefinition
-import com.github.unchama.seichiassist.subsystems.breakflags.domain.BreakFlagPersistence
+import com.github.unchama.seichiassist.subsystems.breakflags.domain.{BreakFlag, BreakFlagName, BreakFlagPersistence}
 import com.github.unchama.seichiassist.subsystems.breakflags.persistence.JdbcBreakFlagPersistence
+import org.bukkit.entity.Player
 
-trait System[F[_]] extends Subsystem[F] {
-
+trait System[F[_], Player] extends Subsystem[F] {
+  val api: BreakFlagAPI[F, Player]
 }
 
 object System {
 
   import cats.implicits._
 
-  def wired[F[_]: SyncEffect]: F[System[F]] = {
+  def wired[F[_]: SyncEffect]: F[System[F, Player]] = {
     implicit val breakFlagPersistence: BreakFlagPersistence[F] = new JdbcBreakFlagPersistence[F]
 
     for {
@@ -25,6 +26,28 @@ object System {
     } yield {
       val breakFlagRepository = breakFlagRepositoryControls.repository
 
+      new System[F, Player] {
+        override val api: BreakFlagAPI[F, Player] = new BreakFlagAPI[F, Player] {
+          override def turnOnBreakFlag(player: Player, breakFlagName: BreakFlagName): F[Unit] =
+            breakFlagRepository(player).update { breakFlags =>
+              breakFlags.filterNot(_.flagName == breakFlagName) :+ BreakFlag(breakFlagName, flag = true)
+            }
+
+          override def turnOffBreakFlag(player: Player, breakFlagName: BreakFlagName): F[Unit] =
+            breakFlagRepository(player).update { breakFlags =>
+              breakFlags.filterNot(_.flagName == breakFlagName) :+ BreakFlag(breakFlagName, flag = false)
+            }
+
+          override def breakFlag(player: Player, breakFlagName: BreakFlagName): F[Boolean] = for {
+            flags <- breakFlagRepository(player).get
+          } yield {
+            flags.find(_.flagName == breakFlagName) match {
+              case Some(value) => value.flag
+              case None => true // 破壊フラグのデフォルト値はtrue(破壊する)
+            }
+          }
+        }
+      }
     }
   }
 
