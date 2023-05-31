e1595ec0b267ed7c9b5595621764323c9c46b897
rito528 <rotoyutoriapp@gmail.com>
2023-05-31 09:35:44 +0900

    feat: BreakFlagPersistenceを定義

---
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/breakflags/domain/BreakFlagPersistence.scala
@@ -0,0 +1,7 @@
+package com.github.unchama.seichiassist.subsystems.breakflags.domain
+
+import com.github.unchama.generic.RefDict
+
+import java.util.UUID
+
+trait BreakFlagPersistence[F[_]] extends RefDict[F, UUID, BreakFlag]
