e6154ae7a1f64bcc81a866911f1ab005d94e86ad
rito528 <rotoyutoriapp@gmail.com>
2023-06-01 14:31:44 +0900

    refactor: break_flagsのflag_nameをENUM型に変更する

--- src/main/resources/db/migration/V1.17.0__Create_break_flag_table.sql
+++ src/main/resources/db/migration/V1.17.0__Create_break_flag_table.sql
@@ -2,6 +2,6 @@ USE seichiassist;
 
 CREATE TABLE break_flags(
     uuid CHAR(36) NOT NULL PRIMARY KEY,
-    flag_name VARCHAR(20) NOT NULL,
+    flag_name ENUM('Chest', 'MadeFromNetherQuartz') NOT NULL,
     can_break BOOL NOT NULL DEFAULT TRUE
 );
