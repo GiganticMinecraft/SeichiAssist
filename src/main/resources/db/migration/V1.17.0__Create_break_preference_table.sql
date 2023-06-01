06d47218827f6ec80858236abc9b657d88566256
rito528 <rotoyutoriapp@gmail.com>
2023-06-01 14:41:50 +0900

    feat: player_break_preferenceテーブルで、uuidとflag_nameを主キー制約をつける

--- src/main/resources/db/migration/V1.17.0__Create_break_preference_table.sql
+++ src/main/resources/db/migration/V1.17.0__Create_break_preference_table.sql
@@ -1,8 +1,9 @@
 USE seichiassist;
 
 CREATE TABLE player_break_preference(
-    uuid CHAR(36) NOT NULL PRIMARY KEY,
+    uuid CHAR(36) NOT NULL,
     flag_name ENUM('Chest', 'MadeFromNetherQuartz') NOT NULL,
     include BOOL NOT NULL DEFAULT TRUE,
+    PRIMARY KEY(uuid, flag_name),
     FOREIGN KEY (uuid) REFERENCES playerdata(uuid)
 );
