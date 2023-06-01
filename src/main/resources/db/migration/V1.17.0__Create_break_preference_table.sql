614007f67afa2f0eea978a5262888511af694086
rito528 <rotoyutoriapp@gmail.com>
2023-06-01 14:40:25 +0900

    feat: uuidカラムをplayerdataテーブルに対して外部キー制約をかける

--- src/main/resources/db/migration/V1.17.0__Create_break_preference_table.sql
+++ src/main/resources/db/migration/V1.17.0__Create_break_preference_table.sql
@@ -3,5 +3,6 @@ USE seichiassist;
 CREATE TABLE player_break_preference(
     uuid CHAR(36) NOT NULL PRIMARY KEY,
     flag_name ENUM('Chest', 'MadeFromNetherQuartz') NOT NULL,
-    include BOOL NOT NULL DEFAULT TRUE
+    include BOOL NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (uuid) REFERENCES playerdata(uuid)
 );
