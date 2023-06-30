ab68cb662293eb386988a7cf5d90a85d882401ad
rito528 <39003544+rito528@users.noreply.github.com>
2023-06-30 20:12:26 +0900

    chore: break_category -> block_category

--- src/main/resources/db/migration/V1.17.0__Create_break_preference_table.sql
+++ src/main/resources/db/migration/V1.17.0__Create_break_preference_table.sql
@@ -2,8 +2,8 @@ USE seichiassist;
 
 CREATE TABLE player_break_preference(
     uuid CHAR(36) NOT NULL,
-    break_category ENUM('Chest', 'MadeFromNetherQuartz') NOT NULL,
+    block_category ENUM('Chest', 'MadeFromNetherQuartz') NOT NULL,
     do_break BOOL NOT NULL DEFAULT TRUE,
-    PRIMARY KEY(uuid, break_category),
+    PRIMARY KEY(uuid, block_category),
     INDEX index_player_break_preference_on_uuid (uuid)
 );
