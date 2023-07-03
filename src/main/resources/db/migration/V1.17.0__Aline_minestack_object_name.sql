2b786c99da449e58a3f6123c4a4d3a85ef7b6fab
rito528 <39003544+rito528@users.noreply.github.com>
2023-07-03 14:21:34 +0900

    fix: SQLファイルのコメントの形式が間違っていたので修正

--- src/main/resources/db/migration/V1.17.0__Aline_minestack_object_name.sql
+++ src/main/resources/db/migration/V1.17.0__Aline_minestack_object_name.sql
@@ -1,8 +1,8 @@
 USE seichiassist;
 
-# gachadataはガチャ景品に対して依存を持つが、ガチャ景品のIDより語尾の数が2つ小さいので、
-# gachadata0_[ガチャ景品ID]という形式にする
+-- gachadataはガチャ景品に対して依存を持つが、ガチャ景品のIDより語尾の数が2つ小さいので、
+-- gachadata0_[ガチャ景品ID]という形式にする
 UPDATE mine_stack SET object_name = CONVERT(INT, REPLACE('gachadata0_', '')) + 2  LIKE 'gachadata0_%';
 
-# 上の変換により、mine_stack_gacha_objectsを永続化する必要がなくなったので削除する
+-- 上の変換により、mine_stack_gacha_objectsを永続化する必要がなくなったので削除する
 DROP TABLE mine_stack_gacha_objects;
