bb33dc2ffd64fa78319379bd7c953c3b63ff57bd
rito528 <39003544+rito528@users.noreply.github.com>
2023-07-15 22:32:48 +0900

    WIP: gachadataを更新することができるように

--- prepare-docker.bat
+++ prepare-docker.bat
@@ -2,7 +2,20 @@ call rd /s /q target\build
 
 call sbt assembly || goto :onerror
 
+if %1 == update-gachadata (
+    call docker compose down
+    echo Updating gachadata...
+    call docker compose up -d db
+
+    @REM ここで遅延を入れないとdbが起動する前にgachadataを更新するスクリプトが走ってしまう
+    timeout 3
+
+    call docker exec -it seichiassist-db-1 /update-gachadata.sh
+    echo Completed update gachadata.
+)
+
 call docker compose down
+
 call docker compose up --build
 
 exit /b
