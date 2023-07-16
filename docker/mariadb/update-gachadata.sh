57137b88f571cf4c164d1c51321091ecb024d37d
rito528 <39003544+rito528@users.noreply.github.com>
2023-07-16 16:25:40 +0900

    chore: docker-entrypoint-initdb.dにはshファイルのみ置く

--- docker/mariadb/update-gachadata.sh
+++ docker/mariadb/update-gachadata.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget -O gachadata.sql -P /docker-entrypoint-initdb.d https://redmine.seichi.click/attachments/download/997/gachadata.sql
-
 # TODO: 定期的にdumpされたgachadata.sqlの最新版をダウンロードできるようにする https://github.com/GiganticMinecraft/SeichiAssist/issues/2172
+wget -O gachadata.sql -P / https://redmine.seichi.click/attachments/download/997/gachadata.sql
+
 # 外部キー制約がかかっているgachadataテーブルをDROPしたいので、一旦外部キー制約のチェックをオフにする
 mysql -uroot -punchamaisgod -e '
   SET foreign_key_checks = 0;
   DROP TABLE seichiassist.gachadata;
   USE seichiassist;
-  SOURCE /docker-entrypoint-initdb.d/gachadata.sql;
+  SOURCE gachadata.sql;
   SET foreign_key_checks = 1;'
