e1c2cadf5bca7383eee38eee5de69b523f5d6101
rito528 <39003544+rito528@users.noreply.github.com>
2023-09-01 23:20:34 +0900

    chore: マナ妖精機能をメンテナンス中として使えなくする

--- src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/bukkit/BukkitFairySummonRequest.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/vote/subsystems/fairy/bukkit/BukkitFairySummonRequest.scala
@@ -11,6 +11,7 @@ import com.github.unchama.seichiassist.subsystems.vote.subsystems.fairy.domain.{
   FairySpawnRequestErrorOrSpawn,
   FairySummonRequest
 }
+import com.github.unchama.targetedeffect.commandsender.{MessageEffect, MessageEffectF}
 import org.bukkit.entity.Player
 
 class BukkitFairySummonRequest[F[_]: Sync, G[_]: ContextCoercion[*[_], F]](
@@ -20,28 +21,29 @@ class BukkitFairySummonRequest[F[_]: Sync, G[_]: ContextCoercion[*[_], F]](
   summonFairy: SummonFairy[F, Player]
 ) extends FairySummonRequest[F, Player] {
 
-  import cats.implicits._
+//  import cats.implicits._
 
   override def summonRequest(player: Player): F[FairySpawnRequestErrorOrSpawn[F]] = {
-    val uuid = player.getUniqueId
-    for {
-      usingState <- fairyPersistence.isFairyUsing(uuid)
-      effectPoints <- voteAPI.effectPoints(player)
-      fairySummonCost <- fairyPersistence.fairySummonCost(uuid)
-      seichiAmountRepository <- ContextCoercion(
-        breakCountAPI.seichiAmountDataRepository(player).read
-      )
-      seichiLevel = seichiAmountRepository.levelCorrespondingToExp.level
-    } yield {
-      if (seichiLevel < 10)
-        Left(FairySummonRequestError.NotEnoughSeichiLevel)
-      else if (usingState)
-        Left(FairySummonRequestError.AlreadyFairySummoned)
-      else if (effectPoints.value < fairySummonCost.value * 2)
-        Left(FairySummonRequestError.NotEnoughEffectPoint)
-      else
-        Right(summonFairy.summon(player))
-    }
+//    val uuid = player.getUniqueId
+//    for {
+//      usingState <- fairyPersistence.isFairyUsing(uuid)
+//      effectPoints <- voteAPI.effectPoints(player)
+//      fairySummonCost <- fairyPersistence.fairySummonCost(uuid)
+//      seichiAmountRepository <- ContextCoercion(
+//        breakCountAPI.seichiAmountDataRepository(player).read
+//      )
+//      seichiLevel = seichiAmountRepository.levelCorrespondingToExp.level
+//    } yield {
+//      if (seichiLevel < 10)
+//        Left(FairySummonRequestError.NotEnoughSeichiLevel)
+//      else if (usingState)
+//        Left(FairySummonRequestError.AlreadyFairySummoned)
+//      else if (effectPoints.value < fairySummonCost.value * 2)
+//        Left(FairySummonRequestError.NotEnoughEffectPoint)
+//      else
+//        Right(summonFairy.summon(player))
+//    }
+    Sync[F].pure(Right(MessageEffectF[F]("マナ妖精機能は不具合のため一時停止中です。").apply(player)))
   }
 
 }
