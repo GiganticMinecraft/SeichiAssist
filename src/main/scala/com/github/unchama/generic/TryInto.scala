89e06077fed2de35234275e9cca564859c9aeaff
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-09-03 22:37:13 +0900

    refactor: use SAM interface syntax sugar
    Signed-off-by: Kisaragi Marine <kisaragi.effective@gmail.com>

--- src/main/scala/com/github/unchama/generic/TryInto.scala
+++ src/main/scala/com/github/unchama/generic/TryInto.scala
@@ -3,16 +3,14 @@ package com.github.unchama.generic
 import eu.timepit.refined.api.{Refined, Validate}
 import eu.timepit.refined.refineV
 
-sealed trait TryInto[From, To, ConversionErr] {
+// https://www.scala-lang.org/files/archive/spec/2.13/06-expressions.html#sam-conversion
+trait TryInto[From, To, ConversionErr] {
   def tryInto(from: From): Either[ConversionErr, To]
 }
 
 object TryInto {
-  private def fromFunction[F, T, E](fn: F => Either[E, T]): TryInto[F, T, E] = {
-    new TryInto[F, T, E] {
-      override def tryInto(from: F): Either[E, T] = fn(from)
-    }
-  }
+  private def fromFunction[F, T, E](fn: F => Either[E, T]): TryInto[F, T, E] =
+    (f: F) => fn(f)
 
   implicit def refineByPredicate[A, P: Validate[A, *]]: TryInto[A, A Refined P, String] =
     fromFunction(refineV(_))
