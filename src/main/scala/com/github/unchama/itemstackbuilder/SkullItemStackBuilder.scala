11dd549665faa21e32cb8f010c620f73fe6996f5
rito528 <39003544+rito528@users.noreply.github.com>
2023-09-17 23:58:24 +0900

    fix: ガチャ券のタイトルとロールが正しく設定されないのを修正

--- src/main/scala/com/github/unchama/itemstackbuilder/SkullItemStackBuilder.scala
+++ src/main/scala/com/github/unchama/itemstackbuilder/SkullItemStackBuilder.scala
@@ -32,7 +32,7 @@ class SkullItemStackBuilder(private val owner: SkullOwnerReference)
    */
   def this(ownerName: String) = this(SkullOwnerName(ownerName))
 
-  override def transformItemMetaOnBuild(meta: SkullMeta): Unit = {
+  override protected def transformItemMetaOnBuild(meta: SkullMeta): Unit = {
     owner match {
       case SkullOwnerUuid(uuid) =>
         meta.setOwningPlayer(Bukkit.getOfflinePlayer(uuid))
