eb0eb3dab0a477d76125a4d5058619199e0e0816
rito528 <39003544+rito528@users.noreply.github.com>
2023-09-20 18:19:21 +0900

    fix: ガチャが引けない不具合を修正

--- src/main/scala/com/github/unchama/seichiassist/SkullOwners.scala
+++ src/main/scala/com/github/unchama/seichiassist/SkullOwners.scala
@@ -1,13 +1,15 @@
 package com.github.unchama.seichiassist
 
-import com.github.unchama.itemstackbuilder.{SkullOwnerName, SkullOwnerReference}
+import com.github.unchama.itemstackbuilder.{SkullOwnerName, SkullOwnerReference, SkullOwnerUuid}
+
+import java.util.UUID
 
 /**
  * プレーヤーヘッドにownerとして設定されるプレーヤー達に関する定数を保持するオブジェクト
  */
 object SkullOwners {
   val whitecat_haru: SkullOwnerReference = "whitecat_haru".asSkullOwnerReference()
-  val unchama: SkullOwnerReference = "unchama".asSkullOwnerReference()
+  val unchama: SkullOwnerReference = UUID.fromString("b66cc3f6-a045-42ad-b4b8-320f20caf140").asSkullOwnerReference()
 
   val MHF_ArrowUp: SkullOwnerReference = "MHF_ArrowUp".asSkullOwnerReference()
   val MHF_ArrowDown: SkullOwnerReference = "MHF_ArrowDown".asSkullOwnerReference()
@@ -20,6 +22,10 @@ object SkullOwners {
 
   implicit class StringOps(val string: String) {
     def asSkullOwnerReference(): SkullOwnerReference = SkullOwnerName(string)
+  }
+
+  implicit class UuidOps(val uuid: UUID) {
+    def asSkullOwnerReference(): SkullOwnerReference = SkullOwnerUuid(uuid)
   }
 
 }
