6cafbc74ce05843016f705a5bbf2ee6f95c6c746
rito528 <39003544+rito528@users.noreply.github.com>
2023-10-07 18:33:26 +0900

    refactor: SkullOwnerReferenceから名前のみで構築する方法を廃止する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/ranking/infrastructure/JdbcVoteRankingRecordPersistence.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/ranking/infrastructure/JdbcVoteRankingRecordPersistence.scala
@@ -8,12 +8,20 @@ import com.github.unchama.seichiassist.subsystems.ranking.domain.{
 }
 import scalikejdbc.{DB, scalikejdbcSQLInterpolationImplicitDef}
 
+import java.util.UUID
+
 class JdbcVoteRankingRecordPersistence[F[_]: Sync]
     extends RankingRecordPersistence[F, VoteCount] {
   override def getAllRankingRecords: F[Vector[RankingRecord[VoteCount]]] = Sync[F].delay {
     DB.readOnly { implicit session =>
       sql"SELECT playerdata.name,vote_number FROM vote INNER JOIN playerdata ON vote.uuid = playerdata.uuid"
-        .map { rs => RankingRecord(rs.string("name"), VoteCount(rs.int("vote_number"))) }
+        .map { rs =>
+          RankingRecord(
+            rs.string("name"),
+            UUID.fromString(rs.string("uuid")),
+            VoteCount(rs.int("vote_number"))
+          )
+        }
         .list()
         .apply()
         .toVector
