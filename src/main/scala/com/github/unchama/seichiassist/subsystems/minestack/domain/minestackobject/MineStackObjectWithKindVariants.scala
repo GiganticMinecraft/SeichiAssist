da01a11f72ce569351dc21ded1f9695fa47b96bf
rito528 <39003544+rito528@users.noreply.github.com>
2023-10-08 15:51:52 +0900

    chore: マインスタックのアイテムを纏める

--- src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/minestackobject/MineStackObjectWithKindVariants.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/minestack/domain/minestackobject/MineStackObjectWithKindVariants.scala
@@ -1,10 +1,10 @@
 package com.github.unchama.seichiassist.subsystems.minestack.domain.minestackobject
 
-case class MineStackObjectWithColorVariants[ItemStack](
+case class MineStackObjectWithKindVariants[ItemStack](
   representative: MineStackObject[ItemStack],
-  coloredVariants: List[MineStackObject[ItemStack]]
+  kindVariants: List[MineStackObject[ItemStack]]
 ) {
-  require(coloredVariants.forall(_.category == representative.category))
+  require(kindVariants.forall(_.category == representative.category))
 
   def category: MineStackObjectCategory = representative.category
 }
