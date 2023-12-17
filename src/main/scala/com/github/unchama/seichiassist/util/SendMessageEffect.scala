273074f00b82b7789015c2c07596680bafde306e
rito528 <39003544+rito528@users.noreply.github.com>
2023-12-17 23:57:57 +0900

    fix: 全体通知を表示しない設定でもGT大当たり通知が表示される不具合を修正

--- src/main/scala/com/github/unchama/seichiassist/util/SendMessageEffect.scala
+++ src/main/scala/com/github/unchama/seichiassist/util/SendMessageEffect.scala
@@ -1,9 +1,10 @@
 package com.github.unchama.seichiassist.util
 
 import cats.Monad
-import cats.effect.IO
+import cats.effect.{IO, LiftIO}
 import com.github.unchama.minecraft.actions.GetConnectedPlayers
 import com.github.unchama.minecraft.bukkit.actions.GetConnectedBukkitPlayers
+import com.github.unchama.seichiassist.SeichiAssist
 import com.github.unchama.seichiassist.concurrent.PluginExecutionContexts.onMainThread
 import org.bukkit.entity.Player
 
@@ -24,6 +25,26 @@ object SendMessageEffect {
     for {
       players <- GetConnectedPlayers[F, Player].now
       _ <- players.traverse(ev.send(_, content))
+    } yield ()
+  }
+
+  def sendMessageToEveryone[T, F[_]: Monad: LiftIO: GetConnectedPlayers[*[_], Player]](
+    content: T
+  )(implicit ev: PlayerSendable[T, F]): F[Unit] = {
+    import cats.implicits._
+
+    for {
+      players <- GetConnectedPlayers[F, Player].now
+      _ <- players.traverse { player =>
+        for {
+          playerSettings <- SeichiAssist
+            .playermap(player.getUniqueId)
+            .settings
+            .getBroadcastMutingSettings
+            .to[F]
+          _ <- ev.send(player, content).unlessA(playerSettings.shouldMuteMessages)
+        } yield ()
+      }
     } yield ()
   }
 }
