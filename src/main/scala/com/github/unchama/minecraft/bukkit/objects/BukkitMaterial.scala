3b455d67979bb34c0fdc087a88918a15b005504f
outductor <inductor.kela+seichi@gmail.com>
2023-12-31 18:26:35 +0900

    scalafmt

--- src/main/scala/com/github/unchama/minecraft/bukkit/objects/BukkitMaterial.scala
+++ src/main/scala/com/github/unchama/minecraft/bukkit/objects/BukkitMaterial.scala
@@ -4,10 +4,7 @@ import com.github.unchama.minecraft.objects.MinecraftMaterial
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
-class
-
-
-BukkitMaterial extends MinecraftMaterial[Material, ItemStack] {
+class BukkitMaterial extends MinecraftMaterial[Material, ItemStack] {
   override def toItemStack(material: Material): ItemStack =
     new ItemStack(material, 1)
 }
