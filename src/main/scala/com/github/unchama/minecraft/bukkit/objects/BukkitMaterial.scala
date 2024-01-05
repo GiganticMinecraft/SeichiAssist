f2d9507d7ad10536f16bf7d0e8d41dc72eae80d1
rito528 <39003544+rito528@users.noreply.github.com>
2024-01-05 19:10:01 +0900

    fix: 通常破壊されたブロックがチェストなどであった場合、MineStackに入れない

--- src/main/scala/com/github/unchama/minecraft/bukkit/objects/BukkitMaterial.scala
+++ src/main/scala/com/github/unchama/minecraft/bukkit/objects/BukkitMaterial.scala
@@ -7,4 +7,5 @@ import org.bukkit.inventory.ItemStack
 class BukkitMaterial extends MinecraftMaterial[Material, ItemStack] {
   override def toItemStack(material: Material): ItemStack =
     new ItemStack(material, 1)
+
 }
