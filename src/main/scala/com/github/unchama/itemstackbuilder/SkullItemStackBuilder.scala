42c70d3c3f9c608fa58f7c81f5e5e5e8ad694c5d
rito528 <39003544+rito528@users.noreply.github.com>
2024-01-07 21:14:08 +0900

    fix: menuでアイコンが正しく表示されないのを修正

--- src/main/scala/com/github/unchama/itemstackbuilder/SkullItemStackBuilder.scala
+++ src/main/scala/com/github/unchama/itemstackbuilder/SkullItemStackBuilder.scala
@@ -5,8 +5,11 @@ import com.mojang.authlib.properties.Property
 import org.apache.commons.codec.binary.Base64
 import org.bukkit.inventory.meta.SkullMeta
 import org.bukkit.{Bukkit, Material}
+import org.jline.utils.InputStreamReader
 
-import java.net.URI
+import java.io.BufferedReader
+import java.net.{HttpURLConnection, URI, URL}
+import java.nio.charset.StandardCharsets
 import java.util.UUID
 
 /**
@@ -28,16 +31,65 @@ class SkullItemStackBuilder(private val owner: SkullOwnerReference)
    */
   def this(ownerUUID: UUID) = this(SkullOwnerUuid(ownerUUID))
 
+  private def getHttpRequest(url: String): Option[String] = {
+    val _url = new URL(url)
+
+    val connection = _url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.connect()
+
+    val responseCode = connection.getResponseCode
+
+    Option.when(responseCode == HttpURLConnection.HTTP_OK) {
+      val inputStream = connection.getInputStream
+      val inputStreamReader = new InputStreamReader(inputStream, "UTF-8")
+      val bufferedReader = new BufferedReader(inputStreamReader)
+
+      val result = Iterator.continually(bufferedReader.readLine()).takeWhile(_ != null).mkString
+
+      bufferedReader.close()
+      inputStreamReader.close()
+      inputStream.close()
+
+      result
+    }
+  }
+
+  import io.circe.parser._
+
   override protected def transformItemMetaOnBuild(meta: SkullMeta): Unit = {
     owner match {
       case SkullOwnerUuidWithName(uuid, name) =>
         meta.setOwningPlayer(Bukkit.getOfflinePlayer(uuid))
-        val textureUrl =
-          s"http://textures.minecraft.net/texture/${uuid.toString.replaceAll("-", "")}"
 
-        val profile = Bukkit.createPlayerProfile(uuid, name)
-        profile.getTextures.setSkin(URI.create(textureUrl).toURL)
-        meta.setOwnerProfile(profile)
+        val textureUrl = for {
+          profile <- getHttpRequest(s"https://api.mojang.com/users/profiles/minecraft/$name")
+          id <- parse(profile).toOption.flatMap(_.hcursor.get[String]("id").toOption)
+          playerData <- getHttpRequest(
+            s"https://sessionserver.mojang.com/session/minecraft/profile/$id"
+          )
+          base64TextureProperties <- parse(playerData)
+            .toOption
+            .flatMap(
+              _.hcursor
+                .downField("properties")
+                .values
+                .flatMap(_.head.hcursor.get[String]("value").toOption)
+            )
+          url <- parse(
+            new String(Base64.decodeBase64(base64TextureProperties), StandardCharsets.UTF_8)
+          ).toOption
+            .flatMap(
+              _.hcursor.downField("textures").downField("SKIN").get[String]("url").toOption
+            )
+        } yield url
+
+        textureUrl match {
+          case Some(url) =>
+            val playerProfile = Bukkit.createPlayerProfile(uuid, name)
+            playerProfile.getTextures.setSkin(URI.create(url).toURL)
+            meta.setOwnerProfile(playerProfile)
+          case None =>
+        }
       case SkullOwnerUuid(uuid) =>
         meta.setOwningPlayer(Bukkit.getOfflinePlayer(uuid))
         val gameProfile = new GameProfile(uuid, null)
