5949cd744a2cf50c7b9c21d7c1141f8631f9e821
rito528 <39003544+rito528@users.noreply.github.com>
2024-01-07 22:55:29 +0900

    pref: 頭のテクスチャを描画するときのパフォーマンスを改善

--- src/main/scala/com/github/unchama/itemstackbuilder/SkullOwnerReference.scala
+++ src/main/scala/com/github/unchama/itemstackbuilder/SkullOwnerReference.scala
@@ -4,11 +4,16 @@ import java.util.UUID
 
 sealed trait SkullOwnerReference
 
-case class SkullOwnerUuidWithName(uuid: UUID, name: String) extends SkullOwnerReference
-
 case class SkullOwnerUuid(uuid: UUID) extends SkullOwnerReference
 
 /**
  * UUID指定だけでは、カスタムテクスチャをヘッドに設定することができない場合に備えて、`TextureValue`を指定するもの
  */
 case class SkullOwnerTextureValue(textureValue: String) extends SkullOwnerReference
+
+/**
+ * `textureUrl`は以下の記事の方法で取得されたスキンのURL
+ * @see https://qiita.com/yuta0801/items/edb4804dfb867ea82c5a
+ */
+case class SkullOwnerUuidWithNameWithTextureUrl(uuid: UUID, name: String, textureUrl: String)
+    extends SkullOwnerReference
