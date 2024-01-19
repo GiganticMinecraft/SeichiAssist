2ae5c6b00c4e41b6e900dae60b9992db84ab66db
rito528 <39003544+rito528@users.noreply.github.com>
2024-01-19 16:48:57 +0900

    fix: 落下ブロックによるダメージを無効化する

--- src/main/scala/com/github/unchama/seichiassist/subsystems/canceldamagebyfallingblocks/bukkit/listeners/PlayerDamageByBlockEvents.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/canceldamagebyfallingblocks/bukkit/listeners/PlayerDamageByBlockEvents.scala
@@ -1,16 +1,19 @@
 package com.github.unchama.seichiassist.subsystems.canceldamagebyfallingblocks.bukkit.listeners
 
-import org.bukkit.entity.Player
-import org.bukkit.event.entity.EntityDamageByBlockEvent
+import org.bukkit.entity.{EntityType, Player}
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 import org.bukkit.event.{EventHandler, Listener}
+import com.github.unchama.seichiassist.ManagedWorld._
 
 object PlayerDamageByBlockEvents extends Listener {
 
   @EventHandler
-  def onDamage(e: EntityDamageByBlockEvent): Unit = {
+  def onDamage(e: EntityDamageEvent): Unit = {
     e.getEntity match {
-      case _: Player =>
-        println(s"block: ${e.getDamager.getType}")
+      // 整地ワールドでは落下ダメージを無効化する
+      case player: Player
+          if e.getCause == DamageCause.FALLING_BLOCK && player.getWorld.isSeichi =>
         e.setDamage(0.0)
       case _ =>
     }
