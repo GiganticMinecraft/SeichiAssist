e3c07ba41af4d10d3d9bc89cec508fe0698a2c2c
rito528 <39003544+rito528@users.noreply.github.com>
2024-01-19 14:34:34 +0900

    chore: ダメージを受けたブロックのログを追加

--- src/main/scala/com/github/unchama/seichiassist/subsystems/canceldamagebyfallingblocks/bukkit/listeners/PlayerDamageByBlockEvents.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/canceldamagebyfallingblocks/bukkit/listeners/PlayerDamageByBlockEvents.scala
@@ -1,17 +1,17 @@
 package com.github.unchama.seichiassist.subsystems.canceldamagebyfallingblocks.bukkit.listeners
 
-import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageByBlockEvent
 import org.bukkit.event.{EventHandler, Listener}
-import org.bukkit.entity.Player
 
 object PlayerDamageByBlockEvents extends Listener {
 
   @EventHandler
   def onDamage(e: EntityDamageByBlockEvent): Unit = {
     e.getEntity match {
-      case player: Player if e.getDamager.getType == Material.POINTED_DRIPSTONE =>
-        e.setCancelled(true)
+      case _: Player =>
+        println(s"block: ${e.getDamager.getType}")
+        e.setDamage(0.0)
       case _ =>
     }
   }
