6fb35380e6c6c1c517c7615f4d64ea5c3a6a1b13
rito528 <39003544+rito528@users.noreply.github.com>
2024-02-15 15:24:58 +0900

    Revert "fix: nmsパッケージパスを変更"
    This reverts commit fe4fd1b53a402521c6570cb7927ae12324d34880.

--- src/main/scala/com/github/unchama/util/nms/v1_18_2/world/WorldChunkSaving.scala
+++ src/main/scala/com/github/unchama/util/nms/v1_18_2/world/WorldChunkSaving.scala
@@ -7,12 +7,12 @@ object WorldChunkSaving {
   import scala.jdk.CollectionConverters._
 
   private object Reflection {
-    private val nmsPackage_1_18 = "net.minecraft"
+    private val nmsPackage_1_18_R2 = "net.minecraft.server.v1_18_R1"
     private val craftBukkitPackage_1_18_R2 = "org.bukkit.craftbukkit.v1_18_R2"
 
     object FileIOThread {
       private[Reflection] lazy val clazz: Class[_] =
-        Class.forName(s"$nmsPackage_1_18.FileIOThread")
+        Class.forName(s"$nmsPackage_1_18_R2.FileIOThread")
 
       // public static FileIOThread method()
       lazy val getInstance: () => AnyRef = {
@@ -32,7 +32,7 @@ object WorldChunkSaving {
 
     object Entity {
       private[Reflection] lazy val clazz: Class[_] =
-        Class.forName(s"$nmsPackage_1_18.entity.Entity")
+        Class.forName(s"$nmsPackage_1_18_R2.Entity")
 
       // public int field
       lazy val chunkX: AnyRef => Int = {
@@ -56,7 +56,7 @@ object WorldChunkSaving {
     }
 
     object Chunk {
-      private[Reflection] lazy val clazz: Class[_] = Class.forName(s"$nmsPackage_1_18.Chunk")
+      private[Reflection] lazy val clazz: Class[_] = Class.forName(s"$nmsPackage_1_18_R2.Chunk")
 
       // public void method(Entity)
       lazy val untrackEntity: AnyRef => AnyRef => Unit = {
@@ -66,8 +66,7 @@ object WorldChunkSaving {
     }
 
     object World {
-      private[Reflection] lazy val clazz: Class[_] =
-        Class.forName(s"$nmsPackage_1_18.world.World")
+      private[Reflection] lazy val clazz: Class[_] = Class.forName(s"$nmsPackage_1_18_R2.World")
 
       // public final List<Entity> field
       lazy val entityList: AnyRef => java.util.List[_ <: AnyRef] = {
