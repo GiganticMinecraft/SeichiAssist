ba105271138317dec47be402385b69613d0f0bc4
rito528 <39003544+rito528@users.noreply.github.com>
2024-02-19 15:48:02 +0900

    fix: PlayerHeadSkinAPIをプレイヤーのUUIDから呼び出せるように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/playerheadskin/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/playerheadskin/System.scala
@@ -6,8 +6,13 @@ import com.github.unchama.seichiassist.subsystems.playerheadskin.domain.{
   PlayerHeadSkinUrlFetcher,
   PlayerHeadUrlRepository
 }
-import com.github.unchama.seichiassist.subsystems.playerheadskin.infrastructure.PlayerHeadSkinUrlFetcherByMojangAPI
+import com.github.unchama.seichiassist.subsystems.playerheadskin.infrastructure.{
+  JdbcPlayerHeadSkinPersistence,
+  PlayerHeadSkinUrlFetcherByMojangAPI
+}
 import org.bukkit.entity.Player
+
+import java.util.UUID
 
 trait System[F[_], Player] extends Subsystem[F] {
   val api: PlayerHeadSkinAPI[F, Player]
@@ -19,10 +24,16 @@ object System {
     implicit val playerHeadSkinUrlFetcher: PlayerHeadSkinUrlFetcher[F] =
       new PlayerHeadSkinUrlFetcherByMojangAPI[F]
     val repository = new PlayerHeadUrlRepository[F]
+    val persistence = new JdbcPlayerHeadSkinPersistence[F]
 
     new System[F, Player] {
-      override val api: PlayerHeadSkinAPI[F, Player] = (player: Player) =>
-        repository.readUrl(player.getName)
+      import cats.implicits._
+
+      override val api: PlayerHeadSkinAPI[F, Player] = (player: UUID) =>
+        for {
+          playerName <- persistence.fetchLastSeenPlayerName(player)
+          url <- playerName.flatTraverse(repository.readUrl)
+        } yield url
     }
   }
 
