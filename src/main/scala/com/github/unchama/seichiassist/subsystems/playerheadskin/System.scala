027c2f4b7abdd2405711e62b3a176dd82a11dc45
rito528 <39003544+rito528@users.noreply.github.com>
2024-02-19 15:11:59 +0900

    fix: スキンのURLのキャッシュをすべてのプレイヤーで共有できるように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/playerheadskin/System.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/playerheadskin/System.scala
@@ -1,15 +1,10 @@
 package com.github.unchama.seichiassist.subsystems.playerheadskin
 
-import cats.effect.{Sync, SyncEffect}
-import com.github.unchama.datarepository.bukkit.player.BukkitRepositoryControls
-import com.github.unchama.datarepository.template.RepositoryDefinition
-import com.github.unchama.generic.ContextCoercion
-import com.github.unchama.minecraft.bukkit.algebra.BukkitPlayerHasName.instance
+import cats.effect.Sync
 import com.github.unchama.seichiassist.meta.subsystem.Subsystem
-import com.github.unchama.seichiassist.subsystems.playerheadskin.application.PlayerHeadUrlRepositoryDefinition
 import com.github.unchama.seichiassist.subsystems.playerheadskin.domain.{
-  HeadSkinUrl,
-  PlayerHeadSkinUrlFetcher
+  PlayerHeadSkinUrlFetcher,
+  PlayerHeadUrlRepository
 }
 import com.github.unchama.seichiassist.subsystems.playerheadskin.infrastructure.PlayerHeadSkinUrlFetcherByMojangAPI
 import org.bukkit.entity.Player
@@ -20,34 +15,14 @@ trait System[F[_], Player] extends Subsystem[F] {
 
 object System {
 
-  import cats.implicits._
+  def wired[F[_]: Sync]: System[F, Player] = {
+    implicit val playerHeadSkinUrlFetcher: PlayerHeadSkinUrlFetcher[F] =
+      new PlayerHeadSkinUrlFetcherByMojangAPI[F]
+    val repository = new PlayerHeadUrlRepository[F]
 
-  def wired[F[_]: Sync, G[_]: SyncEffect: ContextCoercion[*[_], F]]: F[System[F, Player]] = {
-    implicit val playerHeadSkinUrlFetcher: PlayerHeadSkinUrlFetcher[G] =
-      new PlayerHeadSkinUrlFetcherByMojangAPI[G]
-
-    for {
-      playerHeadUrlRepositoryControls <- ContextCoercion(
-        BukkitRepositoryControls.createHandles(
-          RepositoryDefinition
-            .Phased
-            .TwoPhased(
-              PlayerHeadUrlRepositoryDefinition.initialization[G, Player],
-              PlayerHeadUrlRepositoryDefinition.finalization[G, Player]
-            )
-        )
-      )
-    } yield {
-      new System[F, Player] {
-        override val api: PlayerHeadSkinAPI[F, Player] = new PlayerHeadSkinAPI[F, Player] {
-          override def playerHeadSkinUrl(player: Player): F[Option[HeadSkinUrl]] =
-            ContextCoercion(playerHeadUrlRepositoryControls.repository(player).readUrl)
-        }
-
-        override val managedRepositoryControls: Seq[BukkitRepositoryControls[F, _]] = Seq(
-          playerHeadUrlRepositoryControls.coerceFinalizationContextTo[F]
-        )
-      }
+    new System[F, Player] {
+      override val api: PlayerHeadSkinAPI[F, Player] = (player: Player) =>
+        repository.readUrl(player.getName)
     }
   }
 
