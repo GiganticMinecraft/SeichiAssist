3f878674ed1dd89d6a7ec45b5142e1b2a9b99d3a
rito528 <39003544+rito528@users.noreply.github.com>
2024-02-19 15:11:59 +0900

    fix: スキンのURLのキャッシュをすべてのプレイヤーで共有できるように

--- src/main/scala/com/github/unchama/seichiassist/subsystems/playerheadskin/domain/PlayerHeadUrlRepository.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/playerheadskin/domain/PlayerHeadUrlRepository.scala
@@ -2,11 +2,23 @@ package com.github.unchama.seichiassist.subsystems.playerheadskin.domain
 
 import cats.effect.Sync
 import cats.effect.concurrent.Ref
+import com.github.unchama.generic.ApplicativeExtra
 
-class PlayerHeadUrlRepository[F[_]: Sync](headSkinUrl: Option[HeadSkinUrl]) {
+class PlayerHeadUrlRepository[F[_]: Sync](implicit fetcher: PlayerHeadSkinUrlFetcher[F]) {
 
-  private val skinUrl: Ref[F, Option[HeadSkinUrl]] = Ref.unsafe(headSkinUrl)
+  private val skinUrls: Ref[F, Vector[HeadSkinUrl]] = Ref.unsafe(Vector.empty)
 
-  def readUrl: F[Option[HeadSkinUrl]] = skinUrl.get
+  import cats.implicits._
+
+  def readUrl(targetPlayer: String): F[Option[HeadSkinUrl]] = for {
+    urls <- skinUrls.get
+    targetPlayersHeadSkinUrl <- ApplicativeExtra.whenAOrElse(
+      !urls.exists(_.playerName == targetPlayer)
+    )(fetcher.fetchHeadSkinUrl(targetPlayer), None)
+    _ <- skinUrls
+      .update(_ :+ targetPlayersHeadSkinUrl.get)
+      .whenA(targetPlayersHeadSkinUrl.isDefined)
+    resultUrls <- skinUrls.get
+  } yield resultUrls.find(_.playerName == targetPlayer)
 
 }
