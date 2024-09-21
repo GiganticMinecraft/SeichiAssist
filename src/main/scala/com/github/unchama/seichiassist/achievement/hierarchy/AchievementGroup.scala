a01203646992c6f8a95c13f399441b47f52bbff6
kuroma6666 <kuroma.creator@gmail.com>
2024-09-21 19:34:15 +0900

    feat: 実績Noより実績小区分名を逆引きする処理を追加

--- src/main/scala/com/github/unchama/seichiassist/achievement/hierarchy/AchievementGroup.scala
+++ src/main/scala/com/github/unchama/seichiassist/achievement/hierarchy/AchievementGroup.scala
@@ -28,4 +28,24 @@ object AchievementGroup {
   case object VoteCounts extends AchievementGroup("JMS投票数", Specials)
 
   case object Secrets extends AchievementGroup("極秘任務", Specials)
+
+  val entryIdToGroup: Map[Int, AchievementGroup] = 
+    (1001 to 1012).map(id => id -> BrokenBlockRanking).toMap ++
+    (2001 to 2014).map(id => id -> PlacedBlockAmount).toMap ++
+    (3001 to 3019).map(id => id -> BrokenBlockAmount).toMap ++
+    (4001 to 4023).map(id => id -> PlayTime).toMap ++
+    (5101 to 5125).map(id => id -> TotalLogins).toMap ++
+    (5001 to 5008).map(id => id -> ConsecutiveLogins).toMap ++
+    (6001 to 6008).map(id => id -> VoteCounts).toMap ++
+    (7001 to 7027).map(id => id -> OfficialEvent).toMap ++
+    (7901 to 7906).map(id => id -> OfficialEvent).toMap ++
+    (8001 to 8003).map(id => id -> Secrets).toMap ++
+    (9001 to 9047).map(id => id -> Anniversaries).toMap
+
+  def getGroupNameByEntryId(entryId: Int): Option[String] = {
+    entryIdToGroup.get(entryId) match {
+      case Some(group) => Some(group.name)
+      case None => None
+    }
+  }
 }
