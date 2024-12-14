fe0b92367b36d75b916a447ae6b7f43b819bd264
kuroma6666 <kuroma.creator@gmail.com>
2024-12-14 22:01:28 +0900

    fix: DBマイグレーションSQLの修正

--- src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
+++ src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
@@ -3,5 +3,6 @@ USE seichiassist;
 CREATE TABLE player_break_suppression_preference(
     uuid CHAR(36) NOT NULL,
     do_break_suppression_due_to_mana BOOL NOT NULL DEFAULT FALSE,
-    FOREIGN KEY fk_player_break_suppression_preference_uuid REFERENCES playerdata(uuid)
+    PRIMARY KEY (uuid),
+    CONSTRAINT fk_player_break_suppression_preference_uuid FOREIGN KEY (uuid) REFERENCES playerdata(uuid)
 );
