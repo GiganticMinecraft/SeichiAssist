d57d69a1d0f06cf6a7751ec9e6ceb61ab66fe9da
rito528 <39003544+rito528@users.noreply.github.com>
2025-01-20 18:41:40 +0900

    fix: player_break_suppression_preference の作成に失敗するのを修正

--- src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
+++ src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
@@ -1,7 +1,13 @@
 USE seichiassist;
 
 CREATE TABLE IF NOT EXISTS player_break_suppression_preference(
-    uuid CHAR(36) NOT NULL PRIMARY KEY,
+    -- `SET ...` がないと Foreign key constraint is incorrectly formed が発生し
+    -- `SHOW ENGINE INNODB STATUS` を実行すると
+    -- Error in foreign key constraint of table seichiassist.player_break_suppression_preference:
+    -- Create table seichiassist.player_break_suppression_preference with foreign key fk_player_break_suppression_preference_uuid constraint failed.
+    -- Field type or character set for column 'uuid' does not match referenced column 'uuid'
+    -- となり、 playerdata.uuid のデータ型は CHAR(36) 文字コードを指定する
+    uuid CHAR(36) SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
     do_break_suppression_due_to_mana BOOL NOT NULL DEFAULT FALSE,
     FOREIGN KEY fk_player_break_suppression_preference_uuid(uuid) REFERENCES playerdata(uuid)
 );
