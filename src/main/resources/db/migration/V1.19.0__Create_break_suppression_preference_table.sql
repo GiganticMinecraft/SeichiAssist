34071b623ec48ee96b7626fcf20cdf47aec717e7
rito528 <39003544+rito528@users.noreply.github.com>
2025-01-20 18:47:46 +0900

    fix: sql syntax

--- src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
+++ src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS player_break_suppression_preference(
     -- Create table seichiassist.player_break_suppression_preference with foreign key fk_player_break_suppression_preference_uuid constraint failed.
     -- Field type or character set for column 'uuid' does not match referenced column 'uuid'
     -- となり、 playerdata.uuid のデータ型は CHAR(36) 文字コードを指定する
-    uuid CHAR(36) SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
+    uuid CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
     do_break_suppression_due_to_mana BOOL NOT NULL DEFAULT FALSE,
     FOREIGN KEY fk_player_break_suppression_preference_uuid(uuid) REFERENCES playerdata(uuid)
 );
