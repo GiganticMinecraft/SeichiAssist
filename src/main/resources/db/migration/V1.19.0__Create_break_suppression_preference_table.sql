f26f20ff79d7fc21fabffb8a86942520077ac17b
rito528 <39003544+rito528@users.noreply.github.com>
2025-01-20 18:00:28 +0900

    fix: player_break_suppression_preference テーブル作成スキーマの修正

--- src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
+++ src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
@@ -1,8 +1,7 @@
 USE seichiassist;
 
 CREATE TABLE IF NOT EXISTS player_break_suppression_preference(
-    uuid CHAR(36) NOT NULL,
+    uuid CHAR(36) NOT NULL PRIMARY KEY,
     do_break_suppression_due_to_mana BOOL NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (uuid),
-    CONSTRAINT fk_player_break_suppression_preference_uuid FOREIGN KEY (uuid) REFERENCES playerdata(uuid)
+    FOREIGN KEY fk_player_break_suppression_preference_uuid(uuid) REFERENCES playerdata(uuid)
 );
