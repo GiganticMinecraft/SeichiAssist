9c9043012cee096aa3aff2ea1aa09db77de25cdc
rito528 <39003544+rito528@users.noreply.github.com>
2025-02-06 01:09:51 +0900

    fix: sql syntax error

--- src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
+++ src/main/resources/db/migration/V1.19.0__Create_break_suppression_preference_table.sql
@@ -2,5 +2,5 @@ USE seichiassist;
 
 CREATE TABLE IF NOT EXISTS player_break_suppression_preference(
     uuid CHAR(36) NOT NULL PRIMARY KEY,
-    do_break_suppression_due_to_mana BOOL NOT NULL DEFAULT FALSE,
+    do_break_suppression_due_to_mana BOOL NOT NULL DEFAULT FALSE
 );
