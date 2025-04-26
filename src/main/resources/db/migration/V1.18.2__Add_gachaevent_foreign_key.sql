6549113e7523e212b2cae0e3e09e4e2bf6ad04c0
rito528 <39003544+rito528@users.noreply.github.com>
2025-04-26 16:14:56 +0900

    fix: マイグレーションスキーマをまとめる 本番環境に適用されていないバージョンのスキーマなので問題ない

--- src/main/resources/db/migration/V1.18.2__Add_gachaevent_foreign_key.sql
+++
@@ -1,7 +0,0 @@
-USE seichiassist;
-
--- V1.18.1 で外部キー制約を追加しようとしていたが、gacha_events テーブルに存在しない event_id を持つレコードが gachadata テーブルに存在していたため、外部キー制約の追加に失敗していた。
--- しかし、マイグレーション自体は success になっていたので、このバージョンで外部キー制約を追加することで、不整合データを削除する。
-DELETE FROM gachadata WHERE event_id NOT IN (SELECT id FROM gacha_events) AND event_id IS NOT NULL;
-
-ALTER TABLE gachadata ADD FOREIGN KEY (event_id) REFERENCES gacha_events(id) ON DELETE CASCADE;
