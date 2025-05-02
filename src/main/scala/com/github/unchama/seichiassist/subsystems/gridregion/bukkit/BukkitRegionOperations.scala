fd4fc91b421f8d69b4271f56bbbe2fcac4558137
rito528 <39003544+rito528@users.noreply.github.com>
2025-05-02 23:36:58 +0900

    Revert "fix: gridregion を使って一マス保護が作れるのを修正"
    This reverts commit 056d47f101e878d65613fbf1abae8b0df5ffe7a4.

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/bukkit/BukkitRegionOperations.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gridregion/bukkit/BukkitRegionOperations.scala
@@ -105,10 +105,9 @@ class BukkitRegionOperations[F[_]: Sync](
     } yield {
       if (!isGridProtectionEnabled) {
         return Sync[F].pure(RegionCreationResult.WorldProhibitsRegionCreation)
-      } else if (
-        regionCountPerPlayer < maxRegionCountPerWorld && applicableRegions
-          .size() == 0 && worldEditSelection.getX != worldEditSelection.getZ
-      ) {
+      }
+
+      if (regionCountPerPlayer < maxRegionCountPerWorld && applicableRegions.size() == 0) {
         RegionCreationResult.Success
       } else {
         RegionCreationResult.Error
