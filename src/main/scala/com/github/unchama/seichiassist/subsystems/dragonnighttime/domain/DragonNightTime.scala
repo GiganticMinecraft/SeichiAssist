98615f553fb4e61401d4044bbdb0a11e894369d0
Lucky3028 <yubi@clov3r.cc>
2025-05-30 01:30:49 +0900

    refactor: move def effectivePeriod to trait

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/domain/DragonNightTime.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/domain/DragonNightTime.scala
@@ -1,10 +1,16 @@
 package com.github.unchama.seichiassist.subsystems.dragonnighttime.domain
 
-import java.time.LocalDate
+import java.time.{DayOfWeek, LocalDate}
 
 trait DragonNightTime {
   val effectivePeriodOnWeekdays: Period
   val effectivePeriodOnWeekends: Period
 
-  def effectivePeriod(today: LocalDate): Period
+  final def effectivePeriod(today: LocalDate): Period = {
+    val todayWeekday = today.getDayOfWeek
+
+    if (todayWeekday == DayOfWeek.SATURDAY || todayWeekday == DayOfWeek.SUNDAY)
+      effectivePeriodOnWeekends
+    else effectivePeriodOnWeekdays
+  }
 }
