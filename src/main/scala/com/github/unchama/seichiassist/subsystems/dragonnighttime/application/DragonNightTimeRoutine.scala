1b6c53976d7f35ac04f241064599345ebe1b4886
Lucky3028 <yubi@clov3r.cc>
2025-05-30 02:07:29 +0900

    fmt: run fmt

--- src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/DragonNightTimeRoutine.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/dragonnighttime/application/DragonNightTimeRoutine.scala
@@ -4,7 +4,11 @@ import cats.effect.{Concurrent, Sync, Timer}
 import com.github.unchama.concurrent.RepeatingRoutine
 import com.github.unchama.generic.ContextCoercion
 import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.FastDiggingEffectWriteApi
-import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.effect.{FastDiggingAmplifier, FastDiggingEffect, FastDiggingEffectCause}
+import com.github.unchama.seichiassist.subsystems.fastdiggingeffect.domain.effect.{
+  FastDiggingAmplifier,
+  FastDiggingEffect,
+  FastDiggingEffectCause
+}
 import com.github.unchama.seichiassist.subsystems.mana.ManaApi
 import com.github.unchama.seichiassist.subsystems.mana.domain.ManaMultiplier
 import com.github.unchama.util.time.LocalTimeUtil
