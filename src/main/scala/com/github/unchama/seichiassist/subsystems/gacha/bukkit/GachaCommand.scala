ed22c8eb101d9157220ca2047554fb773fecf634
Sho Sakuma <me@m1sk9.dev>
2025-06-08 15:11:41 +0900

    feat: Add list-enabled command

--- src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/bukkit/GachaCommand.scala
+++ src/main/scala/com/github/unchama/seichiassist/subsystems/gacha/bukkit/GachaCommand.scala
@@ -64,6 +64,8 @@ class GachaCommand[F[_]: OnMinecraftServerThread: ConcurrentEffect](
         s"$RED/gacha list (<イベント>)",
         "指定したイベントのガチャリストを表示",
         s"$DARK_GRAY※イベント名を指定しなかった場合は通常排出アイテムを表示します",
+        s"$RED/gacha list-enabled",
+        "現在有効になっているガチャリストを表示",
         s"$RED/gacha remove <番号>",
         "リスト該当番号のガチャ景品を削除",
         s"$RED/gacha setamount <番号> <個数>",
@@ -93,6 +95,7 @@ class GachaCommand[F[_]: OnMinecraftServerThread: ConcurrentEffect](
         "add" -> add,
         "remove" -> remove,
         "list" -> list,
+        "list-enabled" -> listEnabled,
         "setamount" -> setAmount,
         "setprob" -> setProbability,
         "create-event" -> createEvent,
@@ -270,6 +273,22 @@ class GachaCommand[F[_]: OnMinecraftServerThread: ConcurrentEffect](
               s"${RED}合計確率は100%以内に収まるようにしてください。"
             )
           )
+        }
+      }
+
+    val listEnabled: ContextualExecutor =
+      ContextualExecutorBuilder.beginConfiguration.buildWithExecutionCSEffect { _ =>
+        Kleisli.liftF(gachaPrizeAPI.listOfNow).flatMap { events =>
+          val messages = "アイテム | 確率 | 記名の有無" +: events.map { gachaPrize =>
+            val itemStack = gachaPrize.itemStack
+            val probability = gachaPrize.probability.value
+            s"${itemStack.getType.toString} | $probability(${probability * 100}%) | ${if (
+                gachaPrize.signOwner
+              ) "あり"
+              else "なし"}"
+          }
+
+          MessageEffectF(messages.toList)
         }
       }
 
